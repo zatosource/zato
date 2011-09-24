16c67c7ba8a76413e99c83d02b0de0eb614827a6
Dariusz Suchojad <dsuch-github@gefira.pl>
2011-09-24 11:13:27 +0200

    Finished the form for editing of technical accounts.

--- code/zato-admin/src/zato/admin/web/forms/security/tech_account.py
+++ code/zato-admin/src/zato/admin/web/forms/security/tech_account.py
@@ -27,8 +27,8 @@ from zato.common.util import make_repr
 
 
 class CreateTechnicalAccountForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={"class":"required", "style":"width:100%"}))
-    is_active = forms.BooleanField(required=False)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'style':'width:100%'}))
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
 
     def __repr__(self):
         return make_repr(self)
