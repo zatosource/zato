2827e122f3bb8609f1830c1255dbe8b5e96094dc
Dariusz Suchojad <dsuch-github@gefira.pl>
2011-10-28 00:00:21 +0200

    Removed a reference to an old API.

--- code/zato-admin/src/zato/admin/static/js/security/basic-auth.js
+++ code/zato-admin/src/zato/admin/static/js/security/basic-auth.js
@@ -148,7 +148,7 @@ $.fn.zato.security.basic_auth.data_table.new_row = function(item, data, include_
 	row += String.format('<td>{0}</td>', item.is_active ? 'Yes' : 'No');
 	row += String.format('<td>{0}</td>', item.username);
 	row += String.format('<td>{0}</td>', item.domain);
-	row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.security.basic_auth.change_password({0})'>Change password</a>", item.id));
+	row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.data_table.change_password({0})'>Change password</a>", item.id));
 	row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.security.basic_auth.edit('{0}')\">Edit</a>", item.id));
 	row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.security.basic_auth.delete_({0});'>Delete</a>", item.id));
 	row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
