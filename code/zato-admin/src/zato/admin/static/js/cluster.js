0e067fe60a0e1fd7d539d7f4b8a8417a31c2ee5a
Dariusz Suchojad <dsuch-github@gefira.pl>
2011-11-04 11:20:46 +0100

    Working on the cluster creation form.

--- code/zato-admin/src/zato/admin/static/js/cluster.js
+++ code/zato-admin/src/zato/admin/static/js/cluster.js
@@ -3,7 +3,7 @@
 
 $.fn.zato.data_table.Cluster = new Class({
 	toString: function() {
-		var s = '<Cluster id:{0} name:{1}';
+		var s = '<Cluster id:{0} name:{1}>';
 		return String.format(s, this.id ? this.id : '(none)', 
 								this.name ? this.name : '(none)');
 	}
@@ -40,13 +40,13 @@ $.fn.zato.cluster.data_table.new_row = function(item, data, include_tr) {
 	row += "<td class='numbering'>&nbsp;</td>";
 	row += "<td><input type='checkbox' /></td>";
 	row += String.format('<td>{0}</td>', item.name);
-	row += String.format('<td>{0}</td>', item.is_active ? 'Yes' : 'No');
-	row += String.format('<td>{0}</td>', item.username);
-	row += String.format('<td>{0}</td>', item.domain);
-	row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.cluster.edit({0})'>Edit</a>", item.id));
+	row += String.format('<td>{0}</td>', '');//item.is_active ? 'Yes' : 'No');
+	row += String.format('<td>{0}</td>', '');//item.username);
+	row += String.format('<td>{0}</td>', '');//item.domain);
+	row += String.format('<td>{0}</td>', '');//String.format("<a href='javascript:$.fn.zato.cluster.edit({0})'>Edit</a>", item.id));
 	row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.cluster.delete_({0});'>Delete</a>", item.id));
 	row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
-	row += String.format("<td class='ignore'>{0}</td>", item.is_active);
+	row += String.format("<td class='ignore'>{0}</td>", '');
 	
 	if(include_tr) {
 		row += '</tr>';
