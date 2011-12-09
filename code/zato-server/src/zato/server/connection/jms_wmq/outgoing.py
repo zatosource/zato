46432f26768b3e9a9e2f0c661847b33291b426f7
Dariusz Suchojad <dsuch-github@gefira.pl>
2011-12-09 22:50:34 +0100

    Use a string otherwise PyMQI complains.

--- code/zato-server/src/zato/server/connection/jms_wmq/outgoing.py
+++ code/zato-server/src/zato/server/connection/jms_wmq/outgoing.py
@@ -211,8 +211,8 @@ class OutgoingConnector(BaseConnector):
                 self.def_.cache_open_receive_queues,
                 self.def_.use_shared_connections,
                 ssl = self.def_.ssl,
-                ssl_cipher_spec = self.def_.ssl_cipher_spec,
-                ssl_key_repository = self.def_.ssl_key_repository,
+                ssl_cipher_spec = str(self.def_.ssl_cipher_spec),
+                ssl_key_repository = str(self.def_.ssl_key_repository),
                 needs_mcd = self.def_.needs_mcd,
             )
 
