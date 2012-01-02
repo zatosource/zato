635f0a578b38745ac0cfe429282e68511916cb16
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-01-03 00:20:39 +0100

    Fixed __import__s

--- code/zato-server/src/zato/server/connection/jms_wmq/channel.py
+++ code/zato-server/src/zato/server/connection/jms_wmq/channel.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
 import errno, logging, os, socket, sys
