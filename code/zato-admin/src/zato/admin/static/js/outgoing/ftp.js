fc6662a59dcd0a336dd7d9a211fd77f4bd5fbee8
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-01-02 16:01:16 +0100

    Started working on FTP connections.

---
+++ code/zato-admin/src/zato/admin/static/js/outgoing/ftp.js
@@ -0,0 +1,70 @@
+
+// /////////////////////////////////////////////////////////////////////////////
+
+$.fn.zato.data_table.FTP = new Class({
+    toString: function() {
+        var s = '<FTP id:{0} name:{1} is_active:{2}';
+        return String.format(s, this.id ? this.id : '(none)',
+                                this.name ? this.name : '(none)',
+                                this.is_active ? this.is_active : '(none)');
+    }
+});
+
+// /////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function() {
+    $('#data-table').tablesorter();
+    $.fn.zato.data_table.class_ = $.fn.zato.data_table.FTP;
+    $.fn.zato.data_table.new_row_func = $.fn.zato.outgoing.ftp.data_table.new_row;
+    $.fn.zato.data_table.parse();
+    $.fn.zato.data_table.setup_forms(['name', 'host', 'port']);
+})
+
+$.fn.zato.outgoing.ftp.create = function() {
+    $.fn.zato.data_table._create_edit('create', 'Create a new outgoing FTP connection', null);
+}
+
+$.fn.zato.outgoing.ftp.edit = function(id) {
+    $.fn.zato.data_table._create_edit('edit', 'Update the outgoing FTP connection', id);
+}
+
+$.fn.zato.outgoing.ftp.data_table.new_row = function(item, data, include_tr) {
+    var row = '';
+
+    if(include_tr) {
+        row += String.format("<tr id='tr_{0}' class='updated'>", item.id);
+    }
+
+    var is_active = item.is_active == true;
+    var dircache = item.dircache == true;
+    
+    row += "<td class='numbering'>&nbsp;</td>";
+    row += "<td><input type='checkbox' /></td>";
+    row += String.format('<td>{0}</td>', item.name);
+    row += String.format('<td>{0}</td>', is_active ? 'Yes' : 'No');
+    row += String.format('<td>{0}</td>', item.host);
+    row += String.format('<td>{0}</td>', item.user ? item.user : '');
+    row += String.format('<td>{0}</td>', item.passwd ? item.passwd : '');
+    row += String.format('<td>{0}</td>', item.acct ? item.acct : '');
+    row += String.format('<td>{0}</td>', item.timeout ? item.timeout : '');
+    row += String.format('<td>{0}</td>', item.port);
+    row += String.format('<td>{0}</td>', dircache ? 'Yes' : 'No');
+    row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.data_table.change_password({0})'>Change password</a>", item.id));
+    row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.outgoing.ftp.edit('{0}')\">Edit</a>", item.id));
+    row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.outgoing.ftp.delete_({0});'>Delete</a>", item.id));
+    row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
+    row += String.format("<td class='ignore'>{0}</td>", is_active);
+
+    if(include_tr) {
+        row += '</tr>';
+    }
+
+    return row;
+}
+
+$.fn.zato.outgoing.ftp.delete_ = function(id) {
+    $.fn.zato.data_table.delete_(id, 'td.item_id_',
+        'Outgoing FTP connection [{0}] deleted',
+        'Are you sure you want to delete the outgoing FTP connection [{0}]?',
+        true);
+}
--- code/zato-common/src/zato/common/odb/__init__.py
+++ code/zato-common/src/zato/common/odb/__init__.py
@@ -30,9 +30,9 @@ from sqlalchemy.sql import text
 logger = logging.getLogger(__name__)
 
 AMQP_DEFAULT_PRIORITY = 5
-WMQ_DEFAULT_PRIORITY = 5
 S3_DEFAULT_SEPARATOR = '/'
 S3_DEFAULT_KEY_SYNC_TIMEOUT = 1
+WMQ_DEFAULT_PRIORITY = 5
 
 # TODO: Handle extra parameters.
 engine_def = '{engine}://{user}:{password}@{host}/{db_name}'
