652f5b058d946d00251d22c9eb512b570fa3c181
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-02-23 19:53:19 +0100

    Made code work on Python 2.7

--- code/install.sh
+++ code/install.sh
@@ -9,9 +9,10 @@ rm -rf ../code/lib
 rm -rf ../code/parts
 
 sudo apt-get install haproxy uuid-dev uuid-runtime
-sudo apt-get install python2.6-dev swig python-pip libpq-dev
+sudo apt-get install python2.7-dev swig python-pip libpq-dev
 sudo apt-get install libyaml-dev libxml2-dev libxslt1-dev 
-sudo pip install distribute
+sudo pip install --upgrade distribute
+sudo pip install --upgrade virtualenv
 
 virtualenv --no-site-packages .
 ./bin/python bootstrap.py
