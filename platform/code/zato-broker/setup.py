c45f7250956e020e4d561188d4eac103c389db2f
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-02-25 18:14:41 +0100

    Moving the work so far to the 'platform' directory'.

---
+++ platform/code/zato-broker/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = "1.0.0"
+
+setup(
+      name = "zato-broker",
+      version = version,
+
+      author = "Zato",
+      author_email = "Zato",
+      url = "Zato",
+
+      package_dir = {"":"src"},
+      packages = find_packages("src"),
+
+      namespace_packages = ["zato"],
+
+      zip_safe = False,
+)
