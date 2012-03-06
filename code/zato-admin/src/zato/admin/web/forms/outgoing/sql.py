fbf24197c0c0d93d7a2ffc4781ad58f55db802a5
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-03-07 00:52:57 +0100

    Working on SQL forms.

--- code/zato-admin/src/zato/admin/web/forms/outgoing/sql.py
+++ code/zato-admin/src/zato/admin/web/forms/outgoing/sql.py
@@ -28,23 +28,25 @@ from django import forms
 # Zato
 from zato.admin.settings import engine_friendly_name
 
-class SQLConnectionPoolForm(forms.Form):
-    original_pool_name = forms.CharField(widget=forms.HiddenInput())
-    pool_name = forms.CharField(widget=forms.TextInput(attrs={"class":"required"}))
-    engine = forms.ChoiceField(widget=forms.Select(attrs={"class":"required"}))
-    host = forms.CharField(widget=forms.TextInput(attrs={"class":"required"}))
-    db_name = forms.CharField(widget=forms.TextInput(attrs={"class":"required"}))
-    user = forms.CharField(widget=forms.TextInput(attrs={"class":"required"}))
-    pool_size = forms.IntegerField(widget=forms.TextInput(attrs={"class":"required validate-digits", "style":"width:30px"}))
+class CreateForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'required'}))
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+    engine = forms.ChoiceField(widget=forms.Select(attrs={'class':'required'}))
+    host = forms.CharField(widget=forms.TextInput(attrs={'class':'required'}))
+    db_name = forms.CharField(widget=forms.TextInput(attrs={'class':'required'}))
+    user = forms.CharField(widget=forms.TextInput(attrs={'class':'required'}))
+    pool_size = forms.IntegerField(widget=forms.TextInput(attrs={'class':'required validate-digits', 'style':'width:30px'}))
     extra = forms.CharField(widget=forms.Textarea())
-    temp_id = forms.CharField(widget=forms.HiddenInput())
 
     def __init__(self, prefix=None, post_data=None):
-        super(SQLConnectionPoolForm, self).__init__(post_data, prefix=prefix)
-        self.fields["engine"].choices = []
+        super(CreateForm, self).__init__(post_data, prefix=prefix)
+        self.fields['engine'].choices = []
 
         # Sort engines by their friendly name.
         engines = sorted(engine_friendly_name.iteritems(), key=itemgetter(1))
 
         for engine, friendly_name in engines:
-            self.fields["engine"].choices.append([engine, friendly_name])
+            self.fields['engine'].choices.append([engine, friendly_name])
+
+class EditForm(CreateForm):
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
--- code/zato-admin/src/zato/admin/web/views/outgoing/sql.py
+++ code/zato-admin/src/zato/admin/web/views/outgoing/sql.py
@@ -17,8 +17,86 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-def index():
-    pass
+# stdlib
+import logging
+from traceback import format_exc
+
+# Django
+from django.http import HttpResponse, HttpResponseServerError
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+# lxml
+from lxml.objectify import Element
+
+# Validate
+from validate import is_boolean
+
+# Zato
+from zato.admin.web import invoke_admin_service
+from zato.admin.web.views import change_password as _change_password
+from zato.admin.web.forms import ChangePasswordForm, ChooseClusterForm
+from zato.admin.web.forms.outgoing.sql import CreateForm, EditForm
+from zato.admin.web.views import meth_allowed
+from zato.common.odb.model import Cluster, SQLConnectionPool
+from zato.common import zato_namespace, zato_path
+from zato.common.util import TRACE1
+
+logger = logging.getLogger(__name__)
+
+@meth_allowed('GET')
+def index(req):
+    zato_clusters = req.odb.query(Cluster).order_by('name').all()
+    choose_cluster_form = ChooseClusterForm(zato_clusters, req.GET)
+    cluster_id = req.GET.get('cluster')
+    items = []
+
+    create_form = CreateForm()
+    edit_form = EditForm(prefix='edit')
+    change_password_form = ChangePasswordForm()
+
+    if cluster_id and req.method == 'GET':
+
+        cluster = req.odb.query(Cluster).filter_by(id=cluster_id).first()
+        zato_message = Element('{%s}zato_message' % zato_namespace)
+        zato_message.data = Element('data')
+        zato_message.data.cluster_id = cluster_id
+
+        _, zato_message, soap_response  = invoke_admin_service(cluster, 'zato:outgoing.ftp.get-list', zato_message)
+
+        if zato_path('data.item_list.item').get_from(zato_message) is not None:
+
+            for msg_item in zato_message.data.item_list.item:
+
+                id = msg_item.id.text
+                name = msg_item.name.text
+                is_active = is_boolean(msg_item.is_active.text)
+
+                #host = msg_item.host.text if msg_item.host else ''
+                #user = msg_item.user.text if msg_item.user else ''
+                #acct = msg_item.acct.text if msg_item.acct else ''
+                #timeout = msg_item.timeout.text if msg_item.timeout else ''
+                #port = msg_item.port.text if msg_item.port else ''
+                #dircache = is_boolean(msg_item.dircache.text)
+
+                item =  SQLConnectionPool()#id, name, is_active, host, user, None, acct, timeout, port, dircache)
+                items.append(item)
+
+    return_data = {'zato_clusters':zato_clusters,
+        'cluster_id':cluster_id,
+        'choose_cluster_form':choose_cluster_form,
+        'items':items,
+        'create_form':create_form,
+        'edit_form':edit_form,
+        'change_password_form': change_password_form
+        }
+
+    # TODO: Should really be done by a decorator.
+    if logger.isEnabledFor(TRACE1):
+        logger.log(TRACE1, 'Returning render_to_response [{0}]'.format(return_data))
+
+    return render_to_response('zato/outgoing/sql.html', return_data,
+                              context_instance=RequestContext(req))
 
 def create():
     pass
