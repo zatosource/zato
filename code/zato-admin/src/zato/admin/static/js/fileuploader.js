de6cdfecab9e9f8b1f2921ab30b0e05756bf485e
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-04-19 01:32:43 +0200

    Use a more explanatory button description

--- code/zato-admin/src/zato/admin/static/js/fileuploader.js
+++ code/zato-admin/src/zato/admin/static/js/fileuploader.js
@@ -486,7 +486,7 @@ qq.FileUploader = function(o){
                 
         template: '<div class="qq-uploader">' + 
                 '<div class="qq-upload-drop-area"><span>Drop files here to upload</span></div>' +
-                '<div class="qq-upload-button">Upload a file</div>' +
+                '<div class="qq-upload-button">Upload a WSDL</div>' +
                 '<ul class="qq-upload-list"></ul>' + 
              '</div>',
 
