a1ffa49ab608653a1215d1e8970302765a058f68
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-04-26 23:07:13 +0200

    Added a grace time so the connector server has enough time for updating its keep alive status

--- code/zato-cli/src/zato/cli/create_server.py
+++ code/zato-cli/src/zato/cli/create_server.py
@@ -59,9 +59,15 @@ work_dir=../../work
 [singleton]
 initial_sleep_time=500
 
+# If a server doesn't update its keep alive data in 
+# connector_server_keep_alive_job_time * grace_time_multiplier seconds
+# it will be considered down and another server from the cluster will assume
+# the control of connectors
+connector_server_keep_alive_job_time=30 # In seconds
+grace_time_multiplier=3
+
 [spring]
 context_class=zato.server.spring_context.ZatoContext
-
 """
 
 haproxy_conf_contents = """
