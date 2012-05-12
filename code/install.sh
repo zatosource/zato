17b6f5930ef0d000e91e23247a1c94bc18da27a0
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-05-13 01:12:43 +0200

    Added a dependency on libevent-dev

--- code/install.sh
+++ code/install.sh
@@ -8,7 +8,7 @@ rm -rf ../code/.installed.cfg
 rm -rf ../code/lib
 rm -rf ../code/parts
 
-sudo apt-get install haproxy uuid-dev uuid-runtime
+sudo apt-get install haproxy uuid-dev uuid-runtime libevent-dev
 sudo apt-get install python2.7-dev swig python-pip libpq-dev
 sudo apt-get install libyaml-dev libxml2-dev libxslt1-dev 
 sudo pip install --upgrade distribute
