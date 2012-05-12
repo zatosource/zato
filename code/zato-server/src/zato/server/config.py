0c4caff9c62acb14a254de9cc4f39a94ad6e9bae
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-05-13 01:27:16 +0200

    Import mx.Tools back again

--- code/zato-server/src/zato/server/config.py
+++ code/zato-server/src/zato/server/config.py
@@ -23,6 +23,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from copy import deepcopy
 from threading import RLock
 
+# mx
+from mx.Tools import NotGiven
+
 # Paste
 from paste.util.multidict import MultiDict
 
