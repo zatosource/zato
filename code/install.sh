e51f0af59bb5303345bb895c979ebd3943d823e2
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-05-13 15:41:56 +0200

    Added a dependency on python-zdaemon

--- code/install.sh
+++ code/install.sh
@@ -9,7 +9,7 @@ rm -rf ../code/lib
 rm -rf ../code/parts
 
 sudo apt-get install haproxy uuid-dev uuid-runtime libevent-dev bzr
-sudo apt-get install python2.7-dev swig python-pip libpq-dev
+sudo apt-get install python2.7-dev swig python-pip libpq-dev python-zdaemon
 sudo apt-get install libyaml-dev libxml2-dev libxslt1-dev 
 sudo pip install --upgrade distribute
 sudo pip install --upgrade virtualenv
