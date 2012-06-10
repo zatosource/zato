961338a85e673b578d9e1ba4bfd1e9968d59bf1f
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-06-10 23:40:56 +0200

    Stop and restart HAProxy using the SIGUSR1 signal instead of zdaemon

--- code/zato-cli/src/zato/cli/stop.py
+++ code/zato-cli/src/zato/cli/stop.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
-import os, signal, sys
+import json, os, signal, sys
 
 # Zato
 from zato.cli import ManageCommand
@@ -29,6 +29,27 @@ class Stop(ManageCommand):
 
     command_name = 'stop'
     description = 'Stops a Zato component'
+    
+    def _signal(self, pid_file, component_name, signal_name, signal_code):
+        """ Sends a signal to a process known by its ID.
+        """
+        if not os.path.exists(pid_file):
+            msg = 'Did not find the expected file {0}, quitting now'.format(pid_file)
+            print(msg)
+            sys.exit(4) # TODO: Document exit codes
+
+        pid = open(pid_file).read().strip()
+        if not pid:
+            msg = 'Did not attempt to stop the {} because the file {} is empty'.format(component_name, pid_file)
+            print(msg)
+            sys.exit(5) # TODO: Document exit codes
+
+        pid = int(pid)
+        msg = 'Will now send {} to pid {} (as found in the {} file)'.format(signal_name, pid, pid_file)
+        print(msg)
+
+        os.kill(pid, signal_code)
+        open(pid_file, 'w').truncate()
 
     def _on_server(self):
         ports_pids = self._zdaemon_command('status')
@@ -43,29 +64,17 @@ class Stop(ManageCommand):
         if not any(ports_pids.values()):
             print('Zato load balancer and agent at {0} are not running'.format(self.component_dir))
         else:
+            # Stops the load balancer
+            json_config = json.loads(open(os.path.join(self.component_dir, 'config', 'lb-agent.conf')).read())
+            pid_file = os.path.abspath(os.path.join(self.component_dir, json_config['pid_file']))
+            self._signal(pid_file, 'load balancer', 'SIGUSR1', signal.SIGUSR1)
+            
+            # Stops the agent
             self._zdaemon_command('stop')
             print('Zato load balancer and agent at {0} have been stopped'.format(self.component_dir))
 
     def _on_zato_admin(self):
-        pid_file = os.path.join(self.component_dir, '.zato-admin.pid')
-        if not os.path.exists(pid_file):
-            msg = 'Did not find the expected file {0}, quitting now'.format(pid_file)
-            print(msg)
-            sys.exit(4) # TODO: Document exit codes
-
-        pid = open(pid_file).read().strip()
-        if not pid:
-            msg = 'Did not attempt to stop the ZatoAdmin instance because file {0} is empty'.format(pid_file)
-            print(msg)
-            sys.exit(5) # TODO: Document exit codes
-
-        pid = int(pid)
-        msg = 'Will now send SIGTERM to pid {0} (as found in the {1} file)'.format(pid, pid_file)
-        print(msg)
-
-        os.kill(pid, signal.SIGTERM)
-        open(pid_file, 'w').truncate()
-
+        self._signal(os.path.join(self.component_dir, '.zato-admin.pid'), 'ZatoAdmin instance', 'SIGTERM', signal.SIGTERM)
         print('ZatoAdmin stopped OK.')
         
     def _on_broker(self):
