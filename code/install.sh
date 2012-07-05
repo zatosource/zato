8dfb49a7787227ef5c6de6133a35897cb1fd9779
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-07-05 17:58:17 +0200

    Lean on the system-installed SciPy for statistics. Pinned a couple of other dependencies.

--- code/install.sh
+++ code/install.sh
@@ -10,12 +10,15 @@ rm -rf ../code/parts
 
 sudo apt-get install haproxy uuid-dev uuid-runtime libevent-dev bzr
 sudo apt-get install python2.7-dev swig python-pip libpq-dev python-zdaemon
-sudo apt-get install libyaml-dev libxml2-dev libxslt1-dev 
-sudo apt-get install libatlas-dev libblas3gf libatlas3gf-base libgfortran3 liblapack3gf libumfpack5.4.0 gfortran
+sudo apt-get install libyaml-dev libxml2-dev libxslt1-dev
+sudo apt-get install libatlas-dev libblas3gf libatlas3gf-base libumfpack5.4.0
+sudo apt-get install liblapack-dev libgfortran3 liblapack3gf gfortran
+
 sudo pip install --upgrade distribute
 sudo pip install --upgrade virtualenv
+sudo pip install --upgrade scipy
 
-virtualenv --no-site-packages .
+virtualenv --system-site-packages .
 ./bin/python bootstrap.py
 ./bin/buildout
 echo OK
