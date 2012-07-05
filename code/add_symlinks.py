98004ab2bb9949a47de52313a58d78698e89bea5
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-07-05 19:50:32 +0200

    Seems I've finally convinced buildout to use system-wide SciPy

---
+++ code/add_symlinks.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2012 Dariusz Suchojad <dsuch at gefira.pl>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+if __name__ == '__main__':
+    from os.path import dirname
+    packages = ['scipy']
+    for name in packages:
+        target = dirname(__import__(name).__file__)
+        source = open('./eggs/{}.egg-link'.format(name), 'w')
+        source.write(target)
+        source.close()
