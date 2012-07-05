01c857b0faf25fc0d72a741f3c3def1530ad09a3
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-07-05 22:52:26 +0200

    Wrestling with scipy and zc.buildout

--- code/install.sh
+++ code/install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-mkdir ../code/eggs
-python ./add_egglinks.py
-
-exit 1
-
 rm -rf ../code/bin
 rm -rf ../code/develop-eggs
 rm -rf ../code/downloads
@@ -13,17 +8,23 @@ rm -rf ../code/include
 rm -rf ../code/.installed.cfg
 rm -rf ../code/lib
 rm -rf ../code/parts
+rm -rf ../code/zato_extra_paths
 
 sudo apt-get install haproxy uuid-dev uuid-runtime libevent-dev bzr
 sudo apt-get install python2.7-dev swig python-pip libpq-dev python-zdaemon
 sudo apt-get install libyaml-dev libxml2-dev libxslt1-dev
 sudo apt-get install libatlas-dev libblas3gf libatlas3gf-base libumfpack5.4.0
 sudo apt-get install liblapack-dev libgfortran3 liblapack3gf gfortran
+sudo apt-get install python-scipy
+
+mkdir ../code/zato_extra_paths
+ln -s /usr/local/lib/python2.7/dist-packages/numpy ../code/zato_extra_paths
+ln -s /usr/local/lib/python2.7/dist-packages/scipy ../code/zato_extra_paths
 
 sudo pip install --upgrade distribute
 sudo pip install --upgrade virtualenv
 
-virtualenv --no-site-packages --extra-search-dir=/usr/local/lib/python2.7/dist-packages/scipy .
+virtualenv .
 
 ./bin/python bootstrap.py
 ./bin/buildout
