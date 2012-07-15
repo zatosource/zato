b50ee59d05c879c63dd864f837046f132e97a674
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-07-16 00:09:20 +0200

    Made Top N statistics tables sortable

--- code/zato-admin/src/zato/admin/static/js/stats/top-n.js
+++ code/zato-admin/src/zato/admin/static/js/stats/top-n.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
 
     var sparklines_options = {'width':'110px', 'height':'15px', 'lineColor':'#555', 'spotColor':false, 'disableHiddenCheck':true, 'fillColor':false}
     $('.trend').sparkline('html', sparklines_options);
+    
+    $('.stats-table').tablesorter();
 
 })
