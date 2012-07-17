dda4781b9df982de0c52a64286112c26ccfefa72
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-07-17 23:29:20 +0200

    A first stab at showing the right side of Top N stats view

--- code/zato-admin/src/zato/admin/static/js/stats/top-n.js
+++ code/zato-admin/src/zato/admin/static/js/stats/top-n.js
@@ -1,46 +1,80 @@
-$(document).ready(function() {
-
-    var sparklines_options = {'width':'46px', 'height':'15px', 'lineColor':'#555', 'spotColor':false, 'fillColor':false}
-
-    var _callback = function(data, status) {
-        var json = $.parseJSON(data.responseText);
-        $('.loading-tr').remove();
-        
-        var n_types = ['mean', 'usage'];
-        $.each(n_types, function(idx, n_type) {
-            $(String.format('#left-{0}-tr', n_type)).after(json[n_type]);
-            $(String.format('#left-{0}', n_type)).tablesorter();
-        });
-
-        if(json.has_stats) {
-            $('.left-csv').removeClass('hidden').addClass('visible');
-            $('.trend').sparkline('html', sparklines_options);
-            $('#left-usage-csv').attr('href', json.usage_csv_href);
-            $('#left-mean-csv').attr('href', json.mean_csv_href);
-            
-            var _switch_highlight = function(this_, remove_class, add_class) { 
-                var id = $(this_).attr('id');
-                id = _.last(_.str.words(id, '-'));
-                if(id) {
-                  $('tr[id*="' +id+'"] > td').removeClass(remove_class).addClass(add_class);
-                };
-            };
-            
-            $('.stats-table tr').mouseover(function() {
-                _switch_highlight(this, 'default', 'hover');
-            });
-                    
-            $('.stats-table tr').mouseout(function(){
-                _switch_highlight(this, 'hover', 'default');
-           });
-            
+$.fn.zato.stats.top_n.show_hide = function(selectors, show) {
+    $.each(selectors, function(idx, selector) {
+        var result = $(selector);
+        if(show) {
+            result.removeClass('hidden').addClass('visible');
         }
         else {
-            $('.left-csv').removeClass('visible').addClass('hidden');
+            result.removeClass('visible').addClass('hidden');
         }
+    })
+};
+
+$.fn.zato.stats.top_n.data_callback = function(data, status) {
+
+    var side = this['side'];
+
+    var json = $.parseJSON(data.responseText);
+    $(String.format('.{0}-loading-tr', side)).remove();
+    
+    var n_types = ['mean', 'usage'];
+    $.each(n_types, function(idx, n_type) {
+        $(String.format('#{0}-{1}-tr', side, n_type)).after(json[n_type]);
+        $(String.format('#{0}-{1}', side, n_type)).tablesorter();
+    });
+    
+    var show_hide = [String.format('.{0}-csv', side), '#compare_to'];
+
+    if(json.has_stats) {
+
+        var sparklines_options = {'width':'36px', 'height':'15px', 'lineColor':'#555', 'spotColor':false, 'fillColor':false}    
+
+        $.fn.zato.stats.top_n.show_hide(show_hide, true);
         
+        $(String.format('.{0}-trend', side)).sparkline('html', sparklines_options);
+        $(String.format('#{0}-usage-csv', side)).attr('href', json.usage_csv_href);
+        $(String.format('#{0}-mean-csv', side)).attr('href', json.mean_csv_href);
+        
+        var _switch_highlight = function(this_, remove_class, add_class) { 
+            var id = $(this_).attr('id');
+            id = _.last(_.str.words(id, '-'));
+            if(id) {
+              $('tr[id*="' +id+'"] > td').removeClass(remove_class).addClass(add_class);
+            };
+        };
+        
+        $('.stats-table tr').mouseover(function() {
+            _switch_highlight(this, 'default', 'hover');
+        });
+                
+        $('.stats-table tr').mouseout(function(){
+            _switch_highlight(this, 'hover', 'default');
+       });
+        
+    }
+    else {
+        $.fn.zato.stats.top_n.show_hide(show_hide, false);
+    }
+};
+
+$.fn.zato.stats.top_n.compare_to = function() {
+    var compare_to_label = $(this).find(':selected').val();
+    
+    if(compare_to_label) {
+        $.fn.zato.stats.top_n.show_hide(['#right-side'], true);
+        var data = {};
+        var keys = ['left_start', 'left_stop', 'n', 'cluster_id', 'id_compare_to'];
+        
+        $.each(keys, function(idx, key) {
+            data[key] = $('#'+key).val();
+        });
     };
     
+    $.fn.zato.post('../data/', $.fn.zato.stats.top_n.data_callback, data, 'json', true, {'side': 'right'});
+};
+
+$(document).ready(function() {
+
     var data = {};
     var keys = ['cluster_id', 'left_start', 'left_stop', 'n'];
     var value = null;
@@ -50,6 +84,8 @@ $(document).ready(function() {
         data[key.replace('left_', '')] = value;
     });
     
-    $.fn.zato.post('../data/', _callback, data, 'json', true);
+    $.fn.zato.post('../data/', $.fn.zato.stats.top_n.data_callback, data, 'json', true, {'side': 'left'});
+    
+    $('#id_compare_to').change($.fn.zato.stats.top_n.compare_to);
         
 })
