9cf37e8b919df61242346915b10ca6f55be89052
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-08-16 00:14:05 +0200

    Fixed typo

--- code/zato-server/src/zato/server/main.py
+++ code/zato-server/src/zato/server/main.py
@@ -40,7 +40,7 @@ def run(host, port, base_dir, start_singleton):
 
     # We're doing it here even if someone doesn't use PostgreSQL at all
     # so we're not suprised when someone suddenly starts using PG.
-    # TODO: Make sure it's registered for each subprocess when the code's
+    # TODO: Make sure it's registered for each of the subprocess when the code's
     #       finally modified to use subprocesses.
     psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
     psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
