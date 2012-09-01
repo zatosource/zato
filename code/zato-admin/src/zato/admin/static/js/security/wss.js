ec955def0e3e173e01e603ec7da6bea21b5a7fd9
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-09-01 18:22:04 +0200

    Don't show checkboxes

--- code/zato-admin/src/zato/admin/static/js/security/wss.js
+++ code/zato-admin/src/zato/admin/static/js/security/wss.js
@@ -43,7 +43,7 @@ $.fn.zato.security.wss.data_table.new_row = function(item, data, include_tr) {
 	}
 	
 	row += "<td class='numbering'>&nbsp;</td>";
-	row += "<td><input type='checkbox' /></td>";
+	row += "<td class='impexp'><input type='checkbox' /></td>";
 	row += String.format('<td>{0}</td>', item.name);
 	row += String.format('<td>{0}</td>', item.is_active ? 'Yes' : 'No');
 	row += String.format('<td>{0}</td>', item.username);
