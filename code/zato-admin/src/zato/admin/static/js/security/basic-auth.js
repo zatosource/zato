795c0a1100ee3bc93f16844ff7af849a129fddc7
Dariusz Suchojad <dsuch-github@gefira.pl>
2012-09-01 17:38:20 +0200

    Don't show the checkbox for now

--- code/zato-admin/src/zato/admin/static/js/security/basic-auth.js
+++ code/zato-admin/src/zato/admin/static/js/security/basic-auth.js
@@ -41,7 +41,7 @@ $.fn.zato.security.basic_auth.data_table.new_row = function(item, data, include_
     var is_active = item.is_active == true
 
     row += "<td class='numbering'>&nbsp;</td>";
-    row += "<td><input type='checkbox' /></td>";
+    row += "<td class='impexp'><input type='checkbox' /></td>";
     row += String.format('<td>{0}</td>', item.name);
     row += String.format('<td>{0}</td>', is_active ? 'Yes' : 'No');
     row += String.format('<td>{0}</td>', item.username);
