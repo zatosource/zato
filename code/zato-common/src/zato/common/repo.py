0054061849b409faa05dc36aadcc81568ee85d7e
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-01-13 19:35:22 +0100

    Made sure a bzr repo has always a 'whoami' set

--- code/zato-common/src/zato/common/repo.py
+++ code/zato-common/src/zato/common/repo.py
@@ -20,12 +20,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
-import logging, os
+import logging, os, socket
 
 # Bazaar
 import bzrlib
+from bzrlib.branch import Branch
 from bzrlib.bzrdir import BzrDir
+from bzrlib.errors import NoWhoami
 from bzrlib.workingtree import WorkingTree
+
+# Zato
+from zato.common.util import get_current_user
 
 logger = logging.getLogger(__name__)
 
@@ -37,12 +42,19 @@ class RepoManager(object):
         """ Makes sure the self.repo_location directory is a Bazaar branch.
         The repo and Bazaar branch will be created if they don't already exist.
         Any unknown or modified files will be commited to the branch.
+        Also, 'bzr whoami' will be set to the current user so that all commands
+        can be traced back to an actual person (assuming everyone has their
+        own logins).
         """
+
         try:
             BzrDir.open(self.repo_location)
         except bzrlib.errors.NotBranchError, e:
             logger.info('Location [{}] is not a Bazaar branch. Will turn it into one.'.format(self.repo_location))
             BzrDir.create_branch_convenience(self.repo_location)
+            
+        c = Branch.open(self.repo_location).get_config_stack()
+        c.set('email', '{}@{}'.format(get_current_user(), socket.getfqdn()))
 
         self.tree = WorkingTree.open(self.repo_location)
         delta = self.tree.changes_from(self.tree.basis_tree(), want_unversioned=True)
