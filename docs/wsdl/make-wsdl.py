aca7778355fe3354ca5e04b41b6c6d104088970c
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-01-17 11:56:50 +0100

    Broken out WSDL into individual pieces

---
+++ docs/wsdl/make-wsdl.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2013 Dariusz Suchojad <dsuch at gefira.pl>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# stdlib
+import os
+
+# lxml
+from lxml import etree
+
+include_prefix = '<!-- include'
+include_suffix = ' -->'
+
+def main():
+    out = []
+    template = open('./wsdl-template.xml')
+    for line in template:
+        if line.startswith(include_prefix):
+            path = line.replace(include_prefix, '').replace(include_suffix, '').strip()
+            path = os.path.abspath(path)
+            include = open(path).read().rstrip() + '\n'
+            out.append(include)
+        else:
+            out.append(line)
+        
+    out = ''.join(out)
+    open('./zato.xml', 'w').write(out)
+        
+if __name__ == '__main__':
+    main()
