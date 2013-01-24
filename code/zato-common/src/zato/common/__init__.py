603eb7a733e04191ebcfeb8b9fec2f98f0d673da
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-01-24 14:47:36 +0100

    Getting rid of the old quoting style

--- code/zato-common/src/zato/common/__init__.py
+++ code/zato-common/src/zato/common/__init__.py
@@ -100,13 +100,13 @@ DONT_DEPLOY_ATTR_NAME = 'zato_dont_import'
 
 # A convenient constant used in several places, simplifies passing around
 # arguments which are, well, not given (as opposed to being None, an empty string etc.)
-ZATO_NOT_GIVEN = b"ZATO_NOT_GIVEN"
+ZATO_NOT_GIVEN = b'ZATO_NOT_GIVEN'
 
 # Also used in a couple of places.
-ZATO_OK = "ZATO_OK"
-ZATO_ERROR = "ZATO_ERROR"
-ZATO_WARNING = "ZATO_WARNING"
-ZATO_NONE = b"ZATO_NONE"
+ZATO_OK = 'ZATO_OK'
+ZATO_ERROR = 'ZATO_ERROR'
+ZATO_WARNING = 'ZATO_WARNING'
+ZATO_NONE = b'ZATO_NONE'
 
 # Used when there's a need for encrypting/decrypting a well-known data.
 ZATO_CRYPTO_WELL_KNOWN_DATA = 'ZATO'
