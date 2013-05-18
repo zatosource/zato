a6bcac3a371cba3a0c31aa75b920ac4bc0dd5e3e
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-05-18 17:37:02 +0200

    Made ./install.sh independent of where it's run from

--- code/install.sh
+++ code/install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+#
+# Taken from https://gist.github.com/josephwecker/2884332
+#
+CURDIR="${BASH_SOURCE[0]}";RL="readlink";([[ `uname -s`=='Darwin' ]] || RL="$RL -f")
+while([ -h "${CURDIR}" ]) do CURDIR=`$RL "${CURDIR}"`; done
+N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
+
 function symlink_py {
     ln -s `python -c 'import '${1}', os.path, sys; sys.stdout.write(os.path.dirname('${1}'.__file__))'` ../code/zato_extra_paths
 }
 
-rm -rf ../code/bin
-rm -rf ../code/develop-eggs
-rm -rf ../code/downloads
-rm -rf ../code/eggs
-rm -rf ../code/include
-rm -rf ../code/.installed.cfg
-rm -rf ../code/lib
-rm -rf ../code/parts
-rm -rf ../code/zato_extra_paths
+rm -rf $CURDIR/bin
+rm -rf $CURDIR/develop-eggs
+rm -rf $CURDIR/downloads
+rm -rf $CURDIR/eggs
+rm -rf $CURDIR/include
+rm -rf $CURDIR/.installed.cfg
+rm -rf $CURDIR/lib
+rm -rf $CURDIR/parts
+rm -rf $CURDIR/zato_extra_paths
 
 sudo apt-get install bzr gfortran haproxy  \
     ruby-sass libatlas-dev libatlas3gf-base libblas3gf \
@@ -20,9 +27,8 @@ sudo apt-get install bzr gfortran haproxy  \
     libpq-dev libyaml-dev libxml2-dev libxslt1-dev libumfpack5.4.0 \
     openssl python2.7-dev python-m2crypto python-numpy python-pip \
     python-scipy python-zdaemon swig uuid-dev uuid-runtime
-    
 
-mkdir ../code/zato_extra_paths
+mkdir $CURDIR/zato_extra_paths
 
 symlink_py 'M2Crypto'
 symlink_py 'numpy'
@@ -33,8 +39,8 @@ sudo pip install --upgrade virtualenv
 
 virtualenv .
 
-./bin/python bootstrap.py -v 1.7.0
-./bin/buildout
+$CURDIR/bin/python bootstrap.py -v 1.7.0
+$CURDIR/bin/buildout
 
 echo
 echo OK
