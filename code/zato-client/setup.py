3064e84ee8c88dfc8237fcd3dae428b7b49b78c5
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-06-16 15:07:12 +0200

    Updated metadata

--- code/zato-client/setup.py
+++ code/zato-client/setup.py
@@ -8,7 +8,9 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 from setuptools import setup, find_packages
 
-version = '1.1'
+version = '1.1.3'
+
+long_description = description = 'Convenience Python client for Zato ESB and app server (https://zato.io)'
 
 setup(
       name = 'zato-client',
@@ -17,11 +19,50 @@ setup(
       author = 'Zato Developers',
       author_email = 'info@zato.io',
       url = 'https://zato.io',
+      license = 'GNU Lesser General Public License v3 (LGPLv3)',
+      platforms = 'OS Independent',
+      description = description,
+      long_description = description,
 
       package_dir = {'':'src'},
       packages = find_packages('src'),
-
       namespace_packages = ['zato'],
+      
+      install_requires=[
+          'anyjson==0.3.3',
+          'bunch==1.0.1',
+          'lxml==3.0.1',
+          'requests==0.14.2',
+          'zato-common >=1.1,<1.2'
+          ],
+      
+      keywords=('soa eai esb middleware messaging queueing asynchronous integration performance http zeromq framework events agile broker messaging server jms enterprise python middleware clustering amqp nosql websphere mq wmq mqseries ibm amqp zmq'),
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Framework :: Buildout',
+          'Intended Audience :: Customer Service',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Healthcare Industry',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Telecommunications Industry',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: C',
+          'Programming Language :: Python :: 2 :: Only',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          'Topic :: Communications',
+          'Topic :: Database',
+          'Topic :: Internet',
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+          'Topic :: Internet :: File Transfer Protocol (FTP)',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: Object Brokering',
+          ],
 
       zip_safe = False,
 )
