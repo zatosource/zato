329c4241eddcd52fca61a22c5c2e1d94397cddc7
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-06-16 15:07:12 +0200

    Updated metadata

--- code/zato-common/setup.py
+++ code/zato-common/setup.py
@@ -8,7 +8,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 from setuptools import setup, find_packages
 
-version = '1.1'
+version = '1.1.1'
 
 setup(
       name = 'zato-common',
@@ -17,11 +17,59 @@ setup(
       author = 'Zato Developers',
       author_email = 'info@zato.io',
       url = 'https://zato.io',
+      license = 'GNU Lesser General Public License v3 (LGPLv3)',
+      platforms = 'OS Independent',
+      description = 'Constants and utils common across the whole of Zato ESB and app server (https://zato.io)',
 
       package_dir = {'':'src'},
       packages = find_packages('src'),
-
       namespace_packages = ['zato'],
+      
+      install_requires=[
+          'anyjson==0.3.3',
+          'bunch==1.0.1',
+          'configobj==4.7.2',
+          'bzr==2.5',
+          'distutils2==1.0a4',
+          'lxml==3.0.1',
+          'mock==0.8.0',
+          'nose==1.2.1',
+          'pip==1.1',
+          'python-dateutil==2.1',
+          'pytz==2012h',
+          'pyzmq==2.2.0.1',
+          'pyzmq-static==2.2',
+          'redis==2.4.13',
+          'springpython==1.3.0RC1',
+          'urllib3==1.5',
+          ],
+      
+      keywords=('soa eai esb middleware messaging queueing asynchronous integration performance http zeromq framework events agile broker messaging server jms enterprise python middleware clustering amqp nosql websphere mq wmq mqseries ibm amqp zmq'),
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Framework :: Buildout',
+          'Intended Audience :: Customer Service',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Healthcare Industry',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Telecommunications Industry',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: C',
+          'Programming Language :: Python :: 2 :: Only',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Database',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          'Topic :: Internet',
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+          'Topic :: Internet :: File Transfer Protocol (FTP)',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: Object Brokering',
+          ],
 
       zip_safe = False,
 )
