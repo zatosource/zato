d444a12c30f1137af30851a9df70fc224b7f1b12
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-07-04 21:17:43 +0200

    Adding script for clearing out everything that's not raw source code

---
+++ code/clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURDIR="${BASH_SOURCE[0]}";RL="readlink";([[ `uname -s`=='Darwin' ]] || RL="$RL -f")
+while([ -h "${CURDIR}" ]) do CURDIR=`$RL "${CURDIR}"`; done
+N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
+
+function symlink_py {
+    ln -s `python -c 'import '${1}', os.path, sys; sys.stdout.write(os.path.dirname('${1}'.__file__))'` $CURDIR/zato_extra_paths
+}
+
+rm -rf $CURDIR/bin
+rm -rf $CURDIR/develop-eggs
+rm -rf $CURDIR/downloads
+rm -rf $CURDIR/eggs
+rm -rf $CURDIR/include
+rm -rf $CURDIR/.installed.cfg
+rm -rf $CURDIR/lib
+rm -rf $CURDIR/parts
+rm -rf $CURDIR/zato_extra_paths
+
