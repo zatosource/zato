c988504b43f53eee90d9348b3f3b204da3fc85c2
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-08-22 11:43:26 +0200

    Using n/a instead of never makes more sense for edit action

--- code/zato-web-admin/src/zato/admin/static/js/pattern/delivery/definition/index.js
+++ code/zato-web-admin/src/zato/admin/static/js/pattern/delivery/definition/index.js
@@ -58,7 +58,7 @@ $.fn.zato.pattern.delivery.data_table.new_row = function(item, data, include_tr)
     row += String.format('<td>n/a <a href="../in-doubt/{0}/{1}/{2}/{3}/">(list)</a></td>', item.name, item.target_type, item.target, item.id);
     row += String.format('<td>n/a <a href="../arch-success/{0}/{1}/{2}/{3}/">(list)</a></td>', item.name, item.target_type, item.target, item.id);
     row += String.format('<td>n/a <a href="../arch-failed/{0}/{1}/{2}/{3}/">(list)</a></td>', item.name, item.target_type, item.target, item.id);
-    row += String.format('<td><span class="form_hint">{0}</span></td>', '(Never)');
+    row += String.format('<td><span class="form_hint">{0}</span></td>', 'n/a');
     row += String.format('<td><a href="javascript:$.fn.zato.pattern.delivery.edit(\'{0}\')">Edit</a></td>', item.id);
     row += String.format('<td><a href="javascript:$.fn.zato.pattern.delivery.delete_(\'{0}\')">Delete</a></td>', item.id);
     row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
