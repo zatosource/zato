80559acfdd2b558f7c42a79db3654be2c1daf674
Dariusz Suchojad <dsuch-github@gefira.pl>
2013-09-01 21:59:04 +0200

    Cut the Service.name column to 300 characters so MySQL is happy

--- code/zato-common/src/zato/common/odb/model.py
+++ code/zato-common/src/zato/common/odb/model.py
@@ -359,7 +359,7 @@ class Service(Base):
     __table_args__ = (UniqueConstraint('name', 'cluster_id'), {})
 
     id = Column(Integer, Sequence('service_id_seq'), primary_key=True)
-    name = Column(String(2000), nullable=False)
+    name = Column(String(300), nullable=False)
     is_active = Column(Boolean(), nullable=False)
     impl_name = Column(String(2000), nullable=False)
     is_internal = Column(Boolean(), nullable=False)
