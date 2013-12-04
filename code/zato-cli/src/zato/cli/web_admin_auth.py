236eb7cd1c8292fe8738fbb146787adb51891f66
Dariusz Suchojad <dsuch-github@m.zato.io>
2013-12-04 15:40:14 +0100

    GH #122: Added CLI for updating claimed IDs (OpenID)

--- code/zato-cli/src/zato/cli/web_admin_auth.py
+++ code/zato-cli/src/zato/cli/web_admin_auth.py
@@ -9,11 +9,13 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
-import json, os
+import json, os, sys
 
 # Zato
 from zato.admin.zato_settings import update_globals
 from zato.cli import ManageCommand
+
+# ################################################################################################################################
 
 class _WebAdminAuthCommand(ManageCommand):
     def _prepare(self, args):
@@ -23,26 +25,13 @@ class _WebAdminAuthCommand(ManageCommand):
         config['config_dir'] = os.path.abspath(args.path)
         update_globals(config, base_dir)
         os.environ['DJANGO_SETTINGS_MODULE'] = 'zato.admin.settings'
-        
+
     def _ok(self, args):
         # Needed because Django took over our logging config
         self.reset_logger(args, True)
         self.logger.info('OK')
 
-class UpdatePassword(_WebAdminAuthCommand):
-    """ Updates a web admin user's password
-    """
-    opts = [
-        {'name': 'username', 'help': 'Username to change the password of'},
-    ]
-    
-    def execute(self, args):
-        self._prepare(args)
-        
-        from django.contrib.auth.management.commands.changepassword import Command
-        Command().handle(args.username)
-        
-        self._ok(args)
+# ################################################################################################################################
         
 class CreateUser(_WebAdminAuthCommand):
     """ Creates a new web admin user
@@ -53,15 +42,75 @@ class CreateUser(_WebAdminAuthCommand):
         """
         def __init__(self, logger):
             self.logger = logger
-            
+
         def write(self, msg):
             self.logger.info(msg.strip())
-        
+
     def execute(self, args):
         self._prepare(args)
-        
+
         from django.contrib.auth.management.commands.createsuperuser import Command
         Command.stdout = CreateUser._FakeStdout(self.logger)
         Command().handle(interactive=True)
 
         self._ok(args)
+
+# ################################################################################################################################
+
+class UpdatePassword(_WebAdminAuthCommand):
+    """ Updates a web admin user's password
+    """
+    opts = [
+        {'name': 'username', 'help': 'Username to change the password of'},
+    ]
+
+    def execute(self, args):
+        self._prepare(args)
+
+        from django.contrib.auth.management.commands.changepassword import Command
+        Command().handle(args.username)
+
+        self._ok(args)
+
+# ################################################################################################################################
+
+class UpdateOpenID(_WebAdminAuthCommand):
+    """ Updates a claimed OpenID for a user.
+    """
+    opts = [
+        {'name': 'username', 'help': 'Username to change a claimed OpenID of'},
+        {'name': 'claimed-id', 'help': 'Claimed OpenID to set of a user'},
+    ]
+
+    def execute(self, args):
+        self._prepare(args)
+
+        from django.contrib.auth.models import User
+        from django_openid_auth.models import UserOpenID
+
+        # Django took over logging and we need it back
+        self.reset_logger(args, True)
+
+        try:
+            user = User.objects.get(username=args.username)
+        except User.DoesNotExist:
+
+            # We can only give up at that point, the user must exist.
+            self.logger.error('Error: No such user [{}]'.format(args.username))
+            sys.exit(self.SYS_ERROR.NO_SUCH_WEB_ADMIN_USER)
+
+        claimed_id = getattr(args, 'claimed-id')
+
+        try:
+            user_open_id = UserOpenID.objects.get(user=user)
+        except UserOpenID.DoesNotExist:
+            user_open_id = UserOpenID()
+            user_open_id.user = user
+
+        user_open_id.claimed_id = claimed_id
+        user_open_id.display_id = claimed_id # Same value for display_id
+        user_open_id.save()
+
+        self._ok(args)
+
+# ################################################################################################################################
