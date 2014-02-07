28d9771e7d2bbd9cf857d61949b35ec86ebc51c6
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-02-08 00:37:25 +0100

    GH #152 - Properly pass serialization_type to edit form.

--- code/zato-web-admin/src/zato/admin/static/js/http_soap/index.js
+++ code/zato-web-admin/src/zato/admin/static/js/http_soap/index.js
@@ -119,6 +119,7 @@ $.fn.zato.http_soap.data_table.new_row = function(item, data, include_tr) {
     if(is_outgoing) {
         row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.ping_method);
         row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.pool_size);
+        row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.serialization_type);
     }
 
     if(is_channel) {
