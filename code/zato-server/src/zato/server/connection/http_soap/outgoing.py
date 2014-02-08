88b6fcfa3be5d42dabf78356599acab54a998790
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-02-08 17:42:26 +0100

    GH #152 - Start Suds SOAP clients when a server is starting.

--- code/zato-server/src/zato/server/connection/http_soap/outgoing.py
+++ code/zato-server/src/zato/server/connection/http_soap/outgoing.py
@@ -12,9 +12,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 from copy import deepcopy
 from cStringIO import StringIO
-from datetime import datetime
+from datetime import datetime, timedelta
 from json import dumps, loads
 from traceback import format_exc
+
+# gevent
+import gevent
+from gevent.queue import Queue
 
 # parse
 from parse import PARSE_RE
@@ -28,8 +32,10 @@ from zato.common.util import get_component_name, security_def_type
 
 logger = logging.getLogger(__name__)
 
-class HTTPSOAPWrapper(object):
-    """ A thin wrapper around the API exposed by the 'requests' package.
+# ################################################################################################################################
+
+class BaseHTTPSOAPWrapper(object):
+    """ Base class for HTTP/SOAP connections wrappers.
     """
     def __init__(self, config, requests_module=None):
         self.config = config
@@ -37,9 +43,91 @@ class HTTPSOAPWrapper(object):
         self.config_no_sensitive['password'] = '***'
         self.requests_module = requests_module or requests
         self.session = self.requests_module.session(pool_maxsize=self.config['pool_size'])
-        
         self._component_name = get_component_name()
+
+        self.address = None
+        self.path_params = []
+
+        self.set_address_data()
+        self.set_auth()
+
+    def ping(self, cid):
+        """ Pings a given HTTP/SOAP resource
+        """
+        if logger.isEnabledFor(logging.DEBUG):
+            msg = 'About to ping:[{}]'.format(self.config_no_sensitive)
+            logger.debug(msg)
+
+        # session will write some info to it ..
+        verbose = StringIO()
+
+        start = datetime.utcnow()
+
+        def zato_pre_request_hook(hook_data, *args, **kwargs):
+            entry = '{} (UTC) {} {}\n'.format(datetime.utcnow().isoformat(), 
+                hook_data['request'].method, hook_data['request'].url)
+            verbose.write(entry)
+
+        # .. invoke the other end ..
+        response = self.session.request(self.config['ping_method'], self.address, 
+                auth=self.requests_auth, headers=self._create_headers(cid, {}),
+                hooks={'zato_pre_request':zato_pre_request_hook})
         
+        # .. store additional info, get and close the stream.
+        verbose.write('Code: {}'.format(response.status_code))
+        verbose.write('\nResponse time: {}'.format(datetime.utcnow() - start))
+        value = verbose.getvalue()
+        verbose.close()
+
+        return value
+
+    def _create_headers(self, cid, user_headers):
+        headers = {
+            'X-Zato-CID': cid,
+            'X-Zato-Component':self._component_name,
+            'X-Zato-Msg-TS':datetime.utcnow().isoformat(),
+            }
+
+        if self.config.get('transport') == URL_TYPE.SOAP:
+            headers['SOAPAction'] = self.config.get('soap_action')
+
+        headers.update(user_headers)
+
+        return headers
+
+    def set_auth(self):
+        """ Configures the security for requests, if any is to be configured at all.
+        """
+        self.requests_auth = self.auth if self.config['sec_type'] == security_def_type.basic_auth else None
+        if self.config['sec_type'] == security_def_type.wss:
+            self.soap[self.config['soap_version']]['header'] = self.soap[self.config['soap_version']]['header_template'].format(
+                Username=self.config['username'], Password=self.config['password'])
+
+
+    def set_address_data(self):
+        """Sets the full address to invoke and parses input URL's configuration, 
+        to extract any named parameters that will have to be passed in by users
+        during actual calls to the resource.
+        """
+        self.address = '{}{}'.format(self.config['address_host'], self.config['address_url_path'])
+        groups = PARSE_RE.split(self.config['address_url_path'])
+
+        logger.debug('self.address:[%s], groups:[%s]', self.address, groups)
+
+        for group in groups:
+            if group and group[0] == '{':
+                self.path_params.append(group[1:-1])
+
+        logger.debug('self.address:[%s], self.path_params:[%s]', self.address, self.path_params)
+
+# ################################################################################################################################
+
+class HTTPSOAPWrapper(BaseHTTPSOAPWrapper):
+    """ A thin wrapper around the API exposed by the 'requests' package.
+    """
+    def __init__(self, config, requests_module=None):
+        super(HTTPSOAPWrapper, self).__init__(config, requests_module)
+
         self.soap = {}
         self.soap['1.1'] = {}
         self.soap['1.1']['content_type'] = 'text/xml; charset=utf-8'
@@ -74,12 +162,6 @@ class HTTPSOAPWrapper(object):
           </wsse:Security>
         </s12:Header>
         """
-        
-        self.address = None
-        self.path_params = []
-        
-        self.set_address_data()
-        self.set_auth()
 
 # ##############################################################################
         
@@ -89,31 +171,7 @@ class HTTPSOAPWrapper(object):
     __repr__ = __str__
     
 # ##############################################################################
-        
-    def set_auth(self):
-        """ Configures the security for requests, if any is to be configured at all.
-        """
-        self.requests_auth = self.auth if self.config['sec_type'] == security_def_type.basic_auth else None
-        if self.config['sec_type'] == security_def_type.wss:
-            self.soap[self.config['soap_version']]['header'] = self.soap[self.config['soap_version']]['header_template'].format(
-                Username=self.config['username'], Password=self.config['password'])
-            
-    def set_address_data(self):
-        """	Sets the full address to invoke and parses input URL's configuration, 
-        to extract any named parameters that will have to be passed in by users
-        during actual calls to the resource.
-        """
-        self.address = '{}{}'.format(self.config['address_host'], self.config['address_url_path'])
-        groups = PARSE_RE.split(self.config['address_url_path'])
-        
-        logger.debug('self.address:[%s], groups:[%s]', self.address, groups)
 
-        for group in groups:
-            if group and group[0] == '{':
-                self.path_params.append(group[1:-1])
-
-        logger.debug('self.address:[%s], self.path_params:[%s]', self.address, self.path_params)
-    
     def format_address(self, cid, params):
         """ Formats a URL path to an external resource. Note that exception raised
         do not contain anything except for CID. This is in order to keep any potentially
@@ -158,55 +216,11 @@ class HTTPSOAPWrapper(object):
         return auth
     
     auth = property(fget=_get_auth, doc=_get_auth)
-    
-    def _create_headers(self, cid, user_headers):
-        headers = {
-            'X-Zato-CID': cid,
-            'X-Zato-Component':self._component_name,
-            'X-Zato-Msg-TS':datetime.utcnow().isoformat(),
-            }
 
-        if self.config.get('transport') == URL_TYPE.SOAP:
-            headers['SOAPAction'] = self.config.get('soap_action')
-
-        headers.update(user_headers)
-        
-        return headers
-    
     def _enforce_is_active(self):
         if not self.config['is_active']:
             raise Inactive(self.config['name'])
-    
-    def ping(self, cid):
-        """ Pings a given HTTP/SOAP resource
-        """
-        if logger.isEnabledFor(logging.DEBUG):
-            msg = 'About to ping:[{}]'.format(self.config_no_sensitive)
-            logger.debug(msg)
-         
-        # session will write some info to it ..
-        verbose = StringIO()
-        
-        start = datetime.utcnow()
-        
-        def zato_pre_request_hook(hook_data, *args, **kwargs):
-            entry = '{} (UTC) {} {}\n'.format(datetime.utcnow().isoformat(), 
-                hook_data['request'].method, hook_data['request'].url)
-            verbose.write(entry)
-        
-        # .. invoke the other end ..
-        response = self.session.request(self.config['ping_method'], self.address, 
-                auth=self.requests_auth, headers=self._create_headers(cid, {}),
-                hooks={'zato_pre_request':zato_pre_request_hook})
-        
-        # .. store additional info, get and close the stream.
-        verbose.write('Code: {}'.format(response.status_code))
-        verbose.write('\nResponse time: {}'.format(datetime.utcnow() - start))
-        value = verbose.getvalue()
-        verbose.close()
-        
-        return value
-    
+
     def _soap_data(self, data, headers):
         """ Wraps the data in a SOAP-specific messages and adds the headers required.
         """
@@ -286,4 +300,54 @@ class HTTPSOAPWrapper(object):
     def patch(self, cid, data='', params=None, *args, **kwargs):
         return self.http_request('PATCH', cid, data, params, *args, **kwargs)
 
-# ##############################################################################
+# ################################################################################################################################
+
+class SudsSOAPWrapper(BaseHTTPSOAPWrapper):
+    """ A thin wrapper around the suds SOAP library
+    """
+    def __init__(self, config):
+        super(SudsSOAPWrapper, self).__init__(config)
+        self.config = config
+        self.config_no_sensitive = deepcopy(self.config)
+        self.config_no_sensitive['password'] = '***'
+        self.client_queue = Queue(self.config['pool_size'])
+
+    def build_client_queue(self):
+
+        logger.warn(self.config)
+        logger.warn(self.config_no_sensitive)
+
+        # Lazily-imported here to make sure gevent monkey patches everything well in advance
+        from suds.client import Client
+        from suds.transport.http import HttpAuthenticated
+        from suds.transport.https import WindowsHttpAuthenticated
+        from suds.wsse import UsernameToken
+
+        url = '{}{}'.format(self.config['address_host'], self.config['address_url_path'])
+
+        def add_client():
+            client = Client(url, autoblend=True)
+            self.client_queue.put(client)
+            logger.debug('Adding Suds SOAP client to [%s]', url)
+
+        for x in range(self.client_queue.maxsize):
+            gevent.spawn(add_client)
+
+        start = datetime.utcnow()
+        build_until = start + timedelta(seconds=self.config['queue_build_cap'])
+
+        while not self.client_queue.full():
+            gevent.sleep(0.5)
+
+            now = datetime.utcnow()
+            if  now >= build_until:
+
+                msg = 'Built {}/{} Suds SOAP clients to {} within {} seconds, cannot continue'
+                logger.critical(
+                    msg.format(self.client_queue.qsize(), self.client_queue.maxsize, url, self.config['queue_build_cap']))
+                raise Exception(msg)
+
+            logger.info('%d/%d Suds SOAP clients connected to [%s] after %s (cap: %ss)',
+                self.client_queue.qsize(), self.client_queue.maxsize, url, now - start, self.config['queue_build_cap'])
+
+        logger.info('Obtained %d Suds SOAP clients to [%s]', self.client_queue.maxsize, url)
