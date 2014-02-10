c468872e6c84d59e5fefb61c3b50364dea495afa
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-02-10 23:17:29 +0100

    GH #102 - yield messages and store in-flight IDs for later inspection.

--- code/zato-common/src/zato/common/pubsub/__init__.py
+++ code/zato-common/src/zato/common/pubsub/__init__.py
@@ -16,7 +16,7 @@ patch_all()
 
 # stdlib
 from datetime import datetime
-from json import dumps
+from json import dumps, loads
 from logging import getLogger
 from sys import maxint
 from traceback import format_exc
@@ -46,11 +46,12 @@ class Topic(object):
 class Message(object):
     """ A published or received message.
     """
-    def __init__(self, payload='', msg_id=None, mime_type='text/plain', priority=5, expiration=60, expire_at=None):
+    def __init__(self, payload='', msg_id=None, mime_type='text/plain', priority=5, expiration=60, expire_at=None, topic=None):
         self.payload = payload
         self.msg_id = msg_id or new_cid()
         self.mime_type = mime_type
         self.priority = priority # In 1-9 range where 9 is top priority
+        self.topic = topic
 
         # expiration -> how many seconds a message should live
         # expire_at -> a datetime, in UTC, when it should expire
@@ -59,12 +60,16 @@ class Message(object):
         self.expiration = expiration
         self.expire_at = expire_at
 
+    def __repr__(self):
+        return make_repr(self)
+
     def dumps(self):
         return dumps({
             'msg_id': self.msg_id,
             'payload': self.payload,
             'mime_type': self.mime_type,
             'priority': self.priority,
+            'topic': self.topic
         })
 
 # ################################################################################################################################
@@ -91,9 +96,9 @@ class SubCtx(object):
 class GetCtx(object):
     """ A set of data describing where to fetch messages from.
     """
-    def __init__(self, sub_key=None, batch_max_size=100, is_fifo=True):
+    def __init__(self, sub_key=None, max_batch_size=100, is_fifo=True):
         self.sub_key = sub_key
-        self.batch_max_size = batch_max_size
+        self.max_batch_size = max_batch_size
         self.is_fifo = is_fifo # Fetch in FIFO or LIFO order
 
 # ################################################################################################################################
@@ -109,13 +114,15 @@ class AckCtx(object):
 class PubSub(object):
     """ An entry point to the pub/sub mechanism. Must be partly subclassed by concrete implementation classes.
     """
-    LUA_MOVE_TO_TARGET_QUEUES = 'move-to-target-queues'
 
     def __init__(self, *ignored_args, **ignored_kwargs):
         self.logger = getLogger(self.__class__.__name__)
 
-        self.lua_programs = {}
-        self.update_lock = RLock() # Held when modifying sets of currently known consumers and producers
+        # Held when modifying sets of currently known consumers and producers
+        self.update_lock = RLock()
+
+        # Held when in-flight messages are manipulated
+        self.in_flight_lock = RLock()
 
         # All existing topics, key = topic name, value = topic object
         self.topics = {}
@@ -134,12 +141,6 @@ class PubSub(object):
     def add_topic(self, topic):
         with self.update_lock:
             self.topics[topic.name] = topic
-
-    def add_lua_program(self, name, program):
-        self.lua_programs[name] = self.kvdb.register_script(program)
-
-    def run_lua(self, name, keys, args):
-        return self.lua_programs[name](keys, args)
 
     def add_subscription(self, sub_key, client_id, topic):
         """ Adds subscription for a given sub_id, client and topic. Must be called with self.update_lock held.
@@ -166,21 +167,29 @@ class PubSub(object):
 class RedisPubSub(PubSub):
     """ Publish/subscribe based on Redis.
     """
-    ID_PREFIX = 'zato:pubsub:id:{}'
-    ID_TO_TOPIC_KEY = 'zato:pubsub:id-to-topic'
-    ID_EXP_PREFIX = 'zato:pubsub:id-exp:{}:{}'
-    BACKUP_PREFIX = 'zato:pubsub:backup:{}'
+    MSG_IDS_PREFIX = 'zato:pubsub:msg-ids:{}'
     BACKLOG_FULL_KEY = 'zato:pubsub:backlog-full'
-    CONS_ID_PREFIX = 'zato:pubsub:cons:id:{}'
-    MSG_PREFIX = 'zato:pubsub:msg:{}'
-    IN_FLIGHT_PREFIX = 'zato:pubsub:in-flight:{}'
-    IN_FLIGHT_EXP_PREFIX = 'zato:pubsub:in-flight-exp:{}:{}'
+    CONSUMER_MSG_IDS_PREFIX = 'zato:pubsub:consumer:msg-ids:{}'
+    CONSUMER_IN_FLIGHT_PREFIX = 'zato:pubsub:consumer:in-flight:{}'
+    MSG_VALUES_KEY = 'zato:pubsub:msg-values'
+
+    LUA_MOVE_TO_TARGET_QUEUES = 'move-to-target-queues'
+    LUA_GET_FROM_CONS_QUEUE = 'get-from-cons-queue'
 
     # ############################################################################################################################
 
     def __init__(self, kvdb):
         super(RedisPubSub, self).__init__()
         self.kvdb = kvdb
+        self.lua_programs = {}
+
+    # ############################################################################################################################
+
+    def add_lua_program(self, name, program):
+        self.lua_programs[name] = self.kvdb.register_script(program)
+
+    def run_lua(self, name, keys=[], args=[]):
+        return self.lua_programs[name](keys, args)
 
     # ############################################################################################################################
 
@@ -202,9 +211,10 @@ class RedisPubSub(PubSub):
                 self.logger.warn('Producer `%s` cannot publish to `%s`', ctx.client_id, ctx.topic)
                 raise ValueError("Can't publish to `{}`".format(ctx.topic))
 
-        id_key = self.ID_PREFIX.format(ctx.topic)
-        id_exp_key = self.ID_EXP_PREFIX.format(ctx.topic, ctx.msg.msg_id)
-        msg_key = self.MSG_PREFIX.format(ctx.topic)
+        id_key = self.MSG_IDS_PREFIX.format(ctx.topic)
+
+        # Each message will carry information what topic it's intended for
+        ctx.msg.topic = ctx.topic
 
         # The score is built by prefixing the number of milliseconds since UNIX epoch with message's priority. Hence higher
         # priority messages will get higher score whereas messages of equal priority will be still scored according
@@ -221,9 +231,7 @@ class RedisPubSub(PubSub):
         with self.kvdb.pipeline() as p:
 
             p.zadd(id_key, score, ctx.msg.msg_id)
-            p.hset(msg_key, ctx.msg.msg_id, ctx.msg.dumps())
-            #p.hset(self.ID_TO_TOPIC_KEY, ctx.msg.msg_id, ctx.topic)
-            #p.setex(id_exp_key, ctx.msg.expiration, '')
+            p.hset(self.MSG_VALUES_KEY, ctx.msg.msg_id, ctx.msg.dumps())
 
             try:
                 p.execute()
@@ -231,7 +239,7 @@ class RedisPubSub(PubSub):
                 self.logger.error('Pub error `%s`', format_exc(e))
                 raise
             else:
-                self.logger.info('Published: `%s` to `%s', ctx.msg.msg_id, ctx.topic)
+                self.logger.debug('Published: `%s` to `%s', ctx.msg.msg_id, ctx.topic)
 
     # ############################################################################################################################
 
@@ -245,21 +253,37 @@ class RedisPubSub(PubSub):
                 # TODO: Resolve topic here - it can be a pattern instead of a concrete name
                 self.add_subscription(sub_key, ctx.client_id, topic)
 
-        self.logger.info('Client `%s` sub to topics `%s`', ctx.client_id, ', '.join(ctx.topics))
+        self.logger.debug('Client `%s` sub to topics `%s`', ctx.client_id, ', '.join(ctx.topics))
         return sub_key
 
     # ############################################################################################################################
 
     def get(self, ctx):
-        self.logger.info('Fetching for sub_key `%s`', ctx.sub_key)
-        with self.update_lock:
+        self.logger.debug('Get by sub_key `%s`', ctx.sub_key)
 
-            client_id = self.sub_to_cons.get(ctx.sub_key)
+        with self.in_flight_lock:
+            with self.update_lock:
+    
+                # Grab the client's ID, if it's a valid subscription key.
+                client_id = self.sub_to_cons.get(ctx.sub_key)
+                if not client_id:
+                    msg = 'Invalid sub_key `{}`'.format(ctx.sub_key)
+                    self.logger.warn(msg)
+                    raise ValueError(msg)
 
-            if not client_id:
-                msg = 'Invalid sub_key `{}`'.format(ctx.sub_key)
-                self.logger.warn(msg)
-                raise ValueError(msg)
+                # Now that the client is known to be a valid one we can get all their messages
+                cons_queue = self.CONSUMER_MSG_IDS_PREFIX.format(ctx.sub_key)
+                cons_in_flight = self.CONSUMER_IN_FLIGHT_PREFIX.format(ctx.sub_key)
+    
+                # TODO: Make it run using 'with self.lock(cons_queue)'
+    
+                messages = self.run_lua(
+                    self.LUA_GET_FROM_CONS_QUEUE,
+                    [cons_queue, cons_in_flight, self.MSG_VALUES_KEY],
+                    [ctx.max_batch_size, datetime.utcnow().isoformat()])
+
+                for msg in messages:
+                    yield Message(**loads(msg))
 
     # ############################################################################################################################
 
@@ -269,10 +293,9 @@ class RedisPubSub(PubSub):
 
         with self.kvdb.pipeline() as p:
 
-            p.zrem(self.ID_PREFIX.format(topic), ctx.msg_id)
+            p.zrem(self.MSG_IDS_PREFIX.format(topic), ctx.msg_id)
             p.hdel(self.ID_TO_TOPIC_KEY, ctx.msg_id)
             p.delete(self.ID_EXP_PREFIX.format(topic, ctx.msg_id))
-            p.hdel(self.MSG_PREFIX.format(topic), ctx.msg_id)
 
             p.execute()
 
@@ -293,7 +316,7 @@ class RedisPubSub(PubSub):
         with self.update_lock:
             for topic in self.topic_to_prod:
 
-                source_queue = self.ID_PREFIX.format(topic)
+                source_queue = self.MSG_IDS_PREFIX.format(topic)
                 topic_info = self.topics[topic]
 
                 # Keys the Lua program will operate on
@@ -310,15 +333,15 @@ class RedisPubSub(PubSub):
                 consumers = self.topic_to_cons[topic]
                 for consumer in consumers:
                     sub_key = self.cons_to_sub[consumer]
-                    self.logger.info('Sub `%s` for topic `%s` from consumer `%s`', sub_key, topic, consumer)
+                    self.logger.debug('Sub `%s` for topic `%s` to consumer `%s`', sub_key, topic, consumer)
 
-                    keys.append(self.CONS_ID_PREFIX.format(sub_key))
+                    keys.append(self.CONSUMER_MSG_IDS_PREFIX.format(sub_key))
 
                 if self.logger.isEnabledFor(logging.DEBUG):
                     self.logger.debug('keys `%s`', ', '.join(keys))
 
                 move_result = self.run_lua(self.LUA_MOVE_TO_TARGET_QUEUES, keys, args)
-                self.logger.info('move_result `%s` `%s`', move_result, type(move_result))
+                self.logger.debug('move_result `%s` `%s`', move_result, type(move_result))
 
 # ################################################################################################################################
 
@@ -326,8 +349,8 @@ if __name__ == '__main__':
 
     import logging
 
-    log_format = '%(asctime)s - %(levelname)s - %(process)d:%(threadName)s - %(name)s:%(lineno)d - %(message)s'
-    logging.basicConfig(level=logging.DEBUG, format=log_format)
+    log_format = '%(asctime)s - %(levelname)s - %(name)s:%(lineno)d - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_format)
 
     from redis import StrictRedis
     kvdb = StrictRedis()
@@ -365,17 +388,42 @@ if __name__ == '__main__':
         end
 
         local target_queues = get_target_queues(KEYS)
-        local ids = unpack(redis.pcall(zset_command, source_queue, 0, max_depth))
+        local ids = redis.pcall(zset_command, source_queue, 0, max_depth)
 
-        for idx, target_queue in ipairs(target_queues) do
-            redis.pcall('sadd', target_queue, ids)
+        for queue_idx, target_queue in ipairs(target_queues) do
+            for id_idx, id in ipairs(ids) do
+                redis.call('lpush', target_queue, id)
+            end
         end
 
-        redis.pcall('zrem', source_queue, ids)
+        for id_idx, id in ipairs(ids) do
+            redis.pcall('zrem', source_queue, id)
+        end
+        """
+
+    lua_get_from_cons_queue = """
+
+       local cons_queue = KEYS[1]
+       local cons_in_flight = KEYS[2]
+       local msg_key = KEYS[3]
+
+       local max_batch_size = tonumber(ARGV[1])
+       local now = ARGV[2]
+        
+       local ids = redis.pcall('lrange', cons_queue, 0, max_batch_size)
+       local values = redis.pcall('hmget', msg_key, unpack(ids))
+
+        for id_idx, id in ipairs(ids) do
+            redis.pcall('hset', cons_in_flight, id, now)
+        end
+
+       return values
     """
 
     ps = RedisPubSub(kvdb)
+
     ps.add_lua_program(ps.LUA_MOVE_TO_TARGET_QUEUES, lua_move_to_target_queues)
+    ps.add_lua_program(ps.LUA_GET_FROM_CONS_QUEUE, lua_get_from_cons_queue)
 
     ps.add_topic(topic1)
     ps.add_topic(topic2)
@@ -402,10 +450,13 @@ if __name__ == '__main__':
     # Publish
     pub_ctx = PubCtx()
     pub_ctx.client_id = client_id
-    pub_ctx.topic = topic1.name
-    pub_ctx.msg = Message('aaa')
 
-    ps.publish(pub_ctx)
+    for topic in(topic1, topic2):
+        for x in range(10):
+            pub_ctx.topic = topic.name
+            pub_ctx.msg = Message(new_cid())
+    
+            ps.publish(pub_ctx)
 
     # Move messages to target queues
     # TODO: This will be done in background
