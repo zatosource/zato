a531c39e9700445adeca87fc8635838286e10423
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-02-16 17:55:44 +0100

    GH #102 - Added services to invoke background tasks.

--- code/zato-server/src/zato/server/service/internal/pubsub.py
+++ code/zato-server/src/zato/server/service/internal/pubsub.py
@@ -14,9 +14,42 @@ from gevent import sleep, spawn
 # Zato
 from zato.server.service.internal import AdminService
 
+# ################################################################################################################################
+
+class DeleteExpired(AdminService):
+    """ Invoked when a server is starting - periodically spawns a greenlet deleting expired messages.
+    """
+    def _delete_expired(self):
+        self.logger.debug('Deleted expired messages %s', self.pubsub.impl.delete_expired())
+
+    def handle(self):
+        interval = float(self.server.fs_server_config.pubsub.delete_expired_interval)
+
+        while True:
+            self.logger.debug('Deleting expired messages, interval %rs', interval)
+            spawn(self._delete_expired)
+            sleep(interval)
+
+# ################################################################################################################################
+
+class InvokeCallbacks(AdminService):
+    """ Invoked when a server is starting - periodically spawns a greenlet invoking consumer URL callbacks.
+    """
+    def _invoke_callbacks(self):
+        pass
+
+    def handle(self):
+        interval = float(self.server.fs_server_config.pubsub.invoke_callbacks_interval)
+
+        while True:
+            self.logger.debug('Invoking callbacks, interval %rs', interval)
+            spawn(self._invoke_callbacks)
+            sleep(interval)
+
+# ################################################################################################################################
+
 class MoveToTargetQueues(AdminService):
-    """ Invoked when a server is starting - spawns a greenlet periodically
-    in order to move published messages to recipient queues.
+    """ Invoked when a server is starting - periodically spawns a greenlet moving published messages to recipient queues.
     """
     def _move_to_target_queues(self):
         self.pubsub.impl.move_to_target_queues()
@@ -24,8 +57,10 @@ class MoveToTargetQueues(AdminService):
 
     def handle(self):
         interval = float(self.server.fs_server_config.pubsub.move_to_target_queues_interval)
-        self.logger.info('Moving messages to target queues, interval %rs', interval)
 
         while True:
+            self.logger.debug('Moving messages to target queues, interval %rs', interval)
             spawn(self._move_to_target_queues)
             sleep(interval)
+
+# ################################################################################################################################
