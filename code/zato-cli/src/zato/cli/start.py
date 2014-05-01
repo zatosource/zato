9659852d0a076e6a9da0559482de143c82501243
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-05-01 22:06:30 +0200

    GH #222 - Simplying 'zato start' invocations.

--- code/zato-cli/src/zato/cli/start.py
+++ code/zato-cli/src/zato/cli/start.py
@@ -9,7 +9,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
-import os
+import os, sys
 
 # Bunch
 from bunch import Bunch
@@ -17,95 +17,70 @@ from bunch import Bunch
 # ConfigObj
 from configobj import ConfigObj
 
+# Sarge
+from sarge import capture_both, run
+
 # Zato
 from zato.cli import ManageCommand
 from zato.cli.check_config import CheckConfig
 from zato.common.util import get_executable
 
-zdaemon_conf_name_contents = """<runner>
-    program {program}
-    socket-name {socket_name}
-</runner>
-<eventlog>
-    <logfile>
-        path {logfile_path}
-    </logfile>
-</eventlog>
-"""
-
 class Start(ManageCommand):
-    """ Starts a Zato component installed in the 'path'. The same command is used for starting servers, load-balancer agents and web admin instances.
-'path' must point to an existing directory into which the given component has been installed.
+    """Starts a Zato component installed in the 'path'. The same command is used for starting servers, load-balancer and web admin instances. 'path' must point to a directory into which the given component has been installed.
 
 Examples:
   - Assuming a Zato server has been installed in /opt/zato/server1, the command to start the server is 'zato start /opt/zato/server1'.
-  - If a load-balancer's agent has been installed in /home/zato/lb-agent1, the command to start it is 'zato start /home/zato/lb-agent1'."""
-    
-    def _on_server(self, show_output=True, *ignored):
+  - If a load-balancer has been installed in /home/zato/lb1, the command to start it is 'zato start /home/zato/lb1'."""
 
-        server_conf = ConfigObj(os.path.join(self.config_dir, 'repo', 'server.conf'))
-        port = server_conf['main']['gunicorn_bind'].split(':')[1]
-        server_prefix = '{0}'.format(port).zfill(5)
+    opts = [
+        {'name':'--fg', 'help':'If given, the component will run in foreground', 'action':'store_true'}
+    ]
 
-        socket_name = 'server-{0}.sock'.format(server_prefix)
-        socket_name = os.path.join(self.config_dir, 'zdaemon', socket_name)
+    def check_pidfile(self):
 
-        # If we have a socket of that name then we already have a running
+        pidfile = os.path.join(self.config_dir, 'pidfile')
+
+        # If we have a pidfile of that name then we already have a running
         # server, in which case we refrain from starting new processes now.
-        if os.path.exists(socket_name):
-            msg = 'Server at {0} is already running'.format(self.component_dir)
+        if os.path.exists(pidfile):
+            msg = 'Error - found pidfile `{}`'.format(self.pidfile)
             self.logger.info(msg)
             return self.SYS_ERROR.COMPONENT_ALREADY_RUNNING
 
+    def start_component(self, py_path, name, program_dir):
+        """ Starts a component in background or foreground, depending on the 'fg' flag.
+        """
+        program = '{} -m {} {}'.format(get_executable(), py_path, program_dir)
+        func, async = (run, False) if self.args.fg else (capture_both, True)
+        
+        try:
+            func(program, async=async)
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+        if self.show_output:
+            if not self.args.fg and self.verbose:
+                self.logger.debug('Zato {} `{}` starting in background'.format(name, self.component_dir))
+            else:
+                self.logger.info('OK')
+
+    def _on_server(self, show_output=True, *ignored):
+
+        # Perhaps it's already running
+        #self.check_pidfile()
+
+        # Check config before starting anything
         cc = CheckConfig(self.args)
         cc.show_output = False
         cc.execute(Bunch(path='.'))
 
-        zdaemon_conf_name = 'zdaemon-{0}.conf'.format(port)
-        socket_prefix = 'server-{0}'.format(port)
-        program = '{} -m zato.server.main {}'.format(get_executable(), self.component_dir)
-        logfile_path_prefix = 'zdaemon-{}'.format(port)
-
-        self._zdaemon_start(zdaemon_conf_name_contents, zdaemon_conf_name, socket_prefix,
-                            logfile_path_prefix, program)
-
-        if self.show_output:
-            if self.verbose:
-                self.logger.debug('Zato server at {0} has been started'.format(self.component_dir))
-            else:
-                self.logger.info('OK')
+        # Good to go now
+        self.start_component('zato.server.main', 'server', self.component_dir)
 
     def _on_lb(self, *ignored):
 
-        # Start the agent which will in turn start the load balancer
-        repo_dir = os.path.join(self.config_dir, 'repo')
-
-        zdaemon_conf_name = 'zdaemon-lb.conf'
-        socket_prefix = 'lb-agent'
-        program = '{} -m zato.agent.load_balancer.main {}'.format(get_executable(), repo_dir)
-        logfile_path_prefix = 'zdaemon-lb-agent'
-        
-        self._zdaemon_start(zdaemon_conf_name_contents, zdaemon_conf_name, socket_prefix, logfile_path_prefix, program)
-
-        # Now start HAProxy
-
-        if self.show_output:
-            if self.verbose:
-                self.logger.debug('Zato load balancer and agent started in {0}'.format(self.component_dir))
-            else:
-                self.logger.info('OK')
+        # TODO: Starting it in foreground means we need to remember about haproxy still being started in background
+        self.start_component('zato.agent.load_balancer.main', 'load-balancer', os.path.join(self.config_dir, 'repo'))
 
     def _on_web_admin(self, *ignored):
-
-        zdaemon_conf_name = 'zdaemon-web-admin.conf'
-        socket_prefix = 'web-admin'
-        program = '{} -m zato.admin.main'.format(get_executable())
-        logfile_path_prefix = 'zdaemon-web-admin'
-        
-        self._zdaemon_start(zdaemon_conf_name_contents, zdaemon_conf_name, socket_prefix, logfile_path_prefix, program)
-
-        if self.show_output:
-            if self.verbose:
-                self.logger.debug('Zato web admin started in {0}'.format(self.component_dir))
-            else:
-                self.logger.info('OK')
+        self.start_component('zato.admin.main', 'load-balancer', '')
