99dc2e3b5daef7fe83877aea592e4f3d4d36ce70
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-05-06 10:34:16 +0200

    GH #108 - Set misc.return_internal_objects to False by default.

--- code/zato-cli/src/zato/cli/create_server.py
+++ code/zato-cli/src/zato/cli/create_server.py
@@ -87,6 +87,7 @@ grace_time_multiplier=3
 context_class=zato.server.spring_context.ZatoContext
 
 [misc]
+return_internal_objects=False
 internal_services_may_be_deleted=False
 initial_cluster_name = {initial_cluster_name}
 initial_server_name = {initial_server_name}
