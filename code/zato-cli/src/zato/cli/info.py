de25d6aa1acda130cd40c57843dee559137126d2
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-05-09 12:10:07 +0200

    GH #237 - Updating 'zato info' to use YAML as well.

--- code/zato-cli/src/zato/cli/info.py
+++ code/zato-cli/src/zato/cli/info.py
@@ -10,11 +10,20 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # stdlib
 import os
-from anyjson import dumps, loads
+from anyjson import dumps as json_dumps, loads as json_loads
+from cStringIO import StringIO
 from datetime import datetime
+from itertools import chain, groupby
+from operator import attrgetter, itemgetter
+
+# pyaml
+import pyaml
 
 # psutil
-from psutil import Process
+from psutil import CONN_LISTEN, Process
+
+# PyYAML
+import yaml
 
 # pytz
 from pytz import UTC
@@ -24,25 +33,56 @@ from texttable import Texttable
 
 # Zato
 from zato.cli import ManageCommand, ZATO_INFO_FILE
-from zato.common import MISC
+from zato.common import INFO_FORMAT, MISC
 from zato.common.util import current_host
 
 DEFAULT_COLS_WIDTH = '30,90'
 
+class _Dumper(yaml.SafeDumper):
+    def represent_none(self, data):
+        return self.represent_scalar('tag:yaml.org,2002:null', '')
+
 class Info(ManageCommand):
     """ Shows detailed information regarding a chosen Zato component
     """
+    # Changed in 2.0 - --json replaced with --format
     opts = [
-        {'name':'--json', 'help':'Whether to return the output in JSON', 'action':'store_true'},
+        {'name':'--format', 'help':'Output format, must be one of text, json or yaml, default: {}'.format(INFO_FORMAT.TEXT),
+           'default':INFO_FORMAT.TEXT},
         {'name':'--cols_width', 'help':'A list of columns width to use for the table output, default: {}'.format(DEFAULT_COLS_WIDTH)}
     ]
-    
+
+    def format_connections(self, conns, format):
+        """ Formats a list of connections according to the output format.
+        """
+        groups = (groupby(conns, key=attrgetter('status')))
+        out = {}
+
+        for status, items in groups:
+            items = list(items)
+            items.sort(key=attrgetter('raddr'))
+            out_items = out.setdefault(status, [])
+            for item in items:
+                laddr_str = ':'.join(str(elem) for elem in item.laddr).ljust(21).decode('utf-8')
+                raddr_str = ':'.join(str(elem) for elem in item.raddr).rjust(21).decode('utf-8')
+                if item.raddr:
+                    out_item = '{} -> {}'.format(laddr_str, raddr_str)
+                    out_items.append(out_item)
+                else:
+                    out_items.append('{}:{}'.format(*item.laddr))
+
+        #print()
+        from pprint import pprint
+        #pprint(out_items)
+
+        return out
+
     def _on_server(self, args):
-        
+
         os.chdir(self.original_dir)
         abs_args_path = os.path.abspath(args.path)
         component_details = open(os.path.join(abs_args_path, ZATO_INFO_FILE)).read()
-        
+
         out = {
             'component_details': component_details,
             'component_full_path': abs_args_path,
@@ -71,8 +111,8 @@ class Info(ManageCommand):
             out['component_running'] = True
             master_proc = Process(master_proc_pid)
             workers_pids = sorted(elem.pid for elem in master_proc.children())
-            
-            out['master_proc_connections'] = master_proc.connections()
+
+            out['master_proc_connections'] = self.format_connections(master_proc.connections(), self.args.format)
             out['master_proc_pid'] = master_proc.pid
             out['master_proc_create_time'] = datetime.fromtimestamp(master_proc.create_time()).isoformat()
             out['master_proc_create_time_utc'] = datetime.fromtimestamp(master_proc.create_time(), UTC).isoformat()
@@ -80,32 +120,43 @@ class Info(ManageCommand):
             out['master_proc_name'] = master_proc.name()
             out['master_proc_workers_no'] = len(workers_pids)
             out['master_proc_workers_pids'] = workers_pids
-            
+
             for pid in workers_pids:
                 worker = Process(pid)
                 out['worker_{}_create_time'.format(pid)] = datetime.fromtimestamp(worker.create_time()).isoformat()
                 out['worker_{}_create_time_utc'.format(pid)] = datetime.fromtimestamp(worker.create_time(), UTC).isoformat()
-                out['worker_{}_connections'.format(pid)] = worker.connections()
-            
-        if getattr(args, 'json', False):
-            out['component_details'] = loads(out['component_details'])
-            self.logger.info(dumps(out))
+                out['worker_{}_connections'.format(pid)] = self.format_connections(worker.connections(), self.args.format)
+
+        if(args.format in INFO_FORMAT.JSON, INFO_FORMAT.YAML):
+            out['component_details'] = json_loads(out['component_details'])
+
+        if args.format == INFO_FORMAT.JSON:
+            self.logger.info(json_dumps(out))
+
+        elif args.format == INFO_FORMAT.YAML:
+            buff = StringIO()
+            yaml.dump_all([out], default_flow_style=False, indent=4, Dumper=_Dumper, stream=buff)
+            value = buff.getvalue()
+            buff.close()
+
+            self.logger.info(value)
+
         else:
             cols_width = args.cols_width if args.cols_width else DEFAULT_COLS_WIDTH
             cols_width = (elem.strip() for elem in cols_width.split(','))
             cols_width = [int(elem) for elem in cols_width]
-            
+
             table = Texttable()
             table.set_cols_width(cols_width)
-            
+
             # Use text ('t') instead of auto so that boolean values don't get converted into ints
             table.set_cols_dtype(['t', 't'])
-            
+
             rows = [['Key', 'Value']]
             rows.extend(sorted(out.items()))
-            
+
             table.add_rows(rows)
-            
+
             self.logger.info(table.draw())
-        
+
     _on_lb = _on_web_admin = _on_server
