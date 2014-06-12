04a1316b9c0f02a3a9cbdb9df8e9a3fc3cde695f
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-06-12 14:54:47 +0200

    GH #258 - More of the internal scheduler's API.

--- code/zato-common/test/zato/common/test_scheduler.py
+++ code/zato-common/test/zato/common/test_scheduler.py
@@ -15,10 +15,10 @@ from unittest import TestCase
 from paodate import Delta
 
 # Zato
-from zato.common.scheduler import Interval
-from zato.common.test import rand_int
+from zato.common.scheduler import Interval, Job
+from zato.common.test import is_like_cid, rand_bool, rand_date_utc, rand_int, rand_string
 
-class TestInterval(TestCase):
+class IntervalTestCase(TestCase):
 
     def test_interval_has_in_seconds(self):
         in_seconds = rand_int()
@@ -26,7 +26,6 @@ class TestInterval(TestCase):
         self.assertEquals(interval.in_seconds, in_seconds)
 
     def test_interval_compute_in_seconds(self):
-
         for days, hours, minutes, seconds, expected in (
             (55, 83, 69, 75, 5055015.0),
             (31, 2, 6, 23, 2685983.0),
@@ -39,3 +38,31 @@ class TestInterval(TestCase):
 
             interval = Interval(days, hours, minutes, seconds)
             self.assertEquals(interval.in_seconds, expected)
+
+class JobTestCase(TestCase):
+    def test_get_context(self):
+        name = rand_string()
+        start_time = rand_date_utc()
+        interval_in_seconds = rand_int()
+        max_runs_reached = rand_bool()
+        current_runs, failure_runs, success_runs = rand_int(count=3)
+
+        job = Job(name, start_time, Interval(in_seconds=interval_in_seconds))
+        job.current_runs = current_runs
+        job.failure_runs = failure_runs
+        job.success_runs = success_runs
+
+        ctx = job.get_context()
+        cid = ctx.pop('cid')
+
+        self.assertTrue(is_like_cid(cid))
+
+        self.assertDictEqual(ctx, {
+            'current_runs': current_runs,
+            'failure_runs': failure_runs,
+            'success_runs': success_runs,
+            'interval_in_seconds': interval_in_seconds,
+            'name': name,
+            'start_time': start_time.isoformat(),
+            'max_runs_reached': max_runs_reached
+        })
