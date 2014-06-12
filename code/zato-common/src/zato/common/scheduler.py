fb5ff86dde6ffeb009958a7d6f846b6a24bf697c
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-06-12 04:44:18 +0200

    GH #258 - Working on job callbacks.

--- code/zato-common/src/zato/common/scheduler.py
+++ code/zato-common/src/zato/common/scheduler.py
@@ -10,9 +10,16 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # stdlib
 from logging import basicConfig, getLogger, INFO
+from traceback import format_exc
+
+# gevent
+import gevent
 
 # paodate
 from paodate import Delta
+
+# Zato
+from zato.common.util import make_repr
 
 logger = getLogger(__name__)
 
@@ -28,20 +35,61 @@ class Interval(object):
         self.seconds = seconds
         self.in_seconds = in_seconds or self.compute_in_seconds()
 
+    def __str__(self):
+        return xmake_repr(self)
+
     def compute_in_seconds(self):
         return Delta(days=self.days, hours=self.hours, minutes=self.minutes, seconds=self.seconds).total_seconds
 
 # ################################################################################################################################
 
 class Job(object):
-    def __init__(self, name, start_time, interval):
+    def __init__(self, name, start_time, interval, callback=None, cb_args=None, cb_kwargs=None, max_runs=None):
         self.logger = getLogger(self.__class__.__name__)
         self.name = name
         self.start_time = start_time
         self.interval = interval
+        self.callback = callback
+        self.cb_args = cb_args or ()
+        self.cb_kwargs = cb_kwargs or {}
+        self.max_runs = None
+        self.current_runs = 0 # Starts over each time scheduler is started
+        self.success_runs = 0
+        self.failure_runs = 0
+        self.keep_running = True
 
     def __str__(self):
-        return '<{} at {}, name:`{}`>'.format(self.__class__.__name__, hex(id(self)), self.name)
+        return make_repr(self)
+
+    def main_loop(self):
+        while self.keep_running:
+
+            # Perhaps we've already been run enough times
+            if self.max_runs and self.current_runs == self.max_runs:
+                self.keep_running = False
+
+            # Pause the greenlet for however long is needed
+            gevent.sleep(self.interval.in_seconds)
+
+            try:
+                # Invoke callback in a new greenlet so it doesn't block the current one.
+                gevent.spawn(self.callback, name=self.name, *self.cb_args, **self.cb_kwargs)
+                self.success_runs += 1
+            except Exception, e:
+                self.failure_runs += 1
+                self.logger.warn(format_exc(e))
+            finally:
+                self.current_runs += 1
+
+    def init(self):
+        self.logger.info('Init job `%s`', self)
+
+        # If the job has a start time in the future, sleep until it's ready to go.
+        if self.start_time:
+            #self.wait_for_start_time()
+            self.main_loop()
+        else:
+            self.main_loop()
 
 # ################################################################################################################################
 
@@ -52,15 +100,27 @@ class Scheduler(object):
 
     def create(self, job):
         self.logger.info('Creating job `%s`', job)
+        self.jobs.append(job)
+
+    def on_job_executed(self, *args, **kwargs):
+        self.logger.warn('%s %s %s', 1, args, kwargs)
 
     def run(self):
-        pass
+        for job in self.jobs:
+            job.callback = self.on_job_executed
+            gevent.spawn(job.init)
+
+        while True:
+            gevent.sleep(0.1)
 
 # ################################################################################################################################
 
 if __name__ == '__main__':
     sched = Scheduler()
-    interval = Interval(minutes=3, seconds=20)
-    job = Job('test')
+    interval = Interval(minutes=0, seconds=1)
+    job = Job('test', None, interval)
     sched.create(job)
     sched.run()
+
+    while True:
+        gevent.sleep(1)
