6d6626ebfd4b4465eb500a100770877e334d46fa
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-06-25 01:12:45 +0200

    GH #268 - Store release information outside of Python code.

--- .post-commit.sh
+++ .post-commit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git log -n 1 --pretty=format:"%H" > `pwd`/code/zato-common/src/zato/common/revision.zato
+git log -n 1 --pretty=format:"%H" > `pwd`/release-info/revision.txt
--- code/zato-cli/setup.py
+++ code/zato-cli/setup.py
@@ -8,9 +8,18 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 # flake8: noqa
 
+# flake8: noqa
+import os
+from json import loads
 from setuptools import setup, find_packages
 
-version = '1.1'
+curdir = os.path.dirname(os.path.abspath(__file__))
+release_info_dir = os.path.join(curdir, '..', 'release-info')
+release = open(os.path.join(release_info_dir, 'release.json')).read()
+release = loads(release)
+revision = open(os.path.join(release_info_dir, 'revision.txt')).read()[:8]
+
+version = '{}.{}.{}.rev-{}'.format(release['major'], release['minor'], release['micro'], revision)
 
 setup(
       name = 'zato-cli',
