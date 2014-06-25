99fb4fd3200d646240d5e1ac8fe2d99f5cd4b602
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-06-25 20:31:54 +0200

    GH #265 - Added create, edit and delete actions to workers.

--- code/zato-server/src/zato/server/connection/cassandra.py
+++ code/zato-server/src/zato/server/connection/cassandra.py
@@ -11,6 +11,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # stdlib
 from copy import deepcopy
 from logging import getLogger
+from traceback import format_exc
 
 # bunch
 from bunch import Bunch
@@ -34,23 +35,24 @@ class CassandraConnStore(object):
     """ Stores connections to Cassandra.
     """
     def __init__(self):
-        self.conns = {}
+        self.sessions = {}
         self.lock = RLock()
 
     def __getitem__(self, name):
-        return self.conns[name]
+        return self.sessions[name]
 
     def get(self, name):
-        return self.conns.get(name)
+        return self.sessions.get(name)
 
-    def add(self, name, config):
-        with self.lock:
+    def _add(self, name, config):
+        """ Actually adds a new definition, must be called with self.lock held.
+        """
+        config_no_sensitive = deepcopy(config)
+        config_no_sensitive['password'] = PASSWORD_SHADOW
 
-            config_no_sensitive = deepcopy(config)
-            config_no_sensitive['password'] = PASSWORD_SHADOW
+        item = Bunch(config=config, config_no_sensitive=config_no_sensitive, is_connected=False, conn=None)
 
-            item = Bunch(config=config, config_no_sensitive=config_no_sensitive)
-
+        try:
             cluster = Cluster(
                 config.contact_points.splitlines(), int(config.port), cql_version=config.cql_version,
                 protocol_version=int(config.proto_version), executor_threads=int(config.exec_size))
@@ -61,6 +63,38 @@ class CassandraConnStore(object):
             session.set_keyspace(config.default_keyspace)
 
             logger.debug('Connected to `%s`', config_no_sensitive)
-
+        except Exception, e:
+            logger.warn('Could not connect to Cassandra `%s`, config:`%s`, e:`%s`', name, config_no_sensitive, format_exc(e))
+        else:
             item.conn = session
-            self.conns[name] = item
+            item.is_connected = True
+
+        self.sessions[name] = item
+
+    def create(self, name, config):
+        """ Adds a new connection definition.
+        """
+        with self.lock:
+            self._add(name, config)
+
+    def _delete(self, name):
+        """ Actually deletes a definition. Must be called with self.lock held.
+        """
+        try:
+            if self.sessions[name].is_connected:
+                self.sessions[name].conn.shutdown()
+        except Exception, e:
+            logger.warn('Error while shutting down session `%s`, e:`%s`', name, format_exc(e))
+        finally:
+            del self.sessions[name]
+
+    def delete(self, name):
+        """ Deletes an existing connection.
+        """
+        with self.lock:
+            self._delete(name)
+
+    def edit(self, del_name, config):
+        with self.lock:
+            self._delete(del_name)
+            self._add(config.name, config)
