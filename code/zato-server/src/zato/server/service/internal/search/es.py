cb9b630652e7e503b3772ba56d0b273e44634f73
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-06-28 21:10:57 +0200

    GH #270 - Added metaclasses-based API services.

--- code/zato-server/src/zato/server/service/internal/search/es.py
+++ code/zato-server/src/zato/server/service/internal/search/es.py
@@ -16,45 +16,25 @@ from zato.common.broker_message import SEARCH
 from zato.common.odb.model import ElasticSearch
 from zato.common.odb.query import search_es_list
 from zato.server.service.internal import AdminService
-from zato.server.service.meta import CreateEditMeta, GetListMeta
+from zato.server.service.meta import CreateEditMeta, DeleteMeta, GetListMeta
 
 logger = getLogger(__name__)
 
-class DeleteMeta(type):
-    def __init__(cls, name, bases, attrs):
-        logger.warn('111111 %r %r %r %r', cls, name, bases, attrs)
+elem = 'search_es'
+model = ElasticSearch
+label = 'an ElasticSearch connection'
+broker_message = SEARCH
+broker_message_prefix = 'ES_'
+list_func = search_es_list
 
 class GetList(AdminService):
     __metaclass__ = GetListMeta
-    elem = 'search_es'
-    output_required = ElasticSearch
-    get_data_func = search_es_list
 
 class Create(AdminService):
     __metaclass__ = CreateEditMeta
-    is_create_edit = True
-    is_edit = False
-    elem = 'search_es'
-    model = ElasticSearch
-    input_required = ElasticSearch
-    label = 'an ElasticSearch connection'
-    broker_message = SEARCH
-    broker_message_prefix = 'ES_'
 
 class Edit(AdminService):
     __metaclass__ = CreateEditMeta
-    is_create_edit = True
-    is_edit = True
-    elem = 'search_es'
-    model = ElasticSearch
-    input_required = ElasticSearch
-    label = 'an ElasticSearch connection'
-    broker_message = SEARCH
-    broker_message_prefix = 'ES_'
 
 class Delete(AdminService):
     __metaclass__ = DeleteMeta
-    model = ElasticSearch
-    label = 'an ElasticSearch connection'
-    broker_message = SEARCH
-    broker_message_prefix = 'ES_'
