a6fc9e03603d63a3e4ce164aa7848ab830810f47
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-10-09 15:07:04 +0200

    GH #289 - Added deleting of CA certs.

--- code/zato-server/src/zato/server/service/internal/security/tls/ca_cert.py
+++ code/zato-server/src/zato/server/service/internal/security/tls/ca_cert.py
@@ -8,11 +8,14 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+# stdlib
+import errno, os
+
 # Zato
 from zato.common.broker_message import SECURITY
 from zato.common.odb.model import TLSCACert
 from zato.common.odb.query import tls_ca_cert_list
-from zato.common.util import get_tls_cert_info_from_payload, store_tls_ca_cert
+from zato.common.util import get_tls_cert_full_path, get_tls_cert_info_from_payload, store_tls_ca_cert
 from zato.server.service.internal import AdminService, AdminSIO
 from zato.server.service.meta import CreateEditMeta, DeleteMeta, GetListMeta
 
@@ -23,7 +26,7 @@ broker_message = SECURITY
 broker_message_prefix = 'TLS_CA_CERT_'
 list_func = tls_ca_cert_list
 skip_input_params = ['info',]
-output_required_extra = ['info',]
+output_optional_extra = ['info',]
 
 def instance_hook(service, input, instance, attrs):
 
@@ -33,6 +36,20 @@ def instance_hook(service, input, instance, attrs):
     with service.lock():
         full_path = store_tls_ca_cert(service.server.tls_dir, service.request.input.value)
         service.logger.info('CA certificate saved under `%s`'.format(full_path))
+
+def response_hook(service, input, instance, attrs, service_type):
+    if service_type == 'create_edit':
+        service.response.payload.info = instance.info
+
+def delete_hook(service, input, instance, attrs):
+    try:
+        os.remove(get_tls_cert_full_path(service.server.tls_dir, instance.info))
+    except OSError, e:
+        if e.errno == errno.ENOENT:
+            # It's ok - some other worker must have deleted it already
+            pass
+        else:
+            raise
 
 class GetList(AdminService):
     __metaclass__ = GetListMeta
