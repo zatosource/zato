6100d2c3407a7e18d74eb921d96cab7c59c8bae7
Dariusz Suchojad <dsuch-github@m.zato.io>
2014-10-22 17:25:41 +0200

    GH #105 - Working on Odoo connections.

--- code/zato-common/src/zato/common/__init__.py
+++ code/zato-common/src/zato/common/__init__.py
@@ -571,12 +571,11 @@ class PUB_SUB:
                 return iter((self.OBJECT, self.JSON, self.XML))
 
 class EMAIL:
-
     class DEFAULT:
         TIMEOUT = 10
         PING_ADDRESS = 'invalid@invalid'
         GET_CRITERIA = 'UNSEEN'
-        IMAP_DEBUG_LEVEL=0
+        IMAP_DEBUG_LEVEL = 0
 
     class IMAP:
         class MODE(Constants):
@@ -619,6 +618,11 @@ class TLS:
         PREFIX_KEYS = 'tls/keys/'
         PREFIX_CERTS = 'tls/certs/'
         PREFIX_CA_CERTS = 'tls/ca-certs/'
+
+class ODOO:
+    class DEFAULT:
+        PORT = 8069
+        POOL_SIZE = 20
 
 # Need to use such a constant because we can sometimes be interested in setting
 # default values which evaluate to boolean False.
---
+++ code/zato-web-admin/src/zato/admin/web/forms/outgoing/odoo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2014 Dariusz Suchojad <dsuch at zato.io>
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# stdlib
+from ftplib import FTP_PORT
+
+# Django
+from django import forms
+
+class CreateForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+    host = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    port = forms.CharField(initial=FTP_PORT, widget=forms.TextInput(attrs={'style':'width:10%'}))
+    user = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    acct = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    timeout = forms.CharField(widget=forms.TextInput(attrs={'style':'width:10%'}))
+    dircache = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+    
+    def __init__(self, prefix=None, post_data=None):
+        super(CreateForm, self).__init__(post_data, prefix=prefix)
+
+class EditForm(CreateForm):
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    dircache = forms.BooleanField(required=False, widget=forms.CheckboxInput())
