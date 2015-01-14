85b627f28927ac01364a6a8bd61e59195495e795
Dariusz Suchojad <dsuch-github@m.zato.io>
2015-01-14 20:42:01 +0100

    GH #356 - Added config patterns matching.

--- code/zato-common/test/zato/common/test_match.py
+++ code/zato-common/test/zato/common/test_match.py
@@ -27,15 +27,18 @@ default_config = Bunch({
     'qwe.*.zxc': False,
 })
 
+# ################################################################################################################################
+
 class MatcherTestCase(TestCase):
+
+# ################################################################################################################################
+
     def test_read_config(self):
 
-        config = deepcopy(default_config)
-
         m = Matcher()
-        m.read_config(config)
+        m.read_config(default_config)
 
-        self.assertEquals(m.config, config)
+        self.assertEquals(m.config, default_config)
         self.assertEquals(m.order1, False)
         self.assertEquals(m.order2, True)
 
@@ -44,17 +47,21 @@ class MatcherTestCase(TestCase):
         self.assertListEqual(m.items[True], [ 'abc.*', '*.zxc',])
         self.assertListEqual(m.items[False], ['qwe.*.zxc', '*'])
 
+# ################################################################################################################################
+
     def test_is_allowed_order_false_true(self):
 
-        config = deepcopy(default_config)
-
         m = Matcher()
-        m.read_config(config)
+        m.read_config(default_config)
 
-        # ########################################################################################################################
+        self.assertEquals(m.config, default_config)
+        self.assertEquals(m.order1, False)
+        self.assertEquals(m.order2, True)
+
+        # ###########################################################################################
         #
         # The value of 'aaa.zxc' is allowed because
-        # 1) We match from True to False
+        # 1) We match from False to True
         # 2) qwe.*.zxc does not match it
         # 3) *         says nothing is allowed
         # 4) *.zxc     says it is allowed overriding bullet #3
@@ -64,10 +71,10 @@ class MatcherTestCase(TestCase):
         self.assertIs(is_allowed, True)
         self.assertDictEqual(m.is_allowed_cache, {'aaa.zxc':True})
 
-        # ########################################################################################################################
+        # ###########################################################################################
         #
         # The value of 'qwe.333.zxc' is also allowed because
-        # 1) We match from True to False
+        # 1) We match from False to True
         # 2) qwe.*.zxc disallowes it
         # 3) *         says nothing is allowed
         # 4) *.zxc     this one allows it even though #1 and #2 said no
@@ -77,10 +84,10 @@ class MatcherTestCase(TestCase):
         self.assertIs(is_allowed, True)
         self.assertDictEqual(m.is_allowed_cache, {'aaa.zxc':True, 'qwe.333.zxc':True})
 
-        # ########################################################################################################################
+        # ###########################################################################################
         #
         # The value of 'qwe.444.aaa' is not allowed
-        # 1) We match from True to False
+        # 1) We match from False to True
         # 2) qwe.*.zxc does not match it at all
         # 3) *         says nothing is allowed
         # 4) *.zxc     does not match it at all so the last match of #2 is taken into account
@@ -88,6 +95,140 @@ class MatcherTestCase(TestCase):
         is_allowed = m.is_allowed('qwe.444.aaa')
         self.assertIs(is_allowed, False)
         self.assertDictEqual(m.is_allowed_cache, {'aaa.zxc':True, 'qwe.333.zxc':True, 'qwe.444.aaa': False})
+
+# ################################################################################################################################
+
+    def test_is_allowed_order_true_false(self):
+
+        config = deepcopy(default_config)
+        config.order = TRUE_FALSE
+
+        m = Matcher()
+        m.read_config(config)
+
+        self.assertEquals(m.config, config)
+        self.assertEquals(m.order1, True)
+        self.assertEquals(m.order2, False)
+
+        # ###########################################################################################
+        #
+        # The value of 'aaa.zxc' is not allowed because
+        # 1) We match from True to False
+        # 2) *.zxc     says it is allowed
+        # 3) qwe.*.zxc matches it and says it's not allowed
+        # 4) *         matches again and confirms it's not allowed
+        #
+
+        is_allowed = m.is_allowed('aaa.zxc')
+        self.assertIs(is_allowed, False)
+        self.assertDictEqual(m.is_allowed_cache, {'aaa.zxc':False})
+
+        # ###########################################################################################
+        #
+        # The value of 'qwe.333.zxc' is also not allowed because
+        # 1) We match from True to False
+        # 2) *.zxc     says it is allowed
+        # 3) qwe.*.zxc matches it and says it's not allowed
+        # 4) *         matches again and confirms it's not allowed
+        #
+
+        is_allowed = m.is_allowed('qwe.333.zxc')
+        self.assertIs(is_allowed, False)
+        self.assertDictEqual(m.is_allowed_cache, {'aaa.zxc':False, 'qwe.333.zxc':False})
+
+        # ###########################################################################################
+
+        config2 = deepcopy(default_config)
+        del config2['*']
+        config2['*.aaa'] = True
+        config2.order = TRUE_FALSE
+
+        m2 = Matcher()
+        m2.read_config(config2)
+
+        self.assertEquals(m2.config, config2)
+        self.assertEquals(m2.order1, True)
+        self.assertEquals(m2.order2, False)
+
+        # ###########################################################################################
+        #
+        # The value of 'qwe.444.aaa' is allowed
+        # 1) We match from True to False
+        # 2) *.aaa     matches and allows it
+        # 3) *.zxc     does not match
+        # 4) abc.*     does not match
+        # 5) qwe.*zxc  does not match
+        #
+
+        is_allowed = m2.is_allowed('qwe.444.aaa')
+        self.assertIs(is_allowed, True)
+        self.assertDictEqual(m2.is_allowed_cache, {'qwe.444.aaa':True})
+
+# ################################################################################################################################
+
+    def test_is_allowed_true_only_has_order(self):
+        config = Bunch({'order':FALSE_TRUE, 'abc':True, 'zxc':True})
+
+        m = Matcher()
+        m.read_config(config)
+
+        self.assertTrue(m.is_allowed('abc'))
+        self.assertTrue(m.is_allowed('zxc'))
+
+        self.assertFalse(m.is_allowed('111'))
+        self.assertFalse(m.is_allowed('222'))
+
+        self.assertDictEqual(m.is_allowed_cache, {'abc':True, 'zxc':True, '111':False, '222':False})
+
+# ################################################################################################################################
+
+    def test_is_allowed_false_only_has_order(self):
+        config = Bunch({'order':FALSE_TRUE, 'abc':False, 'zxc':False})
+
+        m = Matcher()
+        m.read_config(config)
+
+        self.assertFalse(m.is_allowed('abc'))
+        self.assertFalse(m.is_allowed('zxc'))
+
+        self.assertFalse(m.is_allowed('111'))
+        self.assertFalse(m.is_allowed('222'))
+
+        self.assertDictEqual(m.is_allowed_cache, {'abc':False, 'zxc':False, '111':False, '222':False})
+
+# ################################################################################################################################
+
+    def test_is_allowed_true_only_no_order(self):
+        config = Bunch({'abc':True, 'zxc':True})
+
+        m = Matcher()
+        m.read_config(config)
+
+        self.assertTrue(m.is_allowed('abc'))
+        self.assertTrue(m.is_allowed('zxc'))
+
+        self.assertFalse(m.is_allowed('111'))
+        self.assertFalse(m.is_allowed('222'))
+
+        self.assertDictEqual(m.is_allowed_cache, {'abc':True, 'zxc':True, '111':False, '222':False})
+
+# ################################################################################################################################
+
+    def test_is_allowed_false_only_no_order(self):
+        config = Bunch({'abc':False, 'zxc':False})
+
+        m = Matcher()
+        m.read_config(config)
+
+        self.assertFalse(m.is_allowed('abc'))
+        self.assertFalse(m.is_allowed('zxc'))
+
+        self.assertFalse(m.is_allowed('111'))
+        self.assertFalse(m.is_allowed('222'))
+
+        self.assertDictEqual(m.is_allowed_cache, {'abc':False, 'zxc':False, '111':False, '222':False})
+
+# ################################################################################################################################
 
     def test_is_allowed_no_match(self):
 
@@ -97,9 +238,15 @@ class MatcherTestCase(TestCase):
         m = Matcher()
         m.read_config(config)
 
+        self.assertEquals(m.config, config)
+        self.assertEquals(m.order1, False)
+        self.assertEquals(m.order2, True)
+
         is_allowed = m.is_allowed('abc')
         self.assertIs(is_allowed, False)
         self.assertDictEqual(m.is_allowed_cache, {'abc':False})
+
+# ################################################################################################################################
 
     def test_is_allowed_no_order(self):
 
@@ -109,5 +256,8 @@ class MatcherTestCase(TestCase):
         m = Matcher()
         m.read_config(config)
 
+        self.assertEquals(m.config, config)
         self.assertEquals(m.order1, False)
         self.assertEquals(m.order2, True)
+
+# ################################################################################################################################
