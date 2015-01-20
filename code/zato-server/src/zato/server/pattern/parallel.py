1c391f5769421fa480fc8618e58830c9a15570e5
Dariusz Suchojad <dsuch-github@m.zato.io>
2015-01-20 21:46:03 +0100

    Added parallel execution based on fan-out/fan-in reusable internals.

---
+++ code/zato-server/src/zato/server/pattern/parallel.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2015 Dariusz Suchojad <dsuch at zato.io>
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Zato
+from zato.common import CHANNEL, KVDB
+from zato.server.pattern import ParallelBase
+
+class ParallelExec(ParallelBase):
+    pattern_name = 'ParallelExec'
+    lock_pattern = KVDB.LOCK_PARALLEL_EXEC_PATTERN
+    counter_pattern = KVDB.PARALLEL_EXEC_COUNTER_PATTERN
+    data_pattern = KVDB.PARALLEL_EXEC_DATA_PATTERN
+    call_channel = CHANNEL.PARALLEL_EXEC_CALL
+    on_target_channel = CHANNEL.PARALLEL_EXEC_ON_TARGET
+    on_final_channel = CHANNEL.PARALLEL_EXEC_ON_FINAL
+
+    def invoke(self, targets, on_target, cid=None):
+        return super(ParallelExec, self).invoke(targets, None, on_target, cid)
