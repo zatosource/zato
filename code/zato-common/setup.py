dfee180c78950661d628f194875ebbcee2d01a5c
Dariusz Suchojad <dsuch-github@m.zato.io>
2015-10-25 23:41:32 +0100

    Version upped to 2.0.7

--- code/zato-common/setup.py
+++ code/zato-common/setup.py
@@ -17,7 +17,7 @@ try:
     execfile(_version_py, _locals)
     version = _locals['version']
 except IOError:
-    version = '2.0.6'
+    version = '2.0.7'
 
 def parse_requirements(requirements):
     ignored = ['#', 'setuptools', '-e']
