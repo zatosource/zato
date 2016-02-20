75682e92f9b8d50e5ed9cd16267947bfa88ddb24
Dariusz Suchojad <dsuch-github@m.zato.io>
2016-02-20 20:55:35 +0100

    GH #490 - Data format is optional in AMQP channels.

--- code/zato-server/src/zato/server/service/internal/channel/amqp.py
+++ code/zato-server/src/zato/server/service/internal/channel/amqp.py
@@ -32,7 +32,8 @@ class GetList(AdminService):
         response_elem = 'zato_channel_amqp_get_list_response'
         input_required = ('cluster_id',)
         output_required = ('id', 'name', 'is_active', 'queue', 'consumer_tag_prefix',
-            'def_name', 'def_id', 'service_name', 'data_format')
+            'def_name', 'def_id', 'service_name')
+        output_optional = ('data_format',)
 
     def get_data(self, session):
         return channel_amqp_list(session, self.request.input.cluster_id, False)
