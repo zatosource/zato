ea9290ce266c12d5931fc412ac9393f86e8b00f9
Dariusz Suchojad <dsuch-github@m.zato.io>
2016-06-11 01:21:04 +0200

    GH #582 - Moved ZeroMQ classes to their own package.

--- code/zato-zmq/src/zato/zmq_/outgoing.py
+++ code/zato-zmq/src/zato/zmq_/outgoing.py
@@ -7,3 +7,14 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+# Zato
+from zato.zmq_ import BaseZMQSimple
+
+# ################################################################################################################################
+
+class OutZMQSimple(BaseZMQSimple):
+    """ An outgoing ZeroMQ connection of a type other than Majordomo (MDP).
+    """
+    # Does not override or implement anything above what the parent already does
