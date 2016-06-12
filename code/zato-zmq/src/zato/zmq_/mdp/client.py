b16088138450eb9d0060ef9685c827dcd25a3add
Dariusz Suchojad <dsuch-github@m.zato.io>
2016-06-12 22:03:45 +0200

    GH #582 - Always return bytes in responses.

--- code/zato-zmq/src/zato/zmq_/mdp/client.py
+++ code/zato-zmq/src/zato/zmq_/mdp/client.py
@@ -70,5 +70,5 @@ class Client(BaseZMQConnection):
 if __name__ == '__main__':
     c = Client(log_details=True)
     c.connect()
-    reply = c.send(b'zato.helpers.input-logger', b'{"id":"123", "data":"mydata"}', True)
-    print('Reply', reply.body)
+    reply = c.send(b'zmq2.my-service', b'{"id":"123", "data":"mydata"}', True)
+    print('Reply', repr(reply.body))
