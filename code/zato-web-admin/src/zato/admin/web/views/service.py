329e18a9c1a6b23b9b455b00aecdc38bbec0ef61
Dariusz Suchojad <dsuch-github@m.zato.io>
2016-06-26 20:46:16 +0200

    GH #677 - Display services according to input criteria.

--- code/zato-web-admin/src/zato/admin/web/views/service.py
+++ code/zato-web-admin/src/zato/admin/web/views/service.py
@@ -156,7 +156,7 @@ class Index(_Index):
 
     class SimpleIO(_Index.SimpleIO):
         input_required = ('cluster_id',)
-        input_optional = ('name_filter',)
+        input_optional = ('query',)
         output_required = ('id', 'name', 'is_active', 'is_internal', 'impl_name', 'may_be_deleted', 'usage', 'slow_threshold')
         output_repeated = True
 
