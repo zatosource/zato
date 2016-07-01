a1648f48937af16a3f8eab34d0d908eaae57a5b1
Dariusz Suchojad <dsuch-github@m.zato.io>
2016-07-01 15:42:10 +0200

    Removed old comment.

--- code/install.sh
+++ code/install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#
-# I'm just waiting for someone to tell me they use apt-get on RHEL
-# or yum on Ubuntu. But I'm really not going to chase and compare all the options
-# and environment variables various shells and distros use. (dsuch 1 VI 2013)
-#
-# Please help out with it by advocating among your distribution's developers
-# that Zato be included in your system as a package out of the box instead. Many thanks!
-#
-
 CURDIR="${BASH_SOURCE[0]}";RL="readlink";([[ `uname -s`=='Darwin' ]] || RL="$RL -f")
 while([ -h "${CURDIR}" ]) do CURDIR=`$RL "${CURDIR}"`; done
 N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
