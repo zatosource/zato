54103244a7477d5ddfe07ffa5362e341ccce8aab
Rafał Krysiak <rafal@zato.io>
2016-08-03 19:49:24 +0200

    Removed unneeded lines of code and added a full path to install.sh.

--- code/zato_local.sh
+++ code/zato_local.sh
@@ -13,10 +13,6 @@ else
     useradd foxway -g foxway
 fi
 
-cp -r /vagrant/zato /home/foxway/
-chmod +x /home/foxway/zato/code/_install-fedora.sh
-chmod +x /home/foxway/zato/code/install_dependencies.sh
-chown -R foxway:foxway /home/foxway/zato
 yum -y install git bzr gcc-gfortran haproxy \
     gcc-c++ git bzip2 bzip2-devel libffi libffi-devel \
     libevent-devel libyaml-devel libxml2-devel libxslt-devel \
@@ -28,5 +24,4 @@ sudo su - foxway -c "cd $ZATO_TARGET_DIR"
 sudo su - foxway -c "wget https://github.com/zatosource/zato/archive/u/foxway.zip"
 sudo su - foxway -c "unzip foxway.zip"
 sudo su - foxway -c "mv zato-u-foxway/* $ZATO_TARGET_DIR"
-sudo su - foxway -c "cd $ZATO_TARGET_DIR/code"
-sudo su - foxway -c "./install.sh"
+sudo su - foxway -c "$ZATO_TARGET_DIR/code/install.sh"
