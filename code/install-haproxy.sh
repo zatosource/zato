9bc0661c5c750a87192dcb42fe488a4ee85316e5
Rafał Krysiak <rafal@zato.io>
2016-08-04 21:15:47 +0200

    Uncommenting wget command.

--- code/install-haproxy.sh
+++ code/install-haproxy.sh
@@ -8,7 +8,7 @@ HAPROXY_VERSION=1.6.7
 cd /home/foxway
 mkdir haproxy
 mkdir $HOME/haproxy
-#wget http://www.haproxy.org/download/1.6/src/haproxy-$HAPROXY_VERSION.tar.gz
+wget http://www.haproxy.org/download/1.6/src/haproxy-$HAPROXY_VERSION.tar.gz
 tar -xzvf haproxy-$HAPROXY_VERSION.tar.gz --strip 1 -C $HOME/haproxy/
 cd $HOME/haproxy
 make TARGET=generic USE_OPENSSL=1
