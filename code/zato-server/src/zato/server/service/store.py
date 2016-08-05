9b80351f0985e46391b0696e9c3f1d059109393e
Dariusz Suchojad <dsuch-github@m.zato.io>
2016-08-05 23:05:42 +0200

    GH #678 - pyflakes cleanup.

--- code/zato-server/src/zato/server/service/store.py
+++ code/zato-server/src/zato/server/service/store.py
@@ -9,7 +9,6 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
-import imp
 import inspect
 import logging
 import os
