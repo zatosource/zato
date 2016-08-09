92336b01de5f534216b2f6a1606e0b2f7b76c72e
Rafał Krysiak <rafal@zato.io>
2016-08-09 14:46:39 +0200

    Including directories with Foxway services, server objects and DLM and DDL.

--- code/create-package.sh
+++ code/create-package.sh
@@ -2,4 +2,5 @@
 
 cd /home/foxway
 tar -czvf zato-2.0.7.tar.gz .bash_profile .bashrc bin/ bzr/ .cache/ \
-    haproxy/ include/ lib/ .local/ opt/ .pki/ redis-3.2.3/
+    haproxy/ include/ lib/ .local/ opt/ .pki/ redis-3.2.3/ \
+    server_objects services sql
