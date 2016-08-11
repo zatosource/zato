c7c0bca5d36a04e793c250db2ea748203a4d0262
Rafał Krysiak <rafal@zato.io>
2016-08-11 14:57:02 +0200

    Don't pack sql directory.

--- code/create-package.sh
+++ code/create-package.sh
@@ -3,4 +3,4 @@
 cd /home/foxway
 tar -czvf zato-2.0.7.tar.gz .bash_profile .bashrc bin/ bzr/ .cache/ \
     haproxy/ include/ lib/ .local/ opt/ .pki/ redis-3.2.3/ \
-    server_objects services sql
+    server_objects services
