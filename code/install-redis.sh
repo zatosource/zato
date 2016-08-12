7784c470ceec5849b682804f32f5453486029f11
Rafał Krysiak <rafal@zato.io>
2016-08-12 20:22:06 +0200

    Adding back verbose option.

--- code/install-redis.sh
+++ code/install-redis.sh
@@ -6,6 +6,6 @@ ZATO_TARGET_DIR=$ZATO_ROOT_DIR/$ZATO_VERSION
 
 cd $HOME
 wget http://download.redis.io/releases/redis-3.2.3.tar.gz
-tar -xzf redis-3.2.3.tar.gz
+tar -xvzf redis-3.2.3.tar.gz
 cd redis-3.2.3/
 make
