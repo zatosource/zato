17068f34d5ad1fa1ccb50c57518139e087df6f4a
Rafał Krysiak <rafal@zato.io>
2016-08-12 15:33:15 +0200

    Change .tar.gz format to .tar.

--- code/create-package.sh
+++ code/create-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/foxway
-tar -czvf zato-2.0.7.tar.gz .bash_profile .bashrc bin/ bzr/ .cache/ \
+cd $HOME
+tar -czvf zato-2.0.7.tar .bash_profile .bashrc bin/ bzr/ .cache/ \
     haproxy/ include/ lib/ .local/ opt/ .pki/ redis-3.2.3/ \
-    server_objects services
+    server_objects/ services/
