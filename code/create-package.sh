500f908b7a92d2769a85c29d38d741536a50b315
Rafał Krysiak <rafal@zato.io>
2016-08-12 16:44:12 +0200

    Adding extra-libs directory to the package.

--- code/create-package.sh
+++ code/create-package.sh
@@ -3,4 +3,4 @@
 cd $HOME
 tar -czvf zato-2.0.7.tar .bash_profile .bashrc bin/ bzr/ .cache/ \
     haproxy/ include/ lib/ .local/ opt/ .pki/ redis-3.2.3/ \
-    server_objects/ services/
+    extra-libs/ server_objects/ services/
