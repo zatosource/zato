b264cf43faeebabf17344469382f0d7ff0e21d46
Rafał Krysiak <rafal@zato.io>
2016-08-12 19:49:24 +0200

    Removed verbose option from a command.

--- code/install-python2.7.sh
+++ code/install-python2.7.sh
@@ -6,7 +6,7 @@ ZATO_TARGET_DIR=$ZATO_ROOT_DIR/$ZATO_VERSION
 
 wget -P $HOME https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz
 cd $HOME
-tar -xvzf Python-2.7.6.tgz &> /dev/null
+tar -xzf Python-2.7.6.tgz &> /dev/null
 cd $HOME/Python-2.7.6
 ./configure --prefix=$ZATO_TARGET_DIR/code
 make && make altinstall
