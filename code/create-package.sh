62da66fb0fb1b815ff6391905080a04f1eb19d74
Rafał Krysiak <rafal@zato.io>
2016-08-12 19:46:17 +0200

    Create Zato tar package without verbose option on.

--- code/create-package.sh
+++ code/create-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd $HOME
-tar -czvf zato-2.0.7.tar .bash_profile .bashrc bin/ bzr/ .cache/ \
+tar -czf zato-2.0.7.tar .bash_profile .bashrc bin/ bzr/ .cache/ \
     haproxy/ include/ lib/ .local/ opt/ .pki/ redis-3.2.3/ \
     extra-libs/ server_objects/ services/
