8cd7b2e4dfafb1665916d039668077d0ba35984a
Rafał Krysiak <rafal@zato.io>
2016-08-15 13:30:05 +0200

    Use gzip to create an archive.

--- code/create-package.sh
+++ code/create-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd $HOME
-tar -cvzf zato-2.0.7.tar .bash_profile .bashrc bin/ bzr/ .cache/ \
+tar -cvzf zato-2.0.7.tar.gz .bash_profile .bashrc bin/ bzr/ .cache/ \
     haproxy/ include/ lib/ .local/ opt/ .pki/ redis-3.2.3/ \
-    extra-libs/ server_objects/ services/
+    extra-libs/ server-objects/ services/
