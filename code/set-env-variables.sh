a4878dfc95892530f12e031fa8a704c7fb3666be
Rafał Krysiak <rafal@zato.io>
2016-08-15 11:22:44 +0200

    Corrected path to Zato binaries.

--- code/set-env-variables.sh
+++ code/set-env-variables.sh
@@ -4,7 +4,7 @@ ZATO_VERSION=2.0.7
 ZATO_ROOT_DIR=$HOME/opt/zato
 ZATO_TARGET_DIR=$ZATO_ROOT_DIR/$ZATO_VERSION
 
-echo "export PATH=$PATH:$HOME/usr/bin/:$HOME/.local/bin:$ZATO_TARGET_DIR/bin/:$HOME/redis-3.2.3/src/:$HOME/haproxy/" \
+echo "export PATH=$PATH:$HOME/usr/bin/:$HOME/.local/bin:$ZATO_TARGET_DIR/code/bin/:$HOME/redis-3.2.3/src/:$HOME/haproxy/" \
     | tee -a $HOME/.bashrc $HOME/.bash_profile
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib/:$HOME/lib64/:$HOME/usr/lib/:$HOME/usr/lib64/" \
     | tee -a $HOME/.bashrc $HOME/.bash_profile
