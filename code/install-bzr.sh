013aa14bd1ad84f557ca6709adad91eae2369275
Rafał Krysiak <rafal@zato.io>
2016-08-19 10:03:26 +0200

    Corrected path to python2.7.

--- code/install-bzr.sh
+++ code/install-bzr.sh
@@ -5,4 +5,4 @@ mkdir $HOME/bzr
 wget https://launchpad.net/bzr/2.7/2.7.0/+download/bzr-2.7.0.tar.gz
 tar -xzvf bzr-2.7.0.tar.gz --strip 1 -C $HOME/bzr/
 cd $HOME/bzr
-$HOME/opt/zato/2.0.7/bin/python2.7 setup.py install --home $HOME
+$HOME/opt/zato/2.0.7/code/bin/python2.7 setup.py install --home $HOME
