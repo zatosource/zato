4ec95eae3697f6f0d8f904578d3b864fad91edf8
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-01-28 15:52:05 +0100

    GH #725 - Refactored out code to obtain server path to it's easier to diagnose it.

--- code/run-tests.py
+++ code/run-tests.py
@@ -108,18 +108,29 @@ def _apitests():
 
     # We expect a process to be a Zato a server if it keeps open files containing these parts in their names.
     known_file_parts = ('logs/kvdb.log', 'logs/pubsub-overflown.log', 'logs/rbac.log')
+    len_known_file_parts = len(known_file_parts)
+
+    def _get_server_path(open_files):
+
+        known_found = 0
+        last = None
+
+        for known in known_file_parts:
+            for f in open_files:
+                if f.endswith(known):
+                    known_found += 1
+                    last = f
+
+        if known_found == len_known_file_parts:
+            return f.split('/logs/')[0]
 
     for item in psutil.net_connections():
         if item.status == psutil.CONN_LISTEN and item.laddr[1] == 17010:
             proc = psutil.Process(pid=item.pid)
-            known_found = 0
             open_files = proc.open_files()
-            for idx, f in enumerate(open_files):
-                if any(part in f.path for part in known_file_parts):
-                    known_found += 1
 
-            if known_found == len(known_file_parts):
-                server_path = open_files[idx].path.split('/logs/')[0]
+            server_path = _get_server_path([f.path for f in open_files if f.path.endswith('.log')])
+            if server_path:
                 break
 
     # We've got a path that for 99% is a server, but let's run zato info against it to be 100% sure.
@@ -145,9 +156,6 @@ def _apitests():
         apitest_cmd = 'apitest'
         tests_dir = os.path.join(curdir, 'apitest')
         run('{} run {}'.format(apitest_cmd, tests_dir))
-
-        # TODO: The output should be consulted - it's possible that someone
-        # will want to stop the tests if API test don't succeed.
 
 @click.command()
 def apitests():
