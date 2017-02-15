d32167ca0f990914758019cba570a4d3c57528e9
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-02-15 11:23:39 +0100

    GH #725 - Don't treat fixed-width as though it was flat SIO in responses.

--- code/zato-server/src/zato/server/service/internal/checks/sio.py
+++ code/zato-server/src/zato/server/service/internal/checks/sio.py
@@ -793,7 +793,7 @@ class FixedWidthString(Service):
 
     class SimpleIO:
         input_required = (fw.String(1, 'a'), fw.String(2, 'b'), fw.String(3, 'c'), fw.String(4, 'd'))
-        #output_required = (fw.String(1, 'aa'), fw.String(2, 'bb'), fw.String(3, 'cc'), fw.String(4, 'dd'))
+        output_required = (fw.String(1, 'aa'), fw.String(2, 'bb'), fw.String(3, 'cc'), fw.String(4, 'dd'))
 
     def handle(self):
 
@@ -821,14 +821,6 @@ class FixedWidthString(Service):
                 given_value = getattr(line, key)
                 eq_(given_value, expected_value)
 
-        #eq_(self.request.input[0], expected1)
-        #eq_(self.request.input[1], expected2)
-
-        '''
-        self.request.payload.aa = 'q'
-        self.request.payload.bb = 'w'
-        self.request.payload.cc = 'e'
-        self.request.payload.dd = 'r'
-        '''
+        #
 
 # ################################################################################################################################
