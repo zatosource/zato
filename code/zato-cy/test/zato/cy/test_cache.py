bae0e89185b8caa59b3fb9554c72be6356bff937
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-03-23 23:22:42 +0100

    GH #681 - Use locks in all operations. Expire keys on .get and (optionally) .set calls.

--- code/zato-cy/test/zato/cy/test_cache.py
+++ code/zato-cy/test/zato/cy/test_cache.py
@@ -8,12 +8,13 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 # stdlib
 import sys
+from time import sleep
 from unittest import main as unittest_main, TestCase
 from uuid import uuid4
 
 # Zato
 sys.path.insert(0, '.')
-from cache import Cache
+from cache import Cache, KeyExpiredError
 
 # ################################################################################################################################
 
@@ -43,6 +44,42 @@ class CacheTestCace(TestCase):
 
 # ################################################################################################################################
 
+    def test_set_defaults(self):
+
+        key1, expected1 = 'key1', 'value1'
+        key2, expected2 = 'key2', 'value2'
+        key2_expiry = 1000
+
+        c = Cache()
+        c.set(key1, expected1)
+        c.set(key2, expected2, key2_expiry)
+
+        returned1 = c.get(key1, True)
+        sleep(0.01)
+
+        self.assertEquals(returned1.value, expected1)
+        self.assertEquals(returned1.expiry, 0.0)
+        self.assertEquals(returned1.expires_at, 0.0)
+        self.assertEquals(returned1.hits, 1)
+        self.assertEquals(returned1.prev_read, 0.0)
+        self.assertEquals(returned1.prev_write, 0.0)
+        self.assertLess(returned1.last_read, c.get_timestamp())
+        self.assertLess(returned1.last_write, c.get_timestamp())
+
+        returned2 = c.get(key2, True)
+        sleep(0.01)
+
+        self.assertEquals(returned2.value, expected2)
+        self.assertEquals(returned2.expiry, key2_expiry)
+        self.assertLess(c.get_timestamp() - returned2.expires_at, key2_expiry)
+        self.assertEquals(returned2.hits, 1)
+        self.assertLess(returned2.last_read, c.get_timestamp())
+        self.assertLess(returned2.last_write, c.get_timestamp())
+        self.assertEquals(returned2.prev_read, 0.0)
+        self.assertEquals(returned2.prev_write, 0.0)
+
+# ################################################################################################################################
+
     def test_set_get_with_details(self):
 
         key1, expected1 = 'key1', 'value1'
@@ -58,29 +95,29 @@ class CacheTestCace(TestCase):
         now = c.get_timestamp() + 1
 
         returned1 = c.get(key1, True)
-        self.assertEquals(returned1['value'], expected1)
-        self.assertEquals(returned1['position'], 2)
-        self.assertLess(returned1['last_read'], now)
-        self.assertLess(returned1['last_write'], now)
-        self.assertLess(returned1['prev_read'], now)
-        self.assertLess(returned1['prev_write'], now)
+        self.assertEquals(returned1.value, expected1)
+        self.assertEquals(returned1.position, 2)
+        self.assertLess(returned1.last_read, now)
+        self.assertLess(returned1.last_write, now)
+        self.assertLess(returned1.prev_read, now)
+        self.assertLess(returned1.prev_write, now)
 
         
         returned2 = c.get(key2, True)
-        self.assertEquals(returned2['value'], expected2)
-        self.assertEquals(returned2['position'], 2)
-        self.assertLess(returned2['last_read'], now)
-        self.assertLess(returned2['last_write'], now)
-        self.assertLess(returned2['prev_read'], now)
-        self.assertLess(returned2['prev_write'], now)
+        self.assertEquals(returned2.value, expected2)
+        self.assertEquals(returned2.position, 2)
+        self.assertLess(returned2.last_read, now)
+        self.assertLess(returned2.last_write, now)
+        self.assertLess(returned2.prev_read, now)
+        self.assertLess(returned2.prev_write, now)
 
         returned3 = c.get(key3, True)
-        self.assertEquals(returned3['value'], expected3)
-        self.assertEquals(returned3['position'], 2)
-        self.assertLess(returned3['last_read'], now)
-        self.assertLess(returned3['last_write'], now)
-        self.assertLess(returned3['prev_read'], now)
-        self.assertLess(returned3['prev_write'], now)
+        self.assertEquals(returned3.value, expected3)
+        self.assertEquals(returned3.position, 2)
+        self.assertLess(returned3.last_read, now)
+        self.assertLess(returned3.last_write, now)
+        self.assertLess(returned3.prev_read, now)
+        self.assertLess(returned3.prev_write, now)
 
 # ################################################################################################################################
 
@@ -109,6 +146,62 @@ class CacheTestCace(TestCase):
 
 # ################################################################################################################################
 
+    def test_set_already_exists_no_expiry(self):
+
+        key1, expected1 = 'key1', 'value1'
+        expected1_new = 'value1_new'
+
+        c = Cache()
+        c.set(key1, expected1)
+        c.set(key1, expected1_new)
+
+        returned1_a = c.get(key1)
+        returned1_b = c.get(key1, True)
+
+        self.assertEquals(len(c), 1)
+        self.assertEquals(returned1_a, expected1_new)
+        self.assertEquals(returned1_b.value, expected1_new)
+
+# ################################################################################################################################
+
+    def test_set_already_exists_extend_expiry_on_set_true(self):
+
+        key1, expected1 = 'key1', 'value1'
+        expected1_new = 'value1_new'
+
+        c = Cache(extend_expiry_on_set=True)
+        c.set(key1, expected1, 0.1)
+        sleep(0.11)
+        c.set(key1, expected1_new)
+
+        returned1_a = c.get(key1)
+        returned1_b = c.get(key1, True)
+
+        self.assertEquals(len(c), 1)
+        self.assertEquals(returned1_a, expected1_new)
+        self.assertEquals(returned1_b.value, expected1_new)
+
+# ################################################################################################################################
+
+    def test_set_already_exists_extend_expiry_on_set_false(self):
+
+        key1, expected1 = 'key1', 'value1'
+        expected1_new = 'value1_new'
+
+        c = Cache(extend_expiry_on_set=False)
+        c.set(key1, expected1, 0.1)
+        sleep(0.11)
+
+        self.assertRaises(KeyExpiredError, c.set, key1, expected1_new)
+        self.assertEquals(len(c), 0)
+        self.assertListEqual(c._expired_on_op, [key1])
+
+        deleted = c.delete_expired()
+        self.assertEquals(sorted(deleted), [key1])
+        self.assertListEqual(c._expired_on_op, [])
+
+# ################################################################################################################################
+
     def test_hits_per_position(self):
 
         max_size = 2
@@ -121,9 +214,11 @@ class CacheTestCace(TestCase):
         c.set(key2, expected2)
         c.set(key3, expected3)
 
+
         self.assertEquals(len(c.hits_per_position), 2)
         self.assertEquals(c.hits_per_position[0], 0)
         self.assertEquals(c.hits_per_position[1], 0)
+
 
         # Right now index0 is key3, so when we look up this key 3 times, we always find it at index0
 
@@ -132,10 +227,12 @@ class CacheTestCace(TestCase):
         c.get('key3')
         self.assertEquals(c.hits_per_position[0], 3)
 
+
         # Now get key2 which will be found at index1 and getting it will move it to index0
 
         c.get('key2')
         self.assertEquals(c.hits_per_position[1], 1)
+
 
         # Now key2 is at index0 so getting this key will increase index0's counter
 
@@ -144,15 +241,93 @@ class CacheTestCace(TestCase):
         c.get('key2')
         self.assertEquals(c.hits_per_position[0], 6)
 
+
         # Make sure index1's counter is still the same as it was
         self.assertEquals(c.hits_per_position[1], 1)
 
+
         # Looking up unknown keys should not increase any counters
-        c.get(uuid4().hex)
+        try:
+            c.get(uuid4().hex)
+        except KeyError:
+            pass
 
         self.assertEquals(len(c.hits_per_position), 2)
         self.assertEquals(c.hits_per_position[0], 6)
         self.assertEquals(c.hits_per_position[1], 1)
+
+# ################################################################################################################################
+
+    def test_del(self):
+
+        key1, expected1 = 'key1', 'value1'
+        key2, expected2 = 'key2', 'value2'
+        key3, expected3 = 'key3', 'value3'
+
+        c = Cache()
+        c.set(key1, expected1)
+        c.set(key2, expected2)
+        c.set(key3, expected3)
+
+        c.delete(key1)
+
+        self.assertEquals(len(c), 2)
+        self.assertNotIn(key1, c)
+        self.assertIn(key2, c)
+        self.assertIn(key3, c)
+
+# ################################################################################################################################
+
+    def test_delete_expired(self):
+
+        key1, expected1 = 'key1', 'value1'
+        key2, expected2 = 'key2', 'value2'
+        key3, expected3 = 'key3', 'value3'
+
+        c = Cache()
+        c.set(key1, expected1, 0.1)
+        c.set(key2, expected2)
+        c.set(key3, expected3, 0.03)
+
+        sleep(0.12)
+
+        deleted = c.delete_expired()
+        self.assertEquals(sorted(deleted), [key1, key3])
+
+        self.assertEquals(len(c), 1)
+        self.assertNotIn(key1, c)
+        self.assertIn(key2, c)
+        self.assertNotIn(key3, c)
+
+# ################################################################################################################################
+
+    def test_get_deletes_expired_key(self):
+
+        key1, expected1 = 'key1', 'value1'
+        key2, expected2 = 'key2', 'value2'
+        key3, expected3 = 'key3', 'value3'
+
+        c = Cache()
+        c.set(key1, expected1, 0.03)
+        c.set(key2, expected2)
+        c.set(key3, expected3, 0.05)
+
+        sleep(0.12)
+
+        self.assertRaises(KeyExpiredError, c.get, key1)
+        self.assertRaises(KeyExpiredError, c.get, key3)
+
+        self.assertEquals(len(c), 1)
+        self.assertNotIn(key1, c)
+        self.assertIn(key2, c)
+        self.assertNotIn(key3, c)
+
+        # It will be arranged in the same order as .get were called in
+        self.assertListEqual(c._expired_on_op, [key1, key3])
+
+        deleted = c.delete_expired()
+        self.assertEquals(sorted(deleted), [key1, key3])
+        self.assertListEqual(c._expired_on_op, [])
 
 # ################################################################################################################################
 
