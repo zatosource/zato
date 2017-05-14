46a8d833f0a0cdc06836332d75efb99b55b7b52f
Dick Knol <dick.knol@hyproca.nl>
2017-05-14 22:33:06 +0200

    Include freetds-dev in installer

--- code/_install-deb.sh
+++ code/_install-deb.sh
@@ -42,7 +42,7 @@ sudo apt-get install -y git bzr gfortran haproxy  \
     libevent-dev libgfortran3 liblapack-dev $LIBLAPACK3 \
     libpq-dev libyaml-dev libxml2-dev libxslt1-dev libumfpack$LIBUMFPACK_VERSION \
     openssl python2.7-dev python-numpy python-pip \
-    python-scipy python-zdaemon swig uuid-dev uuid-runtime libffi-dev libssl-dev \
+    python-scipy python-zdaemon swig uuid-dev uuid-runtime libffi-dev \
     freetds-dev
 
 # On Debian and Ubuntu the binary goes to /usr/sbin/haproxy so we need to symlink it
