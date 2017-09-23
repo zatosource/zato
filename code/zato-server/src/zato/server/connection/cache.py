a01d91e6d657ce437d3c4b1d7df1986c5690b532
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-09-23 19:06:14 +0200

    GH #681 - Added a basic cache API for the built-in self.cache access.

--- code/zato-server/src/zato/server/connection/cache.py
+++ code/zato-server/src/zato/server/connection/cache.py
@@ -10,16 +10,93 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # stdlib
 from logging import getLogger
+from traceback import format_exc
 
 # gevent
+from gevent import sleep, spawn
 from gevent.lock import RLock
 
 # Zato
-from zato.cache import Cache
+from zato.cache import Cache as _CyCache
 
 # ################################################################################################################################
 
 logger = getLogger(__name__)
+
+# ################################################################################################################################
+
+class Cache(object):
+    """ The cache API through which services access the built-in self.cache objects.
+    Attribute self.impl is the actual Cython-based cache implementation.
+    """
+    def __init__(self, config):
+        self.config = config
+        self.impl = _CyCache(self.config.max_size, self.config.max_item_size, self.config.extend_expiry_on_get,
+            self.config.extend_expiry_on_set)
+        spawn(self._delete_expired)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def __getattr__(self, key):
+        return self.get(key)
+
+    def __setattr__(self, key, value):
+        self.set(key, value)
+
+    def __contains__(self, key):
+        return key in self.impl
+
+    def __len__(self, key):
+        return len(self.impl)
+
+    def set(self, key, value, expiry=0.0):
+        """ Sets key to a given value. Key must be string/unicode. Value must be an integer or string/unicode.
+        Expiry is in seconds (or a fraction of).
+        """
+        self.impl.set(key, value, expiry)
+
+    def get(self, key, details=False):
+        """ Returns a value stored under a given key. If details is True, return metadata about the key as well.
+        """
+        return self.impl.get(key, details)
+
+    def delete(self, key):
+        """ Deletes a cache entry by its key.
+        """
+        self.impl.delete(key)
+
+    def keys(self):
+        """ Returns all keys in the cache - like dict.keys().
+        """
+        return self.impl.keys()
+
+    def values(self):
+        """ Returns all values in the cache - like dict.values().
+        """
+        return self.impl.values()
+
+    def items(self):
+        """ Returns all items in the cache - like dict.items().
+        """
+        return self.impl.items()
+
+    def _delete_expired(self, interval=5, _sleep=sleep):
+        """ Invokes in its own greenlet in background to delete expired cache entries.
+        """
+        while True:
+            try:
+                _sleep(interval)
+                deleted = self.impl.delete_expired()
+            except Exception, e:
+                logger.warn(format_exc(e))
+                _sleep(2)
+            else:
+                if deleted:
+                    logger.info('Deleted keys expired in the last %ss - %s', interval, deleted)
 
 # ################################################################################################################################
 
@@ -51,7 +128,7 @@ class CacheAPI(object):
 # ################################################################################################################################
 
     def _create_builtin(self, config):
-        return Cache(config.max_size, config.max_item_size, config.extend_expiry_on_get, config.extend_expiry_on_set)
+        return Cache(config)
 
 # ################################################################################################################################
 
