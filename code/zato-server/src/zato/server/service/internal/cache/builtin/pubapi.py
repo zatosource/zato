0f7d8d270d15050c1941960aa01f9a71a5c0e2e2
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-10-15 22:23:46 +0200

    GH #681 - Added public API calls for Set* operations.

--- code/zato-server/src/zato/server/service/internal/cache/builtin/pubapi.py
+++ code/zato-server/src/zato/server/service/internal/cache/builtin/pubapi.py
@@ -14,7 +14,7 @@ from datetime import datetime
 # Zato
 from zato.common import ZATO_NOT_GIVEN
 from zato.common.exception import NotFound
-from zato.server.service import Bool, Dict, ListOfDicts, Service
+from zato.server.service import Bool, Dict, Float, ListOfDicts, Service
 
 # ################################################################################################################################
 
@@ -25,7 +25,22 @@ datetime_keys = ('last_read', 'prev_read', 'last_write', 'prev_write', 'expires_
 
 # ################################################################################################################################
 
-class _BaseGet(Service):
+class _BaseService(Service):
+    def _get_cache_(self, input):
+        cache_name = input['cache']
+        if cache_name:
+            try:
+                cache = self.cache.builtin[cache_name]
+            except KeyError:
+                raise NotFound(self.cid, 'No such cache `{}`'.format(cache_name))
+        else:
+            cache = self.cache.default
+
+        return cache
+
+# ################################################################################################################################
+
+class _BaseGet(_BaseService):
 
     # Must be provided by subclasses
     input_key = None
@@ -48,21 +63,9 @@ class _BaseGet(Service):
         return item_dict
 
     def handle(self):
-
         input = self.request.input
-        cache_name = input['cache']
-
-        if cache_name:
-            try:
-                cache = self.cache.builtin[cache_name]
-            except KeyError:
-                raise NotFound(self.cid, 'No such cache `{}`'.format(cache_name))
-        else:
-            cache = self.cache.default
-
         default = input['default'] if input.get('default') else ZATO_NOT_GIVEN
-
-        self._cache_handle(cache, input[self.input_key], input.get('details'), default)
+        self._cache_handle(self._get_cache_(input), input[self.input_key], self.request.input.get('details'), default)
 
     def _cache_handle(self, *args, **kwargs):
         raise NotImplementedError('Must be implemented in subclasses')
@@ -168,5 +171,100 @@ class GetContainsAll(_BaseGetList):
 class GetContainsAny(_BaseGetList):
     def _cache_get_list(self, cache, data, details):
         return cache.get_contains_any(data, details)
+
+# ################################################################################################################################
+
+class _BaseSet(_BaseService):
+
+    # Must be provided by subclasses
+    input_key = None
+
+    class SimpleIO:
+        response_elem = None
+        input_optional = ('cache', Bool('return_found'), Float('expiry'))
+
+# ################################################################################################################################
+
+class Set(_BaseSet):
+
+    input_key = 'key'
+
+    class SimpleIO(_BaseSet.SimpleIO):
+        input_required = ('key', 'value')
+        output_optional = ('prev_value',)
+        skip_empty_keys = True
+
+    def handle(self):
+        input = self.request.input
+        prev_value = self._get_cache_(input).set(input[self.input_key], input.value, input.get('expiry') or 0.0)
+
+        if input.return_found:
+            self.response.payload.prev_value = prev_value
+
+# ################################################################################################################################
+
+class _SetList(_BaseSet):
+
+    input_key = 'data'
+
+    class SimpleIO(_BaseSet.SimpleIO):
+        input_required = ('data', 'value')
+        output_optional = ('key', 'prev_value',)
+        output_repeated = True
+        skip_empty_keys = True
+
+    def handle(self):
+        input = self.request.input
+        return_found = input.get('return_found')
+        cache = self._get_cache_(input)
+
+        result = self._get_cache_func(cache)(input[self.input_key], input.value, input.get('expiry') or 0.0, return_found)
+        if return_found:
+            self.response.payload[:] = [{'key':key, 'prev_value':value} for key, value in result.iteritems()]
+
+    def _get_cache_func(self, cache):
+        raise NotImplementedError('Must be implemented in subclasses')
+
+# ################################################################################################################################
+
+class SetByPrefix(_SetList):
+    def _get_cache_func(self, cache):
+        return cache.set_by_prefix
+
+# ################################################################################################################################
+
+class SetBySuffix(_SetList):
+    def _get_cache_func(self, cache):
+        return cache.set_by_suffix
+
+# ################################################################################################################################
+
+class SetByRegex(_SetList):
+    def _get_cache_func(self, cache):
+        return cache.set_by_regex
+
+# ################################################################################################################################
+
+class SetContains(_SetList):
+    def _get_cache_func(self, cache):
+        return cache.set_contains
+
+# ################################################################################################################################
+
+class SetNotContains(_SetList):
+    def _get_cache_func(self, cache):
+        return cache.set_not_contains
+
+# ################################################################################################################################
+
+class SetContainsAll(_SetList):
+    def _get_cache_func(self, cache):
+        return cache.set_contains_all
+
+# ################################################################################################################################
+
+class SetContainsAny(_SetList):
+    def _get_cache_func(self, cache):
+        return cache.set_contains_any
 
 # ################################################################################################################################
