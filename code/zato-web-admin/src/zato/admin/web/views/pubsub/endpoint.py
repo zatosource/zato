44eccc0f00aee173bd75ad4197075f4fa2b6c41d
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-10-24 04:28:06 +0200

    GH #755 - Update endpoints listing after edit actions.

--- code/zato-web-admin/src/zato/admin/web/views/pubsub/endpoint.py
+++ code/zato-web-admin/src/zato/admin/web/views/pubsub/endpoint.py
@@ -19,8 +19,10 @@ from django.template.response import TemplateResponse
 
 try:
     from django.core.urlresolvers import reverse as django_url_reverse # Django < 1.10
+    from django.utils.text import slugify
 except ImportError:
     from django.urls import reverse as django_url_reverse              # Django >= 1.10
+    from django.utils import slugify
 
 # Zato
 from zato.admin.web.forms.pubsub.endpoint import CreateForm, EditForm
@@ -31,6 +33,58 @@ from zato.common.odb.model import PubSubEndpoint
 # ################################################################################################################################
 
 logger = logging.getLogger(__name__)
+
+# ################################################################################################################################
+
+def enrich_item(cluster_id, item):
+    item.topic_patterns = item.topic_patterns or ''
+    item.queue_patterns = item.queue_patterns or ''
+
+    item.topic_patterns_html = '<br/>'.join(item.topic_patterns.splitlines())
+    item.queue_patterns_html = '<br/>'.join(item.queue_patterns.splitlines())
+
+    # Making a copy because it will be replaced with a concatenation of sec_type and security_id,
+    # yet we still need it for the client string.
+    security_id = item.security_id
+
+    if item.security_id:
+        item.security_id = '{}/{}'.format(item.sec_type, item.security_id)
+
+    # Client is a string representation of a WebSockets channel or HTTP credentials
+    client = ''
+
+    if security_id or item.ws_channel_id:
+        if security_id:
+            path_name = 'security-basic-auth'
+            id_value = security_id
+            name = item.sec_name
+            protocol = 'HTTP'
+
+        elif item.ws_channel_id:
+            path_name = 'channel-web-socket'
+            id_value = item.ws_channel_id
+            name = item.ws_channel_name
+            protocol = 'WebSockets'
+
+        path = django_url_reverse(path_name)
+        client = '<span style="font-size:smaller">{}</span><br/><a href="{}?cluster={}&amp;highlight={}">{}</a>'.format(
+            protocol, path, cluster_id, id_value, name)
+
+    item.client_html = client
+
+    # This is needed by the edit action to link to an endpoint's details
+    item.endpoint_details_html = '<a href="{}">{}</a>'.format(
+        django_url_reverse('pubsub-endpoint-details',
+            kwargs={'cluster_id':cluster_id, 'endpoint_id':item.id, 'name_slug':slugify(item.name)}),
+        item.name)
+
+    # This is also needed by the edit action so as not to construct it in JavaScript
+    if item.is_internal:
+        item.delete_html = '<span class="form_hint">Delete</span>'
+    else:
+        item.delete_html = """<a href="javascript:$.fn.zato.pubsub.endpoint.delete_('{}')">Delete</a>""".format(item.id)
+
+    return item
 
 # ################################################################################################################################
 
@@ -50,43 +104,7 @@ class Index(_Index):
         output_repeated = True
 
     def on_before_append_item(self, item):
-
-        item.topic_patterns = item.topic_patterns or ''
-        item.queue_patterns = item.queue_patterns or ''
-
-        item.topic_patterns_html = item.topic_patterns.replace('\n', '<br/>')
-        item.queue_patterns_html = item.queue_patterns.replace('\n', '<br/>')
-
-        # Making a copy because it will be replaced with a concatenation of sec_type and security_id,
-        # yet we still need it for the client string.
-        security_id = item.security_id
-
-        if item.security_id:
-            item.security_id = '{}/{}'.format(item.sec_type, item.security_id)
-
-        # Client is a string representation of a WebSockets channel or HTTP credentials
-        client = ''
-
-        if security_id or item.ws_channel_id:
-            if security_id:
-                path_name = 'security-basic-auth'
-                id_value = security_id
-                name = item.sec_name
-                protocol = 'HTTP'
-
-            elif item.ws_channel_id:
-                path_name = 'channel-web-socket'
-                id_value = item.ws_channel_id
-                name = item.ws_channel_name
-                protocol = 'WebSockets'
-
-            path = django_url_reverse(path_name)
-            client = '<span style="font-size:smaller">{}</span><br/><a href="{}?cluster={}&amp;highlight={}">{}</a>'.format(
-                protocol, path, self.req.zato.cluster_id, id_value, name)
-
-        item.client_html = client
-
-        return item
+        return enrich_item(self.req.zato.cluster_id, item)
 
     def handle(self):
 
@@ -118,6 +136,15 @@ class _CreateEdit(CreateEdit):
             self.input.security_id = int(self.input.security_id.split('/')[1])
         else:
             self.input.security_id = None
+
+    def post_process_return_data(self, return_data):
+
+        response = self.req.zato.client.invoke('endpoint1.get', {
+            'cluster_id': self.req.zato.cluster_id,
+            'id': return_data['id'],
+        }).data['response']
+
+        return_data.update(enrich_item(self.req.zato.cluster_id, response))
 
     def success_message(self, item):
         return 'Successfully {} the pub/sub endpoint `{}`'.format(self.verb, item.name)
