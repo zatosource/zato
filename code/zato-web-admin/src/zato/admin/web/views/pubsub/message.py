8e0afa9ca2e9fbfd8b8abf41f3a51b37d7608a2c
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-10-30 11:40:21 +0100

    GH #755 - Working on the message publish form.

--- code/zato-web-admin/src/zato/admin/web/views/pubsub/message.py
+++ code/zato-web-admin/src/zato/admin/web/views/pubsub/message.py
@@ -20,17 +20,28 @@ from django.template.response import TemplateResponse
 from zato.admin.web import from_utc_to_user
 from zato.admin.web.forms.pubsub import MsgForm
 from zato.admin.web.views import method_allowed
+from zato.admin.web.views.pubsub import get_endpoint_html
 
 # ################################################################################################################################
 
 @method_allowed('GET')
 def get(req, cluster_id, object_type, object_id, msg_id):
 
-    return_data = req.zato.client.invoke('zato.pubsub.message.get', {
+    return_data = {
+        'action': 'update',
+    }
+
+    return_data['cluster_id'] = cluster_id
+    return_data['object_type'] = object_type
+    return_data['{}_id'.format(object_type)] = object_id
+    return_data['endpoint_html'] = get_endpoint_html(return_data, cluster_id)
+
+    return_data.update(req.zato.client.invoke('zato.pubsub.message.get', {
         'cluster_id': cluster_id,
         'msg_id': msg_id,
-    }).data.response
+    }).data.response)
 
+    return_data['form'] = MsgForm(return_data)
     return_data.pub_time = from_utc_to_user(return_data.pub_time+'+00:00', req.zato.user_profile)
 
     if return_data.ext_pub_time:
@@ -39,18 +50,11 @@ def get(req, cluster_id, object_type, object_id, msg_id):
     if return_data.expiration_time:
         return_data.expiration_time = from_utc_to_user(return_data.expiration_time+'+00:00', req.zato.user_profile)
 
-    return_data.cluster_id = cluster_id
-    return_data.object_type = object_type
-    return_data['{}_id'.format(object_type)] = object_id
-    return_data.form = MsgForm(return_data)
-    return_data.endpoint_html = get_endpoint_html(return_data, cluster_id)
-
     return TemplateResponse(req, 'zato/pubsub/message-details.html', return_data)
 
 # ################################################################################################################################
 
-@method_allowed('POST')
-def update(req, cluster_id, msg_id):
+def _publish_update_action(req, cluster_id, action, msg_id=None, topic_id=None):
 
     expiration = req.POST.get('expiration')
     correl_id = req.POST.get('correl_id')
@@ -64,16 +68,20 @@ def update(req, cluster_id, msg_id):
         expiration_time = None
         size = None
 
-        response = req.zato.client.invoke('zato.pubsub.message.update', {
+        input = {
             'cluster_id': cluster_id,
-            'msg_id': msg_id,
             'data': data,
             'expiration': expiration,
             'correl_id': correl_id,
             'in_reply_to': in_reply_to,
             'priority': priority,
             'mime_type': mime_type,
-        }).data.response
+        }
+
+        if msg_id:
+            input['msg_id'] = msg_id
+
+        response = req.zato.client.invoke('zato.pubsub.message.{}'.format(action), input).data.response
 
     except Exception, e:
         is_ok = False
@@ -81,7 +89,7 @@ def update(req, cluster_id, msg_id):
 
     else:
         is_ok = True
-        message = 'Message updated'
+        message = 'Message {}'.format
         size = response.size
         if response.expiration_time:
             expiration_time = from_utc_to_user(response.expiration_time+'+00:00', req.zato.user_profile)
@@ -92,6 +100,28 @@ def update(req, cluster_id, msg_id):
         'expiration_time': expiration_time,
         'size': size
     }))
+
+# ################################################################################################################################
+
+@method_allowed('POST')
+def update_action(req, cluster_id, msg_id):
+    _publish_update_action(req, cluster_id, 'update', msg_id)
+
+# ################################################################################################################################
+
+@method_allowed('GET')
+def publish(req, cluster_id, topic_id):
+    return_data = {
+        'action': 'publish',
+        'form': MsgForm()
+    }
+    return TemplateResponse(req, 'zato/pubsub/message-details.html', return_data)
+
+# ################################################################################################################################
+
+@method_allowed('POST')
+def publish_action(req, cluster_id, topic_id):
+    _publish_update_action(req, cluster_id, 'publish', topic_id=topic_id)
 
 # ################################################################################################################################
 
@@ -109,25 +139,3 @@ def delete(req, cluster_id, msg_id):
         return HttpResponse('Deleted message `{}`'.format(msg_id))
 
 # ################################################################################################################################
-
-@method_allowed('GET')
-def publish(req, cluster_id, topic_id):
-
-    try:
-        req.zato.client.invoke('zato.pubsub.message.create', {
-            'cluster_id': cluster_id,
-            'msg_id': msg_id,
-        })
-    except Exception, e:
-        return HttpResponseServerError(format_exc(e))
-    else:
-        return HttpResponse('Deleted message `{}`'.format(msg_id))
-
-# ################################################################################################################################
-
-@method_allowed('POST')
-def publish_action(req, cluster_id, topic_id):
-    pass
-
-# ################################################################################################################################
-
