d6065d7a8cb085ba9f3ef6a817b18cddb80838b0
David Wilson <dw@botanicus.net>
2017-11-13 07:03:52 -0800

    GH #795: apispec: support returning internal services via a parameter.
    Needed to allow enmasse to enumerate them with the cluster's public API.

--- code/zato-server/src/zato/server/service/internal/apispec/__init__.py
+++ code/zato-server/src/zato/server/service/internal/apispec/__init__.py
@@ -13,7 +13,7 @@ from json import dumps
 
 # Zato
 from zato.server.apispec import Generator
-from zato.server.service import Service
+from zato.server.service import Bool, Service
 
 # ################################################################################################################################
 
@@ -21,14 +21,18 @@ class GetAPISpec(Service):
     """ Returns API specifications for all services.
     """
     class SimpleIO:
-        input_optional = ('cluster_id', 'query',)
+        input_optional = ('cluster_id', 'query', Bool('return_internal'))
 
     def handle(self):
         cluster_id = self.request.input.get('cluster_id')
+        if self.request.input.get('return_internal'):
+            ignore_prefix = ''
+        else:
+            ignore_prefix = 'zato'
 
         if cluster_id and cluster_id != self.server.cluster_id:
             raise ValueError('Input cluster ID `%s` different than ours `%s`', cluster_id, self.server.cluster_id)
 
-        self.response.payload = dumps(Generator(self.server.service_store.services, self.request.input.query).get_info())
+        self.response.payload = dumps(Generator(self.server.service_store.services, self.request.input.query).get_info(ignore_prefix=ignore_prefix))
 
 # ################################################################################################################################
