64b27b87012f37ab7caffa32529e7239aa345254
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-11-25 21:59:42 +0100

    GH #407 - Auto-delete stale pidfiles if they are known not to be needed.

--- code/zato-cli/src/zato/cli/check_config.py
+++ code/zato-cli/src/zato/cli/check_config.py
@@ -9,6 +9,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
+import os
 from distutils.version import LooseVersion
 from json import loads
 from os.path import abspath, exists, join
@@ -19,8 +20,13 @@ from bunch import Bunch
 # ConfigObj
 from configobj import ConfigObj
 
+# psutil
+from psutil import AccessDenied, Process, NoSuchProcess
+
 # Zato
 from zato.cli import ManageCommand
+from zato.common import INFO_FORMAT
+from zato.common.component_info import get_info
 from zato.common.crypto import CryptoManager
 from zato.common.kvdb import KVDB
 from zato.common.haproxy import validate_haproxy_config
@@ -97,10 +103,73 @@ class CheckConfig(ManageCommand):
         if self.show_output:
             self.logger.info('Redis connection OK')
 
-    def ensure_no_pidfile(self):
+    def ensure_no_pidfile(self, log_file_marker):
         pidfile = abspath(join(self.component_dir, 'pidfile'))
+
+        # Pidfile exists ..
         if exists(pidfile):
-            raise Exception('Cannot proceed, found pidfile `{}`'.format(pidfile))
+
+            # .. but raise an error only if the PID it points to belongs
+            # to an already running component. Otherwise, it must be a stale pidfile
+            # that we can safely delete.
+            pid = open(pidfile).read().strip()
+            try:
+                pid = int(pid)
+            except ValueError:
+                raise Exception('Could not parse pid value `{}` as an integer ({})'.format(pid, pidfile))
+            else:
+                try:
+                    info = get_info(self.component_dir, INFO_FORMAT.DICT)
+                except AccessDenied:
+                    # This could be another process /or/ it can be our own component started by another user,
+                    # so to be on the safe side, indicate an error instead of deleting the pidfile
+                    raise Exception('Access denied to PID `{}` found in `{}`'.format(pid, pidfile))
+                except NoSuchProcess:
+                    # This is fine, there is no process of that PID,
+                    # which means that this PID does not belong to our component
+                    # (because it doesn't belong to any process), so we may just delete this pidfile safely ..
+                    os.remove(pidfile)
+
+                    # .. but, if the component is load-balancer, we also need to delete its agent's pidfile.
+                    # The assumption is that if the load-balancer is not running then so isn't its agent.
+                    if log_file_marker == 'lb-agent':
+                        lb_agent_pidfile = abspath(join(self.component_dir, 'zato-lb-agent.pid'))
+                        os.remove(lb_agent_pidfile)
+
+                else:
+                    #
+                    # This PID exists, but it still still possible that it belongs to another process
+                    # that took over a PID previously assigned to a Zato component,
+                    # in which case we can still delete the pidfile.
+                    #
+                    # We decide that a process is actually an already running Zato component if it has
+                    # opened log files that should belong that kind of component, as indicated by log_file_marker,
+                    # otherwise we assume this PID belongs to a completely different process and we can delete pidfile.
+                    #
+                    has_log = False
+                    has_lock = False
+
+                    log_path = abspath(join(self.component_dir, 'logs', '{}.log'.format(log_file_marker)))
+                    lock_path = abspath(join(self.component_dir, 'logs', '{}.lock'.format(log_file_marker)))
+
+                    for name in Process(pid).open_files():
+                        if name.path == log_path:
+                            has_log = True
+                        elif name.path == lock_path:
+                            has_lock = True
+
+                    # Both files exist - this is our component and it's running so we cannot continue
+                    if has_log and has_lock:
+                        raise Exception('Cannot proceed, found pidfile `{}`'.format(pidfile))
+
+                    # This must be an unrelated process, so we can delete pidfile ..
+                    os.remove(pidfile)
+
+                    # .. again, if the component is load-balancer, we also need to delete its agent's pidfile.
+                    # The assumption is that if the load-balancer is not running then so isn't its agent.
+                    if log_file_marker == 'lb-agent':
+                        lb_agent_pidfile = abspath(join(self.component_dir, 'zato-lb-agent.pid'))
+                        os.remove(lb_agent_pidfile)
 
         if self.show_output:
             self.logger.info('No such pidfile `%s`, OK', pidfile)
@@ -130,13 +199,13 @@ class CheckConfig(ManageCommand):
         self.on_server_check_kvdb(cm, conf)
 
         if getattr(args, 'ensure_no_pidfile', False):
-            self.ensure_no_pidfile()
+            self.ensure_no_pidfile('server')
 
         if getattr(args, 'check_server_port_available', False):
             self.on_server_check_port_available(conf)
 
     def _on_lb(self, *ignored_args, **ignored_kwargs):
-        self.ensure_no_pidfile()
+        self.ensure_no_pidfile('lb-agent')
         repo_dir = join(self.config_dir, 'repo')
 
         lba_conf = self.get_json_conf('lb-agent.conf')
@@ -169,7 +238,7 @@ class CheckConfig(ManageCommand):
             self.get_crypto_manager_conf(priv_key_location=join(repo_dir, 'web-admin-priv-key.pem'), repo_dir=repo_dir)[0],
             self.get_json_conf('web-admin.conf', repo_dir))
 
-        self.ensure_no_pidfile()
+        self.ensure_no_pidfile('web-admin')
         self.ensure_json_config_port_free('Web admin', 'web-admin.conf')
 
     def _on_scheduler(self, *ignored_args, **ignored_kwargs):
@@ -178,4 +247,4 @@ class CheckConfig(ManageCommand):
         self.check_sql_odb_server_scheduler(cm, conf)
         self.on_server_check_kvdb(cm, conf, 'broker')
 
-        self.ensure_no_pidfile()
+        self.ensure_no_pidfile('scheduler')
