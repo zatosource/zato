c673c7d1b3e81fdb8d44c8c103c38e82ebc85c11
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-11-27 20:36:37 +0100

    GH #755 - Dynamically update endpoint form for WSX, REST, SOAP and services.

--- code/zato-web-admin/src/zato/admin/web/forms/pubsub/endpoint.py
+++ code/zato-web-admin/src/zato/admin/web/forms/pubsub/endpoint.py
@@ -12,35 +12,50 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from django import forms
 
 # Zato
-from zato.admin.web.forms import add_select, add_security_select
+from zato.admin.web.forms import add_security_select, add_select, add_services
 from zato.common import PUBSUB
 
 # ################################################################################################################################
 
 class CreateForm(forms.Form):
+
+    # Common ones
     id = forms.CharField(widget=forms.HiddenInput())
     name = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'style':'width:100%'}))
     endpoint_type = forms.ChoiceField(widget=forms.Select())
     is_internal = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
     role = forms.ChoiceField(widget=forms.Select())
     topic_patterns = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%; height:120px'}))
-    security_id = forms.ChoiceField(widget=forms.Select())
-    ws_channel_id = forms.ChoiceField(widget=forms.Select())
-    sub_key = forms.CharField(widget=forms.HiddenInput())
 
-    def __init__(self, security_list=None, ws_channel_list=None, prefix=None, post_data=None, req=None):
+    # REST/SOAP
+    security_id = forms.ChoiceField(widget=forms.Select())
+
+    # REST
+    out_rest_id = forms.ChoiceField(widget=forms.Select())
+    out_rest_callback = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+
+    # Service
+    service_id = forms.ChoiceField(widget=forms.Select())
+
+    # SOAP
+    out_soap_id = forms.ChoiceField(widget=forms.Select())
+
+    # WebSockets
+    ws_channel_id = forms.ChoiceField(widget=forms.Select())
+
+    def __init__(self, req, data_list, prefix=None, post_data=None):
         super(CreateForm, self).__init__(post_data, prefix=prefix)
 
         self.fields['role'].choices = []
         self.fields['ws_channel_id'].choices = []
 
-        security_list = security_list or []
-        ws_channel_list = ws_channel_list or []
-
-        add_security_select(self, security_list, field_name='security_id', needs_no_security=False, needs_rbac=False)
-        add_select(self, 'ws_channel_id', ws_channel_list)
+        add_security_select(self, data_list.security_list, field_name='security_id', needs_no_security=False, needs_rbac=False)
+        add_select(self, 'out_rest_id', data_list.out_rest_list)
+        add_select(self, 'out_soap_id', data_list.out_soap_list)
+        add_select(self, 'ws_channel_id', data_list.ws_channel_list)
         add_select(self, 'role', PUBSUB.ROLE)
         add_select(self, 'endpoint_type', PUBSUB.ENDPOINT_TYPE, needs_initial_select=False)
+        add_services(self, req)
 
         # Let's assume the default type of pub/sub endpoint will be REST clients
         self.initial['endpoint_type'] = PUBSUB.ENDPOINT_TYPE.WEB_SOCKETS.id
