648a503a4ddfa5548e4c99e2f60ffcdb7620beb8
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-11-29 15:59:10 +0100

    GH #755 - Working on the subscriptions form.

--- code/zato-web-admin/src/zato/admin/web/forms/pubsub/subscription.py
+++ code/zato-web-admin/src/zato/admin/web/forms/pubsub/subscription.py
@@ -20,11 +20,31 @@ from zato.admin.web.forms import add_security_select, add_select
 class CreateForm(forms.Form):
 
     id = forms.CharField(widget=forms.HiddenInput())
-    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
     endpoint_type = forms.ChoiceField(widget=forms.Select())
     endpoint_id = forms.ChoiceField(widget=forms.Select())
-    topic_list_text = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%; height:120px'}))
-    topic_list_json = forms.CharField(widget=forms.Textarea(attrs={'display':'none'}))
+
+    active_status = forms.ChoiceField(widget=forms.Select())
+    is_staging_enabled = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+
+    delivery_batch_size = forms.CharField(widget=forms.TextInput(attrs={'style':'width:15%'}))
+    wrap_one_msg_in_list = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+
+    delivery_max_size = forms.CharField(widget=forms.TextInput(attrs={'style':'width:15%'}))
+
+    delivery_max_retry = forms.CharField(widget=forms.TextInput(attrs={'style':'width:15%'}))
+    delivery_err_should_block = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+
+    wait_sock_err = forms.CharField(widget=forms.TextInput(attrs={'style':'width:15%'}))
+    wait_non_sock_err = forms.CharField(widget=forms.TextInput(attrs={'style':'width:15%'}))
+
+    delivery_method = forms.ChoiceField(widget=forms.Select())
+    delivery_data_format = forms.ChoiceField(widget=forms.Select())
+
+    delivery_endpoint = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    out_http_soap_id = forms.ChoiceField(widget=forms.Select())
+    out_amqp_id = forms.ChoiceField(widget=forms.Select())
+
+    hook_service_id = forms.ChoiceField(widget=forms.Select())
 
     # REST/SOAP
     security_id = forms.ChoiceField(widget=forms.Select())
@@ -34,6 +54,9 @@ class CreateForm(forms.Form):
 
     # WebSockets
     ws_channel_id = forms.ChoiceField(widget=forms.Select())
+
+    topic_list_text = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%; height:120px'}))
+    topic_list_json = forms.CharField(widget=forms.Textarea(attrs={'display':'none'}))
 
     def __init__(self, req, data_list, *args, **kwargs):
         super(CreateForm, self).__init__(*args, **kwargs)
