72927fbe996950d75dd3c5f57e6336328f400726
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-12-04 23:32:49 +0100

    GH #755 - Release mmap/shmem when servers shut down.

--- code/zato-common/src/zato/common/posix_ipc_util.py
+++ code/zato-common/src/zato/common/posix_ipc_util.py
@@ -21,7 +21,7 @@ from zato.common import megabyte
 # ################################################################################################################################
 
 _shmem_pattern = '/zato-shmem-{}'
-_shmem_size = megabyte * 32
+_shmem_size = megabyte * 100
 
 # ################################################################################################################################
 
@@ -30,7 +30,13 @@ class SharedMemoryIPC(object):
     backed by shared memory. All data in shared memory is kept as a dictionary and serialized as JSON
     each time any read or write is needed.
     """
-    def __init__(self, shmem_suffix, size=500):
+    def __init__(self):
+        self.shmem_name = ''
+        self.size = -1
+
+    def create(self, shmem_suffix, size=_shmem_size):
+        """ Creates all IPC structures.
+        """
         self.shmem_name = _shmem_pattern.format(shmem_suffix)
         self.size = size
 
@@ -40,13 +46,12 @@ class SharedMemoryIPC(object):
         # Map memory to mmap
         self._mmap = mmap(self._mem.fd, self.size)
 
-        # We can close the shared memory's FD now that we have an mmap object
-        self._mem.close_fd()
-
         # Write initial data so that JSON .loads always succeeds
         self.store_initial()
 
     def store(self, data):
+        """ Serializes input data as JSON and stores it in RAM, overwriting any previous data.
+        """
         self._mmap.seek(0)
         self._mmap.write(dumps(data))
         self._mmap.flush()
@@ -60,15 +65,24 @@ class SharedMemoryIPC(object):
             self.store({})
 
     def load(self, needs_loads=True):
+        """ Reads in all data from RAM and, optionally, loads it as JSON.
+        """
         self._mmap.seek(0)
         data = self._mmap.read(self.size).strip('\x00')
         return loads(data) if needs_loads else data
 
     def close(self):
+        """ Closes all underlying in-RAM structures.
+        """
         self._mmap.close()
-        self._mem.unlink()
+        try:
+            self._mem.unlink()
+        except ipc.ExistentialError:
+            pass
 
     def get_parent(self, parent_path):
+        """ Returns element pointed to by parent_path, creating all elements along the way, if neccessary.
+        """
         data = self.load()
         parent_path = [elem for elem in parent_path.split('/') if elem]
 
@@ -81,7 +95,8 @@ class SharedMemoryIPC(object):
         return data, current
 
     def set_key(self, parent, key, value):
-
+        """ Set key to value under element called 'parent'.
+        """
         # Get parent to add our key to - will create it if needed
         data, parent = self.get_parent(parent)
 
@@ -96,8 +111,8 @@ class SharedMemoryIPC(object):
 class ServerStartupIPC(SharedMemoryIPC):
     """ A shared memory-backed IPC object for server startup initialization.
     """
-    def __init__(self, deployment_key):
-        super(ServerStartupIPC, self).__init__('startup-{}'.format(deployment_key))
+    def create(self, deployment_key):
+        super(ServerStartupIPC, self).create('startup-{}'.format(deployment_key))
 
     def set_pubsub_sub_key_pid(self, sub_key, pid):
         self.set_key('/pubsub/sub_key_to_pid', sub_key, pid)
