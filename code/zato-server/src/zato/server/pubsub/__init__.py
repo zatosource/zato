05a7a30276c281d737b0dc16749a1800639ff78e
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-12-07 00:47:43 +0100

    GH #755 - Invoke hook services before deliveries.

--- code/zato-server/src/zato/server/pubsub/__init__.py
+++ code/zato-server/src/zato/server/pubsub/__init__.py
@@ -175,50 +175,6 @@ class HookCtx(object):
 
 # ################################################################################################################################
 
-class Message(object):
-    """ Base container class for pub/sub message wrappers.
-    """
-    __slots__ = ('topic', 'sub_key', 'pub_msg_id', 'pub_correl_id', 'in_reply_to', 'ext_client_id', 'group_id',
-        'position_in_group', 'pub_time', 'data', 'mime_type', 'priority', 'expiration', 'expiration_time', 'has_gd',
-        'delivery_status', 'pattern_matched', 'size', 'published_by_id', 'topic_id', 'cluster_id', 'ext_pub_time',
-        'data_prefix', 'data_prefix_short')
-
-    def __init__(self):
-        self.topic = None
-        self.sub_key = None
-        self.pub_msg_id = None
-        self.pub_correl_id = None
-        self.in_reply_to = None
-        self.ext_client_id = None
-        self.group_id = None
-        self.position_in_group = None
-        self.pub_time = None
-        self.ext_pub_time = None
-        self.data = None
-        self.data_prefix = None
-        self.data_prefix_short = None
-        self.mime_type = None
-        self.priority = None
-        self.expiration = None
-        self.expiration_time = None
-        self.has_gd = None
-        self.delivery_status = None
-        self.pattern_matched = None
-        self.size = None
-        self.published_by_id = None
-        self.topic_id = None
-        self.cluster_id = None
-
-    def to_dict(self):
-        out = {}
-        for key in Message.__slots__:
-            if key != 'topic':
-                out[key] = getattr(self, key)
-
-        return out
-
-# ################################################################################################################################
-
 class SubKeyServer(object):
     """ Holds information about which server has subscribers to an individual sub_key.
     """
