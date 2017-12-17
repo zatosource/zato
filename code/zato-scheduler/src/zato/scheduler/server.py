76aa18d821e10d79bcba14525d3aa9ba5903e63c
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-12-18 00:15:37 +0100

    GH #773 - Make use of crypto_manager.decrypt for scheduler's connections to KVDB.

--- code/zato-scheduler/src/zato/scheduler/server.py
+++ code/zato-scheduler/src/zato/scheduler/server.py
@@ -56,16 +56,19 @@ class SchedulerServer(object):
     """ Main class spawning scheduler-related tasks and listening for HTTP API requests.
     """
     def __init__(self, config, repo_location):
+        self.odb = config.odb
         self.config = config
         self.repo_location = repo_location
         self.sql_pool_store = PoolStore()
 
+        # Set up the crypto manager that will be used by both ODB and, possibly, KVDB
+        self.config.crypto_manager = get_crypto_manager(self.repo_location, None, config.main, crypto_manager=CryptoManager())
+
         # ODB connection
         self.odb = ODBManager()
-        self.odb.crypto_manager = get_crypto_manager(self.repo_location, None, config.main, crypto_manager=CryptoManager())
 
         if self.config.main.odb.engine != 'sqlite':
-            self.config.main.odb.password = self.odb.crypto_manager.decrypt(config.main.odb.password)
+            self.config.main.odb.password = self.config.crypto_manager.decrypt(config.main.odb.password)
             self.config.main.odb.host = config.main.odb.host
             self.config.main.odb.pool_size = config.main.odb.pool_size
             self.config.main.odb.username = config.main.odb.username
@@ -81,6 +84,10 @@ class SchedulerServer(object):
 
         # API server
         self.api_server = WSGIServer((main.bind.host, int(main.bind.port)), self, keyfile=priv_key, certfile=cert)
+
+        self.odb.pool = self.sql_pool_store[ZATO_ODB_POOL_NAME].pool
+        self.odb.init_session(ZATO_ODB_POOL_NAME, self.config.main.odb, self.odb.pool, False)
+        self.config.odb = self.odb
 
         # Scheduler
         self.scheduler = Scheduler(self.config)
