2745523d9172cabbffa11f1437876216624aae34
David Wilson <dw@botanicus.net>
2018-02-06 01:25:09 +0545

    GH #818 - Fix _install-rhel.sh typo.
    Forgot "/tmp" after -P

--- code/_install-rhel.sh
+++ code/_install-rhel.sh
@@ -26,7 +26,7 @@ then
     )
 fi
 
-wget -P https://bootstrap.pypa.io/get-pip.py
+wget -P /tmp https://bootstrap.pypa.io/get-pip.py
 sudo $(type -p python2.7) /tmp/get-pip.py
 sudo $(type -p python2.7) -m pip install -U setuptools virtualenv==15.1.0
 
