464be6628e2d8c6cb75e7f130e4b0693b22b8711
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-02-10 18:28:12 +0100

    GH #825 - Moved password parameter names to zato.common.

--- code/zato-common/src/zato/common/__init__.py
+++ code/zato-common/src/zato/common/__init__.py
@@ -1000,6 +1000,12 @@ NO_DEFAULT_VALUE = 'NO_DEFAULT_VALUE'
 
 ZATO_INFO_FILE = b'.zato-info'
 
+class SECRETS:
+    # These parameters will be automatically encrypted in SimpleIO input
+    PARAMS = ('password', 'password1', 'password2', 'secret_key', 'auth_token')
+
+    PREFIX = 'zato.secf.' # Zato secret (Fernet)
+
 class path(object):
     def __init__(self, path, raise_on_not_found=False, ns='', text_only=False):
         self.path = path
