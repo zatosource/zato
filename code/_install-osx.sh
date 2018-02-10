69edb47258139516d4101395a1d6aa600d55f0a8
David Wilson <dw@botanicus.net>
2018-02-10 14:01:10 +0545

    GH #822 - Use Travis-built Python 2.7.

--- code/_install-osx.sh
+++ code/_install-osx.sh
@@ -11,8 +11,7 @@ brew install \
     libyaml openldap openssl ossp-uuid postgresql python2 swig \
     || true
 
-wget -P /tmp https://bootstrap.pypa.io/get-pip.py
-sudo /usr/local/bin/python2.7 /tmp/get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py | sudo /usr/local/bin/python2.7
 sudo /usr/local/bin/python2.7 -m pip install -U setuptools virtualenv==15.1.0
 
 /usr/local/bin/python2.7 -m virtualenv .
