e32c278a95ec67c6407aa7c342b5eb9ebe2cb251
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-02-14 16:35:34 +0100

    GH #820 - Added app_list validation to user signup.

--- code/zato-server/src/zato/server/service/internal/sso/signup/__init__.py
+++ code/zato-server/src/zato/server/service/internal/sso/signup/__init__.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (C) 2018, Zato Source s.r.o. https://zato.io
-
-Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
-"""
-
-# Zato
-from zato.common.util.sso import new_confirm_token
-from zato.server.service import List, Service
-
 '''
 # -*- coding: utf-8 -*-
 
@@ -56,10 +44,10 @@ class Validate(Service):
 
 # ################################################################################################################################
 
-    def _has_whitespace(self, data, _regexp=re.compile('\s', re.UNICODE), _multiline=re.MULTILINE):
+    def _has_whitespace(self, data, _regexp=re.compile(r'\s', re.MULTILINE | re.UNICODE)):
         """ Returns True if data contains ASCII whitespace of any sort.
         """
-        return _regexp.match(data, _multiline)
+        return _regexp.search(data)
 
 # ################################################################################################################################
 
@@ -148,9 +136,17 @@ class Validate(Service):
 
 # ################################################################################################################################
 
-    def _validate_app_list(self, session, sso_conf, app_list):
+    def _validate_app_list(self, session, sso_conf, current_app, app_list):
         """ Raises ValidationError if input app_list is invalid, e.g. includes an unknown one.
         """
+        # All of input apps must have been already defined in configuration
+        for app in app_list:
+            if app not in sso_conf.apps.all:
+                raise ValidationError(reason_code.app_list.invalid, sso_conf.signup.inform_if_app_invalid)
+
+        # Current app, the one the user is signed up through, must allow user signup
+        if current_app not in sso_conf.apps.signup_allowed:
+            raise ValidationError(reason_code.app_list.no_signup, sso_conf.signup.inform_if_app_invalid)
 
 # ################################################################################################################################
 
@@ -187,7 +183,7 @@ class SignUp(Service):
     """ Lets users sign up with the system.
     """
     class SimpleIO:
-        input_required = ('username', 'password', List('app_list'))
+        input_required = ('username', 'password', 'current_app', List('app_list'))
         input_optional = ('email', 'display_name', 'first_name', 'middle_name', 'last_name')
         output_optional = ('confirm_token', List('reason_code'),)
         encrypt_secrets = False
@@ -195,6 +191,11 @@ class SignUp(Service):
         skip_empty_keys = True
 
     def handle_POST(self):
+
+        # By default, this is always returned, no matter if successful or not, to prevent exploitation
+        # by attackers trying to find out if a given user/email exists or not.
+        if self.server.sso_config.signup.return_confirm_token:
+            self.response.payload.confirm_token = new_confirm_token()
 
         # Local shortcut
         input = self.request.input
@@ -207,17 +208,13 @@ class SignUp(Service):
             'username': input.username,
             'email': input.email,
             'password': input.password,
+            'current_app': input.current_app,
             'app_list': input.app_list,
         }, as_bunch=True)
 
         # Reason code list is returned only if validation failed and it was confirmed that we are safe to return it
         # so we can just assign it to payload unconditionally.
         self.response.payload.reason_code = validation_response.reason_code
-
-        # By default, this is always returned, no matter if successful or not, to prevent exploitation
-        # by attackers trying to find out if a given user/email exists or not.
-        if self.server.sso_config.signup.return_confirm_token:
-            self.response.payload.confirm_token = new_confirm_token()
 
 # ################################################################################################################################
 '''
