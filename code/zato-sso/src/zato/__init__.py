8c396a8b549c8b08f79175fc1007f3955a2009f3
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-02-19 19:45:12 +0100

    GH #820 - Brought zato-sso back.

---
+++ code/zato-sso/src/zato/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2018, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+__import__('pkg_resources').declare_namespace(__name__)
