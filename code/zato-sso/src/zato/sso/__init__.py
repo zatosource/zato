3cbf30d5a7e91e105729e2e1647c12b49b07b91d
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-02-24 20:41:48 +0100

    GH #820 - Removed an unused sub-status code.

--- code/zato-sso/src/zato/sso/__init__.py
+++ code/zato-sso/src/zato/sso/__init__.py
@@ -56,7 +56,6 @@ class status_code:
         locked                = 'E005002'
         invalid_signup_status = 'E005003'
         not_approved          = 'E005004'
-        no_such_user          = 'E005005'
 
     class metadata:
         not_allowed = 'E006001'
