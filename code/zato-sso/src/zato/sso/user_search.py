3758559375a8cf3a0f00fb4619abd217c7ea645c
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-03-07 13:21:01 +0100

    GH #820 - Added ORDER BY to user search.

--- code/zato-sso/src/zato/sso/user_search.py
+++ code/zato-sso/src/zato/sso/user_search.py
@@ -22,24 +22,111 @@ from zato.sso.odb.query import _user_basic_columns
 
 # ################################################################################################################################
 
-class search:
-    columns = []
-    asc = 'asc'
-    desc = 'desc'
+def query_wrapper(func):
+    """ A decorator for queries which works out whether a given query function should return the result only
+    or a column list retrieved in addition to the result. This is useful because some callers prefer the former
+    and some need the latter. Also, paginates the results if requested to by the caller.
+    """
+    @wraps(func)
+    def inner(*args, **kwargs):
 
-    class order_by:
+        # Each query function will have a keyword argument 'needs_columns' as the last one
+        # in its definition, e.g. needs_columns=False.
+        func_spec = getargspec(func)
+        needs_columns = func_spec.defaults[-1]
 
-        @staticmethod
-        def default():
-            return (
-                {'display_name': search.asc},
-                {'username': search.asc},
-                {'sign_up_time': search.asc},
-                {'user_id': search.asc},
+        tool = _SearchWrapper(func(*args), **kwargs)
+        result = _SearchResults(tool.q, tool.q.all(), tool.q.statement.columns, tool.total)
+
+        if needs_columns:
+            return result, result.columns
+
+        return result
+
+    return inner
+
+# ################################################################################################################################
+
+class SSOSearch(object):
+    """ SSO search functions, constants and defaults.
+    """
+    def __init__(self):
+        self.columns = []
+        self.order_by = self.OrderBy()
+
+# ################################################################################################################################
+
+
+    def set_up(self):
+        for column in _user_basic_columns:
+            self.columns.append(getattr(SSOUser, column.name))
+
+# ################################################################################################################################
+
+    class OrderBy(object):
+        """ Defaults for the SQL ORDER BY clause.
+        """
+        def __init__(self):
+            self.asc = 'asc'
+            self.desc = 'desc'
+
+            # All ORDER BY directions allowed
+            self.dir_allowed = set((self.asc, self.desc))
+
+            # All columns that results may be ordered by
+            self.columns_allowed = set(('display_name', 'username', 'sign_up_time', 'user_id'))
+
+            # How results will be sorted if no user-defined order is given
+            self.default = (
+                {'display_name': self.asc},
+                {'username': self.asc},
+                {'sign_up_time': self.asc},
+                {'user_id': self.asc},
             )
 
-for column in _user_basic_columns:
-    search.columns.append(getattr(SSOUser, column.name))
+# ################################################################################################################################
+
+    @query_wrapper
+    def sql_search_func(self, session, ignored_cluster_id, order_by, needs_columns=False):
+        return session.query(*Search.columns).\
+               order_by(*order_by)
+
+# ################################################################################################################################
+
+    def search(self, session, config):
+        """ Looks up users with the configuration given on input.
+        """
+        # Build the order by list, validating the input first to make sure
+        # only explicitly allowed columns and sort directions are used.
+
+        order_by = config.get('order_by')
+        order_by = order_by if order_by else self.order_by.default
+        _order_by = []
+
+        for item in order_by:
+            items = item.items()
+            if len(items) != 1 or len(items[0]) != 2:
+                raise ValueError('Invalid order_by config `{}`'.format(items))
+            else:
+                column, dir = items[0]
+
+                if column not in self.order_by.columns_allowed:
+                    raise ValueError('Invalid order_by column `{}`'.format(column))
+
+                if dir not in self.order_by.dir_allowed:
+                    raise ValueError('Invalid order_by dir `{}`'.format(dir))
+
+                # Columns and directions are valid, we can construct the ORDER BY clause now
+
+                func = asc if dir == self.order_by.asc else desc
+                _order_by.append(func(column))
+
+        return util_search(self.sql_search_func, config, [], session, None, _order_by)
+
+# ################################################################################################################################
+
+sso_search = SSOSearch()
+sso_search.set_up()
 
 # ################################################################################################################################
 
@@ -82,39 +169,6 @@ class _SearchWrapper(object):
 
 # ################################################################################################################################
 
-def query_wrapper(func):
-    """ A decorator for queries which works out whether a given query function should return the result only
-    or a column list retrieved in addition to the result. This is useful because some callers prefer the former
-    and some need the latter. Also, paginates the results if requested to by the caller.
-    """
-    @wraps(func)
-    def inner(*args, **kwargs):
-
-        # Each query function will have a keyword argument 'needs_columns' as the last one
-        # in its definition, e.g. needs_columns=False.
-        func_spec = getargspec(func)
-        needs_columns = func_spec.defaults[-1]
-
-        tool = _SearchWrapper(func(*args), **kwargs)
-        result = _SearchResults(tool.q, tool.q.all(), tool.q.statement.columns, tool.total)
-
-        if needs_columns:
-            return result, result.columns
-
-        return result
-
-    return inner
-
-# ################################################################################################################################
-
-@query_wrapper
-def search_func(session, cluster_id, order_by, needs_columns=False):
-    print(333, session, cluster_id, order_by, needs_columns)
-    return session.query(*search.columns).\
-           order_by('id')
-
-# ################################################################################################################################
-
 def sql_search(self, data, current_ust, current_app, remote_addr):
     """ Looks up users by specific search criteria from the 'data' dictionary.
     Must be called with a UST belonging to a super-user.
@@ -126,10 +180,8 @@ def sql_search(self, data, current_ust, current_app, remote_addr):
         'page_size': 33,
     }
 
-    order_by = search.order_by.default()
-
     with closing(self.odb_session_func()) as session:
-        out = util_search(search_func, config, [], session, None, order_by)
+        out = sso_search.search(session, config)
 
         print()
         print(out.num_pages)
@@ -142,7 +194,7 @@ class MyService(Service):
     def handle(self):
 
         username = 'admin1'
-        password = '******'
+        password = '****'
         session = self.sso.user.login(username, password, 'CRM', '127.0.0.1', 'my-user-agent', False, False)
 
         # Request metadata
