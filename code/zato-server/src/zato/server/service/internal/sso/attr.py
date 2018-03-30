eec9259db02b09bad5aad94709c0bfe0e158ea8b
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-03-30 15:36:28 +0200

    GH #820 - Added _AttrExists and _AttrNames.

--- code/zato-server/src/zato/server/service/internal/sso/attr.py
+++ code/zato-server/src/zato/server/service/internal/sso/attr.py
@@ -31,29 +31,8 @@ class _DataElem(object):
 class _AttrBase(object):
     """ Utility base class for attribute-related services.
     """
-    def get_api_call_data(self, cid, ctx, api_name, logger):
+    _result_elem = None
 
-        if ctx.input.name is not _invalid:
-            func_name = api_name
-            data_elem_name = 'name'
-            data_elem_value = ctx.input.name
-        elif ctx.input.data is not _invalid:
-            func_name = '{}_many'.format(api_name)
-            data_elem_name = 'data'
-            data_elem_value = ctx.input.data
-        else:
-            logger.info('Could not find input in `name` nor `data`')
-            raise ValidationError(status_code.common.invalid_input)
-
-        user = self.sso.user.get_user_by_id(cid, ctx.input.user_id, ctx.input.ust, ctx.input.current_app, ctx.remote_addr)
-        func = getattr(user.attr, func_name)
-        return _DataElem(func, data_elem_name, data_elem_value)
-
-# ################################################################################################################################
-
-class _Attr(_AttrBase, BaseRESTService):
-    """ Handles access to most of attribute-related REST APIs.
-    """
     class SimpleIO(BaseSIO):
         input_required = ('ust', 'current_app')
         input_optional = (AsIs('user_id'), 'name', 'value', Opaque('data'), Bool('decrypt'), Bool('serialize_dt'),
@@ -64,10 +43,35 @@ class _Attr(_AttrBase, BaseRESTService):
 
 # ################################################################################################################################
 
-    def _modify_sso_attr(self, ctx, api_name, needs_encrypt=True, needs_expiration=True):
-        """ A common function for all calls modifying attributes.
+    def get_api_call_data(self, cid, ctx, api_name, logger, needs_input):
+
+        if needs_input:
+            if ctx.input.name is not _invalid:
+                func_name = api_name
+                data_elem_name = 'name'
+                data_elem_value = ctx.input.name
+            elif ctx.input.data is not _invalid:
+                func_name = '{}_many'.format(api_name)
+                data_elem_name = 'data'
+                data_elem_value = ctx.input.data
+            else:
+                logger.info('Could not find input in `name` nor `data`')
+                raise ValidationError(status_code.common.invalid_input)
+        else:
+            func_name = api_name
+            data_elem_name = None
+            data_elem_value = None
+
+        user = self.sso.user.get_user_by_id(cid, ctx.input.user_id, ctx.input.ust, ctx.input.current_app, ctx.remote_addr)
+        func = getattr(user.attr, func_name)
+        return _DataElem(func, data_elem_name, data_elem_value)
+
+# ################################################################################################################################
+
+    def _access_sso_attr(self, ctx, api_name, needs_encrypt=True, needs_expiration=True, needs_result=False, needs_input=True):
+        """ A common function for most calls accessing attributes.
         """
-        call_data = self.get_api_call_data(self.cid, ctx, api_name, self.logger)
+        call_data = self.get_api_call_data(self.cid, ctx, api_name, self.logger, needs_input)
 
         kwargs = {
             call_data.elem_name: call_data.elem_value,
@@ -83,45 +87,54 @@ class _Attr(_AttrBase, BaseRESTService):
             kwargs['value'] = ctx.input.value
 
         try:
-            call_data.func(**kwargs)
+            result = call_data.func(**(kwargs if needs_input else {}))
         except Exception:
             self.logger.warn(format_exc())
             raise ValidationError(status_code.common.invalid_input)
+        else:
+            if needs_result:
+                self.response.payload.result = result
+
+# ################################################################################################################################
+
+class _Attr(_AttrBase, BaseRESTService):
+    """ Handles access to most of attribute-related REST APIs.
+    """
 
 # ################################################################################################################################
 
     def _handle_sso_POST(self, ctx):
         """ Creates a new attribute.
         """
-        self._modify_sso_attr(ctx, 'create')
+        self._access_sso_attr(ctx, 'create')
 
 # ################################################################################################################################
 
     def _handle_sso_PATCH(self, ctx):
         """ Updates an existing attribute.
         """
-        self._modify_sso_attr(ctx, 'update')
+        self._access_sso_attr(ctx, 'update')
 
 # ################################################################################################################################
 
     def _handle_sso_PUT(self, ctx):
         """ Creates a new or updates an existing attribute.
         """
-        self._modify_sso_attr(ctx, 'set')
+        self._access_sso_attr(ctx, 'set')
 
 # ################################################################################################################################
 
     def _handle_sso_DELETE(self, ctx):
         """ Deletes an existing attribute.
         """
-        self._modify_sso_attr(ctx, 'delete', False, False)
+        self._access_sso_attr(ctx, 'delete', False, False)
 
 # ################################################################################################################################
 
     def _handle_sso_GET(self, ctx):
         """ Returns data of and metadata about an attribute.
         """
-        call_data = self.get_api_call_data(self.cid, ctx, 'get', self.logger)
+        call_data = self.get_api_call_data(self.cid, ctx, 'get', self.logger, True)
         decrypt = ctx.input.decrypt
         decrypt = True if (decrypt is _invalid or decrypt == '') else ctx.input.decrypt
 
@@ -154,12 +167,18 @@ class _Attr(_AttrBase, BaseRESTService):
 
 # ################################################################################################################################
 
-class _AttrExists(BaseRESTService):
-    pass
+class _AttrExists(_AttrBase, BaseRESTService):
+    """ Checks if an attribute or attributes given on input actually exist(s).
+    """
+    def _handle_sso_GET(self, ctx):
+        self._access_sso_attr(ctx, 'exists', False, False, True)
 
 # ################################################################################################################################
 
-class _AttrNames(BaseRESTService):
-    pass
+class _AttrNames(_AttrBase, BaseRESTService):
+    """ Returns names of all attributes defined.
+    """
+    def _handle_sso_GET(self, ctx):
+        self._access_sso_attr(ctx, 'names', False, False, True, False)
 
 # ################################################################################################################################
