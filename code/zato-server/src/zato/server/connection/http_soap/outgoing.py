3abe7e1c393a918e3a00df937fd73a3988756ae7
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-05-01 14:39:02 +0200

    GH #755 - Removing debug calls.

--- code/zato-server/src/zato/server/connection/http_soap/outgoing.py
+++ code/zato-server/src/zato/server/connection/http_soap/outgoing.py
@@ -336,9 +336,8 @@ class HTTPSOAPWrapper(BaseHTTPSOAPWrapper):
         if isinstance(data, unicode):
             data = data.encode('utf-8')
 
-        #logger.info(
-        #    'CID:`%s`, address:`%s`, qs:`%s`, auth_user:`%s`, kwargs:`%s`', cid, address, qs_params, self.username, kwargs)
-        #logger.info('..')
+        logger.info(
+            'CID:`%s`, address:`%s`, qs:`%s`, auth_user:`%s`, kwargs:`%s`', cid, address, qs_params, self.username, kwargs)
 
         response = self.invoke_http(cid, method, address, data, headers, {}, params=qs_params, *args, **kwargs)
 
