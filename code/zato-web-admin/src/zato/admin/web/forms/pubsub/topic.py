b722c8382241eb872a7b4354fd7dabdee3d2bc36
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-05-08 18:43:36 +0200

    GH #755 - Adding delivery task sync options.

--- code/zato-web-admin/src/zato/admin/web/forms/pubsub/topic.py
+++ code/zato-web-admin/src/zato/admin/web/forms/pubsub/topic.py
@@ -21,17 +21,27 @@ class CreateForm(forms.Form):
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
     has_gd = forms.BooleanField(required=False, widget=forms.CheckboxInput())
     is_api_sub_allowed = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    hook_service_id = forms.ChoiceField(widget=forms.Select())
+
     max_depth_gd = forms.CharField(widget=forms.TextInput(
         attrs={'class':'required', 'style':'width:20%'}), initial=PUBSUB.DEFAULT.TOPIC_MAX_DEPTH_GD)
     max_depth_non_gd = forms.CharField(widget=forms.TextInput(
         attrs={'class':'required', 'style':'width:20%'}), initial=PUBSUB.DEFAULT.TOPIC_MAX_DEPTH_NON_GD)
+
     depth_check_freq = forms.CharField(widget=forms.TextInput(
-        attrs={'class':'required', 'style':'width:20%'}), initial=PUBSUB.DEFAULT.DEPTH_CHECK_FREQ)
+        attrs={'class':'required', 'style':'width:15%'}), initial=PUBSUB.DEFAULT.DEPTH_CHECK_FREQ)
+
     pub_buffer_size_gd = forms.CharField(widget=forms.TextInput(
         attrs={'class':'required', 'style':'width:20%'}), initial=PUBSUB.DEFAULT.PUB_BUFFER_SIZE_GD)
+
     pub_buffer_size_non_gd = forms.CharField(widget=forms.TextInput(
         attrs={'class':'required', 'style':'width:20%'}), initial=PUBSUB.DEFAULT.PUB_BUFFER_SIZE_NON_GD)
-    hook_service_id = forms.ChoiceField(widget=forms.Select())
+
+    deliv_task_sync_interv_gd = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'required', 'style':'width:20%'}), initial=PUBSUB.DEFAULT.DELIV_TASK_SYNC_INTERVAL_GD)
+
+    deliv_task_sync_interv_non_gd = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'required', 'style':'width:20%'}), initial=PUBSUB.DEFAULT.DELIV_TASK_SYNC_INTERVAL_NON_GD)
 
     def __init__(self, req, *args, **kwargs):
         super(CreateForm, self).__init__(*args, **kwargs)
