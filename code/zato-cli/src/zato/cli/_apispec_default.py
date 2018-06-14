d8c842f721fc2e2393e12e10d450bc03a8bde8c0
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-06-14 19:35:14 +0200

    GH #841 - Working on default Sphinx files.

--- code/zato-cli/src/zato/cli/_apispec_default.py
+++ code/zato-cli/src/zato/cli/_apispec_default.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2018, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 # All APISpec-related files that will be created for each server
 apispec_files = {}
