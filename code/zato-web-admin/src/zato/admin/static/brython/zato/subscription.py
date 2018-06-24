0dfaad3d1b2e0d5a06b94a4a6b1cc317c908272b
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-06-24 11:09:06 +0200

    GH #847 - Brought Brython back for pub/sub forms.

---
+++ code/zato-web-admin/src/zato/admin/static/brython/zato/subscription.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2018, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+from json import loads
+
+# Brython
+from browser import document as doc, window
+
+# ################################################################################################################################
+
+row_prefix = 'endpoint_row_id_'
+
+# ################################################################################################################################
