5fe6f35db4a8c28164dad0c67f8f2e721531b7a0
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-06-25 19:53:52 +0200

    GH #874 - Use Python built from source.

--- code/_install-rhel.sh
+++ code/_install-rhel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PYTHON_VER="2.7.13"
-PYTHON_URL="https://travis-ci-integration.s3.amazonaws.com/python27/python27.tar.bz2"
-PYTHON_PREFIX="/opt/zato/python/$PYTHON_VER"
-PATH="$PYTHON_PREFIX/bin:$PATH"
+#PYTHON_VER="2.7.13"
+#PYTHON_URL="https://travis-ci-integration.s3.amazonaws.com/python27/python27.tar.bz2"
+#PYTHON_PREFIX="/opt/zato/python/$PYTHON_VER"
+#PATH="$PYTHON_PREFIX/bin:$PATH"
 
 sudo yum -y install \
     bzip2 bzip2-devel curl cyrus-sasl-devel gcc-c++ git haproxy \
@@ -12,15 +12,15 @@ sudo yum -y install \
     openssl-devel patch postgresql-devel python-devel suitesparse swig uuid \
     uuid-devel wget
 
-if ! [ "$(type -p python2.7)" ]
-then
-    # CentOS 6.x requires python2.7 build.
-    curl "$PYTHON_URL" | sudo tar -C / -jx
-fi
+#if ! [ "$(type -p python2.7)" ]
+#then
+#    # CentOS 6.x requires python2.7 build.
+#    curl "$PYTHON_URL" | sudo tar -C / -jx
+#fi
 
-curl https://bootstrap.pypa.io/get-pip.py | sudo $(type -p python2.7)
-sudo $(type -p python2.7) -m pip install -U setuptools virtualenv==15.1.0
+curl https://bootstrap.pypa.io/get-pip.py | sudo /opt/zato/code/bin/python2.7
+sudo /opt/zato/code/bin/python2.7 -m pip install -U setuptools virtualenv==15.1.0
 
-python2.7 -m virtualenv .
+/opt/zato/code/bin/python2.7 -m virtualenv .
 source ./bin/activate
 source ./_postinstall.sh
