8a782fef406a378b186b6ec1744d95f71c39ee86
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-09-03 18:37:28 +0200

    GH #839 - Mass-updating copyright year.

--- code/zato-web-admin/src/zato/admin/web/views/search/es.py
+++ code/zato-web-admin/src/zato/admin/web/views/search/es.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2013 Dariusz Suchojad <dsuch at zato.io>
+Copyright (C) 2018, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
