0d19fc754da3fde83e73312222341ca39b50893d
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-10-25 11:25:14 +0200

    GH #755 - Log response details too.

--- code/zato-server/src/zato/server/connection/web_socket/__init__.py
+++ code/zato-server/src/zato/server/connection/web_socket/__init__.py
@@ -573,7 +573,8 @@ class WebSocket(_WebSocket):
 
         serialized = response.serialize()
 
-        logger.info('Sending response %s', serialized)
+        logger.info('Sending response `%s` to `%s` `%s` `%s` `%s`', serialized,
+            self.pub_client_id, self.ext_client_id, self.ext_client_name, self.peer_conn_info_pretty)
 
         try:
             self.send(serialized)
