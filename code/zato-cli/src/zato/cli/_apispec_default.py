38f02dae52d0133f68f27de799ec2f7312bb8326
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-11-23 10:12:49 +0100

    GH #841 - Manually copy download files to _build/html/_downloads.

--- code/zato-cli/src/zato/cli/_apispec_default.py
+++ code/zato-cli/src/zato/cli/_apispec_default.py
@@ -293,6 +293,8 @@ help:
 # "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
 %: Makefile
     @$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+    mkdir -p ./$(BUILDDIR)/html/_downloads
+    cp -p ./download/* ./$(BUILDDIR)/html/_downloads
 
 """.lstrip().replace('    ', '\t')
 
