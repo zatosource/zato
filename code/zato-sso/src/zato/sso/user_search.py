46855784073a68eda288710da7bfbd518a7c8dda
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-01-11 20:45:46 +0100

    GH #706 - Python 3 syntax cleanup in zato-sso.

--- code/zato-sso/src/zato/sso/user_search.py
+++ code/zato-sso/src/zato/sso/user_search.py
@@ -12,6 +12,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from sqlalchemy import asc, desc
 from sqlalchemy.sql import and_ as sql_and, or_ as sql_or
 
+# Python 2/3 compatibility
+from past.builtins import basestring
+
 # Zato
 from zato.common.odb.model import SSOUser
 from zato.common.odb.query import query_wrapper
