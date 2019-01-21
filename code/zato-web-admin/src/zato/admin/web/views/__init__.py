132e76328ced774b6198df917f085c215ca338db
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-01-21 12:20:10 +0100

    GH #755 - Working on GUI dict values.

--- code/zato-web-admin/src/zato/admin/web/views/__init__.py
+++ code/zato-web-admin/src/zato/admin/web/views/__init__.py
@@ -282,7 +282,7 @@ class Index(_BaseView):
     """ A base class upon which other index views are based.
     """
     url_name = 'url_name-must-be-defined-in-a-subclass'
-    template = 'template-must-be-defined-in-a-subclass'
+    template = 'template-must-be-defined-in-a-subclass-or-get-template-name'
 
     output_class = None
 
@@ -324,6 +324,11 @@ class Index(_BaseView):
     def get_initial_input(self):
         return {}
 
+    def get_template_name(self):
+        """ May be overridden by subclasses to dynamically decide which template to use,
+        otherwise self.template will be employed.
+        """
+
     def invoke_admin_service(self):
         if self.req.zato.get('cluster'):
             func = self.req.zato.client.invoke_async if self.async_invoke else self.req.zato.client.invoke
@@ -337,10 +342,6 @@ class Index(_BaseView):
         and fills it in with received attributes.
         """
         names = tuple(chain(self.SimpleIO.output_required, self.SimpleIO.output_optional))
-
-        #print()
-        #print(222, item_list)
-        #print()
 
         for msg_item in item_list:
             item = self.output_class()
@@ -423,7 +424,7 @@ class Index(_BaseView):
 
             return_data = self.handle_return_data(return_data)
 
-            return TemplateResponse(req, self.template, return_data)
+            return TemplateResponse(req, self.get_template_name() or self.template, return_data)
 
         except Exception, e:
             return HttpResponseServerError(format_exc(e))
