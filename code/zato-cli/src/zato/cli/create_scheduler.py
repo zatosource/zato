405d9fc4597b300e31fd978467325469ca6bc2ab
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-01-24 18:14:36 +0100

    GH #755 - Brought back zato.pubsub.cleanup scheduler task.

--- code/zato-cli/src/zato/cli/create_scheduler.py
+++ code/zato-cli/src/zato/cli/create_scheduler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2018, Zato Source s.r.o. https://zato.io
+Copyright (C) 2019, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
@@ -129,6 +129,11 @@ is_extra_list=True
 [zato.wsx.cleanup]
 minutes=30
 service=pub.zato.channel.web-socket.cleanup-wsx
+
+[zato.pubsub.cleanup]
+minutes=5
+service=zato.pubsub.cleanup-service
+extra=
 """
 
 # ################################################################################################################################
