745c15d4ce6d1a141bac0e0de471193e528d5ec4
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-01-29 06:18:37 +0100

    GH #755 - Added update.sh.

---
+++ code/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURDIR="${BASH_SOURCE[0]}";RL="readlink";([[ `uname -s`=='Darwin' ]] || RL="$RL -f")
+while([ -h "${CURDIR}" ]) do CURDIR=`$RL "${CURDIR}"`; done
+N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
+
+echo "Downloading updates"
+git -C $CURDIR pull
+
+echo "Installing updates"
+$CURDIR/bin/pip install -e $CURDIR/zato-cy
+$CURDIR/bin/pip install -r $CURDIR/requirements.txt
