a2cfbe99354a63c5c849b10dfeb0c1a79bc6989a
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-02-07 22:24:19 +0100

    GH #706 - Mass-updating copyright year.

--- code/zato-server/src/zato/server/pattern/parallel.py
+++ code/zato-server/src/zato/server/pattern/parallel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2015 Dariusz Suchojad <dsuch at zato.io>
+Copyright (C) 2019, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
