64c5875e412e8807043eea409aed4887a945b263
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-02-07 22:24:19 +0100

    GH #706 - Mass-updating copyright year.

--- code/zato-cli/src/zato/cli/zato_command.py
+++ code/zato-cli/src/zato/cli/zato_command.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2018, Zato Source s.r.o. https://zato.io
+Copyright (C) 2019, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
