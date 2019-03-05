18fd3c0cd8e77a6f2879963a45d071169b18e1c2
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-03-05 17:37:41 +0100

    GH #225 - Working on LDAP connections.

---
+++ code/zato-web-admin/src/zato/admin/static/js/outgoing/ldap.js
@@ -0,0 +1,99 @@
+
+// /////////////////////////////////////////////////////////////////////////////
+
+$.fn.zato.data_table.LDAP = new Class({
+    toString: function() {
+        var s = '<LDAP id:{0} name:{1} is_active:{2}';
+        return String.format(s, this.id ? this.id : '(none)',
+                                this.name ? this.name : '(none)',
+                                this.is_active ? this.is_active : '(none)');
+    }
+});
+
+// /////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function() {
+    $('#data-table').tablesorter();
+    $.fn.zato.data_table.password_required = true;
+    $.fn.zato.data_table.class_ = $.fn.zato.data_table.LDAP;
+    $.fn.zato.data_table.new_row_func = $.fn.zato.outgoing.ldap.data_table.new_row;
+    $.fn.zato.data_table.parse();
+    $.fn.zato.data_table.setup_forms(['name', 'address', 'security_def']);
+})
+
+$.fn.zato.outgoing.ldap.create = function() {
+    $.fn.zato.data_table._create_edit('create', 'Create a new outgoing LDAP connection', null);
+}
+
+$.fn.zato.outgoing.ldap.edit = function(id) {
+    $.fn.zato.data_table._create_edit('edit', 'Update the outgoing LDAP connection', id);
+}
+
+$.fn.zato.outgoing.ldap.data_table.new_row = function(item, data, include_tr) {
+    var row = '';
+
+    if(include_tr) {
+        row += String.format("<tr id='tr_{0}' class='updated'>", item.id);
+    }
+
+    var is_active = item.is_active == true;
+    var is_zato = item.is_zato == true;
+    var has_auto_reconnect = item.has_auto_reconnect == true;
+
+    row += "<td class='numbering'>&nbsp;</td>";
+    row += "<td class='impexp'><input type='checkbox' /></td>";
+    row += String.format('<td>{0}</td>', item.name);
+    row += String.format('<td>{0}</td>', is_active ? 'Yes' : 'No');
+    row += String.format('<td>{0}</td>', item.address);
+    row += String.format('<td>{0}</td>', is_zato ? 'Yes' : 'No');
+
+    if(data.on_connect_service_name) {
+        row += String.format('<td>{0}</td>',
+            String.format("<a href='/zato/service/overview/{0}/?cluster={1}'>{0}</a>",
+            data.on_connect_service_name, item.cluster_id));
+    }
+    else {
+        row += $.fn.zato.empty_table_cell;
+    }
+
+    if(data.on_message_service_name) {
+        row += String.format('<td>{0}</td>',
+            String.format("<a href='/zato/service/overview/{0}/?cluster={1}'>{0}</a>",
+            data.on_message_service_name, item.cluster_id));
+    }
+    else {
+        row += $.fn.zato.empty_table_cell;
+    }
+
+    if(data.on_close_service_name) {
+        row += String.format('<td>{0}</td>',
+            String.format("<a href='/zato/service/overview/{0}/?cluster={1}'>{0}</a>",
+            data.on_close_service_name, item.cluster_id));
+    }
+    else {
+        row += $.fn.zato.empty_table_cell;
+    }
+
+    row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.outgoing.ldap.edit('{0}')\">Edit</a>", item.id));
+    row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.outgoing.ldap.delete_({0});'>Delete</a>", item.id));
+    row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
+    row += String.format("<td class='ignore'>{0}</td>", is_active);
+    row += String.format("<td class='ignore'>{0}</td>", is_zato);
+    row += String.format("<td class='ignore'>{0}</td>", item.on_connect_service_id);
+    row += String.format("<td class='ignore'>{0}</td>", item.security_def);
+    row += String.format("<td class='ignore'>{0}</td>", item.subscription_list);
+    row += String.format("<td class='ignore'>{0}</td>", has_auto_reconnect);
+
+    if(include_tr) {
+        row += '</tr>';
+    }
+
+    return row;
+}
+
+$.fn.zato.outgoing.ldap.delete_ = function(id) {
+    $.fn.zato.data_table.delete_(id, 'td.item_id_',
+        'Outgoing LDAP connection `{0}` deleted',
+        'Are you sure you want to delete outgoing LDAP connection `{0}`?',
+        true);
+}
