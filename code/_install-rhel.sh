45c5285d1842c9f6d393eaf848becbc94d21f4b8
Anielkis Herrera <aherrera@zato.io>
2019-03-09 22:35:28 -0500

    GH #921 - pip install setuptools fails in some environments

--- code/_install-rhel.sh
+++ code/_install-rhel.sh
@@ -24,9 +24,9 @@ fi
 curl https://bootstrap.pypa.io/get-pip.py | sudo $(type -p $PY_BINARY)
 
 if [[ "$EUID" -ne 0 ]];then
-  sudo $(type -p $PY_BINARY) -m pip install -U setuptools virtualenv==15.1.0
+  sudo $(type -p $PY_BINARY) -m pip install -U virtualenv==15.1.0
 else
-  $(type -p $PY_BINARY) -m pip install -U setuptools virtualenv==15.1.0
+  $(type -p $PY_BINARY) -m pip install -U virtualenv==15.1.0
 fi
 
 $PY_BINARY -m virtualenv .
