c9cee9d06f505d645e1e4413a64ccffbf811488f
Anielkis Herrera <aherrera@zato.io>
2019-03-09 21:15:57 -0500

    GH #921 - Do not use sudo if not required (docker containers runs commands as root)

--- code/_install-alpine.sh
+++ code/_install-alpine.sh
@@ -13,7 +13,14 @@ sudo apk add \
     patch postgresql-dev python2 python2-dev suitesparse swig wget yaml-dev
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo $PY_BINARY
-sudo $PY_BINARY -m pip install -U setuptools virtualenv==15.1.0
+if [[ $PY_BINARY != python2* ]];then
+  PY_V=3
+fi
+if [[ "$EUID" -ne 0 ]];then
+  sudo pip${PY_V} install -U setuptools virtualenv==15.1.0
+else
+  pip${PY_V} install -U setuptools virtualenv==15.1.0
+fi
 
 $PY_BINARY -m virtualenv .
 source ./bin/activate
