ea3c43a4d32f4c21566f47237b06414d61bf7d3e
Anielkis Herrera <aherrera@zato.io>
2019-03-11 21:45:38 -0500

    GH #921 - Some rollbacks

--- code/_install-alpine.sh
+++ code/_install-alpine.sh
@@ -13,12 +13,7 @@ sudo apk add \
     patch postgresql-dev python2 python2-dev suitesparse swig wget yaml-dev
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo $PY_BINARY
-
-if [[ "$EUID" -ne 0 ]];then
-  sudo $(type -p $PY_BINARY) -m pip install -U virtualenv==15.1.0
-else
-  $(type -p $PY_BINARY) -m pip install -U virtualenv==15.1.0
-fi
+sudo $PY_BINARY -m pip install -U setuptools virtualenv==15.1.0
 
 $PY_BINARY -m virtualenv .
 source ./bin/activate
