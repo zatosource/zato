ee2c4460932d4fcf1fb361b250eefdf5986dd33d
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-03-17 11:17:47 +0100

    GH #924 - Added GUI forms for Slack connections.

--- code/zato-web-admin/src/zato/admin/static/js/outgoing/im/slack.js
+++ code/zato-web-admin/src/zato/admin/static/js/outgoing/im/slack.js
@@ -16,20 +16,20 @@ $(document).ready(function() {
     $('#data-table').tablesorter();
     $.fn.zato.data_table.password_required = true;
     $.fn.zato.data_table.class_ = $.fn.zato.data_table.Slack;
-    $.fn.zato.data_table.new_row_func = $.fn.zato.outgoing.slack.data_table.new_row;
+    $.fn.zato.data_table.new_row_func = $.fn.zato.outgoing.im.slack.data_table.new_row;
     $.fn.zato.data_table.parse();
-    $.fn.zato.data_table.setup_forms(['name', 'token', 'http_proxy_list', 'https_proxy_list']);
+    $.fn.zato.data_table.setup_forms(['name']);
 })
 
-$.fn.zato.outgoing.slack.create = function() {
+$.fn.zato.outgoing.im.slack.create = function() {
     $.fn.zato.data_table._create_edit('create', 'Create a new outgoing Slack connection', null);
 }
 
-$.fn.zato.outgoing.slack.edit = function(id) {
+$.fn.zato.outgoing.im.slack.edit = function(id) {
     $.fn.zato.data_table._create_edit('edit', 'Update the outgoing Slack connection', id);
 }
 
-$.fn.zato.outgoing.slack.data_table.new_row = function(item, data, include_tr) {
+$.fn.zato.outgoing.im.slack.data_table.new_row = function(item, data, include_tr) {
     var row = '';
 
     if(include_tr) {
@@ -47,9 +47,9 @@ $.fn.zato.outgoing.slack.data_table.new_row = function(item, data, include_tr) {
     row += String.format('<td>{0}</td>', item.http_proxy_list ? item.http_proxy_list : $.fn.zato.empty_value);
     row += String.format('<td>{0}</td>', item.https_proxy_list ? item.https_proxy_list : $.fn.zato.empty_value);
 
-    row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.data_table.change_password({0})'>Change token</a>", item.id));
-    row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.outgoing.slack.edit('{0}')\">Edit</a>", item.id));
-    row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.outgoing.slack.delete_({0});'>Delete</a>", item.id));
+    row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.data_table.change_password(\"{0}\", \"{1}\")'>Change token</a>", item.id, 'Change token'));
+    row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.outgoing.im.slack.edit('{0}')\">Edit</a>", item.id));
+    row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.outgoing.im.slack.delete_({0});'>Delete</a>", item.id));
     row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.data_table.ping({0});'>Ping</a>", item.id));
 
     row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
@@ -66,7 +66,7 @@ $.fn.zato.outgoing.slack.data_table.new_row = function(item, data, include_tr) {
     return row;
 }
 
-$.fn.zato.outgoing.slack.delete_ = function(id) {
+$.fn.zato.outgoing.im.slack.delete_ = function(id) {
     $.fn.zato.data_table.delete_(id, 'td.item_id_',
         'Outgoing Slack connection `{0}` deleted',
         'Are you sure you want to delete outgoing Slack connection `{0}`?',
