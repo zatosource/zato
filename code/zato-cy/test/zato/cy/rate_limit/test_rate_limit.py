bb381743b1d5ad3e23f92742fe343122d8d36e5e
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-04-13 21:33:22 +0200

    GH #945 - Working on DefinitionParser class.

--- code/zato-cy/test/zato/cy/rate_limit/test_rate_limit.py
+++ code/zato-cy/test/zato/cy/rate_limit/test_rate_limit.py
@@ -10,13 +10,111 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from unittest import main as unittest_main, TestCase
 
 # ################################################################################################################################
+# ################################################################################################################################
 
 class RateLimitTestCace(TestCase):
     pass
 
+# ################################################################################################################################
 # ################################################################################################################################
 
 if __name__ == '__main__':
     unittest_main()
 
 # ################################################################################################################################
+
+'''
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2019, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+# Python 2/3 compatibility
+from past.builtins import unicode
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class DefinitionItem(object):
+    __slots__ = 'config_line', 'from_', 'rate', 'unit'
+
+    def __init__(self):
+        self.config_line = None # type: int
+        self.from_ = None # type: unicode
+        self.rate = None  # type: int
+        self.unit = None  # type: unicode
+
+    def __repr__(self):
+        return '<{} at {}; cl:{}, f:{}, r:{}, u:{}>'.format(self.__class__.__name__, hex(id(self)), self.config_line,
+            self.from_, self.rate, self.unit)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class DefinitionParser(object):
+    """ Parser for user-provided rate limiting definitions.
+    """
+    def _get_lines(self, definition):
+        # type: (unicode) -> list
+
+        out = []
+
+        for idx, line in enumerate(definition.splitlines(), 1): # type: int, unicode
+            line = line.strip()
+
+            if (not line) or line.startswith('#'):
+                continue
+
+            line = line.split('=')
+            from_, rate_info = line # type: unicode, unicode
+
+            from_ = from_.strip()
+            rate_info = rate_info.strip()
+
+            rate, unit = rate_info.split('/') # type: unicode, unicode
+
+            rate = int(rate.strip())
+            unit = unit.strip()
+
+            item = DefinitionItem()
+            item.config_line = idx
+            item.from_ = from_
+            item.rate = rate
+            item.unit = unit
+
+            out.append(item)
+
+        return out
+
+    def parse(self, definition):
+        # type: (unicode)
+
+        lines = self._get_lines(definition.strip())
+
+        print(111, lines)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if __name__ == '__main__':
+
+    definition = """
+    * = 1/m
+    * = 2/h
+    * = 3/d
+
+    192.168.1.123 = 11/m
+    #10.210.0.0/18 = 22/h
+    127.0.0.1/32  = 33/d
+    """
+
+    dp = DefinitionParser()
+    dp.parse(definition)
+
+# ################################################################################################################################
+'''
