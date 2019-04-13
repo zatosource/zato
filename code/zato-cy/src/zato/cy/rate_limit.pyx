dc672340b74e903561d082d263fbc4e846a75fcc
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-04-13 19:34:55 +0200

    GH #945 - Added rate limiting to zato-cy.

---
+++ code/zato-cy/src/zato/cy/rate_limit.pyx
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2019, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# stdlib
+from datetime import datetime
+from logging import getLogger
+
+# ################################################################################################################################
+
+logger = getLogger(__name__)
+
+# ################################################################################################################################
