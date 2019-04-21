e474f168d3b60fcd035915c1b0ba2085e4596568
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-04-21 15:16:43 +0200

    GH #945 - Provide more details in RateLimitReached exception.

--- code/zato-common/src/zato/common/rate_limiting/common.py
+++ code/zato-common/src/zato/common/rate_limiting/common.py
@@ -54,17 +54,21 @@ class ObjectInfo(object):
 # ################################################################################################################################
 
 class DefinitionItem(object):
-    __slots__ = 'config_line', 'from_', 'rate', 'unit'
+    __slots__ = 'config_line', 'from_', 'rate', 'unit', 'object_id', 'object_type', 'object_name'
 
     def __init__(self):
         self.config_line = None # type: int
         self.from_ = None # type: object
         self.rate = None  # type: int
         self.unit = None  # type: unicode
+        self.object_id = None   # type: int
+        self.object_type = None # type: unicode
+        self.object_name = None # type: unicode
 
     def __repr__(self):
-        return '<{} at {}; line:{}, from:{}, rate:{}, unit:{}>'.format(self.__class__.__name__, hex(id(self)), self.config_line,
-            self.from_, self.rate, self.unit)
+        return '<{} at {}; line:{}, from:{}, rate:{}, unit:{} ({} {} {})>'.format(
+            self.__class__.__name__, hex(id(self)), self.config_line, self.from_, self.rate, self.unit,
+            self.object_id, self.object_name, self.object_type)
 
 # ################################################################################################################################
 # ################################################################################################################################
