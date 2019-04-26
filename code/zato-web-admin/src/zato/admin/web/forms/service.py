c100a2b522c200dab5aec7e08ae4d0fe164e75ae
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-04-26 21:43:42 +0200

    GH #945 - Default rate_limit_check_parent_def to True.

--- code/zato-web-admin/src/zato/admin/web/forms/service.py
+++ code/zato-web-admin/src/zato/admin/web/forms/service.py
@@ -18,10 +18,11 @@ class CreateForm(WithJSONSchema, WithRateLimiting):
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
     slow_threshold = forms.CharField(widget=forms.TextInput(attrs={'style':'width:15%'}))
     is_rate_limit_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    rate_limit_check_parent_def = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
 
 class EditForm(CreateForm):
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'style':'text-align:left'}))
-    rate_limit_check_parent_def = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    rate_limit_check_parent_def = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
 
 class WSDLUploadForm(UploadForm):
     pass
