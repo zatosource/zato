aeee3f58c74a0b2cbf227c1279217939dc02f8c4
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-04-26 16:58:14 +0200

    GH #945 - Always encrypt all secrets.

--- code/zato-server/src/zato/server/base/parallel/config.py
+++ code/zato-server/src/zato/server/base/parallel/config.py
@@ -10,10 +10,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # stdlib
 import os
+from contextlib import closing
 from logging import getLogger
 
 # Zato
 from zato.bunch import Bunch
+from zato.common import SECRETS
 from zato.common.util import asbool
 from zato.common.util.sql import elems_with_opaque
 from zato.common.util.url_dispatcher import get_match_target
@@ -313,6 +315,9 @@ class ConfigLoader(object):
         query = self.odb.get_xpath_sec_list(server.cluster.id, True)
         self.config.xpath_sec = ConfigDict.from_query('xpath_sec', query, decrypt_func=self.decrypt)
 
+        # Encrypt all secrets
+        self._encrypt_secrets()
+
         #
         # Security - end
         #
@@ -457,6 +462,45 @@ class ConfigLoader(object):
         odb_data.is_active = True
 
         return odb_data
+
+# ################################################################################################################################
+
+    def _encrypt_secrets(self):
+        """ All passwords are always encrypted so we need to look up any that are not,
+        for instance, because it is a cluster newly migrated from 2.0 to 3.0, and encrypt them now in ODB.
+        """
+        sec_config_dict_types = ('apikey', 'aws', 'basic_auth', 'jwt', 'ntlm', 'oauth', 'openstack_security',
+            'tls_key_cert', 'wss', 'vault_conn_sec', 'xpath_sec')
+
+        # Global lock to make sure only one server attempts to do it at a time
+        with self.zato_lock_manager('zato_encrypt_secrets'):
+
+            # An SQL session shared by all updates
+            with closing(self.odb.session()) as session:
+
+                # Iterate over all security definitions
+                for sec_config_dict_type in sec_config_dict_types:
+                    config_dicts = getattr(self.config, sec_config_dict_type)
+                    for config in config_dicts.values():
+                        config = config['config']
+
+                        # Continue to encryption only if needed and not already encrypted
+                        if config.get('_encryption_needed'):
+                            if not config['_encrypted_in_odb']:
+                                odb_func = getattr(self.odb, '_migrate_30_encrypt_sec_{}'.format(sec_config_dict_type))
+
+                                # Encrypt all params that are applicable
+                                for secret_param in SECRETS.PARAMS:
+                                    if secret_param in config:
+                                        encrypted = self.encrypt(config[secret_param])
+                                        odb_func(session, config['id'], secret_param, encrypted)
+
+                        # Clean up config afterwards
+                        config.pop('_encryption_needed', None)
+                        config.pop('_encrypted_in_odb', None)
+
+                # Commit to SQL now that all updates are made
+                session.commit()
 
 # ################################################################################################################################
 
