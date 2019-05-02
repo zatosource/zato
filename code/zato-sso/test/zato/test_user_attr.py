126d9193774950f08560cb39d464e121d699b66e
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-05-02 22:03:12 +0200

    GH #820 - Adding tests for UserAttr.create_many.

--- code/zato-sso/test/zato/test_user_attr.py
+++ code/zato-sso/test/zato/test_user_attr.py
@@ -73,7 +73,7 @@ class UserAttrTestCase(BaseTest):
 
 # ################################################################################################################################
 
-    def test_create_another_user_super_user(self):
+    def test_create_for_another_user_by_super_user(self):
 
         username = self._get_random_username()
         password = self._get_random_data()
@@ -103,7 +103,7 @@ class UserAttrTestCase(BaseTest):
 
 # ################################################################################################################################
 
-    def test_create_another_user_regular_user(self):
+    def test_create_for_another_user_by_regular_user(self):
 
         username1 = self._get_random_username()
         password1 = self._get_random_data()
@@ -167,6 +167,109 @@ class UserAttrTestCase(BaseTest):
         })
 
         self.assertEqual(response.status, status_code.ok)
+
+# ################################################################################################################################
+
+    def test_create_many_already_exists(self):
+
+        data = [
+            {'name': self._get_random_data(), 'value': self._get_random_data()},
+            {'name': self._get_random_data(), 'value': self._get_random_data()},
+        ]
+
+        response1 = self.post('/zato/sso/user/attr', {
+            'ust': self.ctx.super_user_ust,
+            'user_id': self.ctx.super_user_id,
+            'data': data,
+        })
+
+        response2 = self.post('/zato/sso/user/attr', {
+            'ust': self.ctx.super_user_ust,
+            'user_id': self.ctx.super_user_id,
+            'data': data,
+        }, False)
+
+        self.assertEqual(response1.status, status_code.ok)
+        self.assertEqual(response2.status, status_code.error)
+        self.assertListEqual(response2.sub_status, [status_code.attr.already_exists])
+
+# ################################################################################################################################
+
+    def test_create_many_for_another_user_by_super_user(self):
+
+        username = self._get_random_username()
+        password = self._get_random_data()
+
+        response = self.post('/zato/sso/user', {
+            'ust': self.ctx.super_user_ust,
+            'username': username,
+            'password': password,
+        })
+
+        user_id = response.user_id
+        self._approve(user_id)
+
+        data = [
+            {'name': self._get_random_data(), 'value': self._get_random_data()},
+            {'name': self._get_random_data(), 'value': self._get_random_data()},
+        ]
+
+        response = self.post('/zato/sso/user/attr', {
+            'ust': self.ctx.super_user_ust,
+            'user_id': user_id,
+            'data': data,
+        })
+
+        self.assertEqual(response.status, status_code.ok)
+
+# ################################################################################################################################
+
+    def test_create_for_another_user_by_regular_user(self):
+
+        username1 = self._get_random_username()
+        password1 = self._get_random_data()
+
+        username2 = self._get_random_username()
+        password2 = self._get_random_data()
+
+        response1 = self.post('/zato/sso/user', {
+            'ust': self.ctx.super_user_ust,
+            'username': username1,
+            'password': password1,
+        })
+
+        response2 = self.post('/zato/sso/user', {
+            'ust': self.ctx.super_user_ust,
+            'username': username2,
+            'password': password2,
+        })
+
+        user_id1 = response1.user_id
+        self._approve(user_id1)
+
+        user_id2 = response2.user_id
+        self._approve(user_id2)
+
+        response = self.post('/zato/sso/user/login', {
+            'username': username1,
+            'password': password1,
+        })
+
+        ust = response.ust
+
+        data = [
+            {'name': self._get_random_data(), 'value': self._get_random_data()},
+            {'name': self._get_random_data(), 'value': self._get_random_data()},
+        ]
+
+        response = self.post('/zato/sso/user/attr', {
+            'ust': ust,
+            'user_id': user_id2,
+            'data': data
+        }, False)
+
+        self.assertEqual(response.status, status_code.error)
+        self.assertEqual(response.sub_status, [status_code.auth.not_allowed])
 
 # ################################################################################################################################
 # ################################################################################################################################
