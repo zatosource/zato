62bae6aac73935ec1ead1818334be661d9be0927
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-05-19 10:18:58 +0200

    GH #755 - Pyflakes cleanup.

--- code/zato-sso/test/zato/test_linked_auth.py
+++ code/zato-sso/test/zato/test_linked_auth.py
@@ -9,7 +9,6 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # stdlib
-from datetime import datetime
 from unittest import main
 
 # Zato
@@ -22,7 +21,7 @@ class LinkedAuthTestCase(BaseTest):
 
     def test_create(self):
 
-        response = self.get('/sso/user/linked', {
+        self.get('/sso/user/linked', {
             'ust': self.ctx.super_user_ust,
         })
 
@@ -33,46 +32,3 @@ if __name__ == '__main__':
     main()
 
 # ################################################################################################################################
-
-'''
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (C) 2019, Zato Source s.r.o. https://zato.io
-
-Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
-"""
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-# Zato
-from zato.server.service.internal.sso import BaseRESTService, BaseSIO
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class LinkedAuth(BaseRESTService):
-
-    name = 'sso.user.linked-auth'
-
-    class SimpleIO(BaseSIO):
-        input_required = 'ust', 'current_app'
-        output_required = 'status',
-
-# ################################################################################################################################
-
-    def _handle_sso_GET(self, ctx):
-        self.logger.warn('GET %s', ctx.input)
-
-        user = self.sso.user.get_current_user(self.cid, ctx.input.ust, ctx.input.current_app, ctx.remote_addr)
-
-        print(111, user.to_dict())
-
-# ################################################################################################################################
-
-    def _handle_sso_POST(self, ctx):
-        self.logger.warn('POST %s', ctx)
-
-# ################################################################################################################################
-# ################################################################################################################################
-'''
