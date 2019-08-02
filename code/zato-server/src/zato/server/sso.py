137717c2efed198011a4134b2d995acda201b13f
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-08-02 16:13:22 +0200

    GH #820 - Moving common functionality to sso.py.

---
+++ code/zato-server/src/zato/server/sso.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2019, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Zato
+from zato.common import RATE_LIMIT, SEC_DEF_TYPE
+
+# ################################################################################################################################
+
+# Type checking
+if 0:
+    from zato.server.base.parallel import ParallelServer
+
+    ParallelServer = ParallelServer
+
+# ################################################################################################################################
+
+# Definitions of these security types may be linked to SSO users and their rate limiting definitions
+_sec_def_sso_rate_limit = SEC_DEF_TYPE.BASIC_AUTH, SEC_DEF_TYPE.JWT
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class SSOTool(object):
+    """ Server-wide utilities related to SSO.
+    """
+    def __init__(self, server):
+        # type: (ParallelServer)
+        self.server = server
+
+    def log_in_external(self, sec, cid, wsgi_environ, _rate_limit_type_sso_user=RATE_LIMIT.OBJECT_TYPE.SSO_USER,
+        _basic_auth=SEC_DEF_TYPE.BASIC_AUTH):
+
+        if sec.sec_def.sec_type in _sec_def_sso_rate_limit:
+
+            # Do we have an SSO user related to this sec_def?
+            auth_id_link_map = self._sso_api_user.auth_id_link_map['zato.{}'.format(
+                sec.sec_def.sec_type)] # type: dict
+
+            sso_user_id = auth_id_link_map.get(sec.sec_def.id)
+
+            if sso_user_id:
+
+                # At this point we have an SSO user and we know that credentials
+                # from the request were valid so we may check rate-limiting
+                # first and then create or extend the user's associated SSO session.
+                # In other words, we can already act as though the user was already
+                # logged in because in fact he or she is logged in, just using
+                # a security definition from sec_def.
+
+                # Check rate-limiting
+                self.server.rate_limiting.check_limit(cid, _rate_limit_type_sso_user,
+                    sso_user_id, wsgi_environ['zato.http.remote_addr'], False)
+
+                # Rate-limiting went fine, we can now create or extend
+                # the person's SSO session linked to credentials from the request.
+
+                current_app = wsgi_environ.get(self.server.sso_config.apps.http_header) or \
+                    self.server.sso_config.apps.default
+
+                session_info = self.server.sso_api.user.session.on_external_auth_succeeded(
+                    cid,
+                    sec.sec_def,
+                    sso_user_id,
+                    sec.sec_def if sec.sec_def.sec_type == _basic_auth else wsgi_environ['HTTP_AUTHORIZATION'],
+                    current_app,
+                    wsgi_environ['zato.http.remote_addr'],
+                    wsgi_environ.get('HTTP_USER_AGENT'),
+                )
+
+                if session_info:
+                    wsgi_environ['zato.http.response.headers']['X-Zato-SSO-UST'] = self.server.encrypt(
+                        session_info.ust, _prefix='')
+
+# ################################################################################################################################
+# ################################################################################################################################
