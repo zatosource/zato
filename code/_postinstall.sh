527a660b10bd13b3d3eb987e2ee8820a1f6383b2
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-09-23 22:44:35 +0200

    GH #278 - Forking outbox to its own ext module.

--- code/_postinstall.sh
+++ code/_postinstall.sh
@@ -74,10 +74,6 @@ echo "$(pwd)/zato_extra_paths" >> eggs/easy-install.pth
 # Apply patches.
 patch -p0 -d eggs < patches/butler/__init__.py.diff
 patch -p0 -d eggs < patches/configobj.py.diff
-patch -p0 -d eggs < patches/outbox/outbox.py.diff
-patch -p0 -d eggs < patches/outbox/outbox.py2.diff
-patch -p0 -d eggs < patches/outbox/outbox.py3.diff
-patch -p0 -d eggs < patches/outbox/outbox.py4.diff
 patch -p0 -d eggs < patches/psycopg2/__init__.py.diff --forward || true
 patch -p0 -d eggs < patches/redis/redis/connection.py.diff
 patch -p0 -d eggs < patches/requests/models.py.diff
