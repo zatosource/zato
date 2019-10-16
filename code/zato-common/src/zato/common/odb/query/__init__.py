d20c2dea7972a94d0fbcd604530873ac9f083dd2
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-10-16 20:49:41 +0200

    GH #298 - Correctly handle RBAC edits.

--- code/zato-common/src/zato/common/odb/query/__init__.py
+++ code/zato-common/src/zato/common/odb/query/__init__.py
@@ -799,12 +799,17 @@ def _service(session, cluster_id):
         filter(Cluster.id==cluster_id).\
         order_by(Service.name)
 
-def service(session, cluster_id, id):
+def service(session, cluster_id, id=None, name=None):
     """ A service.
     """
-    return _service(session, cluster_id).\
-        filter(Service.id==id).\
-        one()
+    q = _service(session, cluster_id)
+
+    if name:
+        q = q.filter(Service.name==name)
+    elif id:
+        q = q.filter(Service.id==id)
+
+    return q.one()
 
 @query_wrapper
 def service_list(session, cluster_id, return_internal=True, needs_columns=False):
@@ -1393,12 +1398,17 @@ def _rbac_permission(session, cluster_id):
         filter(Cluster.id==RBACPermission.cluster_id).\
         order_by(RBACPermission.name)
 
-def rbac_permission(session, cluster_id, id):
+def rbac_permission(session, cluster_id, id=None, name=None):
     """ An RBAC permission.
     """
-    return _rbac_permission(session, cluster_id).\
-        filter(RBACPermission.id==id).\
-        one()
+    q = _rbac_permission(session, cluster_id)
+
+    if name:
+        q = q.filter(RBACPermission.name==name)
+    elif id:
+        q = q.filter(RBACPermission.id==id)
+
+    return q.one()
 
 @query_wrapper
 def rbac_permission_list(session, cluster_id, needs_columns=False):
@@ -1416,12 +1426,17 @@ def _rbac_role(session, cluster_id):
         outerjoin(rbac_parent, rbac_parent.id==RBACRole.parent_id).\
         order_by(RBACRole.name)
 
-def rbac_role(session, cluster_id, id):
+def rbac_role(session, cluster_id, id=None, name=None):
     """ An RBAC role.
     """
-    return _rbac_role(session, cluster_id).\
-        filter(RBACRole.id==id).\
-        one()
+    q = _rbac_role(session, cluster_id)
+
+    if name:
+        q = q.filter(RBACRole.name==name)
+    elif id:
+        q = q.filter(RBACRole.id==id)
+
+    return q.one()
 
 @query_wrapper
 def rbac_role_list(session, cluster_id, needs_columns=False):
