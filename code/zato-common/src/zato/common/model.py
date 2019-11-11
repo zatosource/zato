5e026551fd581a2dc77cde096ee35641d5d3e540
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-11-11 15:35:42 +0100

    GH #999 - Working on the model for FTP channels.

--- code/zato-common/src/zato/common/model.py
+++ code/zato-common/src/zato/common/model.py
@@ -10,3 +10,31 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # Unlike zato.common.odb.model - this place is where DB-independent models are kept,
 # regardless if they're backed by an SQL database or not.
+
+'''
+* Name
+* Address (ftp:// or ftps://) = ftp://0.0.0.0:21021
+* Is active = True
+* Max connections = 300
+* Max connections per IP = 20
+* Command timeout = 300
+* Banner = 'Welcome'
+* Log prefix = '%(remote_ip)s:%(remote_port)s-[%(username)s]'
+* Base directory = './work/ftp'
+* TLS certificate
+* Read throttle = 10
+* Write throttle = 10
+* Masquerade address
+* Passive ports
+* Log level - INFO / DEBUG = INFO
+* Service
+* Service invocation mode sync / async = async (not visible in web-admin)
+'''
+
+class FTPChannel(object):
+    def __init__(self):
+        self.id   = None            # type: int
+        self.name = None            # type: str
+        self.is_active = None       # type: bool
+        self.max_connections = None # type: int
+        self.max_conn_per_ip = None # type: int
