cbfcf346265c0214fc100b018a0a45cbe1d3a89c
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-11-15 21:10:40 +0100

    GH #999 - Working on FTP channels.

--- code/zato-web-admin/src/zato/admin/web/forms/channel/ftp.py
+++ code/zato-web-admin/src/zato/admin/web/forms/channel/ftp.py
@@ -13,22 +13,35 @@ from django import forms
 
 # Zato
 from zato.admin.web.forms import add_services, DataFormatForm
-from zato.common import ZMQ
+from zato.common import FTP
 
 class CreateForm(DataFormatForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
     address = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
-    socket_type = forms.ChoiceField(widget=forms.Select())
-    socket_method = forms.ChoiceField(widget=forms.Select())
     service = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
-    sub_key = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
-    pool_strategy = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:20%'}))
-    service_source = forms.ChoiceField(widget=forms.Select())
+
+    max_connections = forms.CharField(widget=forms.TextInput(attrs={'style':'width:20%'}))
+    max_conn_per_ip = forms.CharField(widget=forms.TextInput(attrs={'style':'width:20%'}))
+    command_timeout = forms.CharField(widget=forms.TextInput(attrs={'style':'width:20%'}))
+
+    banner = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    log_prefix = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    base_directory = forms.CharField(widget=forms.TextInput(attrs={'style':'width:50%'}))
+
+    read_throttle = forms.CharField(widget=forms.TextInput(attrs={'style':'width:20%'}))
+    write_throttle = forms.CharField(widget=forms.TextInput(attrs={'style':'width:20%'}))
+
+    masq_address = forms.CharField(widget=forms.TextInput(attrs={'style':'width:50%'}))
+    passive_ports = forms.CharField(widget=forms.TextInput(attrs={'style':'width:50%'}))
+
+    log_level = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
+    srv_invoke_mode = forms.HiddenInput()
 
     def __init__(self, prefix=None, post_data=None, req=None):
         super(CreateForm, self).__init__(post_data, prefix=prefix)
 
+        '''
         self._add_field('socket_type', ZMQ.CHANNEL)
         self._add_field('socket_method', ZMQ.METHOD)
         self._add_field('pool_strategy', ZMQ.POOL_STRATEGY)
@@ -40,6 +53,7 @@ class CreateForm(DataFormatForm):
         self.fields[field_name].choices = []
         for code, name in source.items():
             self.fields[field_name].choices.append([code, name])
+    '''
 
 class EditForm(CreateForm):
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
