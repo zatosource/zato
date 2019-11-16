6eae4482576583808614481b8954d51cb1c229e7
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-11-16 11:36:36 +0100

    GH #999 - Working on FTP channel templates.

--- code/zato-web-admin/src/zato/admin/web/views/channel/ftp.py
+++ code/zato-web-admin/src/zato/admin/web/views/channel/ftp.py
@@ -19,16 +19,16 @@ from zato.common.model import FTPChannel
 class Index(_Index):
     method_allowed = 'GET'
     url_name = 'channel-ftp'
-    template = 'zato/channel/ftp.html'
+    template = 'zato/channel/ftp/index.html'
     service_name = 'channel.ftp.get-list'
     output_class = FTPChannel
     paginate = True
 
     class SimpleIO(_Index.SimpleIO):
         input_required = 'cluster_id',
-        output_required = 'id',  'name',  'is_active',  'max_connections',  'max_conn_per_ip',  'command_timeout', \
-            'banner',  'log_prefix',  'base_directory',  'read_throttle',  'write_throttle',  'masq_address', \
-            'passive_ports',  'log_level',  'service_name', 'srv_invoke_mode'
+        output_required = 'id', 'name', 'is_active', 'max_connections', 'max_conn_per_ip', 'command_timeout', \
+           'banner', 'log_prefix', 'base_directory', 'read_throttle', 'write_throttle', 'masq_address', \
+           'passive_ports', 'log_level', 'service_name', 'srv_invoke_mode'
         output_repeated = True
 
     def handle(self):
@@ -62,3 +62,31 @@ class Delete(_Delete):
     url_name = 'channel-ftp-delete'
     error_message = 'FTP channel could not be deleted'
     service_name = 'channel.ftp.delete'
+
+'''
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2019, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Zato
+from zato.server.service import Int, Service
+
+class GetList(Service):
+    name = 'channel.ftp.get-list'
+
+    class SimpleIO:
+        input_required = 'cluster_id',
+        output_required = 'id',  'name',  'is_active',  Int('max_connections'),  Int('max_conn_per_ip'),  \
+            Int('command_timeout'), 'banner',  'log_prefix',  'base_directory',  'read_throttle',  'write_throttle', \
+            'masq_address', 'passive_ports', 'log_level',  'service_name', 'srv_invoke_mode'
+        output_repeated = True
+
+    def handle(self):
+        pass
+'''
