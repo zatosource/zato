34f990fcfa5c8b2e709f5d3c65bf4a1864e100f6
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-12-11 21:55:44 +0100

    GH #1003 - Renaming to file transfer.

--- code/zato-web-admin/src/zato/admin/web/forms/channel/file_.py
+++ code/zato-web-admin/src/zato/admin/web/forms/channel/file_.py
@@ -13,27 +13,17 @@ from django import forms
 
 # Zato
 from zato.admin.web.forms import add_select, add_services, add_topics
-from zato.common import SFTP
-
-_default = SFTP.CHANNEL.DEFAULT
 
 class CreateForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
-    address = forms.CharField(widget=forms.TextInput(attrs={'style':'width:35%'}), initial=_default.ADDRESS)
 
     service_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
     topic_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
 
-    users_allowed = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%;height:70px'}))
-    sftp_command = forms.CharField(widget=forms.TextInput(attrs={'style':'width:22%'}), initial=_default.SFTP_COMMAND)
-
-    config_file = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%;height:70px'}), initial=_default.CONFIG)
-
     def __init__(self, prefix=None, post_data=None, req=None):
         super(CreateForm, self).__init__(post_data, prefix=prefix)
-        add_services(self, req)
-        add_topics(self, req, by_id=False)
+
 
 class EditForm(CreateForm):
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
