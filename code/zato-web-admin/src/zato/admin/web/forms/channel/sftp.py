af65ecc7065e2dd019c799317387bb97be92147d
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-12-11 21:27:10 +0100

    GH #999 - Replacing dropbear with sshd.

--- code/zato-web-admin/src/zato/admin/web/forms/channel/sftp.py
+++ code/zato-web-admin/src/zato/admin/web/forms/channel/sftp.py
@@ -20,16 +20,15 @@ _default = SFTP.CHANNEL.DEFAULT
 class CreateForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
-    address = forms.CharField(widget=forms.TextInput(attrs={'style':'width:22%'}), initial=_default.ADDRESS)
+    address = forms.CharField(widget=forms.TextInput(attrs={'style':'width:35%'}), initial=_default.ADDRESS)
 
     service_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
     topic_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
 
-    idle_timeout = forms.CharField(widget=forms.TextInput(attrs={'style':'width:10%'}), initial=_default.IDLE_TIMEOUT)
-    keep_alive_timeout = forms.CharField(widget=forms.TextInput(attrs={'style':'width:10%'}), initial=_default.KEEP_ALIVE_TIMEOUT)
+    users_allowed = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%;height:70px'}))
+    sftp_command = forms.CharField(widget=forms.TextInput(attrs={'style':'width:22%'}), initial=_default.SFTP_COMMAND)
 
-    host_key = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
-    sftp_command = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}), initial=_default.SFTP_COMMAND)
+    config_file = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%;height:70px'}), initial=_default.CONFIG)
 
     def __init__(self, prefix=None, post_data=None, req=None):
         super(CreateForm, self).__init__(post_data, prefix=prefix)
