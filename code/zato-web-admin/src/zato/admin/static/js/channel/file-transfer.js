c5ce52d3895986117b410ab6ebed96c181061ae4
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-12-15 11:39:19 +0100

    GH #1003 - Working on channel forms.

--- code/zato-web-admin/src/zato/admin/static/js/channel/file-transfer.js
+++ code/zato-web-admin/src/zato/admin/static/js/channel/file-transfer.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     $.fn.zato.data_table.class_ = $.fn.zato.data_table.ChannelFileTransfer;
     $.fn.zato.data_table.new_row_func = $.fn.zato.channel.file_transfer.data_table.new_row;
     $.fn.zato.data_table.parse();
-    $.fn.zato.data_table.setup_forms(['name', 'address', 'idle_timeout', 'keep_alive_timeout', 'sftp_command']);
+    $.fn.zato.data_table.setup_forms(['name', 'pickup_from', 'file_patterns']);
 })
 
 $.fn.zato.channel.file_transfer.create = function() {
@@ -55,30 +55,33 @@ $.fn.zato.channel.file_transfer.data_table.new_row = function(item, data, includ
     // 1
     row += String.format('<td>{0}</td>', item.name);
     row += String.format('<td>{0}</td>', is_active ? 'Yes' : 'No');
-    row += String.format('<td>{0}</td>', item.address);
 
     // 2
     row += String.format('<td>{0}</td>', service_link);
-    row += String.format('<td>{0}</td>', topic_link);
-    row += String.format('<td>{0}</td>', item.host_key ? item.host_key : $.fn.zato.empty_value);
+    row += String.format('<td>{0}</td>', item.pickup_from ? item.pickup_from : $.fn.zato.empty_value);
+    row += String.format('<td>{0}</td>', item.file_patterns);
 
     // 3
+    row += String.format('<td>{0}</td>', item.move_processed_to ? item.move_processed_to : $.fn.zato.empty_value);
     row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.channel.file_transfer.edit('{0}')\">Edit</a>", item.id));
     row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.channel.file_transfer.delete_({0});'>Delete</a>", item.id));
-    row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
 
     // 4
-    row += String.format("<td class='ignore'>{0}</td>", is_active);
-    row += String.format("<td class='ignore'>{0}</td>", item.service_name);
-    row += String.format("<td class='ignore'>{0}</td>", item.topic_name);
+    row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
+    row += String.format('<td>{0}</td>', item.is_active);
+    row += String.format('<td>{0}</td>', item.service_list);
 
     // 5
-    row += String.format("<td class='ignore'>{0}</td>", item.idle_timeout);
-    row += String.format("<td class='ignore'>{0}</td>", item.keep_alive_timeout);
-    row += String.format("<td class='ignore'>{0}</td>", item.sftp_command);
+    row += String.format('<td>{0}</td>', item.topic_list);
+    row += String.format('<td>{0}</td>', item.read_on_pickup);
 
     // 6
-    row += String.format("<td class='ignore'>{0}</td>", item.host_key);
+    row += String.format('<td>{0}</td>', item.parse_on_pickup);
+    row += String.format('<td>{0}</td>', item.parse_with);
+    row += String.format('<td>{0}</td>', item.delete_after_pickup);
+
+    // 7
+    row += String.format('<td>{0}</td>', item.is_internal);
 
     if(include_tr) {
         row += '</tr>';
--- code/zato-web-admin/src/zato/admin/web/forms/channel/file_transfer.py
+++ code/zato-web-admin/src/zato/admin/web/forms/channel/file_transfer.py
@@ -13,13 +13,26 @@ from django import forms
 
 # Zato
 from zato.admin.web.forms import add_select, add_services, add_topics
+from zato.common import FILE_TRANSFER
+
+_default = FILE_TRANSFER.DEFAULT
 
 class CreateForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
 
-    service_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:80%', 'class':'multirow'}))
-    topic_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:80%', 'class':'multirow'}))
+    service_list = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:87%', 'class':'multirow'}))
+    topic_list = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:87%', 'class':'multirow'}))
+
+    pickup_from = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    move_processed_to = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+
+    file_patterns = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}), initial=_default.FILE_PATTERNS)
+    parse_with = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+
+    read_on_pickup = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+    parse_on_pickup = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+    delete_after_pickup = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
 
     def __init__(self, prefix=None, post_data=None, req=None):
         super(CreateForm, self).__init__(post_data, prefix=prefix)
@@ -28,4 +41,7 @@ class CreateForm(forms.Form):
 
 class EditForm(CreateForm):
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    read_on_pickup = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    parse_on_pickup = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    delete_after_pickup = forms.BooleanField(required=False, widget=forms.CheckboxInput())
 
