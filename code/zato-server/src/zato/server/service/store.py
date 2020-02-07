2f9648ee5ca1a56bae3110d999c9465a0a7bad23
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-02-07 14:54:34 +0100

    GH #1018 - In Zato 3.2+ after_add_to_store accepts 'server' objects on input.

--- code/zato-server/src/zato/server/service/store.py
+++ code/zato-server/src/zato/server/service/store.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from functools import total_ordering
 from hashlib import sha256
 from importlib import import_module
-from inspect import getmodule, getmro, getsourcefile, isclass
+from inspect import getargspec, getmodule, getmro, getsourcefile, isclass
 from pickle import HIGHEST_PROTOCOL as highest_pickle_protocol
 from shutil import copy as shutil_copy
 from traceback import format_exc
@@ -55,16 +55,17 @@ from zato.server.service.internal import AdminService
 
 # ################################################################################################################################
 
-# Type checking
-import typing
+if 0:
 
-if typing.TYPE_CHECKING:
+    # stdlib
+    from inspect import ArgSpec
 
     # Zato
     from zato.common.odb.api import ODBManager
     from zato.server.base.parallel import ParallelServer
 
     # For pyflakes
+    ArgSpec = ArgSpec
     ConfigDict = ConfigDict
     ODBManager = ODBManager
     ParallelServer = ParallelServer
@@ -646,7 +647,18 @@ class ServiceStore(object):
                 self.impl_name_to_id[item.impl_name] = service_id
                 self.name_to_impl_name[item.name] = item.impl_name
 
-                item.service_class.after_add_to_store(logger)
+                arg_spec = getargspec(item.service_class.after_add_to_store) # type: ArgSpec
+                args = arg_spec.args # type: list
+
+                # GH #1018 made server the argument that the hook receives ..
+                if len(args) == 1 and args[0] == 'server':
+                    hook_arg = self.server
+
+                # .. but for backward-compatibility we provide the hook with the logger object by default.
+                else:
+                    hook_arg = logger
+
+                item.service_class.after_add_to_store(hook_arg)
 
 # ################################################################################################################################
 
