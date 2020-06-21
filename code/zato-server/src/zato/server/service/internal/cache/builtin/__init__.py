fb0c6de70c58cffdcb5e5de59dac9965192e65ac
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-06-21 20:13:27 +0200

    GH #270 - Added skip_if_exists and skip_if_missing to meta services.

--- code/zato-server/src/zato/server/service/internal/cache/builtin/__init__.py
+++ code/zato-server/src/zato/server/service/internal/cache/builtin/__init__.py
@@ -31,6 +31,8 @@ get_list_docs = 'built-in cache definitions'
 broker_message = CACHE
 broker_message_prefix = 'BUILTIN_'
 list_func = cache_builtin_list
+skip_create_integrity_error = True
+skip_if_exists = True
 output_optional_extra = ['current_size', 'cache_id']
 
 # ################################################################################################################################
