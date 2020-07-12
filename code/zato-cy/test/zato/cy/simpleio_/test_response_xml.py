ab4b3fd8d7fcf784814e58b41c928664e214b6c0
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-07-12 15:37:31 +0200

    GH #894 - Use current exception messages.

--- code/zato-cy/test/zato/cy/simpleio_/test_response_xml.py
+++ code/zato-cy/test/zato/cy/simpleio_/test_response_xml.py
@@ -199,9 +199,7 @@ class XMLResponse(BaseTestCase):
             MyService._sio.get_output(data, DATA_FORMAT.XML)
 
         e = ctx.exception # type: SerialisationError
-        self.assertEquals(e.args[0], "Exception `invalid literal for int() with base 10: 'aaa'` while serialising " \
-            "`{'aaa': 'aaa', 'bbb': '222'}` " \
-            "(<class 'test.zato.cy.simpleio_.test_response_xml.XMLResponse.test_response_invalid_input.<locals>.MyService'>)")
+        self.assertEquals(e.args[0], """Exception `ValueError("invalid literal for int() with base 10: 'aaa'",)` while serialising `aaa` (<class 'test.zato.cy.simpleio_.test_response_xml.XMLResponse.test_response_invalid_input.<locals>.MyService'>) ({'aaa': 'aaa', 'bbb': '222'})""")
 
 # ################################################################################################################################
 # ################################################################################################################################
