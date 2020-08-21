c264003b060bb09b51cc4e77aa3e192333174641
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-08-21 23:09:16 +0200

    GH #1069 - Adding a generic filesystem observer.

--- code/zato-server/src/zato/server/pickup/api.py
+++ code/zato-server/src/zato/server/pickup/api.py
@@ -24,7 +24,8 @@ from watchdog.events import FileSystemEventHandler
 from watchdog.utils import platform
 
 if platform.is_linux():
-    from watchdog.observers.polling import PollingObserver as Observer
+    # from watchdog.observers.polling import PollingObserver as Observer
+    from zato.server.pickup.observer import FSOBserver as Observer
 else:
     from zato.server.pickup.observer import FSOBserver as Observer
 
