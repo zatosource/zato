315c07704518425119fa2a8e4449cc3f43132d23
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-10-07 02:23:57 +0200

    GH #1078 - Removing util/json_.py in favour of json_.py.

--- code/zato-common/src/zato/common/json_.py
+++ code/zato-common/src/zato/common/json_.py
@@ -1,22 +1,37 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2019, Zato Source s.r.o. https://zato.io
+Copyright (C) 2020, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+# JSON libraries used by Zato tend to be changed from time to time
+# and this is the place where .dumps and .loads can be imported from
+# so as not to require each part of Zato to know what library to use,
+# unless they have some specific needs in which case they can just
+# import their own required library themselves.
+
 # stdlib
 from datetime import datetime
-from json import dumps as json_dumps
 
-# bson
-from bson.objectid import ObjectId
+# simdjson
+from simdjson import dump, dumps as json_dumps, load, loads
 
 # Python 2/3 compatibility
 from builtins import bytes
+
+# ################################################################################################################################
+
+# These are needed for pyflakes
+
+# Note that dumps is defined in a function below
+dump  = dump
+
+load  = load
+loads = loads
 
 # ################################################################################################################################
 
@@ -43,3 +58,4 @@ def dumps(value, indent=4):
     return json_dumps(value, default=default_json_handler, indent=indent)
 
 # ################################################################################################################################
+
