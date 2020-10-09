63f5de23c2cccbafb860bd0f18195c7a2f3b0bc3
Anielkis Herrera <aherrera@zato.io>
2020-10-08 20:55:31 -0500

    GH #1079 - Update build tools to support CentOS 8

--- code/_install-rhel.sh
+++ code/_install-rhel.sh
@@ -8,27 +8,29 @@ INSTALL_CMD="yum"
 if [ "$(type -p dnf)" ]
 then
     INSTALL_CMD="dnf"
-    ${INSTALL_CMD} update -y
+    sudo ${INSTALL_CMD} update -y
 
     if [ ! "$(type -p lsb_release)" ]
     then
-        ${INSTALL_CMD} install -y redhat-lsb-core
+        sudo ${INSTALL_CMD} install -y redhat-lsb-core
     fi
+fi
+
+if [[ "$(lsb_release -sir)" =~ '^CentOS.8\.' ]]
+    sudo ${INSTALL_CMD} install -y python3
+    sudo ${INSTALL_CMD} -y groupinstall development
+    sudo ${INSTALL_CMD} install -y 'dnf-command(config-manager)'
+    sudo ${INSTALL_CMD} config-manager --set-enabled PowerTools
 fi
 
 sudo ${INSTALL_CMD} install -y \
     bzip2 bzip2-devel curl cyrus-sasl-devel gcc-c++ git haproxy \
     keyutils-libs-devel libev libev-devel libevent-devel libffi libffi-devel \
     libxml2-devel libxslt-devel libyaml-devel openldap-devel openssl \
-    openssl-devel patch postgresql-devel python3 python3-devel suitesparse swig uuid \
+    openssl-devel patch postgresql-devel python3-devel suitesparse swig uuid \
     uuid-devel wget
 
 if [[ "$(lsb_release -sir)" =~ '^CentOS.8\.' ]]
-    ${INSTALL_CMD} install -y python3
-    ${INSTALL_CMD} -y groupinstall development
-    ${INSTALL_CMD} install -y 'dnf-command(config-manager)'
-    ${INSTALL_CMD} config-manager --set-enabled PowerTools
-
     $PY_BINARY -m venv .
     source ./bin/activate
     source ./_postinstall.sh $PY_BINARY
