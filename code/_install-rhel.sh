a8f4990c0c85b44d7d28fd8e0082bc200da884e0
Anielkis Herrera <aherrera@zato.io>
2020-10-08 20:16:43 -0500

    GH #1079 - Update build tools to support CentOS 8

--- code/_install-rhel.sh
+++ code/_install-rhel.sh
@@ -1,29 +1,43 @@
 
 # Python version to use needs to be provided by our caller
-PY_BINARY=$1
+PY_BINARY=python3
 echo "*** Zato RHEL/CentOS installation using $PY_BINARY ***"
 
-PYTHON_VER="2.7.15"
-PYTHON_URL="https://zato.io/support/python27/python27.tar.bz2"
-PYTHON_PREFIX="/opt/zato/python/$PYTHON_VER"
-PATH="$PYTHON_PREFIX/bin:$PATH"
+INSTALL_CMD="yum"
 
-sudo yum -y install \
+if [ "$(type -p dnf)" ]
+then
+    INSTALL_CMD="dnf"
+    ${INSTALL_CMD} update -y
+
+    if [ ! "$(type -p lsb_release)" ]
+    then
+        ${INSTALL_CMD} install -y redhat-lsb-core
+    fi
+fi
+
+sudo ${INSTALL_CMD} install -y \
     bzip2 bzip2-devel curl cyrus-sasl-devel gcc-c++ git haproxy \
     keyutils-libs-devel libev libev-devel libevent-devel libffi libffi-devel \
     libxml2-devel libxslt-devel libyaml-devel openldap-devel openssl \
-    openssl-devel patch postgresql-devel python-devel suitesparse swig uuid \
+    openssl-devel patch postgresql-devel python3 python3-devel suitesparse swig uuid \
     uuid-devel wget
 
-if ! [ "$(type -p $PY_BINARY)" ]
-then
-    # CentOS 6.x requires python2.7 build.
-    curl "$PYTHON_URL" | tac | tac | sudo tar -C / -jx
+if [[ "$(lsb_release -sir)" =~ '^CentOS.8\.' ]]
+    ${INSTALL_CMD} install -y python3
+    ${INSTALL_CMD} -y groupinstall development
+
+    $PY_BINARY -m venv .
+    source ./bin/activate
+    source ./_postinstall.sh $PY_BINARY
+else
+    #  CentOS 7
+    curl https://bootstrap.pypa.io/get-pip.py | sudo $(type -p $PY_BINARY)
+    sudo $(type -p $PY_BINARY) -m pip install -U setuptools virtualenv==15.1.0 pip
+
+    $PY_BINARY -m virtualenv .
+    source ./bin/activate
+    source ./_postinstall.sh $PY_BINARY
 fi
 
-curl https://bootstrap.pypa.io/get-pip.py | sudo $(type -p $PY_BINARY)
-sudo $(type -p $PY_BINARY) -m pip install -U setuptools virtualenv==15.1.0 pip
 
-$PY_BINARY -m virtualenv .
-source ./bin/activate
-source ./_postinstall.sh $PY_BINARY
