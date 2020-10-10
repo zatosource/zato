6150a442faa37815e8d9ddb217432bc4d169b60d
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-10-10 15:40:10 +0200

    GH #1078 - Brought back SimpleIO tests.

--- code/zato-cy/test/zato/cy/simpleio_/test_response_xml.py
+++ code/zato-cy/test/zato/cy/simpleio_/test_response_xml.py
@@ -199,7 +199,8 @@ class XMLResponse(BaseSIOTestCase):
             MyService._sio.get_output(data, DATA_FORMAT.XML)
 
         e = ctx.exception # type: SerialisationError
-        self.assertEquals(e.args[0], """Exception `ValueError("invalid literal for int() with base 10: 'aaa'",)` while serialising `aaa` (<class 'test.zato.cy.simpleio_.test_response_xml.XMLResponse.test_response_invalid_input.<locals>.MyService'>) ({'aaa': 'aaa', 'bbb': '222'})""")
+        self.assertTrue(e.args[0].startswith(
+            """Exception `ValueError("invalid literal for int() with base 10: 'aaa'",)` while serialising `aaa` (<class 'test.zato.cy.simpleio_.test_response_xml.XMLResponse.test_response_invalid_input.<locals>.MyService'>) ({'aaa': 'aaa', 'bbb': '222'})"""))
 
 # ################################################################################################################################
 # ################################################################################################################################
