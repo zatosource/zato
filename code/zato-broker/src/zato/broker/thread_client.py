3510f9c846d8d8dc03daeed1dc17d1e3bfd76deb
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-10-15 22:10:21 +0200

    GH #754 - Correcting imports.

--- code/zato-broker/src/zato/broker/thread_client.py
+++ code/zato-broker/src/zato/broker/thread_client.py
@@ -22,8 +22,8 @@ import redis
 # Zato
 from zato.common import BROKER, TRACE1, ZATO_NONE
 from zato.common.broker_message import KEYS, MESSAGE_TYPE, TOPICS
-from zato.common.json_ import dumps
-from zato.common.util import new_cid
+from zato.common.json_ import dumps, loads
+from zato.common.util.api import new_cid
 
 logger = logging.getLogger(__name__)
 
