631ddb5d20ddd29e46c937e866c0bd22f77c649e
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-10-18 20:28:19 +0200

    GH #1087 - Adding libzato.

--- code/update.sh
+++ code/update.sh
@@ -7,12 +7,8 @@ N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
 echo "*** Downloading updates ***"
 git -C $CURDIR pull
 
-# Uninstall old dependencies
-$CURDIR/bin/pip uninstall -y sec-wall
-$CURDIR/bin/pip uninstall -y springpython
-$CURDIR/bin/pip uninstall -y zato-apitest
-
 echo "*** Installing updates ***"
 $CURDIR/bin/pip install -e $CURDIR/zato-cy
+$CURDIR/bin/pip install -e $CURDIR/zato-lib
 $CURDIR/bin/pip install -r $CURDIR/requirements.txt
 
