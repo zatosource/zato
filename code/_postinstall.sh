2269c1420fefc182625fe12c1ebedcb6db6f84a9
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-10-19 21:22:45 +0200

    GH #1089 - Adding support for SLES.

--- code/_postinstall.sh
+++ code/_postinstall.sh
@@ -13,44 +13,50 @@ then
 fi
 
 PY_BINARY=$1
-
 # Stamp the release hash.
 git log -n 1 --pretty=format:"%H" > ./release-info/revision.txt
 
-$PY_BINARY -m pip install --use-feature=2020-resolver -U setuptools pip
-$PY_BINARY -m pip install --use-feature=2020-resolver -r requirements.txt
+$PY_BINARY -m pip install \
+    --use-feature=2020-resolver \
+    --no-warn-script-location   \
+    -U setuptools pip
+
+$PY_BINARY -m pip install \
+    --use-feature=2020-resolver \
+    --no-warn-script-location   \
+    -r requirements.txt
 
 # zato-common must be first.
 $PY_BINARY -m pip install --use-feature=2020-resolver \
-    -e ./zato-common \
-    -e ./zato-agent \
-    -e ./zato-broker \
-    -e ./zato-cli \
-    -e ./zato-client \
-    -e ./zato-cy \
-    -e ./zato-distlock \
-    -e ./zato-lib \
+    -e ./zato-common    \
+    -e ./zato-agent     \
+    -e ./zato-broker    \
+    -e ./zato-cli       \
+    -e ./zato-client    \
+    -e ./zato-cy        \
+    -e ./zato-distlock  \
+    -e ./zato-lib       \
     -e ./zato-scheduler \
-    -e ./zato-server \
+    -e ./zato-server    \
     -e ./zato-web-admin \
-    -e ./zato-zmq \
-    -e ./zato-sso \
+    -e ./zato-zmq       \
+    -e ./zato-sso       \
     -e ./zato-testing
 
 # Emulate zc.buildout's split-out eggs directory for simpler local development.
-ln -fs $VIRTUAL_ENV/lib/python*/site-packages eggs
+ln -fs $VIRTUAL_ENV/lib/python*/site-packages $VIRTUAL_ENV/eggs
 
 # Emulate zc.buildout's py script. Wrap rather than symlink to ensure argv[0] is correct.
 cat > $VIRTUAL_ENV/bin/py <<-EOF
 #!/bin/sh
-exec "$(pwd)/bin/python" "\$@"
+exec "$VIRTUAL_ENV/bin/python" "\$@"
 EOF
 
 chmod +x $VIRTUAL_ENV/bin/py
 
 # Create and add zato_extra_paths to the virtualenv's sys.path.
 mkdir zato_extra_paths
-echo "$(pwd)/zato_extra_paths" >> eggs/easy-install.pth
+echo "$VIRTUAL_ENV/zato_extra_paths" >> eggs/easy-install.pth
 
 # Create a symlink to zato_extra_paths to make it easier to type it out
 ln -fs $VIRTUAL_ENV/zato_extra_paths extlib
@@ -64,8 +70,17 @@ patch -p0 -d eggs < patches/pykafka/topic.py.diff
 patch -p0 -d eggs < patches/redis/redis/connection.py.diff
 patch -p0 -d eggs < patches/requests/models.py.diff
 patch -p0 -d eggs < patches/requests/sessions.py.diff
-patch -p0 -d eggs < patches/sqlalchemy/sql/crud.py.diff
 patch -p0 -d eggs < patches/ws4py/server/geventserver.py.diff
+
+#
+# On SUSE, SQLAlchemy installs to lib64 instead of lib.
+#
+cat /etc/SUSE-brand > /dev/null 2>&1
+if [ $? == 0 ]; then
+    patch -p0 -d eggs64 < patches/sqlalchemy/sql/crud.py.diff
+else
+    patch -p0 -d eggs < patches/sqlalchemy/sql/crud.py.diff
+fi
 
 # Add the 'zato' command ..
 cat > $VIRTUAL_ENV/bin/zato <<-EOF
@@ -79,6 +94,9 @@ if __name__ == '__main__':
     # stdlib
     import re
     import sys
+
+    # This is needed by SUSE
+    sys.path.append('$VIRTUAL_ENV/lib64/python3.6/site-packages/')
 
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(main())
