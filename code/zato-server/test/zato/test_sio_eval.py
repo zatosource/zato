e149b2f7ffc08471a575a9cd07a8fff23d0377d3
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-10-27 13:53:36 +0100

    GH #1069 - Added SIO.eval_.

---
+++ code/zato-server/test/zato/test_sio_eval.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2020, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# stdlib
+from copy import deepcopy
+from unittest import main
+
+# Zato
+from common import MyService
+from zato.common.test import BaseSIOTestCase
+
+# Zato - Cython
+from zato.simpleio import CySimpleIO
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if 0:
+    from zato.simpleio import SIOServerConfig
+    SIOServerConfig = SIOServerConfig
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class SIOEvalTestCase(BaseSIOTestCase):
+
+    def test_eval_bool(self):
+
+        MyClass = deepcopy(MyService)
+        CySimpleIO.attach_sio(self.get_server_config(), MyClass)
+        sio = MyClass._sio # type: CySimpleIO
+
+        elem_name = 'is_abc'
+        encrypt_func = None
+
+        value = ''
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsInstance(result, bool)
+        self.assertFalse(result)
+
+        value = None
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsInstance(result, bool)
+        self.assertFalse(result)
+
+        value = 't'
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsInstance(result, bool)
+        self.assertTrue(result)
+
+        value = 'true'
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsInstance(result, bool)
+        self.assertTrue(result)
+
+        value = 'on'
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsInstance(result, bool)
+        self.assertTrue(result)
+
+    def test_eval_int(self):
+
+        MyClass = deepcopy(MyService)
+        CySimpleIO.attach_sio(self.get_server_config(), MyClass)
+        sio = MyClass._sio # type: CySimpleIO
+
+        elem_name = 'user_id'
+        encrypt_func = None
+
+        value = ''
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsNone(result)
+
+        value = None
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsNone(result)
+
+        value = '111'
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, 111)
+
+        value = 222
+        result = sio.eval_(elem_name, value, encrypt_func)
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, 222)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if __name__ == '__main__':
+    main()
+
+# ################################################################################################################################
