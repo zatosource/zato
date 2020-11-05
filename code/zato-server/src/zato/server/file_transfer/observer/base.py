fb83adc19cd50af98e2df4182b1d10a8d9f25987
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-11-05 22:43:22 +0100

    GH #1069 - Make sure the directory exists before starting an observer.

--- code/zato-server/src/zato/server/file_transfer/observer/base.py
+++ code/zato-server/src/zato/server/file_transfer/observer/base.py
@@ -6,17 +6,40 @@ Copyright (C) 2020, Zato Source s.r.o. https://zato.io
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# stdlib
+import os
+from logging import getLogger
+
+# Zato
+from zato.common.util.api import spawn_greenlet
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logger = getLogger(__name__)
+
 # ################################################################################################################################
 # ################################################################################################################################
 
 class BaseObserver:
-    def __init__(self, name):
-        # type: (str) -> None
+    def __init__(self, name, is_active, default_timeout):
+        # type: (str, bool, float) -> None
         self.name = name
+        self.is_active = is_active
+        self.default_timeout = default_timeout
         self.event_handler = None
         self.path = '<initial-local-observer>'
         self.is_recursive = False
         self.keep_running = True
+
+    def start(self):
+        if self.is_active:
+            spawn_greenlet(self._start)
+        else:
+            logger.info('Skipping an inactive file transfer channel `%s` (%s)', self.name, self.path)
+
+    def _start(self, *args, **kwargs):
+        raise NotImplementedError('Must be implemented by subclasses')
 
 # ################################################################################################################################
 # ################################################################################################################################
