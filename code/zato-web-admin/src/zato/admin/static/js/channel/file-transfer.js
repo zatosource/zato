edd413f785214cb88b03c57b8d81273d90587916
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-11-09 21:48:48 +0100

    GH #1069 - Removing unused fields.

--- code/zato-web-admin/src/zato/admin/static/js/channel/file-transfer.js
+++ code/zato-web-admin/src/zato/admin/static/js/channel/file-transfer.js
@@ -102,8 +102,8 @@ $.fn.zato.channel.file_transfer.edit = function(id) {
     $.fn.zato.data_table.multirow.remove_multirow_added();
     $.fn.zato.data_table.edit('edit', 'Update the file transfer channel', id, false);
 
-    //$.fn.zato.data_table.multirow.populate_field('service_list', instance.service_list);
-    //$.fn.zato.data_table.multirow.populate_field('topic_list', instance.topic_list);
+    $.fn.zato.data_table.multirow.populate_field('service_list', instance.service_list);
+    $.fn.zato.data_table.multirow.populate_field('topic_list', instance.topic_list);
     $.fn.zato.data_table.multirow.populate_field('outconn_rest_list', instance.outconn_rest_list);
 }
 
@@ -160,8 +160,8 @@ $.fn.zato.channel.file_transfer.data_table.new_row = function(item, data, includ
 
     // 5
     row += String.format("<td class='ignore'>{0}</td>", item.should_read_on_pickup);
-    row += String.format("<td class='ignore'>{0}</td>", JSON.stringify(data.service_list_json));
-    row += String.format("<td class='ignore'>{0}</td>", JSON.stringify(data.topic_list_json));
+    row += String.format("<td class='ignore'>{0}</td>", item.service_list);
+    row += String.format("<td class='ignore'>{0}</td>", item.topic_list);
 
     // 6
     row += String.format("<td class='ignore'>{0}</td>", item.should_parse_on_pickup);
@@ -195,9 +195,6 @@ $.fn.zato.channel.file_transfer.data_table.new_row = function(item, data, includ
     row += String.format("<td class='ignore'>{0}</td>", item.binary_file_patterns);
     row += String.format("<td class='ignore'>{0}</td>", item.data_encoding);
     row += String.format("<td class='ignore'>{0}</td>", item.outconn_rest_list);
-
-    // 13
-    row += String.format("<td class='ignore'>{0}</td>", item.outconn_rest_list_json);
 
     if(include_tr) {
         row += '</tr>';
