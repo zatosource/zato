ff9ffb61cd4a2cfde1e4bb5bd617de6be56444e8
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-11-12 00:34:12 +0100

    GH #1069 - Adding FTP tests.

--- code/zato-server/test/zato/file_transfer/test_ftp.py
+++ code/zato-server/test/zato/file_transfer/test_ftp.py
@@ -7,10 +7,16 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # stdlib
+import logging
+import os
 from unittest import main, TestCase
+from uuid import uuid4
 
 # pyfilesystem
 from fs.ftpfs import FTPFS
+
+# pyftpdlib
+from pyftpdlib.log import config_logging as pyftpdlib_config_logging
 
 # Zato
 from zato.common.util.api import wait_until_port_taken
@@ -20,11 +26,41 @@ from zato.server.connection.file_client.api import FTPFileClient
 # ################################################################################################################################
 # ################################################################################################################################
 
+pyftpdlib_config_logging(level=logging.WARN)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
 
 class FTPFileTransferTestCase(TestCase):
     def test_ftp(self):
 
-        # Create an embedded server ..
+        base_test_dir = 'base_test_dir.{}'.format(uuid4().hex)
+        base_test_path = '/{}'.format(base_test_dir)
+
+        test_file = 'zxc.txt'
+        test_data = 'test_data.{}'.format(uuid4()).encode()
+        test_file_path = os.path.join(base_test_path, test_file)
+
+        def check_directory(client, should_exist):
+            # type: (FTPFileClient, bool)
+
+            result = client.list('/')
+            directory_list = result['directory_list']
+            for item in directory_list:
+                if item['name'] == base_test_dir:
+                    if not should_exist:
+                        raise ValueError('Directory `{}` should not exist'.format(
+                            os.path.normpath(os.path.join(ftp_config.directory, base_test_dir))))
+                    else:
+                        self.assertTrue(item['is_dir'])
+                        break
+            else:
+                if should_exist:
+                    raise ValueError('Expected for directory `{}` to exist'.format(
+                        os.path.normpath(os.path.join(ftp_config.directory, base_test_dir))))
+
+        # Create an embedded FTP server ..
         server = FTPServer()
 
         # .. start it in a new thread ..
@@ -41,10 +77,33 @@ class FTPFileTransferTestCase(TestCase):
             'encoding': 'utf8'
         })
 
-        print(222, client.ping())
+        # .. confirm we are connected ..
+        ping_response = client.ping()
+        self.assertTrue(ping_response)
+
+        # .. create a new directory ..
+        client.create_directory(base_test_dir)
+
+        # .. make sure the directory was created ..
+        check_directory(client, True)
+
+        # .. upload a new file ..
+        client.upload(test_file_path, test_data)
+
+        # .. download the uploaded file ..
+        received_data = client.download(test_file_path)
+
+        # .. compare the downloaded and uploaded data ..
+        self.assertEqual(received_data, test_data)
+
+        # .. delete the test directory ..
+        client.delete_directory(base_test_dir)
+
+        # .. make sure the directory was deleted ..
+        check_directory(client, False)
 
         # .. stop the client ..
-        client.st
+        client.close()
 
         # .. finally, stop the embedded server.
         server.stop()
