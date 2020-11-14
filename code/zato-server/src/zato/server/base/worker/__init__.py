474a41ff66a4f9770f81db79ab745f3ab8ce2b27
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-11-14 19:19:33 +0100

    GH #1095 - Handle all Dropbox messages.

--- code/zato-server/src/zato/server/base/worker/__init__.py
+++ code/zato-server/src/zato/server/base/worker/__init__.py
@@ -999,6 +999,7 @@ class WorkerStore(_WorkerStoreBase, BrokerMessageReceiver):
         # These generic connections are regular - they use common API methods for such connections
         regular_maps = [
             channel_file_transfer_map,
+            cloud_dropbox_map,
             def_kafka_map,
             outconn_im_slack_map,
             outconn_im_telegram_map,
