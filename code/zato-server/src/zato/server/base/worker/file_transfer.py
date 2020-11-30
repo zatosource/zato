5de71f2a8fbf78b91fc7f137f0fab5da75a07c1a
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-11-30 20:33:01 +0100

    GH #1069 - Add or remove channel IDs to scheduler's jobs.

--- code/zato-server/src/zato/server/base/worker/file_transfer.py
+++ code/zato-server/src/zato/server/base/worker/file_transfer.py
@@ -6,8 +6,11 @@ Copyright (C) Zato Source s.r.o. https://zato.io
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# Bunch
+from bunch import bunchify
+
 # Zato
-from zato.common.odb.query.generic_object_ import generic_object
+from zato.common.api import FILE_TRANSFER
 from zato.server.base.worker.common import WorkerImpl
 
 # ################################################################################################################################
@@ -21,30 +24,103 @@ class FileTransfer(WorkerImpl):
 
 # ################################################################################################################################
 
+    def _file_transfer_get_scheduler_job_by_id(self, job_id):
+
+        # This returns a SimpleIO payload object ..
+        response = self.server.invoke('zato.scheduler.job.get-by-id', {
+            'cluster_id': self.server.cluster_id,
+            'id': job_id,
+        }, needs_response=False)
+
+        # .. this returns a dict ..
+        response = response.getvalue()
+
+        # .. and this returns a Bunch.
+        return bunchify(response)
+
+# ################################################################################################################################
+
+    def _file_transfer_get_scheduler_job_list(self):
+
+        # This returns a SimpleIO payload object ..
+        response = self.server.invoke('zato.scheduler.job.get-list', {
+            'cluster_id': self.server.cluster_id,
+            'service_name': FILE_TRANSFER.SCHEDULER_SERVICE,
+        }, needs_response=False)
+
+        # .. this returns a dict with a single key ..
+        response = response.getvalue()
+
+        # .. and we return the key's list data only.
+        return response['zato_scheduler_job_get_list_response']
+
+# ################################################################################################################################
+
+    def _file_transfer_save_scheduler_job(self, data):
+        data['cluster_id'] = self.server.cluster_id
+        data['service'] = data.service_name
+        self.server.invoke('zato.scheduler.job.edit', data)
+
+# ################################################################################################################################
+
+    def _file_transfer_modify_scheduler_job(self, job, job_id, channel_id, is_add):
+        """ Finds a job along with its extra data and either adds or removes a file transfer channel for it.
+        """
+        # We store IDs as string objects
+        channel_id = str(channel_id)
+
+        # Get a scheduler's job by its id if we were not given a job on input
+        job = job or self._file_transfer_get_scheduler_job_by_id(job_id)
+
+        # This is where keep information about channels to run
+        extra = job.extra if isinstance(job.extra, str) else job.extra.decode('utf8')
+
+        # Holds all channel IDs to run
+        extra_set = set()
+
+        # If it exists at all ..
+        if extra:
+
+            # .. it will be a semicolon-separated list of IDs ..
+            extra = [elem.strip() for elem in job.extra.split(';')]
+
+            # .. turn the list into a set ..
+            extra_set.update(extra)
+
+        # .. now, we can just add or remove our own key, no matter if extra existed or not ..
+        if is_add:
+            extra_set.add(channel_id)
+        else:
+            try:
+                extra_set.remove(channel_id)
+            except KeyError:
+                # This is fine, apparently the channel was not assigned to extra before
+                pass
+
+        # .. serialise the set back to a semicolong-separated list ..
+        extra = '; '.join(sorted(extra_set))
+
+        # .. assign it to our job dict ..
+        job['extra'] = extra
+
+        # .. and save it back in ODB.
+        self._file_transfer_save_scheduler_job(job)
+
+# ################################################################################################################################
+
     def _edit_file_transfer_channel(self, msg):
         # type: (Bunch) -> None
 
-        '''
-        print()
-        for key, value in sorted(msg.items()):
-            print(111, key, value)
-        print()
-        '''
-
-        # Get a scheduler's job associated with this channel
+        # If we have a scheduler job on input ..
         if msg.scheduler_job_id:
-            response = self.server.invoke('zato.scheduler.job.get-by-id', {
-                'cluster_id': self.server.cluster_id,
-                'id': msg.scheduler_job_id
-                #'name': 'Transfer Invoices Job',
-            }, needs_response=False)
+            self._file_transfer_modify_scheduler_job(None, msg.scheduler_job_id, msg.id, True)
 
-            response = response.getvalue()
-
-            print()
-            print(222, response)
-            print(333, dir(response))
-            print()
+        # .. otherwise, without a job ID on input, we still need to look them all up
+        # and disassociate our channel from any of the existing jobs.
+        else:
+            for item in self._file_transfer_get_scheduler_job_list():
+                item = bunchify(item)
+                self._file_transfer_modify_scheduler_job(item, None, msg.id, False)
 
 # ################################################################################################################################
 # ################################################################################################################################
