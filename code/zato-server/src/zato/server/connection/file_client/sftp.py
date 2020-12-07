1c0a9d424f3227432876b654cdab7d81ac6c0742
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-12-07 22:28:20 +0100

    GH #1069 - Working on SFTP.

--- code/zato-server/src/zato/server/connection/file_client/sftp.py
+++ code/zato-server/src/zato/server/connection/file_client/sftp.py
@@ -1,141 +1,92 @@
-"""
 # -*- coding: utf-8 -*-
 
+"""
+Copyright (C) Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+from logging import getLogger
+
 # Zato
-from zato.server.connection.file_client.base import BaseFileClient
+from zato.server.connection.file_client.base import BaseFileClient, PathAccessException
 from zato.server.connection.sftp import SFTPIPCFacade
 from zato.server.service import Service
 
 # ################################################################################################################################
 # ################################################################################################################################
 
-class FTPFileClient(BaseFileClient):
+logger = getLogger('zato')
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+def ensure_path_exists(*ignored_args, **ignored_kwargs):
+    def _inner(self, path):
+        # type: (SFTPFileClient, str)
+        if not self.conn.exists(path):
+            raise PathAccessException('Path `{}` could not be accessed'.format(path))
+    return _inner
+
+# ################################################################################################################################
+
+class SFTPFileClient(BaseFileClient):
     def __init__(self, conn, config):
-        # type: (SFTPConnection) -> None
+        # type: (SFTPIPCFacade, dict) -> None
         super().__init__(config)
         self.conn = conn
 
 # ################################################################################################################################
 
     def create_directory(self, path):
-        # self.conn.makedir(path)
-        pass
+        self.conn.create_directory(path)
 
 # ################################################################################################################################
 
-    def xdelete_directory(self, path):
-        # self.conn.removetree(path)
-        pass
+    def delete_directory(self, path):
+        self.conn.delete_directory(path)
 
 # ################################################################################################################################
 
-    def xget(self, path):
-        # (str, str) -> str
-        # return self.conn.readbytes(path)
-        pass
+    def get(self, path):
+        # (str) -> str
+        return self.conn.download(path)
 
 # ################################################################################################################################
 
-    def xget_as_file_object(self, path, file_object):
-        # type: (str, BinaryIO) -> None
-        # self.conn.download(path, file_object)
-        pass
-
-# ################################################################################################################################
-
-    def xstore(self, path, data):
+    def store(self, path, data):
         # type: (str, object) -> None
-        '''
-        if not isinstance(data, bytes):
-            data = data.encode(self.config['encoding'])
-        self.conn.writebytes(path, data)
-        '''
+        self.conn.write(data, path)
+
+        import struct
 
 # ################################################################################################################################
 
-    def xdelete_file(self, path):
-        # type: (str) -> None
-        #self.conn.remove(path)
-        pass
+    def delete_file(self, path):
+        self.conn.delete_file(path)
 
 # ################################################################################################################################
 
-    def xlist(self, path):
-        # Returns a list of directories and files for input path.
+    @ensure_path_exists
+    def list(self, path):
+        """ Returns a list of directories and files for input path.
+        """
         # type: (str) -> list
-        '''
-        # Response to produce
-        out = {
-            'directory_list': [],
-            'file_list': [],
-        }
+        result = self.conn.list(path)
 
-        # Call the remote resource
-        result = self.conn.scandir(path)
-
-        # Process all items produced
-        for item in result: # type: Info
-
-            elem = {
-                'name': item.name,
-                'size': item.size,
-                'is_dir': item.is_dir,
-                'last_modified': item.modified,
-                'has_touch': item.size < touch_max_size
-            }
-
-            if item.is_dir:
-                out['directory_list'].append(elem)
-            else:
-                out['file_list'].append(elem)
-
-        # Sort all items by name
-        self.sort_result(out)
-
-        # Return the response to our caller
-        return out
-        '''
+        return result
 
 # ################################################################################################################################
 
-    def xtouch(self, path):
-        # Touches a remote file by overwriting its contents with itself.
-        '''
-        with self.conn._lock:
-
-            # Get the current size ..
-            size = self.conn.getsize(path)
-
-            # .. make sure the file is not too big ..
-            if size > touch_max_size:
-                raise ValueError('File `{}` is too big for the touch command; size={}; max={}'.format(
-                    path, size, touch_max_size))
-
-            # .. read all data in ..
-            with conn.openbin(path) as f:
-                data = f.read(size)
-
-            # .. and write it under the same path, effectively merely changing its modification time.
-            with conn.openbin(path, 'w') as f:
-                f.write(data)
-        '''
+    def ping(self):
+        return self.conn.ping()
 
 # ################################################################################################################################
 
-    def xping(self):
-        #return self.conn.exists('/')
-        pass
-
-# ################################################################################################################################
-
-    def xclose(self):
-        #self.conn.close()
+    def close(self):
+        # Not applicable
         pass
 
 # ################################################################################################################################
 # ################################################################################################################################
-
-class SFTPService1(Service):
-    pass
-
-"""
