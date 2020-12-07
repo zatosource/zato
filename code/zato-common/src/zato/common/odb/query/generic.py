defc4a3cbab2579af48732187b6946bf586a0e5a
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-12-07 01:17:37 +0100

    GH #1069 - Store snapshots in ODB.

--- code/zato-common/src/zato/common/odb/query/generic.py
+++ code/zato-common/src/zato/common/odb/query/generic.py
@@ -1,22 +1,150 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2019, Zato Source s.r.o. https://zato.io
+Copyright (C) Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# Zato
-from zato.common.odb.model import GenericConn as ModelGenericConn
-from zato.common.odb.query import query_wrapper
+# stdlib
+from logging import getLogger
 
+# SQLAlchemy
+from sqlalchemy import and_, exists, insert, select, update
+
+# Zato
+from zato.common.api import GENERIC, FILE_TRANSFER
+from zato.common.odb.model import GenericConn as ModelGenericConn, GenericObject as ModelGenericObject
+from zato.common.odb.query import query_wrapper
+from zato.common.util.sql import get_dict_with_opaque
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logger = getLogger('zato')
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+_generic_attr_name = GENERIC.ATTR_NAME
+ModelGenericObjectTable = ModelGenericObject.__table__
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class GenericObjectWrapper:
+    """ Wraps access to generic objects.
+    """
+    type_ = None
+    subtype = None
+    model_class = ModelGenericObject
+
+    def __init__(self, session, cluster_id):
+        # type: (object, int):
+        self.session = session
+        self.cluster_id = cluster_id
+
+# ################################################################################################################################
+
+    def _build_get_where_query(self, name):
+        # type: (str) -> object
+        return and_(
+            self.model_class.name==name,
+            self.model_class.type_==self.type_,
+            self.model_class.cluster_id==self.cluster_id,
+        )
+
+# ################################################################################################################################
+
+    def get(self, name):
+        # type: (str) -> object
+        item = self.session.query(self.model_class).\
+            filter(self.model_class.name==name).\
+            filter(self.model_class.type_==self.type_).\
+            filter(self.model_class.cluster_id==self.cluster_id).\
+            one()
+
+        logger.warn('YYY %s', dir(item))
+
+        return get_dict_with_opaque(item)
+
+# ################################################################################################################################
+
+    def exists(self, name):
+        """ Returns a boolean flag indicating whether the input name is already stored in the ODB. False otherwise.
+        """
+        where_query = self._build_get_where_query(name)
+        exists_query = exists().where(where_query)
+        return self.session.query(exists_query).\
+            scalar()
+
+# ################################################################################################################################
+
+    def create(self, name, opaque):
+        """ Creates a new row for input data.
+        """
+        return insert(self.model_class).values(**{
+            'name': name,
+            'type_': self.type_,
+            'subtype': self.subtype,
+            'cluster_id': self.cluster_id,
+            _generic_attr_name: opaque,
+        })
+
+    def update(self, name, opaque):
+        """ Updates an already existing object.
+        """
+        # type: (str, str) -> object
+
+        return update(ModelGenericObjectTable).\
+            values({
+                _generic_attr_name: opaque,
+                }).\
+            where(and_(
+                ModelGenericObjectTable.c.name==name,
+                ModelGenericObjectTable.c.type_==self.type_,
+                ModelGenericObjectTable.c.cluster_id==self.cluster_id,
+            ))
+
+# ################################################################################################################################
+
+    def store(self, name, opaque):
+        """ Inserts new data or updates an already existing row matching the input.
+        """
+        # type: (str, str)
+
+        already_exists = self.exists(name)
+        query = self.update(name, opaque) if already_exists else self.create(name, opaque)
+
+        print()
+        logger.warn('ZZZ-1 %s', already_exists)
+        logger.warn('ZZZ-2 %s', name)
+        logger.warn('ZZZ-2b %s', self.type_)
+        logger.warn('ZZZ-3 %s', opaque)
+        logger.warn('ZZZ-4 %s', query)
+
+        if already_exists:
+            logger.warn('ZZZ-5 %s', self.get(name))
+        print()
+
+        self.session.execute(query)
+        self.session.commit()
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class FileTransferWrapper(GenericObjectWrapper):
+    type_ = GENERIC.CONNECTION.TYPE.CHANNEL_FILE_TRANSFER
+    subtype = FILE_TRANSFER.SOURCE_TYPE.FTP.id
+
+# ################################################################################################################################
 # ################################################################################################################################
 
 @query_wrapper
 def connection_list(session, cluster_id, type_=None, needs_columns=False):
-    """ A list of connections by their type.
+    """ A list of generic connections by their type.
     """
     q = session.query(ModelGenericConn).\
         filter(ModelGenericConn.cluster_id==cluster_id)
@@ -28,4 +156,5 @@ def connection_list(session, cluster_id, type_=None, needs_columns=False):
 
     return q
 
+# ################################################################################################################################
 # ################################################################################################################################
