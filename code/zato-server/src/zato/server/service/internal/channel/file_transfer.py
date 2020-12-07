7fed931026e0388d1170605d185963941ecd6088
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-12-07 18:05:43 +0100

    GH #1069 - Moving the callback service to its final module.

--- code/zato-server/src/zato/server/service/internal/channel/file_transfer.py
+++ code/zato-server/src/zato/server/service/internal/channel/file_transfer.py
@@ -8,34 +8,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 # Zato
 from zato.common.api import FILE_TRANSFER
-from zato.server.service import Service
-
-# ################################################################################################################################
-
-class ChannelFileTransferHandler(Service):
-    """ A no-op marker service uses by file transfer channels.
-    """
-    name = FILE_TRANSFER.SCHEDULER_SERVICE
-
-    def handle(self):
-        self.logger.warn('ZZZ `%s`', self.request.raw_request)
-        pass
-
-# ################################################################################################################################
-
-'''
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (C) Zato Source s.r.o. https://zato.io
-
-Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
-"""
-
-# Zato
-from zato.common.api import FILE_TRANSFER
 from zato.common.util.file_transfer import parse_extra_into_list
-from zato.common.util.api import spawn_greenlet
 from zato.server.service import Service
 
 # ################################################################################################################################
@@ -48,26 +21,19 @@ class ChannelFileTransferHandler(Service):
 
 # ################################################################################################################################
 
-    def run_observer(self, channel_id):
-        # type: (int) -> None
-
-        channel = self.server.worker_store.file_transfer_api.observer_dict
-
-        self.logger.warn('ZZZ-2 `%s`', channel)
-
-# ################################################################################################################################
-
     def handle(self):
 
-        extra = '16;'#; 17' #self.request.raw_request
+        extra = self.request.raw_request
+
+        if not extra:
+            return
 
         # Convert input parameters into a list of channel (observer) IDs ..
         extra = parse_extra_into_list(extra)
 
         # .. and run each observer in a new greenlet.
         for channel_id in extra:
-            spawn_greenlet(self.run_observer, channel_id)
+            self.server.worker_store.file_transfer_api.run_snapshot_observer(channel_id, 1)
 
 # ################################################################################################################################
 # ################################################################################################################################
-'''
