ae46facde8b17494867c00c884e30dc54534619b
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-12-26 01:07:55 +0100

    GH #1110 - Handle arbitrary object types and IDs.

--- code/zato-server/src/zato/server/service/internal/audit_log.py
+++ code/zato-server/src/zato/server/service/internal/audit_log.py
@@ -23,10 +23,8 @@ class GetEventList(AdminService):
 
     class SimpleIO:
         input_optional = 'cluster_id', 'type_', Unicode('object_id')
-        output_required = 'server_name', 'server_pid', 'type_', AsIs('object_id'), 'direction', 'timestamp', \
-            AsIs('msg_id'), AsIs('event_id')
-        output_optional = AsIs('conn_id'), 'in_reply_to', 'data', Int('data_len')
-        response_elem = None
+        output_optional = 'server_name', 'server_pid', 'type_', AsIs('object_id'), 'direction', 'timestamp', \
+            AsIs('msg_id'), AsIs('event_id'), AsIs('conn_id'), 'in_reply_to', 'data', Int('data_len')
 
 # ################################################################################################################################
 
@@ -36,19 +34,20 @@ class GetEventList(AdminService):
         object_id = self.request.input.object_id
 
         result = self.server.audit_log.get_container(type_, object_id) # type: LogContainer
-        result = result.to_dict() # type: dict
+        if result:
+            result = result.to_dict() # type: dict
 
-        out = []
+            out = []
 
-        for value in result.values(): # type: (str, list)
-            for item in value: # type: dict
-                item['server_name'] = self.server.name
-                item['server_pid'] = self.server.pid
-                item['data_len'] = len(item['data']) if item['data'] is not None else 0
-                out.append(item)
+            for value in result.values(): # type: (str, list)
+                for item in value: # type: dict
+                    item['server_name'] = self.server.name
+                    item['server_pid'] = self.server.pid
+                    item['data_len'] = len(item['data']) if item['data'] is not None else 0
+                    out.append(item)
 
-        out.sort(key=itemgetter('timestamp'), reverse=True)
-        self.response.payload[:] = out
+            out.sort(key=itemgetter('timestamp'), reverse=True)
+            self.response.payload[:] = out
 
 # ################################################################################################################################
 # ################################################################################################################################
