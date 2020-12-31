3305490b16be4e24051247894e3811134a6d2be6
Dariusz Suchojad <dsuch-github@m.zato.io>
2020-12-31 01:06:08 +0100

    GH #1106 - Display log messages in HL7 REST channels.

--- code/zato-web-admin/src/zato/admin/static/js/channel/hl7/rest.js
+++ code/zato-web-admin/src/zato/admin/static/js/channel/hl7/rest.js
@@ -57,6 +57,10 @@ $.fn.zato.channel.hl7.rest.data_table.new_row = function(item, data, include_tr)
     row += String.format('<td>{0}</td>', $.fn.zato.data_table.service_text(item.service, cluster_id));
     row += String.format('<td>{0}</td>', data.sec_def_link || $.fn.zato.empty_value);
 
+    // 2b
+    row += String.format('<td><a href="/zato/audit-log/http-soap/{0}/?cluster={1}&amp;object_name={2}&amp;object_type_label={3}">View</a></td>',
+        item.id, cluster_id, item.name, 'HL7 REST channel');
+
     // 3
     row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.channel.hl7.rest.edit('{0}')\">Edit</a>", item.id));
     row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.channel.hl7.rest.delete_({0});'>Delete</a>", item.id));
