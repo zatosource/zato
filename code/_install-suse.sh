ae47a3f9e4800fa72d6cd79c0ea56737e1c3a24f
Anielkis Herrera <aherrera@zato.io>
2021-01-17 19:01:00 -0500

    GH #1116 - Add flag to skip Python package installation

--- code/_install-suse.sh
+++ code/_install-suse.sh
@@ -2,20 +2,19 @@
 
 # Python version to use needs to be provided by our caller
 PY_BINARY=$1
+INSTALL_PYTHON=${2:-y}
 echo "*** Zato SUSE installation using $PY_BINARY ***"
+
+if [[ "$INSTALL_PYTHON" == "y" ]]; then
+  PYTHON_DEPENDENCIES="$PY_BINARY $PY_BINARY-devel $PY_BINARY-pip "
+fi
 
 sudo zypper install -y \
     curl cyrus-sasl-devel gcc gcc-c++ haproxy libbz2-devel libev-devel libev4 \
     libffi-devel keyutils-devel libmemcached-devel libpqxx-devel              \
     openldap2-devel libxml2-devel libxslt-devel libyaml-devel openssl         \
-    patch $PY_BINARY $PY_BINARY-devel $PY_BINARY-pip uuid-devel               \
-    wget zlib-devel
+    patch uuid-devel wget zlib-devel $PYTHON_DEPENDENCIES
 
-#    build-essential curl git haproxy libbz2-dev libev-dev libev4 libevent-dev \
-#    libffi-dev libkeyutils-dev libldap2-dev libmemcached-dev libpq-dev \
-#    libsasl2-dev libssl-dev libxml2-dev libxslt1-dev libyaml-dev openssl \
-#    $PY_BINARY $PY_BINARY-dev $PY_BINARY-pip swig uuid-dev uuid-runtime wget \
-#    zlib1g-dev lsb-release
 
 curl https://bootstrap.pypa.io/get-pip.py | $(type -p $PY_BINARY)
 $PY_BINARY -m pip install       \
--- code/install.sh
+++ code/install.sh
@@ -6,14 +6,19 @@ shopt -s compat31
 
 # Default python binary
 PY_BINARY="python3"
+INSTALL_PYTHON="y"
 
 # Taken from https://stackoverflow.com/a/14203146
 OPTIND=1
-while getopts "p:" opt; do
+while getopts "p:s" opt; do
     case "$opt" in
     p)
         shift
         PY_BINARY=$1
+        ;;
+    k)
+        shift
+        INSTALL_PYTHON=n
         ;;
     esac
 done
@@ -26,45 +31,45 @@ shift $((OPTIND-1))
 
 # Not installed?
 if ! [ -x "$(command -v $PY_BINARY)" ]; then
-  if [ "$(type -p apt-get)" ]
-  then
-      sudo apt-get update
-      sudo apt-get install -y --reinstall ${PY_BINARY}
-  elif [ "$(type -p yum)" ]
-  then
-      if [ "$(type -p dnf)" ]
-      then
-        sudo dnf update -y
-
-        if [ ! "$(type -p lsb_release)" ]
+    if [ "$(type -p apt-get)" ]
+    then
+        sudo apt-get update
+        [ "$INSTALL_PYTHON" == "y" ] && sudo apt-get install -y --reinstall ${PY_BINARY}
+    elif [ "$(type -p yum)" ]
+    then
+        if [ "$(type -p dnf)" ]
         then
-            sudo dnf install -y redhat-lsb-core
-        fi
+            sudo dnf update -y
 
-        if [ ! "$(type -p python3)" ]
-        then
-            sudo dnf install -y python3
-        fi
-      else
-        sudo yum update -y
+            if [ ! "$(type -p lsb_release)" ]
+            then
+                sudo dnf install -y redhat-lsb-core
+            fi
 
-        if [ ! "$(type -p lsb_release)" ]
-        then
-            sudo yum install -y redhat-lsb-core
-        fi
+            if [ ! "$(type -p python3)" ]
+            then
+                [ "$INSTALL_PYTHON" == "y" ] && sudo dnf install -y python3
+            fi
+        else
+            sudo yum update -y
 
-        if [ ! "$(type -p python3)" ]
-        then
-            sudo yum install -y python3
+            if [ ! "$(type -p lsb_release)" ]
+            then
+                sudo yum install -y redhat-lsb-core
+            fi
+
+            if [ ! "$(type -p python3)" ]
+            then
+                [ "$INSTALL_PYTHON" == "y" ] && sudo yum install -y python3
+            fi
         fi
-      fi
-  elif [ "$(uname -s)" = "Darwin" ]
-  then
-      brew install  $PY_BINARY
-  else
-      echo "install.sh: Unsupported OS: could not detect OS X, apt-get or yum." >&2
-      exit 1
-  fi
+    elif [ "$(uname -s)" = "Darwin" ]
+    then
+        [ "$INSTALL_PYTHON" == "y" ] && brew install  $PY_BINARY
+    else
+        echo "install.sh: Unsupported OS: could not detect OS X, apt-get or yum." >&2
+        exit 1
+    fi
 fi
 
 # Confirm such a Python version is accessible
@@ -111,19 +116,19 @@ switch_to_basedir
 if [ "$(type -p apt-get)" ]
 then
     source ./clean.sh
-    source ./_install-deb.sh $PY_BINARY
+    source ./_install-deb.sh $PY_BINARY ${INSTALL_PYTHON}
 elif [ "$(type -p yum)" ] || [ "$(type -p dnf)" ]
 then
     source ./clean.sh
-    source ./_install-rhel.sh $PY_BINARY
+    source ./_install-rhel.sh $PY_BINARY ${INSTALL_PYTHON}
 elif [ "$(uname -s)" = "Darwin" ]
 then
     source ./clean.sh
-    source ./_install-mac.sh $PY_BINARY
+    source ./_install-mac.sh $PY_BINARY ${INSTALL_PYTHON}
 elif [ "$(type -p zypper)" ]
 then
     source ./clean.sh
-    source ./_install-suse.sh $PY_BINARY
+    source ./_install-suse.sh $PY_BINARY ${INSTALL_PYTHON}
 else
     echo "install.sh: Unsupported OS: could not detect Mac, apt-get, yum or zypper." >&2
     exit 1
