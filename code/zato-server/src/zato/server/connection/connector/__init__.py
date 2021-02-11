66fd0b1a216830f5bf781e74a0bb417312fd5f73
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-02-11 22:56:16 +0100

    GH #1125 - Adding typing_extensions to ext libraries.

--- code/zato-server/src/zato/server/connection/connector/__init__.py
+++ code/zato-server/src/zato/server/connection/connector/__init__.py
@@ -19,12 +19,13 @@ from gevent import sleep, spawn
 from gevent.lock import RLock
 
 # Zato
+from zato.common.typing_ import TypedDict
 from zato.common.util.api import spawn_greenlet
 
 # ################################################################################################################################
 
 if 0:
-    from typing import Any, Callable
+    from typing import Any, Callable, Dict as dict_,
     from zato.common.model.connector import ConnectorConfig
 
     Any = Any
@@ -304,13 +305,13 @@ class ConnectorStore(object):
         self.type = type
         self.connector_class = connector_class
         self.parallel_server = parallel_server
-        self.connectors = {}
+        self.connectors = {} # type: dict_[str, Connector]
         self.lock = RLock()
 
 # ################################################################################################################################
 
     def _create(self, name, config, on_message_callback=None, auth_func=None, channels=None, outconns=None, needs_start=False):
-        # type: (str, dict, Callable, Callable, dict, dict, bool)
+        # type: (str, ConnectorConfig, Callable, Callable, dict, dict, bool)
         connector = self.connector_class(
             name, self.type, config, on_message_callback, auth_func, channels, outconns, self.parallel_server)
         self.connectors[name] = connector
@@ -320,7 +321,7 @@ class ConnectorStore(object):
 # ################################################################################################################################
 
     def create(self, name, config, on_message_callback=None, auth_func=None, channels=None, outconns=None, needs_start=False):
-        # type: (str, dict, Callable, Callable, dict, dict, bool)
+        # type: (str, ConnectorConfig, Callable, Callable, dict, dict, bool)
         with self.lock:
             self._create(name, config, on_message_callback, auth_func, channels, outconns, needs_start)
 
