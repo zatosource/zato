a8509768cc3cf6795f6af1deec1f78d2e0a2b9bc
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-02-11 17:39:57 +0100

    GH #262 - Running flake8 cleanup in zato-web-admin.

--- code/zato-web-admin/src/zato/admin/urls.py
+++ code/zato-web-admin/src/zato/admin/urls.py
@@ -1040,7 +1040,7 @@ urlpatterns += [
     url(r'^zato/channel/wsx/connection/invoke/action/(?P<pub_client_id>.*)/$',
         login_required(channel_web_socket.invoke_action), name='channel-web-socket-invoke-action'),
 
-    url(r'^zato/channel/wsx/connection/invoke/(?P<conn_id>.*)/(?P<pub_client_id>.*)/(?P<ext_client_id>.*)/(?P<ext_client_name>.*)/(?P<channel_id>.*)/(?P<channel_name>.*)/$',
+    url(r'^zato/channel/wsx/connection/invoke/(?P<conn_id>.*)/(?P<pub_client_id>.*)/(?P<ext_client_id>.*)/(?P<ext_client_name>.*)/(?P<channel_id>.*)/(?P<channel_name>.*)/$', # noqa: E501
         login_required(channel_web_socket.invoke), name='channel-web-socket-invoke'),
 
     url(r'^zato/channel/wsx/connection-list/(?P<id>.*)/delete/(?P<pub_client_id>.*)/cluster/(?P<cluster_id>.*)/$',
