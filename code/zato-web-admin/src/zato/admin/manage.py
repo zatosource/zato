e37a8b7646c9565d437511cded51b43f646ff1dd
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-02-23 00:41:14 +0100

    GH #678 - Do not install pymysql as a replacement for MySQLdb.

--- code/zato-web-admin/src/zato/admin/manage.py
+++ code/zato-web-admin/src/zato/admin/manage.py
@@ -8,12 +8,6 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-try:
-    import pymysql
-    pymysql.install_as_MySQLdb()
-except ImportError:
-    pass
-
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
