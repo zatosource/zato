fc6847400bfb054e317ed5615d8d0412e9bc4dcf
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-03-29 20:12:56 +0200

    Adding config parsing utils.

--- code/zato-common/src/zato/common/scheduler.py
+++ code/zato-common/src/zato/common/scheduler.py
@@ -66,6 +66,62 @@ service=zato.pubsub.cleanup-service
 extra=
 """
 
+def absolutize(path, base=''):
+    """ Turns a relative path into an absolute one or returns it as is if it's already absolute.
+    """
+    import os
+    from os.path import isabs
+
+    if not isabs(path):
+        path = os.path.expanduser(path)
+
+    if not isabs(path):
+        path = os.path.normpath(os.path.join(base, path))
+
+    return path
+
+def _get_config(conf, bunchified, needs_user_config, repo_location=None):
+    # type: (bool, bool, str) -> Bunch
+
+    import os
+    from bunch import bunchify
+    from configobj import ConfigObj
+
+    conf = bunchify(conf) if bunchified else conf
+
+    if needs_user_config:
+        conf.user_config_items = {}
+
+        user_config = conf.get('user_config')
+        if user_config:
+            for name, path in user_config.items():
+                path = absolutize(path, repo_location)
+                if not os.path.exists(path):
+                    logger.warn('User config not found `%s`, name:`%s`', path, name)
+                else:
+                    user_conf = ConfigObj(path)
+                    user_conf = bunchify(user_conf) if bunchified else user_conf
+                    conf.user_config_items[name] = user_conf
+
+    return conf
+
+def get_config_from_string(data):
+    """ A simplified version of get_config which creates a config object from string, skipping any user-defined config files.
+    """
+    # type: (str) -> Bunch
+    from io import StringIO
+    from configobj import ConfigObj
+
+    buff = StringIO()
+    buff.write(data)
+    buff.seek(0)
+
+    conf = ConfigObj(buff)
+    out = _get_config(conf, True, False)
+
+    buff.close()
+    return out
+
 # ################################################################################################################################
 # ################################################################################################################################
 
