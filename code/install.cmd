f3b97998e6a79a2e5595484e24a299416db84eaf
Anielkis Herrera <aherrera@zato.io>
2021-04-10 23:38:40 -0500

    GH #1070 - Install Git and Build Tools

--- code/install.cmd
+++ code/install.cmd
@@ -1,4 +1,17 @@
 
+echo.&echo ============================================================================&echo Installing Git for Windows
+curl -OL https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe
+
+Git-2.31.1-64-bit.exe /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
+
+setx PATH "%PATH%%ProgramFiles%\Git;"
+
+echo.&echo ============================================================================&echo Installing Visual Studio Build Tools
+curl -OL "https://aka.ms/vs/16/release/vs_buildtools.exe"
+vs_buildtools.exe
+
+pip3 install virtualenv
+
 virtualenv --always-copy .
 call .\Scripts\activate.bat
 .\Scripts\pip install -r .\requirements.txt
