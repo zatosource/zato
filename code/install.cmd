64f720bc28640bfa55df9c6fd93056de6a993b76
Anielkis Herrera <aherrera@zato.io>
2021-04-13 00:27:24 -0500

    GH #1070 - Updating install.cmd

--- code/install.cmd
+++ code/install.cmd
@@ -1,28 +1,36 @@
 
-echo
-echo ============================================================================
-echo Installing Git for Windows
-curl -OL https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe
 
-Git-2.31.1-64-bit.exe /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
+$CURDIR = (Split-Path $myInvocation.MyCommand.Path) -join "`n"
 
-setx PATH "%PATH%%ProgramFiles%\Git\bin;%ProgramFiles%\Git\usr\bin;"
 
-echo.&echo ============================================================================&echo Installing Visual Studio Build Tools
-curl -OL "https://aka.ms/vs/16/release/vs_buildtools.exe"
-vs_buildtools.exe
+Write-Output "Installing Git for Windows"
+Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe" -OutFile "Git-2.31.1-64-bit.exe"
+& Git-2.31.1-64-bit.exe "/VERYSILENT" "/NORESTART" "/NOCANCEL" "/SP-" "/CLOSEAPPLICATIONS" "/RESTARTAPPLICATIONS" '/COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"'
 
-setx PATH "%PATH%%ProgramFiles(x86)%\Microsoft Visual Studio\Shared\Python37_64\;"
+# setx PATH "%PATH%%ProgramFiles%\Git\bin;%ProgramFiles%\Git\usr\bin;"
 
-echo
-echo ============================================================================
-echo Installing Python PIP
-curl -OL https://bootstrap.pypa.io/get-pip.py
+Write-Output "Installing Visual Studio Build Tools"
+Invoke-WebRequest -Uri "https://aka.ms/vs/16/release/vs_buildtools.exe" -OutFile "vs_buildtools.exe"
+& vs_buildtools.exe
 
-@REM TODO: Dynamic path in
-"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python" get-pip.py
-"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Scripts\pip3" install virtualenv
-"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Scripts\virtualenv" --always-copy .
+Write-Output "Installing Python"
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe" -OutFile "python-3.9.4-amd64.exe"
+& python-3.9.4-amd64.exe
+
+Write-Output "Installing Python PIP"
+Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
+
+& python get-pip.py
+& pip3 install virtualenv
+& python -m virtualenv --always-copy .
+
+
+if(!Test-Path "$CURDIR\release-info"){
+    New-Item -ItemType Directory -Name "$CURDIR\release-info"
+}
+New-Item -ItemType File -Name "$CURDIR\release-info\revision.txt"
+$revision = (git log -n 1 --pretty=format:"%H") -join "`n"
+Set-Content "$CURDIR\release-info\revision.txt" $revision
 
 call .\Scripts\activate.bat
 .\Scripts\pip install -r .\requirements.txt
@@ -42,26 +50,47 @@ call .\Scripts\activate.bat
 .\Scripts\pip install -e .\zato-sso
 .\Scripts\pip install -e .\zato-testing
 
+# ln -fs Lib/site-packages eggs
+New-Item -Path "$CURDIR\eggs" -ItemType SymbolicLink -Value "$CURDIR\Lib\site-packages"
 
-@REM TODO: create eggs symlink
-@REM ln -fs Lib/site-packages eggs
+if(!Test-Path "$CURDIR\zato_extra_paths"){
+    New-Item -ItemType Directory -Name "$CURDIR\zato_extra_paths"
+}
+Set-Content "$CURDIR\eggs\easy-install.pth" "$CURDIR\zato_extra_paths"
+
+# Create a symlink to zato_extra_paths to make it easier to type it out
+New-Item -Path "$CURDIR\extlib" -ItemType SymbolicLink -Value "$CURDIR\zato_extra_paths"
+# ln -fs $VIRTUAL_ENV/zato_extra_paths extlib
 
 
-@REM # Create and add zato_extra_paths to the virtualenv's sys.path.
-@REM mkdir zato_extra_paths
-@REM echo "$VIRTUAL_ENV/zato_extra_paths" >> eggs/easy-install.pth
-@REM # Create a symlink to zato_extra_paths to make it easier to type it out
-@REM ln -fs $VIRTUAL_ENV/zato_extra_paths extlib
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\butler\__init__.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\configobj.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\django\db\models\base.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 --binary -d eggs < patches\ntlm\HTTPNtlmAuthHandler.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\pykafka\topic.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\redis\redis\connection.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\requests\models.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\requests\sessions.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\ws4py\server\geventserver.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\sqlalchemy\sql\dialects\postgresql\pg8000.py.diff
+# "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\pg8000\core.py.diff
 
+New-Item -ItemType File -Name "$CURDIR\Scripts\zato"
+# $ZatoScriptContent = @"#! python
 
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\butler\__init__.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\configobj.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\django\db\models\base.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 --binary -d eggs < patches\ntlm\HTTPNtlmAuthHandler.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\pykafka\topic.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\redis\redis\connection.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\requests\models.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\requests\sessions.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\ws4py\server\geventserver.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\sqlalchemy\sql\dialects\postgresql\pg8000.py.diff
-@REM "C:\Program Files\Git\usr\bin\patch" --forward -p0 -d eggs < patches\pg8000\core.py.diff
+# # Zato
+# from zato.cli.zato_command import main
+
+# if __name__ == '__main__':
+
+#     # stdlib
+#     import re
+#     import sys
+
+#     # This is needed by SUSE
+#     sys.path.append('$VIRTUAL_ENV/lib64/python3.6/site-packages/')
+
+#     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+#     sys.exit(main())
+# "@
+# Set-Content "$CURDIR\Scripts\zato" $ZatoScriptContent
