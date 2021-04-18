78ae36a0b25cb3d65063422733231bda76bd6050
Anielkis Herrera <aherrera@zato.io>
2021-04-17 21:36:34 -0500

    GH #1070 - Updating code

--- code/install.ps1
+++ code/install.ps1
@@ -3,142 +3,67 @@ $CURDIR = (Split-Path $myInvocation.MyCommand.Path) -join "`n"
 $LocalAppDataPath = $env:LocalAppData
 $PythonPathVersion = "Python39"
 
-$installed = $null -ne (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -like "Git version*" })
-If(-Not $installed) {
-    Write-Output 'Installing Git for Windows'
-    $exeFile = 'Git-2.31.1-64-bit.exe'
-    if (-not(Test-Path "$CURDIR\$exeFile" -PathType Leaf)){
-        Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe' -OutFile $exeFile
-    }
-    $exeArgs = @('/VERYSILENT', '/NORESTART', '/NOCANCEL', '/SP-', '/CLOSEAPPLICATIONS', '/RESTARTAPPLICATIONS', '/COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"')
-    Start-Process -Filepath "$CURDIR/$exeFile" -ArgumentList $exeArgs -Wait
-} else {
-    Write-Host "Git for Windows is installed."
-}
+. .\_common.ps1
 
+# Git for Windows
+Invoke-InstallIfNotInstalled -ExeFile "Git-2.31.1-64-bit.exe" -URL 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe' -Name "Git for Windows" -InstalledRegistryRegExp "Git version*" -ExeArgs @('/VERYSILENT', '/NORESTART', '/NOCANCEL', '/SP-', '/CLOSEAPPLICATIONS', '/RESTARTAPPLICATIONS', '/COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"')
+
+# Git PATH
 If(-Not ("$env:PATH" -like "*$env:ProgramFiles\Git\usr\bin*")) {
     Write-Output 'Adding Git\usr\bin to PATH'
     $Env:Path += ";$env:ProgramFiles\Git\usr\bin\"
 }
 
-$installed = $null -ne (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -like "*Visual C++*" })
-If(-Not $installed) {
-    Write-Output 'Installing Visual Studio Build Tools'
-    Write-Output 'Please install C++ Build tools'
-    $exeFile = 'vs_buildtools.exe'
-    if (-not(Test-Path "$CURDIR\$exeFile" -PathType Leaf)) {
-        Invoke-WebRequest -Uri 'https://aka.ms/vs/16/release/vs_buildtools.exe' -OutFile $exeFile
-    }
-    Start-Process -Filepath "$CURDIR/$exeFile" -Wait
-} else {
-    Write-Host "Visual Studio Build Tools is installed."
-}
+# C++ Build tools
+Invoke-InstallIfNotInstalled -ExeFile "vs_buildtools.exe" -URL "https://aka.ms/vs/16/release/vs_buildtools.exe" -Name "Visual C++ Build tools" -InstalledRegistryRegExp "*Visual C++*"
 
-$installed = $null -ne (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -like "Python*" })
-If(-Not $installed) {
-    Write-Output 'Installing Python'
-    $exeFile = 'python-3.9.4-amd64.exe'
-    if (-not(Test-Path "$CURDIR\$exeFile" -PathType Leaf)) {
-        Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe' -OutFile $exeFile
-    }
-    Start-Process -Filepath "$CURDIR/$exeFile" -ArgumentList @('/quiet', 'SimpleInstall=1', 'PrependPath=1') -Wait
+# Python
+Invoke-InstallIfNotInstalled -ExeFile "python-3.9.4-amd64.exe" -URL 'https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe' -Name "Python" -InstalledRegistryRegExp "Python*" -ExeArgs @('/quiet', 'SimpleInstall=1', 'PrependPath=1')
 
-} else {
-    Write-Host "Python is installed."
-}
-
+# Python PATH
 If(-Not ("$env:PATH" -like "*\Python*")) {
     Write-Output 'Adding Python to PATH'
     $INCLUDE = "$LocalAppDataPath\Programs\$PythonPathVersion;$LocalAppDataPath\Programs\$PythonPathVersion\Scripts"
-    if (Test-Path "$LocalAppDataPath\Programs\Python\$PythonPathVersion") {
+    If(Test-Path "$LocalAppDataPath\Programs\Python\$PythonPathVersion") {
         $INCLUDE = "$LocalAppDataPath\Programs\Python\$PythonPathVersion;$LocalAppDataPath\Programs\Python\$PythonPathVersion\Scripts"
     }
     $Env:Path += ";$INCLUDE"
 }
-
 # Add Python permanently to PATH
 $oldPATH = $Env:Path
 $Env:Path = $oldPATH.Replace('-', '&')
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $env:PATH
 
-Write-Output "PATH:"
-Write-Output ($env:PATH).split(";")
-
+# virtualenv
 If(-Not (Get-Command "virtualenv.exe" | Select-Object -ExpandProperty Definition)) {
     Write-Output 'Installing virtualenv'
     Start-Process -Filepath (Get-Command "pip.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('install', 'virtualenv') -Wait
 }
 
-if (-not(Test-Path ".\Lib")) {
-    Write-Output 'Creating virtualenv environment'
+# virtual environment
+If(-not(Test-Path ".\Lib")) {
+    Write-Output 'Creating virtual environment'
     Start-Process -Filepath (Get-Command "python.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('-m', 'virtualenv', '--always-copy', '.') -Wait
 }
 
-if (-not(Test-Path ".\release-info")) {
-    New-Item -ItemType Directory -Name ".\release-info"
-}
-if (-not(Test-Path ".\release-info\revision.txt" -PathType Leaf)) {
-    New-Item -ItemType File -Name ".\release-info\revision.txt"
-    $revision = (git log -n 1 --pretty=format:"%H") -join "`n"
-    Set-Content ".\release-info\revision.txt" $revision
-}
+New-Item -ItemType Directory -Name ".\release-info" -Force
+
+$revision = (git log -n 1 --pretty=format:"%H") -join "`n"
+New-Item -ItemType File -Name ".\release-info\revision.txt" -Force -Value $revision
 
 Start-Process -Filepath (Get-Command python | Select-Object -ExpandProperty Definition) -ArgumentList @('-m', 'virtualenv', '--always-copy', '.') -Wait
 
-Set-ExecutionPolicy Unrestricted -Scope Process
-
 .\Scripts\activate.ps1
-.\Scripts\pip.exe install -r .\requirements.txt
-.\Scripts\pip.exe install -e .\zato-common
-.\Scripts\pip.exe install -e .\zato-agent
-.\Scripts\pip.exe install -e .\zato-broker
-.\Scripts\pip.exe install -e .\zato-cli
-.\Scripts\pip.exe install -e .\zato-client
-.\Scripts\pip.exe install -e .\zato-cy
-.\Scripts\pip.exe install -e .\zato-distlock
-.\Scripts\pip.exe install -e .\zato-hl7
-.\Scripts\pip.exe install -e .\zato-lib
-.\Scripts\pip.exe install -e .\zato-scheduler
-.\Scripts\pip.exe install -e .\zato-server
-.\Scripts\pip.exe install -e .\zato-web-admin
-.\Scripts\pip.exe install -e .\zato-zmq
-.\Scripts\pip.exe install -e .\zato-sso
-.\Scripts\pip.exe install -e .\zato-testing
+Invoke-InstallAllWithPip
 
-if (-not(Test-Path ".\eggs" -PathType Any)) {
-    New-Item -Path ".\eggs" -ItemType SymbolicLink -Value ".\Lib\site-packages"
-}
+New-Symlink -Target ".\eggs" -Link ".\Lib\site-packages"
+New-Item -ItemType File -Name ".\eggs\easy-install.pth" -Force -Value "$CURDIR\zato_extra_paths"
 
-if (-not(Test-Path ".\zato_extra_paths")) {
-    New-Item -ItemType Directory -Name ".\zato_extra_paths"
-}
+New-Item -ItemType Directory -Name ".\zato_extra_paths" -Force
+New-Symlink -Target ".\extlib" -Link ".\zato_extra_paths"
 
-if (-not(Test-Path ".\release-info\revision.txt" -PathType Leaf)) {
-    New-Item -ItemType File -Name ".\release-info\revision.txt"
-    $revision = (git log -n 1 --pretty=format:"%H") -join "`n"
-    Set-Content ".\release-info\revision.txt" $revision
-}
-if (-not(Test-Path ".\eggs\easy-install.pth" -PathType Leaf)) {
-    Set-Content ".\eggs\easy-install.pth" "$CURDIR\zato_extra_paths"
-}
 
-if (-not(Test-Path ".\extlib" -PathType Any)) {
-    New-Item -Path ".\extlib" -ItemType SymbolicLink -Value ".\zato_extra_paths"
-}
+# Apply patches
+Invoke-ApplyPatches
 
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\butler\__init__.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\configobj.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\django\db\models\base.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '--binary', '-d', 'eggs', '-i', 'patches\ntlm\HTTPNtlmAuthHandler.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\pykafka\topic.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\redis\redis\connection.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\requests\models.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\requests\sessions.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\ws4py\server\geventserver.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\sqlalchemy\sql\dialects\postgresql\pg8000.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\pg8000\core.py.diff') -Wait
-Start-Process -Filepath (Get-Command "$env:ProgramFiles\Git\usr\bin\patch.exe" | Select-Object -ExpandProperty Definition) -ArgumentList @('--forward', '-p0', '-d', 'eggs', '-i', 'patches\sqlalchemy\sql\crud.py.diff') -Wait
-
-if (-not(Test-Path ".\Scripts\zato.py" -PathType Leaf)) {
-    Copy-Item ".\extras\zato-windows.py" -Destination ".\Scripts\zato.py"
-}
+Copy-Item ".\extras\zato-windows.py" -Destination ".\Scripts\zato.py" -Force
