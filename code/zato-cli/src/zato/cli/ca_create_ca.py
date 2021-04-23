e748cb182eb7c571a5d6620b8f72be0601db61c9
Anielkis Herrera <aherrera@zato.io>
2021-04-22 20:50:53 -0500

    GH #1070 - Updating code for Windows compatibility

--- code/zato-cli/src/zato/cli/ca_create_ca.py
+++ code/zato-cli/src/zato/cli/ca_create_ca.py
@@ -21,11 +21,11 @@ dir                            = {target_dir}
 default_ca                     = CA_default
 
 [ CA_default ]
-serial                         = $dir\\ca-material\\ca-serial
-database                       = $dir\\ca-material\\ca-certindex
-new_certs_dir                  = $dir
-certificate                    = $dir\\ca-material\\ca-cert.pem
-private_key                    = $dir\\ca-material\\ca-key.pem
+serial                         = {ca_serial}
+database                       = {ca_certindex}
+new_certs_dir                  = {target_dir_rel}
+certificate                    = {ca_key}
+private_key                    = {private_key}
 default_days                   = 3650
 default_md                     = sha1
 preserve                       = no
@@ -119,7 +119,12 @@ class Create(ZatoCommand):
             template_args[name] = value
 
         template_args['common_name'] = self._get_arg(args, 'common_name', default_ca_name)
-        template_args['target_dir'] = self.target_dir
+        template_args['target_dir'] = self.target_dir.replace('\\','/')
+        template_args['ca_serial'] = os.path.relpath(os.path.join(self.target_dir, 'ca-material', 'ca-serial')).replace('\\','/')
+        template_args['ca_certindex'] = os.path.relpath(os.path.join(self.target_dir, 'ca-material', 'ca-certindex')).replace('\\','/')
+        template_args['target_dir_rel'] = os.path.relpath(self.target_dir).replace('\\','/')
+        template_args['ca_key'] = os.path.relpath(os.path.join(self.target_dir, 'ca-material', 'ca-cert.pem')).replace('\\','/')
+        template_args['private_key'] = os.path.relpath(os.path.join(self.target_dir, 'ca-material', 'ca-key.pem')).replace('\\','/')
 
         f = tempfile.NamedTemporaryFile(mode='w+')
         f.write(openssl_template.format(**template_args))
@@ -129,9 +134,9 @@ class Create(ZatoCommand):
                    {ca_key} -out {ca_cert} -days 3650 \
                    -config {config} -passout file:{ca_password}""".format(
                        config=f.name, target_dir=self.target_dir,
-                       ca_key=os.path.join(self.target_dir, 'ca-material', 'ca-key.pem'),
-                       ca_cert=os.path.join(self.target_dir, 'ca-material', 'ca-cert.pem'),
-                       ca_password=os.path.relpath(os.path.join(self.target_dir, 'ca-material', 'ca-password'))
+                       ca_key=os.path.join(self.target_dir, 'ca-material', 'ca-key.pem').replace('\\','\\\\'),
+                       ca_cert=os.path.join(self.target_dir, 'ca-material', 'ca-cert.pem').replace('\\','\\\\'),
+                       ca_password=os.path.relpath(os.path.join(self.target_dir, 'ca-material', 'ca-password').replace('\\','\\\\'))
                        )
         os.system(cmd)
         f.close()
