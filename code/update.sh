2559d25b36148f7ebf1b8bd9e4c792837300ad67
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-04-30 11:08:46 +0200

    Porting 44d4a71427c840b3641f708cf6cbd9367e2148a9 to support/3.1-v2.

--- code/update.sh
+++ code/update.sh
@@ -7,6 +7,19 @@ N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
 echo "*** Downloading updates ***"
 git -C $CURDIR pull
 
+while getopts "c:" opt; do
+    case "$opt" in
+    c)
+        ZATO_BRANCH=$OPTARG
+        ;;
+    esac
+done
+
+if [[ -n "${ZATO_BRANCH}" ]];then
+    # Checkout a local branch/commit or create the branch from the remote one
+    git checkout "${ZATO_BRANCH}" 2>/dev/null || git checkout -b "${ZATO_BRANCH}" "origin/${ZATO_BRANCH}"
+fi
+
 # Uninstall old dependencies
 $CURDIR/bin/pip uninstall -y sec-wall
 $CURDIR/bin/pip uninstall -y springpython
