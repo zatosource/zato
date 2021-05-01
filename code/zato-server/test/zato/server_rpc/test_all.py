29141ed564441a233252c0187944b1bae0191f07
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-05-01 19:49:48 +0200

    GH #1125 - Working on unit-tests.

--- code/zato-server/test/zato/server_rpc/test_all.py
+++ code/zato-server/test/zato/server_rpc/test_all.py
@@ -10,12 +10,13 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from unittest import main, TestCase
 
 # Zato
-from zato.server.connection.server.rpc import LocalServer, Server, ServerRPC
+from zato.server.connection.server.rpc import ConfigCtx, LocalServerInvoker, ODBConfigSource, RemoteServerInvoker, \
+     ServerInvoker, ServerRPC
 
 # ################################################################################################################################
 # ################################################################################################################################
 
-class _FakeParallelServer:
+class _FakeCluster:
     def __init__(self, name):
         # type: (str) -> None
         self.name = name
@@ -23,31 +24,50 @@ class _FakeParallelServer:
 # ################################################################################################################################
 # ################################################################################################################################
 
+class _FakeParallelServer:
+    def __init__(self, cluster, odb, server_name):
+        # type: (_FakeCluster, object, str) -> None
+        self.cluster = cluster
+        self.odb = odb
+        self.name = server_name
+
+# ################################################################################################################################
+# ################################################################################################################################
+
 class ServerRPCTestCase(TestCase):
 
-    def xtest_get_item_local_server(self):
+    def test_get_item_local_server(self):
 
+        cluster_name = 'cluster.1'
         server_name = 'abc'
-        parallel_server = _FakeParallelServer(server_name)
+
+        cluster = _FakeCluster(cluster_name)
+        parallel_server = _FakeParallelServer(cluster, None, server_name)
 
         # These three will not be called because the server is local
         get_remote_server_func = object
         get_server_list_func = object
         decrypt_func = object
 
-        rpc = ServerRPC(parallel_server, get_remote_server_func, get_server_list_func, decrypt_func)
+        config_source = ODBConfigSource(parallel_server.odb, cluster.name, parallel_server.name)
+        config_ctx = ConfigCtx(config_source, parallel_server)
+
+        rpc = ServerRPC(config_ctx)
 
         server = rpc[server_name]
 
-        self.assertIsInstance(server, Server)
-        self.assertIsInstance(server, LocalServer)
+        self.assertIsInstance(server, ServerInvoker)
+        self.assertIsInstance(server, LocalServerInvoker)
 
 # ################################################################################################################################
 
     def test_get_item_remote_server(self):
 
+        cluster_name = 'cluster.1'
         server_name = 'abc'
-        parallel_server = _FakeParallelServer(server_name)
+
+        cluster = _FakeCluster(cluster_name)
+        parallel_server = _FakeParallelServer(cluster, None, server_name)
 
         get_remote_server_func = object
         decrypt_func = object
@@ -55,9 +75,15 @@ class ServerRPCTestCase(TestCase):
         # This one will not be called because we access a specific server by name
         get_server_list_func = object
 
-        rpc = ServerRPC(parallel_server, get_remote_server_func, get_server_list_func, decrypt_func)
+        config_source = ODBConfigSource(parallel_server.odb, cluster.name, parallel_server.name)
+        config_ctx = ConfigCtx(config_source, parallel_server)
+
+        rpc = ServerRPC(config_ctx)
 
         server = rpc['not-local']
+
+        self.assertIsInstance(server, ServerInvoker)
+        self.assertIsInstance(server, RemoteServerInvoker)
 
 # ################################################################################################################################
 # ################################################################################################################################
