d12f18d7e34652c921030945f56228f51cccd806
Anielkis Herrera <aherrera@zato.io>
2021-05-01 18:21:34 -0500

    GH #1070 - Adding Windows to TravisCI

--- .travis/test_installing.sh
+++ .travis/test_installing.sh
@@ -9,5 +9,7 @@ if [[ "${TRAVIS_OS_NAME}" == "windows" ]];then
     [[ -f ./install.ps1 ]]
     powershell ./install.ps1
     cd $TRAVIS_BUILD_DIR
+    # Adjusting path for Windows
+    find -type f -name Makefile -exec sed -i -e 's|/bin|/Scripts|' {} \;
     make
 fi
