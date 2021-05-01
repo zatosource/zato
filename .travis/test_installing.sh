0b233d5d2b5d9acc04d22be6cf375693524caeb0
Anielkis Herrera <aherrera@zato.io>
2021-05-01 17:50:28 -0500

    GH #1070 - Adding Windows to TravisCI

--- .travis/test_installing.sh
+++ .travis/test_installing.sh
@@ -4,8 +4,9 @@ set -ex
 
 if [[ "${TRAVIS_OS_NAME}" == "windows" ]];then
     powershell -Command 'Set-ExecutionPolicy -ExecutionPolicy RemoteSigned'
-    [[ -f $TRAVIS_BUILD_DIR/code/install.ps1 ]]
-    powershell $TRAVIS_BUILD_DIR/code/install.ps1
+    cd $TRAVIS_BUILD_DIR/code
+    [[ -f ./install.ps1 ]]
+    powershell ./install.ps1
     cd $TRAVIS_BUILD_DIR
     make
 fi
