fa8d1664ceb1da07d952dfdc5a47eb2da5038c48
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-05-22 18:24:23 +0200

    GH #820 - Added FlowPRTAPI.change_password.

--- code/zato-sso/test/zato/test_flow_prt.py
+++ code/zato-sso/test/zato/test_flow_prt.py
@@ -11,6 +11,7 @@ from unittest import main
 
 # Zato
 from base import BaseTest
+from zato.common.crypto.api import CryptoManager
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -19,7 +20,13 @@ class FlowPRTTestCase(BaseTest):
 
 # ################################################################################################################################
 
-    def test_user_create_token(self):
+    def test_user_reset_password(self):
+
+        # This needs to be filled in manually
+        token = 'q63mn38ah9hav2vaz32625azj'
+
+        # Generated in each test
+        password = CryptoManager.generate_password(to_str=True)
 
         # Request a new PRT ..
         response = self.post('/zato/sso/flow/prt', {
@@ -27,16 +34,24 @@ class FlowPRTTestCase(BaseTest):
         })
 
         # .. access the PRT received ..
-        self.patch('/zato/sso/flow/prt', {
-            'token': 'ttpmrh58v8c0841prbgvj7yqx',
+        response = self.patch('/zato/sso/flow/prt', {
+            'token': token,
         })
 
-        # .. change the password ..
-        # response = self.delete('/zato/sso/flow/prt', {
-        #    'credential': 'admin3',
-        # })
+        # .. read the reset key received from the .patch call ..
+        reset_key = response.reset_key
 
-        # .. confirm that the new password can be used for logging in.
+        # .. change the password now ..
+        response = self.delete('/zato/sso/flow/prt', {
+            'token': token,
+            'reset_key': reset_key,
+            'password': password
+        })
+
+        # .. confirm the status ..
+        self.assertEqual(response.status, 'ok')
+
+        # .. finally, confirm that the new password can be used for logging in.
 
 # ################################################################################################################################
 # ################################################################################################################################
