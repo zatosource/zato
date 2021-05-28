cee67dec45aa3344c16bbcd95c68c846da898b84
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-05-28 19:41:47 +0200

    GH #1077 - Working on statistics.

--- code/zato-server/src/zato/server/connection/connector/subprocess_/impl/events/client.py
+++ code/zato-server/src/zato/server/connection/connector/subprocess_/impl/events/client.py
@@ -6,6 +6,9 @@ Copyright (C) 2021, Zato Source s.r.o. https://zato.io
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# gevent
+from gevent import socket
+
 # Requests
 from requests.sessions import Session
 
@@ -17,30 +20,32 @@ from zato.common.json_internal import dumps
 
 class Client:
     def __init__(self):
-        self.address = ''
+        self.address = None # type: tuple
         self.username = ''
         self.password = ''
         self.auth = None # type: tuple
-        self.session = None # type: Session
 
 # ################################################################################################################################
 
     def init(self):
-        self.auth = (self.username, self.password)
-        self.session = Session()
+        self.socket = socket.socket(type=socket.SOCK_STREAM)
+        self.socket.connect(self.address)
 
 # ################################################################################################################################
 
     def push(self, data):
         # type: (dict) -> None
         data = dumps(data)
-        self.session.post(self.address, data, auth=self.auth)
+        self.socket.send(data.encode('utf8') + b'\n')
+
+    def close(self):
+        self.socket.close()
 
 # ################################################################################################################################
 # ################################################################################################################################
 
 if __name__ == '__main__':
-    address = 'http://localhost:44011/api'
+    address = ('127.0.0.1', 44011)
     username = 'aaa'
     password = 'bbb'
 
@@ -55,7 +60,7 @@ if __name__ == '__main__':
         'data': {}
     }
 
-    n = 400_000
+    n = 60_000
     total = 0
 
     from datetime import datetime
@@ -89,7 +94,151 @@ if __name__ == '__main__':
             }
             client.push(event)
             total += 1
-            print('TOTAL', total)
+            #print('TOTAL', total)
+
+    client.close()
+
+    import time
+    time.sleep(0.1)
 
 # ################################################################################################################################
 # ################################################################################################################################
+
+'''
+# -*- coding: utf-8 -*-
+
+# stdlib
+from datetime import datetime
+
+# Amazon Ion
+import amazon.ion.simpleion as ion
+
+# gevent
+from gevent import spawn, socket
+
+# geventhttpclient
+from geventhttpclient import HTTPClient
+from geventhttpclient.url import URL
+
+# Requests
+from requests import post
+from requests.sessions import Session
+
+# Zato
+from zato.common.json_internal import dumps
+from zato.common.util import spawn_greenlet
+from zato.server.service import Service
+
+address = ('127.0.0.1', 44011)
+
+socket = socket.socket(type=socket.SOCK_STREAM)
+socket.connect(address)
+
+utcnow = datetime.utcnow
+
+'''
+# ################################################################################################################################
+# ################################################################################################################################
+
+class _Session(Session):
+    def merge_environment_settings(self, *ignored_args, **ignored_kwargs):
+        return {'verify': None, 'proxies': None, 'stream': None,
+                'cert': None}
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+#session = _Session()
+
+url = URL('http://localhost:44011')
+client = HTTPClient(url.host, concurrency=1500)
+'''
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class PasswordResetCreateToken(Service):
+    def handle(self):
+
+        current_app = 'CRM'
+        remote_addr = '127.0.0.1'
+        user_agent = self.request.http.user_agent
+
+        # This can be either username or email,
+        # in this particular case it is a username.
+        credential = 'my.username'
+
+        # This will never return an explicit indication
+        # whether the credential was valid or not.
+        self.sso.password_reset.create_token(
+            self.cid, credential, current_app, remote_addr, user_agent)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class PasswordResetAccessToken(Service):
+    def handle(self):
+
+        elem = '111'
+        x = 111
+
+        data = {
+            'action': '107400',
+            'data': {
+                'id': elem + utcnow().isoformat(),
+                'cid': 'cid.' + elem,
+                'timestamp': '2021-05-12T07:07:01.4841' + elem,
+
+                'source_type': 'zato.server' + elem,
+                'source_id': 'server1' + elem,
+
+                'object_type': elem,
+                'object_id': elem,
+
+                'source_type': elem,
+                'source_id': elem,
+
+                'recipient_type': elem,
+                'recipient_id': elem,
+
+                'total_time_ms': x,
+            }
+        }
+
+        data = 'aaa' * 50 #dumps(data)
+
+        with self.server.user_ctx_lock:
+            socket.send(data.encode('utf8') + b'\n')
+
+        #self.logger.warn('QQQ %s', self.server.pid)
+
+        #spawn(session.post, 'http://localhost:44011', '', auth=('aaa', 'bbb'))
+
+        #spawn(client.post, '/', '{}')
+
+        #response = client.post('/', '{}')
+
+        #print(111, response.get_code())
+
+        #self.logger.warn('QQQ %s', client)
+
+        '''
+        current_app = 'CRM'
+        remote_addr = '127.0.0.1'
+        user_agent = self.request.http.user_agent
+
+        # Frontend reads it from the link that the user clicked
+        token = '5k339rv63q82er5wq9qgfw9xkd'
+
+        # The reset key, along with the initial token, can be used
+        # to change the password.
+        reset_key = self.sso.password_reset.access_token(
+            self.cid, token, current_app, remote_addr, user_agent)
+
+        # Return the reset key to our caller
+        self.response.payload = {'reset_key': reset_key}
+        '''
+
+# ################################################################################################################################
+# ################################################################################################################################
+'''
--- code/zato-server/src/zato/server/connection/connector/subprocess_/impl/events/container.py
+++ code/zato-server/src/zato/server/connection/connector/subprocess_/impl/events/container.py
@@ -10,6 +10,50 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from gevent.monkey import patch_all
 patch_all()
 
+# gevent
+from gevent.server import StreamServer
+
+
+# this handler will be run for each incoming connection in a dedicated greenlet
+def echo(socket, address):
+    print('New connection from %s:%s' % address)
+    socket.sendall(b'Welcome to the echo server! Type quit to exit.\r\n')
+    # using a makefile because we want to use readline()
+    rfileobj = socket.makefile(mode='rb')
+    while True:
+        line = rfileobj.readline()
+        if not line:
+            print("client disconnected")
+            break
+        if line.strip().lower() == b'quit':
+            print("client quit")
+            break
+        #socket.sendall(line)
+        #print("echoed %r" % line)
+    rfileobj.close()
+
+if __name__ == '__main__':
+    # to make the server use SSL, pass certfile and keyfile arguments to the constructor
+    server = StreamServer(('127.0.0.1', 44011), echo)
+    # to start the server asynchronously, use its start() method;
+    # we use blocking serve_forever() here because we have no other jobs
+    print('Starting echo server on port 44011')
+    server.serve_forever()
+
+
+'''
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2021, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# This needs to be first
+from gevent.monkey import patch_all
+patch_all()
+
 # stdlib
 from datetime import datetime
 from tempfile import NamedTemporaryFile
@@ -123,3 +167,4 @@ if __name__ == '__main__':
     container.run()
 
 # ################################################################################################################################
+'''
