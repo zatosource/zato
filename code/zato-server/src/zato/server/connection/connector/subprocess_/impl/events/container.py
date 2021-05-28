d01c0b0b96771cdf90dd4fd39f6b677294bdc698
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-05-28 18:17:41 +0200

    GH #1077 - Working on event containers.

--- code/zato-server/src/zato/server/connection/connector/subprocess_/impl/events/container.py
+++ code/zato-server/src/zato/server/connection/connector/subprocess_/impl/events/container.py
@@ -6,19 +6,28 @@ Copyright (C) 2021, Zato Source s.r.o. https://zato.io
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# This needs to be first
+from gevent.monkey import patch_all
+patch_all()
+
 # stdlib
 from datetime import datetime
 from tempfile import NamedTemporaryFile
+from time import time
 from traceback import format_exc
 
 # Bunch
 from bunch import bunchify
+
+# gevent
+from gevent.pywsgi import WSGIHandler, WSGIServer
 
 # Zato
 from zato.common.api import SFTP
 from zato.common.json_internal import dumps
 from zato.common.sftp import SFTPOutput
 from zato.server.connection.connector.subprocess_.base import BaseConnectionContainer, Response
+from zato.server.connection.connector.subprocess_.impl.events.database import EventsDatabase
 
 # ################################################################################################################################
 
@@ -26,6 +35,25 @@ if 0:
     from bunch import Bunch
 
     Bunch = Bunch
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+utcnow = datetime.utcnow
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class _WSGIHandler(WSGIHandler):
+    def log_request(self, *ignored_args, **ignored_kwargs):
+        pass
+
+class _WSGIServer(WSGIServer):
+    handler_class = _WSGIHandler
+
+    def shutdown(self, *ignored_args, **ignored_kwargs):
+        # Do nothing, added only for API completeness with what base.py expects
+        pass
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -40,32 +68,52 @@ class EventsConnectionContainer(BaseConnectionContainer):
 
 # ################################################################################################################################
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        fs_data_path = '/tmp/zzz-parquet'
+        sync_threshold = 120_000
+        sync_interval_ms = 120_000
+
+        self.events_db = EventsDatabase(fs_data_path, sync_threshold, sync_interval_ms)
+
+# ################################################################################################################################
+
     def _on_EVENT_PUSH(self, msg, is_reconnect=False, _utcnow=datetime.utcnow):
         out = {}
+        #data = msg['data']
 
-        #self.logger.warn('QQQ _on_EVENT_PUSH_EXECUTE *********************************')
+        elem = 'aaa' + utcnow().isoformat()
+        x = int(time() * 1_000_000)
 
-        '''
-        connection = self.connections[msg.id] # type: SFTPConnection
-        start_time = _utcnow()
+        data = {
+            'id': elem + utcnow().isoformat(),
+            'cid': 'cid.' + elem,
+            'timestamp': '2021-05-12T07:07:01.4841' + elem,
 
-        try:
-            result = connection.execute(msg.cid, msg.data, msg.log_level) # type: SFTPOutput
-        except ErrorReturnCode as e:
-            out['stdout'] = e.stdout
-            out['stderr'] = e.stderr
-        except Exception:
-            out['stderr'] = format_exc()
-            out['is_ok'] = False
-        else:
-            out.update(result.to_dict())
-        finally:
-            out['cid'] = msg.cid
-            out['command_no'] = connection.command_no
-            out['response_time'] = str(_utcnow() - start_time)
-            '''
+            'source_type': 'zato.server' + elem,
+            'source_id': 'server1' + elem,
+
+            'object_type': elem,
+            'object_id': elem,
+
+            'source_type': elem,
+            'source_id': elem,
+
+            'recipient_type': elem,
+            'recipient_id': elem,
+
+            'total_time_ms': x,
+        }
+
+        self.events_db.push(data)
 
         return Response(data=dumps(out))
+
+# ################################################################################################################################
+
+    def make_server(self):
+        return _WSGIServer((self.host, self.port), self.on_wsgi_request)
 
 # ################################################################################################################################
 
