86872855603eb744436d5d793315dbcf0afc2415
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-06-05 20:00:59 +0200

    GH #1077 - Working on Events DB clients.

--- code/zato-common/src/zato/common/events/client.py
+++ code/zato-common/src/zato/common/events/client.py
@@ -9,12 +9,25 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 # stdlib
 import logging
 import socket
+from datetime import datetime
 from logging import basicConfig, getLogger
 
+# orjson
+from orjson import dumps
+
 # Zato
-from zato.common.events.common import Action
+from zato.common.events.common import Action, PushCtx
+from zato.common.typing_ import asdict
 from zato.common.util.api import new_cid
 from zato.common.util.tcp import read_from_socket, SocketReaderCtx, wait_until_port_taken
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if 0:
+    from zato.common.events.common import PushCtx
+
+    PushCtx = PushCtx
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -36,6 +49,7 @@ class Client:
         self.port = port
         self.socket = socket.socket(type=socket.SOCK_STREAM)
         self.peer_name = '<Client-peer_name-default>'
+        self.peer_name_str = '<Client-peer_name_str-default>'
         self.conn_id = 'zstrcl' + new_cid(bytes=4)
         self.max_wait_time = 30
         self.max_msg_size = 30_000_000
@@ -48,17 +62,18 @@ class Client:
     def connect(self):
         self.socket.connect((self.host, self.port))
         self.peer_name = self.socket.getpeername()
+        self.peer_name_str = '{}:{}'.format(*self.peer_name)
 
 # ################################################################################################################################
 
-    def send(self, data):
+    def send(self, action, data=b''):
         # type: (bytes) -> None
-        self.socket.sendall(data + b'\n')
+        self.socket.sendall(action + data + b'\n')
 
 # ################################################################################################################################
 
     def ping(self):
-        logger.info('Pinging %s (%s)', self.peer_name)
+        logger.info('Pinging %s (%s)', self.peer_name_str, self.conn_id)
 
         # Send the ping message ..
         self.send(Action.Ping)
@@ -78,9 +93,22 @@ class Client:
         response = read_from_socket(ctx)
 
         # .. and raise an exception in case of any error.
-        print()
-        print(111, response)
-        print()
+        if response != Action.PingReply:
+            raise ValueError('Unexpected response received from `{}` -> `{}`'.format(self.peer_name, response))
+
+# ################################################################################################################################
+
+    def push(self, ctx):
+        # type: (PushCtx) -> None
+
+        # Serialise the context to dict ..
+        data = asdict(ctx)
+
+        # .. now to JSON ..
+        data = dumps(data)
+
+        # .. and send it across (there will be no response).
+        self.send(Action.Push, data)
 
 # ################################################################################################################################
 
@@ -113,6 +141,28 @@ if __name__ == '__main__':
 
     client = Client(host, port)
     client.run()
+
+    n_iters = 100_000
+
+    for x in range(n_iters):
+
+        if x % 1000 == 0:
+            logger.info('ZZZ %s', x)
+
+        ctx = PushCtx()
+        ctx.id = new_cid()
+        ctx.cid = new_cid()
+        ctx.object_id = new_cid()
+        ctx.object_type = '001'
+        ctx.recipient_id = new_cid()
+        ctx.recipient_type = 'ABC'
+        ctx.source_id = new_cid()
+        ctx.source_type = 'ZZZ'
+        ctx.timestamp = datetime.utcnow().isoformat()
+        ctx.total_time_ms = x
+
+        client.push(ctx)
+
     client.close()
 
     sleep(2)
