e0ce5d078376eb6e755e92cd22e9f4b1ad35b8f9
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-06-06 10:36:16 +0200

    GH #1077 - Reconnect stream clients if servers disconnect.

--- code/zato-common/src/zato/common/events/client.py
+++ code/zato-common/src/zato/common/events/client.py
@@ -12,6 +12,9 @@ import socket
 from datetime import datetime
 from logging import basicConfig, getLogger
 
+# gevent
+from gevent import sleep
+
 # orjson
 from orjson import dumps
 
@@ -42,12 +45,18 @@ logger = getLogger(__name__)
 # ################################################################################################################################
 # ################################################################################################################################
 
+utcnow = datetime.utcnow
+
+# ################################################################################################################################
+# ################################################################################################################################
+
 class Client:
     def __init__(self, host, port):
         # type: (str, int) -> None
         self.host = host
         self.port = port
-        self.socket = socket.socket(type=socket.SOCK_STREAM)
+        self.remote_addr_str = '{}:{}'.format(self.host, self.port)
+        self.socket = None # type: socket.socket
         self.peer_name = '<Client-peer_name-default>'
         self.peer_name_str = '<Client-peer_name_str-default>'
         self.conn_id = 'zstrcl' + new_cid(bytes=4)
@@ -56,27 +65,44 @@ class Client:
         self.read_buffer_size = 30_000_000
         self.recv_timeout = 30
         self.should_log_messages = True
+        self.is_connected = False
 
 # ################################################################################################################################
 
     def connect(self):
-        self.socket.connect((self.host, self.port))
-        self.peer_name = self.socket.getpeername()
-        self.peer_name_str = '{}:{}'.format(*self.peer_name)
+        # For later use
+        start = utcnow()
+        self.socket = socket.socket(type=socket.SOCK_STREAM)
+
+        while not self.is_connected:
+            logger.info('Connecting to %s', self.remote_addr_str)
+            try:
+                self.socket.connect((self.host, self.port))
+                self.peer_name = self.socket.getpeername()
+                self.peer_name_str = '{}:{}'.format(*self.peer_name)
+            except Exception as e:
+                logger.warn('Connection error `%s` (%s) -> %s', e.args, utcnow() - start, self.remote_addr_str)
+                sleep(1)
+            else:
+                logger.info('Connected to %s after %s', self.remote_addr_str, utcnow() - start)
+                self.is_connected = True
 
 # ################################################################################################################################
 
     def send(self, action, data=b''):
         # type: (bytes) -> None
-        self.socket.sendall(action + data + b'\n')
+        try:
+            self.socket.sendall(action + data + b'\n')
+        except Exception as e:
+            self.is_connected = False
+            logger.warn('Socket send error `%s` -> %s', e.args, self.remote_addr_str)
+            self.close()
+            self.connect()
 
 # ################################################################################################################################
 
-    def ping(self):
-        logger.info('Pinging %s (%s)', self.peer_name_str, self.conn_id)
-
-        # Send the ping message ..
-        self.send(Action.Ping)
+    def read(self):
+        # type: () -> bytes
 
         # .. build a receive context ..
         ctx = SocketReaderCtx(
@@ -90,10 +116,21 @@ class Client:
         )
 
         # .. wait for the reply ..
-        response = read_from_socket(ctx)
+        return read_from_socket(ctx)
+
+# ################################################################################################################################
+
+    def ping(self):
+        logger.info('Pinging %s (%s)', self.peer_name_str, self.conn_id)
+
+        # Send the ping message ..
+        self.send(Action.Ping)
+
+        # .. wait for the reply ..
+        response = self.read()
 
         # .. and raise an exception in case of any error.
-        if response != Action.PingReply:
+        if response and response != Action.PingReply:
             raise ValueError('Unexpected response received from `{}` -> `{}`'.format(self.peer_name, response))
 
 # ################################################################################################################################
@@ -120,7 +157,7 @@ class Client:
     def run(self):
 
         # Make sure that we have a port to connect to ..
-        wait_until_port_taken(self.port, 10)
+        #wait_until_port_taken(self.port, 10)
 
         # .. do connect now ..
         self.connect()
@@ -142,11 +179,11 @@ if __name__ == '__main__':
     client = Client(host, port)
     client.run()
 
-    n_iters = 100_000
+    n_iters = 1000
 
     for x in range(n_iters):
 
-        if x % 1000 == 0:
+        if x % 1 == 0:
             logger.info('ZZZ %s', x)
 
         ctx = PushCtx()
@@ -162,6 +199,8 @@ if __name__ == '__main__':
         ctx.total_time_ms = x
 
         client.push(ctx)
+
+        sleep(0.1)
 
     client.close()
 
