a7351c33dd305ed15435cd56b4dce421c5e09095
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-07-03 21:14:34 +0200

    GH #1077 - Brought back pagination to the list of services.

--- code/zato-server/src/zato/server/service/internal/service.py
+++ code/zato-server/src/zato/server/service/internal/service.py
@@ -61,7 +61,7 @@ class GetList(AdminService):
         request_elem = 'zato_service_get_list_request'
         response_elem = 'zato_service_get_list_response'
         input_required = 'cluster_id'
-        input_optional = 'should_include_scheduler'
+        input_optional = ('should_include_scheduler',) + GetListAdminSIO.input_optional
         output_required = 'id', 'name', 'is_active', 'impl_name', 'is_internal', Boolean('may_be_deleted'), Integer('usage'), \
             Integer('slow_threshold')
         output_optional = 'is_json_schema_enabled', 'needs_json_schema_err_details', 'is_rate_limit_active', \
