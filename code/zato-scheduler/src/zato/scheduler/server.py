4ef73013b70fbc02c89c9f1447fa37bad8e6df56
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-09-02 17:23:31 +0200

    GH #1070 - Use 127.0.0.1 instead of 0.0.0.0

--- code/zato-scheduler/src/zato/scheduler/server.py
+++ code/zato-scheduler/src/zato/scheduler/server.py
@@ -150,7 +150,6 @@ class SchedulerServer(object):
         # .. no configuration, assume this is a default quickstart cluster.
         else:
             # This is what quickstart environments use by default
-            #ZZZ Windows
             server_path = '/opt/zato/env/qs-1'
             return self._set_up_zato_client_by_server_path(server_path)
 
