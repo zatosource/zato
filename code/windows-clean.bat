c712b89efe5d1bffd0776c7b2d14789a56c6108f
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-09-03 13:22:38 +0100

    GH #1070 - Style changes.

--- code/windows-clean.bat
+++ code/windows-clean.bat
@@ -40,7 +40,7 @@ del /s /q %curdir%\zato-cy\*.c
 del /s /q %curdir%\zato-cy\*.html
 
 for /d /r %%i in (*.egg-info*)    do @rmdir /s /q "%%i"
-for /d /r %%i in (*__pycache__*) do @rmdir /s /q "%%i"
+for /d /r %%i in (*__pycache__*)  do @rmdir /s /q "%%i"
 
 echo Done cleaning up
 ver
