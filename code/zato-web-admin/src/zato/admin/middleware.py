bd45396a164534a3bd906f8978eff1720fed7aaa
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-09-08 09:28:00 +0200

    GH #1070 - Under Windows, communicate with servers directly.

--- code/zato-web-admin/src/zato/admin/middleware.py
+++ code/zato-web-admin/src/zato/admin/middleware.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2019, Zato Source s.r.o. https://zato.io
+Copyright (C) 2021, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 # stdlib
 from http.client import OK
+from logging import getLogger
 
 # Bunch
 from bunch import Bunch
@@ -26,10 +25,22 @@ from zato.admin.web.util import get_user_profile
 from zato.client import AnyServiceInvoker
 from zato.common.json_internal import loads
 from zato.common.odb.model import Cluster
+from zato.common.util.platform_ import is_windows
 from zato.common.version import get_version
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logger = getLogger(__name__)
+
+# ################################################################################################################################
+# ################################################################################################################################
 
 # Zato version
 version = get_version()
+
+# ################################################################################################################################
+# ################################################################################################################################
 
 # Code below is taken from http://djangosnippets.org/snippets/136/
 # Slightly modified for Zato's purposes.
@@ -63,11 +74,16 @@ version = get_version()
 # POSSIBILITY OF SUCH DAMAGE.
 ###
 
+# ################################################################################################################################
+# ################################################################################################################################
+
 class Client(AnyServiceInvoker):
     def __init__(self, req, *args, **kwargs):
         kwargs['tls_verify'] = lb_tls_verify
         self.forwarded_for = req.META.get('HTTP_X_FORWARDED_FOR') or req.META.get('REMOTE_ADDR')
         super(Client, self).__init__(*args, **kwargs)
+
+# ################################################################################################################################
 
     def invoke(self, *args, **kwargs):
         response = super(Client, self).invoke(*args, headers={'X-Zato-Forwarded-For': self.forwarded_for}, **kwargs)
@@ -76,9 +92,14 @@ class Client(AnyServiceInvoker):
             raise Exception('CID: {}\nDetails: {}'.format(json_data.get('cid'), json_data.get('details')))
         return response
 
+# ################################################################################################################################
+
     def invoke_async(self, *args, **kwargs):
         response = super(Client, self).invoke_async(*args, headers={'X-Zato-Forwarded-For': self.forwarded_for}, **kwargs)
         return response
+
+# ################################################################################################################################
+# ################################################################################################################################
 
 class ZatoMiddleware(object):
 
@@ -108,10 +129,21 @@ class ZatoMiddleware(object):
                 or 1 # By default, this will be the very first cluster so we can just assume it here as the last option.
 
             if req.zato.cluster_id:
-                req.zato.cluster = req.zato.odb.query(Cluster).filter_by(id=req.zato.cluster_id).one()
 
-                url = 'http{}://{}:{}'.format(
-                    's' if req.zato.lb_use_tls else '', req.zato.cluster.lb_host, req.zato.cluster.lb_port)
+                # Get the cluster we are running under ..
+                req.zato.cluster = req.zato.odb.query(Cluster).\
+                    filter_by(id=req.zato.cluster_id).\
+                    one()
+
+                # .. on Windows, we communicate with servers directly so we can just use a direct address ..
+                if 1:
+                    url = 'http://127.0.0.1:17010'
+
+                # .. but on other systems we go through the load balancer.
+                else:
+                    url = 'http{}://{}:{}'.format(
+                        's' if req.zato.lb_use_tls else '', req.zato.cluster.lb_host, req.zato.cluster.lb_port)
+
                 auth = (ADMIN_INVOKE_NAME, ADMIN_INVOKE_PASSWORD)
                 req.zato.client = Client(req, url, ADMIN_INVOKE_PATH, auth, to_bunch=True)
 
@@ -127,11 +159,15 @@ class ZatoMiddleware(object):
             req.zato.odb.rollback()
             raise
 
+# ################################################################################################################################
+
     def process_response(self, req, resp):
         if getattr(req, 'zato', None):
             req.zato.odb.close()
 
         return resp
+
+# ################################################################################################################################
 
     def process_template_response(self, req, resp):
         if resp.context_data:
@@ -149,3 +185,6 @@ class ZatoMiddleware(object):
         resp.render()
 
         return resp
+
+# ################################################################################################################################
+# ################################################################################################################################
