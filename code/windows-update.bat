0fa0262c256b3de23412043301beedf087015375
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-09-13 15:26:43 +0200

    GH #1070 - Renaming Windows scripts.

--- code/windows-update.bat
+++ code/windows-update.bat
@@ -1,4 +1,3 @@
-
 rem
 rem Local aliases
 rem
@@ -7,8 +6,15 @@ set curdir=%~dp0
 echo *** Downloading updates ***
 git -C %curdir% pull
 
+%curdir%\Scripts\python -m pip uninstall -y pip
+%curdir%\Scripts\python -m ensurepip
+%curdir%\Scripts\python -m pip install -U --upgrade pip
+
+%curdir%\Scripts\python %curdir%\util\post_install.py
+
 echo Updating environment in %curdir%
 %curdir%\Scripts\python %curdir%\util\environment.py update
 
 echo Installation updated
 %curdir%\windows-bin\zato --version
+
