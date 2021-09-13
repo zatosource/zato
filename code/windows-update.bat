e42853ca064f98b73348ba9dcc5004001c574f8d
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-09-13 19:30:09 +0200

    GH #1070 - Post-install tasks need to run first.

--- code/windows-update.bat
+++ code/windows-update.bat
@@ -6,11 +6,11 @@ set curdir=%~dp0
 echo *** Downloading updates ***
 git -C %curdir% pull
 
+%curdir%\Scripts\python %curdir%\util\post_install.py
+
 %curdir%\Scripts\python -m pip uninstall -y pip
 %curdir%\Scripts\python -m ensurepip
 %curdir%\Scripts\python -m pip install -U --upgrade pip
-
-%curdir%\Scripts\python %curdir%\util\post_install.py
 
 echo Updating environment in %curdir%
 %curdir%\Scripts\python %curdir%\util\environment.py update
