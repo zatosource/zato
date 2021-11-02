85a77d242ded955dddca05e61dbba234e3a0fe87
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-11-02 20:53:27 +0100

    GH #1125 - Adding dacite to common/ext.

---
+++ code/zato-common/src/zato/common/ext/dacite/__init__.py
@@ -0,0 +1,53 @@
+"""
+This module is a modified vendor copy of the dacite package from https://pypi.org/project/dacite/
+
+MIT License
+
+Copyright (c) 2018 Konrad Hałas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# flake8: noqa
+
+from dacite.config import Config
+from dacite.core import from_dict
+from dacite.exceptions import (
+    DaciteError,
+    DaciteFieldError,
+    WrongTypeError,
+    MissingValueError,
+    UnionMatchError,
+    StrictUnionMatchError,
+    ForwardReferenceError,
+    UnexpectedDataError,
+)
+
+__all__ = [
+    "Config",
+    "from_dict",
+    "DaciteError",
+    "DaciteFieldError",
+    "WrongTypeError",
+    "MissingValueError",
+    "UnionMatchError",
+    "StrictUnionMatchError",
+    "ForwardReferenceError",
+    "UnexpectedDataError",
+]
