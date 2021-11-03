5146fcaac4877129afdbe85187074e7494ec51b1
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-11-03 15:40:21 +0100

    GH #1125 - Adding MarshalAPI.

--- code/zato-common/src/zato/common/marshal_/api.py
+++ code/zato-common/src/zato/common/marshal_/api.py
@@ -13,8 +13,22 @@ from dataclasses import _FIELDS
 # ################################################################################################################################
 # ################################################################################################################################
 
+if 0:
+    from dataclasses import Field
+
+    Field = Field
+
+# ################################################################################################################################
+# ################################################################################################################################
+
 def is_simple_type(value):
     return issubclass(value, (int, str, float))
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class Model:
+    pass
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -24,44 +38,55 @@ class MarshalAPI:
     def __init__(self):
         self._field_cache = {}
 
-    def from_dict(self, data, DataClass):
+    def from_dict(self, data:dict, DataClass:object):
+
+        # Whether the dataclass defines the __init__method
+        has_init = DataClass.__dataclass_params__.init
+
+        # This will be populated with parameters to the dataclass's __init__ method, assuming that the class has one.
+        init_attrs = {}
+
+        # This will be populated with parameters to be set via setattr, in case the class does not have __init__.
+        setattr_attrs = {}
+
+        # We can check it once upfront here
+        attrs_container = init_attrs if has_init else setattr_attrs
 
         fields = getattr(DataClass, _FIELDS) # type: dict
 
-        print()
-        for field in fields.values():
-            print(111, field.type)
-            print(222, is_simple_type(field.type))
-        print()
+        for field in fields.values(): # type: Field
+
+            # Is this particular field a further dataclass-based model?
+            is_model = issubclass(field.type, Model)
+
+            # Get the value given on input
+            value = data.get(field.name)
+
+            # If this is to be a model, we need a dict as value as it is the only container possible for nested values
+            if is_model and (not isinstance(value, dict)):
+                raise ValueError('Expected for `{}` to be a dict instead of `{}` ({})'.format(
+                    field.name, value, value.__class__.__name__))
+
+            print()
+            print(111, field.name, field.type, field.init)
+            print(222, )
+            print(333, value)
+            print()
+
+            # Add the computed value for later use
+            attrs_container[field.name] = value
+
+        # Create a new instance, potentially with attributes ..
+        instance = DataClass(**init_attrs)
+
+        # .. and add extra ones in case __init__ was not defined.
+        for k, v in setattr_attrs.items():
+            setattr(instance, k, v)
+
+        return instance
 
     def to_dict(self):
         pass
 
 # ################################################################################################################################
 # ################################################################################################################################
-
-'''
-# Zato
-from zato.common.marshal_.api import MarshalAPI
-from zato.common.ext.dataclasses import dataclass
-
-@dataclass(init=False)
-class User:
-    user_name: str
-
-@dataclass(init=False)
-class MyRequest:
-    user_id: int
-    user: User
-
-data = {
-    'user_id': 123,
-    'user': 'zzz'
-}
-
-api = MarshalAPI()
-result = api.from_dict(data, MyRequest)
-
-# result = from_dict(MyRequest, data) # type: MyRequest
-# print(111, result.user.user_name)
-'''
