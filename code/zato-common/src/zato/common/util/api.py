32ea4c9d58bd59f7b73a0f4232a7f9deeb5a594d
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-12-15 09:47:50 +0100

    GH #262 - Adding type hints.

--- code/zato-common/src/zato/common/util/api.py
+++ code/zato-common/src/zato/common/util/api.py
@@ -115,6 +115,7 @@ from zato.hl7.parser import get_payload_from_request as hl7_get_payload_from_req
 if 0:
     from typing import Iterable as iterable
     from simdjson import Parser as SIMDJSONParser
+    from zato.common.typing_ import callable_
 
     iterable = iterable
     SIMDJSONParser = SIMDJSONParser
@@ -324,7 +325,7 @@ def tech_account_password(password_clear, salt):
 
 # ################################################################################################################################
 
-def new_cid(bytes=12, _random=random.getrandbits):
+def new_cid(bytes:'int'=12, _random:'callable_'=random.getrandbits) -> 'str':
     """ Returns a new 96-bit correlation identifier. It is *not* safe to use the ID
     for any cryptographical purposes; it is only meant to be used as a conveniently
     formatted ticket attached to each of the requests processed by Zato servers.
