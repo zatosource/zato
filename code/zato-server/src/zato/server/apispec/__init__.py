afe77860435a12b6f6be58b36436f8e9adb0ffd6
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-12-18 15:28:21 +0100

    GH #805 - Adding type hints.

--- code/zato-server/src/zato/server/apispec/__init__.py
+++ code/zato-server/src/zato/server/apispec/__init__.py
@@ -9,7 +9,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 # stdlib
 from copy import deepcopy
 from fnmatch import fnmatch
-from inspect import isclass, getmodule
+from inspect import getmodule
 
 # Bunch
 from bunch import Bunch, bunchify
@@ -22,9 +22,9 @@ from markdown import markdown
 
 # Zato
 from zato.common.api import APISPEC
-from zato.common.ext.dataclasses import dataclass, Field, MISSING
-from zato.common.marshal_.api import extract_model_class, is_list, Model
+from zato.common.marshal_.api import Model
 from zato.common.marshal_.simpleio import DataClassSimpleIO
+from zato.server.apispec.model import APISpecInfo, Config, Docstring, FieldInfo, Namespace, SimpleIO, SimpleIODescription
 
 # Zato - Cython
 from zato.simpleio import SIO_TYPE_MAP
@@ -39,11 +39,6 @@ if 0:
 
 # ################################################################################################################################
 # ################################################################################################################################
-
-_sio_attrs = (
-    'input',
-    'output'
-)
 
 _SIO_TYPE_MAP = SIO_TYPE_MAP()
 
@@ -81,104 +76,6 @@ def build_field_list(model:'Model', api_spec_info:'any_') -> 'anylist':
 # ################################################################################################################################
 # ################################################################################################################################
 
-@dataclass(init=False)
-class FieldInfo:
-    name:        'str'
-    type:        'str'  = ''
-    subtype:     'str'  = ''
-    is_required: 'bool' = False
-    description: 'str'  = ''
-    ref:         'str'  = ''
-    is_list:     'bool' = False
-
-    @staticmethod
-    def from_python_field(field:'Field', api_spec_info:'Bunch') -> 'FieldInfo':
-
-        if not field.type:
-            raise ValueError('Value missing -> field.type ({})'.format(field))
-
-        info = FieldInfo()
-        info.name = field.name or '<field-no-name>'
-        info.is_required = field.default is MISSING
-        info.description = field.__doc__ or ''
-
-        is_class = isclass(field.type)
-
-        if is_list(field.type, is_class):
-            type_info = '', ''
-            ref = extract_model_class(field.type)
-            info.is_list = True
-            info.ref = '#/components/schemas/{}.{}'.format(ref.__module__, ref.__name__)
-
-        elif issubclass(field.type, bool):
-            type_info = api_spec_info.BOOLEAN
-
-        elif issubclass(field.type, int):
-            type_info = api_spec_info.INTEGER
-
-        elif issubclass(field.type, float):
-            type_info = api_spec_info.FLOAT
-
-        elif issubclass(field.type, Model):
-            type_info = '', ''
-            info.ref = '#/components/schemas/{}.{}'.format(field.type.__module__, field.type.__name__)
-        else:
-            try:
-                type_info = api_spec_info.map[field.__class__]
-            except KeyError:
-                type_info = api_spec_info.DEFAULT
-
-        info.type, info.subtype = type_info
-
-        return info
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-@dataclass(init=False)
-class APISpecInfo:
-    name: str
-    field_list: 'anydict'
-    request_elem: str
-    response_elem: str
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class Config:
-    def __init__(self):
-        self.is_module_level = True
-        self.ns = ''
-        self.services = []
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class Docstring:
-    __slots__ = 'summary', 'description', 'full', 'tags', 'by_tag'
-
-    def __init__(self, tags:'anylist') -> 'None':
-        self.summary = ''
-        self.description = ''
-        self.full = ''
-        self.tags = tags
-        self.by_tag = {} # Keys are tags used, values are documentation for key
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class Namespace:
-
-    name: str
-    docs: str
-
-    def __init__(self):
-        self.name = APISPEC.NAMESPACE_NULL
-        self.docs = ''
-
-# ################################################################################################################################
-# ################################################################################################################################
-
 class _DocstringSegment:
     __slots__ = 'tag', 'summary', 'description', 'full'
 
@@ -197,58 +94,6 @@ class _DocstringSegment:
             'description': self.description,
             'full': self.full,
         }
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class SimpleIO:
-    __slots__ = 'input', 'output', 'request_elem', 'response_elem', 'spec_name', 'description', 'needs_sio_desc'
-
-    def __init__(
-        self,
-        api_spec_info,  # type: APISpecInfo
-        description,    # type: SimpleIODescription
-        needs_sio_desc, # type: bool
-        ) -> 'None':
-
-        self.input = api_spec_info.field_list.get('input', [])
-        self.output = api_spec_info.field_list.get('output', [])
-        self.request_elem = api_spec_info.request_elem
-        self.response_elem = api_spec_info.response_elem
-        self.spec_name = api_spec_info.name
-        self.description = description
-        self.needs_sio_desc = needs_sio_desc
-
-# ################################################################################################################################
-
-    def to_bunch(self):
-        out = Bunch()
-        for name in _sio_attrs + ('request_elem', 'response_elem', 'spec_name'):
-            out[name] = getattr(self, name)
-
-        if self.needs_sio_desc:
-            out.description = self.description.to_bunch()
-
-        return out
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class SimpleIODescription:
-    __slots__ = 'input', 'output'
-
-    def __init__(self):
-        self.input = {}
-        self.output = {}
-
-# ################################################################################################################################
-
-    def to_bunch(self):
-        out = Bunch()
-        out.input = self.input
-        out.output = self.output
-
-        return out
 
 # ################################################################################################################################
 # ################################################################################################################################
