1cd56d6a0ad84bfe67d25820e2b162d80a66f492
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-12-22 08:26:22 +0100

    Revert "GH #755 - Explicitly use UTC +00:00 in datetime_from_ms."
    This reverts commit 00b7cae13acfc20fa31b9d2d71314b9b4c45a04b.

--- code/zato-server/src/zato/server/pubsub/task.py
+++ code/zato-server/src/zato/server/pubsub/task.py
@@ -695,13 +695,13 @@ class Message(PubSubMessage):
     def add_iso_times(self) -> 'None':
         """ Sets additional attributes for datetime in ISO-8601.
         """
-        self.pub_time_iso = cast_('str', datetime_from_ms(self.pub_time))
+        self.pub_time_iso = cast_('str', datetime_from_ms(self.pub_time * 1000))
 
         if self.ext_pub_time:
-            self.ext_pub_time_iso = cast_('str', datetime_from_ms(self.ext_pub_time))
+            self.ext_pub_time_iso = cast_('str', datetime_from_ms(self.ext_pub_time * 1000))
 
         if self.expiration_time:
-            self.expiration_time_iso = cast_('str', datetime_from_ms(self.expiration_time))
+            self.expiration_time_iso = cast_('str', datetime_from_ms(self.expiration_time * 1000))
 
         if self.recv_time:
             self.recv_time_iso = cast_('str', datetime_from_ms(self.recv_time))
