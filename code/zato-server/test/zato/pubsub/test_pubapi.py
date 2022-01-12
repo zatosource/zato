7efddbadb860697d860c10d418a5052173692e4f
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-12 22:30:09 +0100

    GH #755 - Working on WSX pub/sub tests.

--- code/zato-server/test/zato/pubsub/test_pubapi.py
+++ code/zato-server/test/zato/pubsub/test_pubapi.py
@@ -11,8 +11,9 @@ from time import sleep
 
 # Zato
 from zato.common import PUBSUB
-from zato.common.pubsub import MSG_PREFIX, prefix_sk, skip_to_external
-from zato.common.test import rand_date_utc
+from zato.common.pubsub import prefix_sk
+from zato.common.test.config import TestConfig
+from zato.common.test.pubsub import FullPathTester
 from zato.common.test.rest_client import _RESTClient, RESTClientTestCase
 from zato.common.typing_ import cast_
 
@@ -27,9 +28,9 @@ if 0:
 
 _default = PUBSUB.DEFAULT
 
+topic_name = TestConfig.pubsub_topic_name_name
 sec_name   = _default.DEMO_SECDEF_NAME
 username   = _default.DEMO_USERNAME
-topic_name = '/zato/demo/sample'
 
 class config:
     path_publish     = f'/zato/pubsub/topic/{topic_name}'
@@ -88,74 +89,6 @@ class PubSubAPIRestImpl:
 
         # Our caller may want to run its own assertion too
         return response
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class FullPathTester:
-
-    def __init__(self, test:'PubAPITestCase', sub_before_publish:'bool') -> 'None':
-        self.test = test
-        self.sub_before_publish = sub_before_publish
-        self.sub_after_publish = not self.sub_before_publish
-
-# ################################################################################################################################
-
-    def run(self):
-
-        # For type checking
-        sub_key = None
-
-        # Always make sure that we are unsubscribed before the test runs
-        self.test._unsubscribe()
-
-        # We may potentially need to subscribe before the publication
-        if self.sub_before_publish:
-            sub_key = self.test._subscribe()
-
-        # Publish the message
-        data = cast_(str, rand_date_utc(True))
-        response_publish = self.test._publish(data)
-
-        # We expect to have a correct message ID on output
-        msg_id = response_publish['msg_id'] # type: str
-        self.test.assertTrue(msg_id.startswith(MSG_PREFIX.MSG_ID))
-
-        # We may potentially need to subscribe after the publication
-        if self.sub_after_publish:
-            sub_key = self.test._subscribe()
-
-        # Now, read the message back from our own queue - we can do it because
-        # we know that we are subscribed already.
-        response_received = self.test._receive()
-
-        # We do not know how many messages we receive because it is possible
-        # that there may be some left over from previous tests. However, we still
-        # expect that the message that we have just published will be the first one
-        # because messages are returned in the Last-In-First-Out order (LIFO).
-        msg_received = response_received[0]
-
-        self.test.assertEqual(msg_received['data'], data)
-        self.test.assertEqual(msg_received['size'], len(data))
-        self.test.assertEqual(msg_received['sub_key'], sub_key)
-        self.test.assertEqual(msg_received['delivery_count'], 1)
-        self.test.assertEqual(msg_received['priority'],   PUBSUB.PRIORITY.DEFAULT)
-        self.test.assertEqual(msg_received['mime_type'],  PUBSUB.DEFAULT.MIME_TYPE)
-        self.test.assertEqual(msg_received['expiration'], PUBSUB.DEFAULT.EXPIRATION)
-        self.test.assertEqual(msg_received['topic_name'], topic_name)
-
-        # Dates will start with 2nnn, e.g. 2022, or 2107, depending on a particular field
-        date_start = '2'
-
-        self.test.assertTrue(msg_received['pub_time_iso'].startswith(date_start))
-        self.test.assertTrue(msg_received['expiration_time_iso'].startswith(date_start))
-        self.test.assertTrue(msg_received['recv_time_iso'].startswith(date_start))
-
-        # Make sure that keys that are not supposed to be returned to external callers
-        # are not returned in the message.
-        for name in skip_to_external:
-            if name in msg_received:
-                self.test.fail(f'Key `{name}` should not be in message {msg_received}')
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -248,13 +181,13 @@ class PubAPITestCase(RESTClientTestCase):
 # ################################################################################################################################
 
     def xtest_full_path_subscribe_before_publication(self):
-        tester = FullPathTester(self, True)
+        tester = FullPathTester(self, True) # type: ignore
         tester.run()
 
 # ################################################################################################################################
 
     def test_full_path_subscribe_after_publication(self):
-        tester = FullPathTester(self, False)
+        tester = FullPathTester(self, False) # type: ignore
         tester.run()
 
 # ################################################################################################################################
