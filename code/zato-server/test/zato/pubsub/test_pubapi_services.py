8d37830ff2c2b81c8ca8a18324f64efae5edd4ee
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-22 14:18:19 +0100

    GH #755 - Adding WSXServicesInvokerTest tests.

--- code/zato-server/test/zato/pubsub/test_pubapi_services.py
+++ code/zato-server/test/zato/pubsub/test_pubapi_services.py
@@ -17,6 +17,8 @@ from zato.common.test import CommandLineServiceInvoker
 
 class WSXServicesInvokerTest(TestCase):
 
+    maxDiff = 1234567890
+
     def test_wsx_services_invoker(self) -> 'None':
 
         # This service invokes a test suite that invokes all the services
@@ -52,6 +54,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # stdlib
+from json import dumps
 from traceback import format_exc
 
 # Zato
@@ -72,26 +75,24 @@ class MyService(Service):
         from zato.common.test.config import TestConfig
         from zato.common.test.pubsub import FullPathTester, PubSubTestingClass
 
-        topic_name = TestConfig.pubsub_topic_shared
-
         class WSXServicesTestCase(TestCase, PubSubTestingClass):
 
-            def _subscribe(_self, *args, **kwargs): # type: ignore
+            def _subscribe(_self, topic_name): # type: ignore
                 service = 'zato.pubsub.pubapi.subscribe'
                 response = self.invoke(service, {'topic_name': topic_name})
                 return response['sub_key']
 
-            def _unsubscribe(_self, *args, **kwargs): # type: ignore
+            def _unsubscribe(_self, topic_name): # type: ignore
                 service = 'zato.pubsub.pubapi.unsubscribe'
                 response = self.invoke(service, {'topic_name': topic_name})
                 return response
 
-            def _publish(_self, data, **kwargs): # type: ignore
+            def _publish(_self, topic_name, data): # type: ignore
                 service = 'zato.pubsub.pubapi.publish-message'
                 response = self.invoke(service, {'topic_name': topic_name, 'data':data})
                 return response
 
-            def _receive(_self, *args, **kwargs): # type: ignore
+            def _receive(_self, topic_name): # type: ignore
                 service = 'zato.pubsub.pubapi.get-messages'
                 response = self.invoke(service, {'topic_name': topic_name})
                 return response
@@ -101,14 +102,55 @@ class MyService(Service):
                 tester.run()
 
             def test_wsx_services_full_path_subscribe_after_publication(_self):
-                pass
+                tester = FullPathTester(_self, False)
+                tester.run()
 
         try:
-            iters = 5
+            iters = 10
             for _ in range(iters):
                 suite = defaultTestLoader.loadTestsFromTestCase(WSXServicesTestCase)
-                TextTestRunner().run(suite)
-            self.response.payload = 'OK'
+                result = TextTestRunner().run(suite)
+
+                if result.errors or result.failures:
+                    errors   = []
+                    failures = []
+
+                    response = {
+                        'errors':   errors,
+                        'failures': failures,
+                    }
+
+                    for error in result.errors:
+                        test, reason = error
+                        test = str(test)
+                        _error = {
+                            'error_test': test,
+                            'error_reason': reason,
+                        }
+                        self.logger.warn('Test error -> %s', result.errors)
+                        errors.append(_error)
+
+                    for failure in result.failures:
+                        test, reason = failure
+                        test = str(test)
+                        reason = '\n'.join(reason)
+                        _failure = {
+                            'failure_test': test,
+                            'failure_reason': reason,
+                        }
+                        self.logger.warn('Test Failure -> %s', result.errors)
+                        errors.append(_failure)
+
+                    # Serialize all the warnings and errors ..
+                    self.response.payload = dumps(response)
+
+                    # .. and do resume the test.
+                    break
+
+            # If we ar here, it means that there was no error
+            else:
+                self.response.payload = 'OK'
+
         except Exception:
             msg = 'Exception in {} -> {}'.format(self.__class__.__name__, format_exc())
             self.logger.warn(msg)
