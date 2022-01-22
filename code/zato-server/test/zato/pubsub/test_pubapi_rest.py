8a5f47acaa2d82d88fe91b1eb67532b23554c015
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-22 18:50:25 +0100

    GH #755 - Working on PubAPITestCase.

--- code/zato-server/test/zato/pubsub/test_pubapi_rest.py
+++ code/zato-server/test/zato/pubsub/test_pubapi_rest.py
@@ -28,15 +28,15 @@ if 0:
 
 _default = PUBSUB.DEFAULT
 
-topic_name = TestConfig.pubsub_topic_shared
+#topic_name = TestConfig.pubsub_topic_shared
 sec_name   = _default.DEMO_SECDEF_NAME
 username   = _default.DEMO_USERNAME
 
 class config:
-    path_publish     = f'/zato/pubsub/topic/{topic_name}'
-    path_receive     = f'/zato/pubsub/topic/{topic_name}'
-    path_subscribe   = f'/zato/pubsub/subscribe/topic/{topic_name}'
-    path_unsubscribe = f'/zato/pubsub/subscribe/topic/{topic_name}'
+    path_publish     = '/zato/pubsub/topic/'
+    path_receive     = '/zato/pubsub/topic/'
+    path_subscribe   = '/zato/pubsub/subscribe/topic/'
+    path_unsubscribe = '/zato/pubsub/subscribe/topic/'
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -48,38 +48,38 @@ class PubSubAPIRestImpl:
 
 # ################################################################################################################################
 
-    def _publish(self, data:'any_') -> 'stranydict':
+    def _publish(self, topic_name:'str', data:'any_') -> 'stranydict':
         request = {'data': data}
-        response = self.rest_client.post(config.path_publish, request) # type: stranydict
+        response = self.rest_client.post(config.path_publish + topic_name, request) # type: stranydict
         sleep(0.1)
         return response
 
 # ################################################################################################################################
 
-    def _receive(self, needs_sleep:'bool'=True, expect_ok:'bool'=True) -> 'anylist':
+    def _receive(self, topic_name:'str', needs_sleep:'bool'=True, expect_ok:'bool'=True) -> 'anylist':
 
         # If required, wait a moment to make sure a previously published message is delivered -
         # # the server's delivery task runs once in 2 seconds.
         if needs_sleep:
             sleep(2.1)
 
-        return cast_('anylist', self.rest_client.patch(config.path_receive, expect_ok=expect_ok))
+        return cast_('anylist', self.rest_client.patch(config.path_receive + topic_name, expect_ok=expect_ok))
 
 # ################################################################################################################################
 
-    def _subscribe(self, needs_unsubscribe:'bool'=False) -> 'str':
+    def _subscribe(self, topic_name:'str', needs_unsubscribe:'bool'=False) -> 'str':
         if needs_unsubscribe:
-            self._unsubscribe()
-        response = self.rest_client.post(config.path_subscribe)
+            self._unsubscribe(topic_name)
+        response = self.rest_client.post(config.path_subscribe + topic_name)
         sleep(1.1)
         return response['sub_key']
 
 # ################################################################################################################################
 
-    def _unsubscribe(self) -> 'anydict':
+    def _unsubscribe(self, topic_name:'str') -> 'anydict':
 
         # Delete a potential subscription based on our credentials
-        response = self.rest_client.delete(config.path_unsubscribe) # type: anydict
+        response = self.rest_client.delete(config.path_unsubscribe + topic_name) # type: anydict
 
         # Wait a moment to make sure the subscription is deleted
         sleep(0.1)
@@ -107,23 +107,23 @@ class PubAPITestCase(RESTClientTestCase):
 
 # ################################################################################################################################
 
-    def _publish(self, data:'any_') -> 'stranydict':
-        return self.api_impl._publish(data)
+    def _publish(self, topic_name:'str', data:'any_') -> 'stranydict':
+        return self.api_impl._publish(topic_name, data)
 
 # ################################################################################################################################
 
-    def _receive(self, needs_sleep:'bool'=True, expect_ok:'bool'=True) -> 'anylist':
-        return self.api_impl._receive(needs_sleep, expect_ok)
+    def _receive(self, topic_name:'str', needs_sleep:'bool'=True, expect_ok:'bool'=True) -> 'anylist':
+        return self.api_impl._receive(topic_name, needs_sleep, expect_ok)
 
 # ################################################################################################################################
 
-    def _subscribe(self, needs_unsubscribe:'bool'=False) -> 'str':
-        return self.api_impl._subscribe(needs_unsubscribe)
+    def _subscribe(self, topic_name:'str', needs_unsubscribe:'bool'=False) -> 'str':
+        return self.api_impl._subscribe(topic_name, needs_unsubscribe)
 
 # ################################################################################################################################
 
-    def _unsubscribe(self) -> 'anydict':
-        return self.api_impl._unsubscribe()
+    def _unsubscribe(self, topic_name:'str') -> 'anydict':
+        return self.api_impl._unsubscribe(topic_name)
 
 # ################################################################################################################################
 
@@ -132,7 +132,7 @@ class PubAPITestCase(RESTClientTestCase):
         # Before subscribing, make sure we are not currently subscribed
         self._unsubscribe()
 
-        response_initial = self.rest_client.post(config.path_subscribe)
+        response_initial = self.rest_client.post(config.path_subscribe + topic_name)
 
         # Wait a moment to make sure the subscription data is created
         sleep(0.1)
@@ -180,13 +180,13 @@ class PubAPITestCase(RESTClientTestCase):
 
 # ################################################################################################################################
 
-    def xtest_full_path_subscribe_before_publication(self):
+    def test_full_path_subscribe_before_publication(self):
         tester = FullPathTester(self, True) # type: ignore
         tester.run()
 
 # ################################################################################################################################
 
-    def test_full_path_subscribe_after_publication(self):
+    def xtest_full_path_subscribe_after_publication(self):
         tester = FullPathTester(self, False) # type: ignore
         tester.run()
 
@@ -202,7 +202,7 @@ class PubAPITestCase(RESTClientTestCase):
 
         self.assertIsNotNone(response['cid'])
         self.assertEqual(response['result'], 'Error')
-        self.assertEqual(response['details'], f'You are not subscribed to topic `{topic_name}`')
+        self.assertEqual(response['details'], f'You are not subscribed to topic ``')
 
 # ################################################################################################################################
 
--- code/zato-server/test/zato/pubsub/test_pubapi_services.py
+++ code/zato-server/test/zato/pubsub/test_pubapi_services.py
@@ -23,7 +23,7 @@ class WSXServicesInvokerTest(TestCase):
 
         # This service invokes a test suite that invokes all the services
         # that pubapi clients use for publish/subscribe.
-        service = 'pubsub1.my-service'
+        service = 'helpers.pubsub.pubapi-invoker'
 
         # Prepare the invoker
         invoker = CommandLineServiceInvoker(check_stdout=False)
@@ -32,7 +32,7 @@ class WSXServicesInvokerTest(TestCase):
         out = invoker.invoke_and_test(service) # type: str
         out = out.strip()
 
-        # .. make sure that the response points to a success.
+        # .. make sure that the response indicates a success.
         self.assertEqual(out, 'OK')
 
 # ################################################################################################################################
@@ -43,119 +43,3 @@ if __name__ == '__main__':
 
 # ################################################################################################################################
 # ################################################################################################################################
-
-'''
-# -*- coding: utf-8 -*-
-
-"""
-Copyright (C) 2022, Zato Source s.r.o. https://zato.io
-
-Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
-"""
-
-# stdlib
-from json import dumps
-from traceback import format_exc
-
-# Zato
-from zato.server.service import Service
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class MyService(Service):
-    """ Tests services that WebSocket clients use.
-    """
-    def handle(self):
-
-        # stdlib
-        from unittest import defaultTestLoader, TestCase, TextTestRunner
-
-        # Zato
-        from zato.common.test.config import TestConfig
-        from zato.common.test.pubsub import FullPathTester, PubSubTestingClass
-
-        class WSXServicesTestCase(TestCase, PubSubTestingClass):
-
-            def _subscribe(_self, topic_name): # type: ignore
-                service = 'zato.pubsub.pubapi.subscribe'
-                response = self.invoke(service, {'topic_name': topic_name})
-                return response['sub_key']
-
-            def _unsubscribe(_self, topic_name): # type: ignore
-                service = 'zato.pubsub.pubapi.unsubscribe'
-                response = self.invoke(service, {'topic_name': topic_name})
-                return response
-
-            def _publish(_self, topic_name, data): # type: ignore
-                service = 'zato.pubsub.pubapi.publish-message'
-                response = self.invoke(service, {'topic_name': topic_name, 'data':data})
-                return response
-
-            def _receive(_self, topic_name): # type: ignore
-                service = 'zato.pubsub.pubapi.get-messages'
-                response = self.invoke(service, {'topic_name': topic_name})
-                return response
-
-            def test_wsx_services_full_path_subscribe_before_publication(_self):
-                tester = FullPathTester(_self, True)
-                tester.run()
-
-            def test_wsx_services_full_path_subscribe_after_publication(_self):
-                tester = FullPathTester(_self, False)
-                tester.run()
-
-        try:
-            iters = 10
-            for _ in range(iters):
-                suite = defaultTestLoader.loadTestsFromTestCase(WSXServicesTestCase)
-                result = TextTestRunner().run(suite)
-
-                if result.errors or result.failures:
-                    errors   = []
-                    failures = []
-
-                    response = {
-                        'errors':   errors,
-                        'failures': failures,
-                    }
-
-                    for error in result.errors:
-                        test, reason = error
-                        test = str(test)
-                        _error = {
-                            'error_test': test,
-                            'error_reason': reason,
-                        }
-                        self.logger.warn('Test error -> %s', result.errors)
-                        errors.append(_error)
-
-                    for failure in result.failures:
-                        test, reason = failure
-                        test = str(test)
-                        reason = '\n'.join(reason)
-                        _failure = {
-                            'failure_test': test,
-                            'failure_reason': reason,
-                        }
-                        self.logger.warn('Test Failure -> %s', result.errors)
-                        errors.append(_failure)
-
-                    # Serialize all the warnings and errors ..
-                    self.response.payload = dumps(response)
-
-                    # .. and do resume the test.
-                    break
-
-            # If we ar here, it means that there was no error
-            else:
-                self.response.payload = 'OK'
-
-        except Exception:
-            msg = 'Exception in {} -> {}'.format(self.__class__.__name__, format_exc())
-            self.logger.warn(msg)
-            self.response.payload = msg
-
-# ################################################################################################################################
-# ################################################################################################################################
-'''
