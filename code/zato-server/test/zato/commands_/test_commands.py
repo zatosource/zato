f4a226a7887141009908c1b5ec6caef4a0f3adc2
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-25 09:33:45 +0100

    GH #1185 - Added test_invoke_multiline.

--- code/zato-server/test/zato/commands_/test_commands.py
+++ code/zato-server/test/zato/commands_/test_commands.py
@@ -33,3 +33,211 @@ if __name__ == '__main__':
 
 # ################################################################################################################################
 # ################################################################################################################################
+
+'''
+# -*- coding: utf-8 -*-
+
+# stdlib
+import os
+from datetime import datetime
+from logging import getLogger
+from tempfile import gettempdir
+from unittest import TestCase
+
+# Zato
+from zato.common.util.open_ import open_w
+from zato.common.test import rand_csv, rand_string
+from zato.server.commands import CommandResult, Config
+from zato.server.service import Service
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logger = getLogger(__name__)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class CommandsService(Service):
+
+    class SimpleIO:
+        output = CommandResult
+
+    class _CommandsServiceTestCase(TestCase):
+
+        def __init__(self, service:'Service') -> 'None':
+            super().__init__()
+            self.service = service
+
+# ################################################################################################################################
+
+        def test_invoke_core(self):
+
+            # Test data that we will expect to read back from a test file
+            data = rand_csv()
+            len_data = len(data)
+
+            # Where our test data is
+            test_file_name = rand_string(prefix='commands-test_invoke_core') + '.txt'
+            full_path = os.path.join(gettempdir(), test_file_name)
+
+            # Log useful details
+            logger.info('Saving test data `%s` to file `%s`', data, full_path)
+
+            # Populate the file with test data
+            with open_w(full_path) as f:
+                f.write(data)
+
+            # Read the file back
+            command = f'cat {full_path}'
+
+            # To check that results contain correct timestamps
+            now_before_test = datetime.utcnow()
+
+            # Invoke the commands to get the result
+            result = self.service.commands.invoke(command)
+
+            # To check that results contain correct timestamps
+            now_after_test = datetime.utcnow()
+
+            # .. and run the actual tests now ..
+
+            self.assertEqual(result.exit_code, 0)
+            self.assertTrue(result.is_ok)
+            self.assertFalse(result.is_async)
+            self.assertFalse(result.is_timeout)
+            self.assertTrue(result.cid.startswith('zcmd'))
+
+            self.assertEqual(result.timeout_msg, '')
+
+            self.assertEqual(result.timeout,      Config.Timeout)
+            self.assertEqual(result.encoding,     Config.Encoding)
+            self.assertEqual(result.replace_char, Config.ReplaceChar)
+
+            self.assertIsInstance(result.total_time_sec, float)
+
+            total_time_sec_as_str = str(result.total_time_sec)
+            self.assertTrue(result.total_time.endswith(total_time_sec_as_str))
+
+            start_time_as_iso = result.start_time.isoformat()
+            end_time_as_iso   = result.end_time.isoformat()
+
+            self.assertEqual(result.start_time_iso, start_time_as_iso)
+            self.assertEqual(result.end_time_iso,   end_time_as_iso)
+
+            self.assertLess(result.start_time, result.end_time)
+
+            self.assertLess(now_before_test, result.start_time)
+            self.assertLess(now_before_test, result.end_time)
+
+            self.assertGreater(now_after_test, result.start_time)
+            self.assertGreater(now_after_test, result.end_time)
+
+            self.assertEqual(result.stdin,  '')
+            self.assertEqual(result.stderr, '')
+            self.assertEqual(result.stdout, data)
+
+            self.assertEqual(result.len_stderr_bytes, 0)
+            self.assertEqual(result.len_stderr_human, '0 Bytes')
+
+            self.assertEqual(result.len_stdout_bytes, len_data)
+            self.assertEqual(result.len_stdout_human, '{} Bytes'.format(len_data))
+
+# ################################################################################################################################
+
+        def test_invoke_multiline(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_own_cid(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_timeout(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_replace_char(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_stdin(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_callback_function(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_callback_service_class(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_callback_service_name(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_with_callback_topic_name(self):
+            pass
+
+# ################################################################################################################################
+
+        def test_invoke_async_core(self):
+            pass
+
+# ################################################################################################################################
+
+    def _on_command_completed(self, result:'CommandResult') -> 'None':
+        result
+        result
+
+# ################################################################################################################################
+
+    def handle(self):
+
+        # Build and run the test suite
+        test_suite = self._CommandsServiceTestCase(self)
+
+        #
+        # Sync invoke
+        #
+        test_suite.test_invoke_core()
+        # test_suite.test_invoke_multiline()
+        # test_suite.test_invoke_with_own_cid()
+        # test_suite.test_invoke_with_timeout()
+        # test_suite.test_invoke_with_replace_char()
+        # test_suite.test_invoke_with_stdin()
+        # test_suite.test_invoke_with_callback_function()
+        # test_suite.test_invoke_with_callback_service_class()
+        # test_suite.test_invoke_with_callback_service_name()
+        # test_suite.test_invoke_with_callback_topic_name()
+
+        #
+        # Async invoke
+        #
+        # self.test_invoke_async_core()
+
+        """
+        # command = 'rm -rf /tmp/abc && mkdir /tmp/abc && cd /tmp/abc && git clone https://github.com/zatosource/zato'
+        command = """
+        whoami && \
+            uname \
+                -a
+
+        result = self.commands.invoke(command, callback=self._on_command_completed)
+        self.logger.info(result)
+
+        self.response.payload = 'OK'
+        """
+
+# ################################################################################################################################
+# ################################################################################################################################
+'''
