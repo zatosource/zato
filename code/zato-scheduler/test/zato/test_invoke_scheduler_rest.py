1a873a9800f783840734d4270e47a2124df59a43
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-29 11:53:16 +0100

    GH #755 - Moving tests to zato-scheduler.

--- code/zato-scheduler/test/zato/test_invoke_scheduler_rest.py
+++ code/zato-scheduler/test/zato/test_invoke_scheduler_rest.py
@@ -7,7 +7,6 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # stdlib
-from logging import basicConfig, getLogger, DEBUG
 from unittest import main
 
 # Zato
@@ -17,19 +16,7 @@ from zato.common.test.rest_client import RESTClientTestCase
 # ################################################################################################################################
 # ################################################################################################################################
 
-basicConfig(level=DEBUG, format='%(asctime)s - %(message)s')
-logger = getLogger(__name__)
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class Config:
-    SchedulerAddress = '127.0.0.1:31530{}'
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class SchedulerBrokerClientTestCase(RESTClientTestCase):
+class InvokeSchedulerRESTTestCase(RESTClientTestCase):
 
     def setUp(self) -> None:
         super().setUp()
@@ -37,14 +24,14 @@ class SchedulerBrokerClientTestCase(RESTClientTestCase):
 
 # ################################################################################################################################
 
-    def test_server_to_scheduler(self):
+    def test_rest_invoke_server_to_scheduler(self):
 
         # This will check whether the scheduler replies with the expected metadata
         _ = self.post('/')
 
 # ################################################################################################################################
 
-    def test_server_to_scheduler_invalid_request(self):
+    def test_rest_invoke_server_to_scheduler_invalid_request(self):
 
         # This will check whether the scheduler replies with the expected metadata
         response = self.post('/', {'invalid-key':'invalid-value'}, expect_ok=False)
