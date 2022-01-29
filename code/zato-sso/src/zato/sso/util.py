0e6057b202266f9e17da408f12cd9e4730f238c3
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-29 15:44:49 +0100

    GH #820 - Log a message clarifying why the password was rejected.

--- code/zato-sso/src/zato/sso/util.py
+++ code/zato-sso/src/zato/sso/util.py
@@ -121,6 +121,7 @@ def validate_password(sso_conf, password):
     # Password may not contain most commonly used ones
     for elem in sso_conf.password.reject_list:
         if elem in password:
+            logger.warning('Password rejected because it contains a disallowed pattern from sso.conf -> password.reject_list')
             raise ValidationError(status_code.password.invalid, sso_conf.password.inform_if_invalid)
 
 # ################################################################################################################################
