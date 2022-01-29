0c51ddbbb987999ee8e1dee6f8949d893ba756c7
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-29 11:49:04 +0100

    GH #755 - Adding test_server_to_scheduler_invalid_request.

--- code/zato-broker/test/zato/test_broker_client.py
+++ code/zato-broker/test/zato/test_broker_client.py
@@ -38,7 +38,22 @@ class SchedulerBrokerClientTestCase(RESTClientTestCase):
 # ################################################################################################################################
 
     def test_server_to_scheduler(self):
-        _ = self.get('/')
+
+        # This will check whether the scheduler replies with the expected metadata
+        _ = self.post('/')
+
+# ################################################################################################################################
+
+    def test_server_to_scheduler_invalid_request(self):
+
+        # This will check whether the scheduler replies with the expected metadata
+        response = self.post('/', {'invalid-key':'invalid-value'}, expect_ok=False)
+
+        cid = response['cid'] # type: str
+
+        self.assertTrue(cid.startswith('zsch'))
+        self.assertGreaterEqual(len(cid), 20)
+        self.assertEqual(response['status'], 'error')
 
 # ################################################################################################################################
 # ################################################################################################################################
