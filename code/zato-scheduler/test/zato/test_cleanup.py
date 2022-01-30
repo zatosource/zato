91ac29d14af603955ae9e50cf9e0037117d38f6d
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-01-30 12:23:38 +0100

    GH #755 - Added test pub/sub objects.

--- code/zato-scheduler/test/zato/test_cleanup.py
+++ code/zato-scheduler/test/zato/test_cleanup.py
@@ -9,13 +9,17 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 # stdlib
 from unittest import main
 
+# gevent
+from gevent import sleep
+
 # Zato
-from zato.common.test.rest_client import RESTClientTestCase
+from zato.common.test.config import TestConfig
+from zato.common.test.unittest_ import BasePubSubRestTestCase, PubSubConfig
 
 # ################################################################################################################################
 # ################################################################################################################################
 
-class PubSubCleanupTestCase(RESTClientTestCase):
+class PubSubCleanupTestCase(BasePubSubRestTestCase):
 
     def setUp(self) -> None:
         super().setUp()
@@ -24,7 +28,21 @@ class PubSubCleanupTestCase(RESTClientTestCase):
 # ################################################################################################################################
 
     def test_cleanup_old_subscriptions(self):
-        pass
+
+        # In this test, we check subscriptions to shared topics
+        topic_name = TestConfig.pubsub_topic_shared
+
+        # Before subscribing, make sure we are not currently subscribed
+        self._unsubscribe(topic_name)
+
+        # Subscribe to the topic
+        response_initial = self.rest_client.post(PubSubConfig.PathSubscribe + topic_name)
+
+        # Wait a moment to make sure the subscription data is created
+        sleep(0.2)
+
+        sub_key = response_initial['sub_key']
+        sub_key
 
 # ################################################################################################################################
 # ################################################################################################################################
