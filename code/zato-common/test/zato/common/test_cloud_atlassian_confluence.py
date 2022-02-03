aacd8cbf693d491d404d0b284adb2230c383b608
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-02-03 14:13:25 +0100

    GH #1186 - Added PageProperties to Atlassian connections.

---
+++ code/zato-common/test/zato/common/test_cloud_atlassian_confluence.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2022, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+from unittest import main, TestCase
+
+# Zato
+from zato.common.util.cloud.atlassian.confluence import PageProperties
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class PagePropertiesTestCase(TestCase):
+
+    def test_create_page_properties(self):
+
+        param_name = 'My Parameter'
+        local_id = 'abc-my-local-id'
+
+        prop = PageProperties(param_name, local_id=local_id)
+
+        key1 = 'my.key.1'
+        key2 = 'my.key.2'
+        key3 = 'my.user'
+
+        value1 = 'my.value.1'
+        value2 = 'my.value.2'
+        value3 = prop.get_user_link(key3)
+
+        prop.append(key1, value1)
+        prop.append(key2, value2)
+        prop.append(key3, value3)
+
+        result = prop.get_result()
+
+        expected = """
+<ac:structured-macro
+    ac:name="details"
+    ac:schema-version="1"
+    data-layout="default"
+    ac:local-id="structured-macro-local-id-abc-my-local-id"
+    ac:macro-id="macro-id-abc-my-local-id">
+
+    <ac:parameter ac:name="id">My Parameter</ac:parameter>
+    <ac:rich-text-body>
+        <table data-layout="default" ac:local-id="table-local-id-abc-my-local-id">
+            <colgroup>
+                <col style="width: 340.0px;" />
+                <col style="width: 340.0px;" />
+            </colgroup>
+            <tbody>
+<tr>
+    <th>
+        <p>my.key.1</p>
+    </th>
+    <td>
+        <p>my.value.1</p>
+    </td>
+</tr>
+
+<tr>
+    <th>
+        <p>my.key.2</p>
+    </th>
+    <td>
+        <p>my.value.2</p>
+    </td>
+</tr>
+
+<tr>
+    <th>
+        <p>my.user</p>
+    </th>
+    <td>
+        <p><ac:link><ri:user ri:userkey="my.user" /></p>
+    </td>
+</tr>
+            </tbody>
+        </table>
+    </ac:rich-text-body>
+</ac:structured-macro>
+        """.strip()
+
+        self.assertEqual(expected, result)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if __name__ == '__main__':
+    main()
+
+# ################################################################################################################################
+# ################################################################################################################################
