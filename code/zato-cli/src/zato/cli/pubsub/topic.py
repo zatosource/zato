79de4dbda932637b16bdf65d3ff62a852c97b99b
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-02-05 19:05:24 +0100

    GH #755 - Adding PubSubTopicTestCase.

--- code/zato-cli/src/zato/cli/pubsub/topic.py
+++ code/zato-cli/src/zato/cli/pubsub/topic.py
@@ -17,6 +17,7 @@ from zato.cli import ServerAwareCommand
 
 if 0:
     from argparse import Namespace
+    from zato.common.typing_ import anydict
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -27,15 +28,31 @@ class CreateTopic(ServerAwareCommand):
     opts = [
         {'name':'--name', 'help':'Name of the topic to create', 'required':False},
         {'name':'--gd',   'help':'Should the topic use Guaranteed Delivery', 'required':False},
-        {'name':'--path', 'help':'Path to a Zato server',   'required':False},
+        {'name':'--is-internal', 'help':'Is it a topic internal to the platform', 'required':False},
+        {'name':'--is-api-sub-allowed', 'help':'Can applications subscribe to the topic via a public API', 'required':False},
+        {'name':'--limit-retention', 'help':'Limit retention time in topic to that many seconds', 'required':False},
+        {'name':'--limit-message-expiry', 'help':'Limit max. message expiration time to that many seconds', 'required':False},
+        {'name':'--path', 'help':'Path to a Zato server', 'required':False},
     ]
 
 # ################################################################################################################################
 
-    def execute(self, args:'Namespace'):
+    def _invoke_service_and_log_response(self, service:'str', request:'anydict') -> 'None':
 
         # stdlib
         import sys
+
+        response = self.zato_client.invoke(**{
+            'name':    service,
+            'payload': request
+        })
+
+        data = dumps(response.data)
+        sys.stdout.write(data + '\n')
+
+# ################################################################################################################################
+
+    def execute(self, args:'Namespace'):
 
         # Zato
         from zato.common.api import PUBSUB
@@ -44,8 +61,17 @@ class CreateTopic(ServerAwareCommand):
         _default = PUBSUB.DEFAULT
 
         # Topic name will be generated if it is now given on input
-        topic_name = getattr(args, 'name', None)
-        has_gd = getattr(args, 'gd', True)
+        topic_name  = getattr(args, 'name', None)
+
+        has_gd             = getattr(args, 'gd',   False)
+        is_internal        = getattr(args, 'is_internal', None)
+        is_api_sub_allowed = getattr(args, 'is_api_sub_allowed', True)
+
+        limit_expiry    = getattr(args, 'limit_expiry', 0)    or _default.LimitMessageExpiry
+        limit_retention = getattr(args, 'limit_retention', 0) or _default.LimitTopicRetention
+
+        limit_expiry    = int(limit_expiry)
+        limit_retention = int(limit_retention)
 
         if not topic_name:
             topic_name = '/auto/topic.{}'.format(fs_safe_now())
@@ -54,25 +80,20 @@ class CreateTopic(ServerAwareCommand):
         request = {
             'name': topic_name,
             'is_active': True,
-            'is_internal': True,
+            'is_internal': is_internal,
             'has_gd': has_gd,
-            'is_api_sub_allowed': True,
+            'is_api_sub_allowed': is_api_sub_allowed,
             'max_depth_gd': _default.TOPIC_MAX_DEPTH_GD,
             'max_depth_non_gd': _default.TOPIC_MAX_DEPTH_NON_GD,
             'depth_check_freq': _default.DEPTH_CHECK_FREQ,
             'pub_buffer_size_gd': _default.PUB_BUFFER_SIZE_GD,
             'task_sync_interval': _default.TASK_SYNC_INTERVAL,
             'task_delivery_interval': _default.TASK_DELIVERY_INTERVAL,
-            'delete_at': 60,
+            'limit_expiry': limit_expiry,
+            'limit_retention': limit_retention,
         }
 
-        response = self.zato_client.invoke(**{
-            'name': service,
-            'payload': request
-        })
-
-        data = dumps(response.data)
-        sys.stdout.write(data + '\n')
+        self._invoke_service_and_log_response(service, request)
 
 # ################################################################################################################################
 # ################################################################################################################################
