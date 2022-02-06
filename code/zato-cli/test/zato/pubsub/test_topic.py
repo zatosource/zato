de5e6c0478cb486ec16825bdda9087dda4dc4d97
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-02-06 15:01:38 +0100

    GH #755 - Adding get-topics test cases.

--- code/zato-cli/test/zato/pubsub/test_topic.py
+++ code/zato-cli/test/zato/pubsub/test_topic.py
@@ -10,6 +10,9 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 from datetime import datetime
 from unittest import main
 
+# gevent
+from gevent import sleep
+
 # Zato
 from zato.common.test import CommandLineTestCase
 
@@ -17,7 +20,7 @@ from zato.common.test import CommandLineTestCase
 # ################################################################################################################################
 
 if 0:
-    from zato.common.typing_ import anydict
+    from zato.common.typing_ import any_, anydict
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -40,7 +43,7 @@ class PubSubTopicTestCase(CommandLineTestCase):
 
 # ################################################################################################################################
 
-    def test_create_topic_does_not_exist(self) -> 'None':
+    def xtest_create_topic_does_not_exist(self) -> 'None':
 
         # Test data
         expected_prefix = '/auto/topic.2'  # E.g. /auto/topic.2022_01_31T12_28_42_280577
@@ -56,7 +59,7 @@ class PubSubTopicTestCase(CommandLineTestCase):
 
 # ################################################################################################################################
 
-    def test_create_topic_already_exists(self) -> 'None':
+    def xtest_create_topic_already_exists(self) -> 'None':
 
         # Test data
         prefix = 'test.already-exists.'
@@ -89,13 +92,124 @@ class PubSubTopicTestCase(CommandLineTestCase):
 
 # ################################################################################################################################
 
+    def _run_get_topic_no_such_topic(self, command:'str') -> 'None':
+
+        # There will be no such topic
+        topic_name = '/no/such/topic/' + datetime.utcnow().isoformat()
+
+        # Command to invoke ..
+        get_cli_params = ['pubsub', command, '--name', topic_name]
+
+        # Get the result - there should be none
+        out = self.run_zato_cli_json_command(get_cli_params) # type: any_
+        self.assertListEqual(out, [])
+
+# ################################################################################################################################
+
     def _run_get_topic_test_one_topic(self, command:'str') -> 'None':
-        pass
+
+        # Test data
+        prefix = '/test/'
+        topic_name = prefix + datetime.utcnow().isoformat()
+
+        # Command to invoke ..
+        create_cli_params = ['pubsub', 'create-topic', '--name', topic_name]
+
+        # Create the topic once ..
+        create_out = self.run_zato_cli_json_command(create_cli_params) # type: anydict
+
+        # Command to get the topic with ..
+        get_cli_params = ['pubsub', command, '--name', topic_name]
+
+        # Now, we expect to get that one topic back
+        out_get = self.run_zato_cli_json_command(get_cli_params) # type: any_
+
+        # There must be one topic on output
+        self.assertTrue(len(out_get), 1)
+
+        # Extract it now ..
+        out0 = out_get[0] # type: anydict
+
+        # .. and run our assertions.
+
+        self.assertEqual(out0['id'],   create_out['id'])
+        self.assertEqual(out0['name'], create_out['name'])
+        self.assertEqual(out0['name'], topic_name)
+
+        # This is a new topic and we do not expect any sort of publication-related information associated with it
+        self.assertEqual(out0['current_depth_gd'], 0)
+        self.assertIsNone(out0['last_pub_time'])
+        self.assertIsNone(out0['last_pub_msg_id'])
+        self.assertIsNone(out0['last_endpoint_name'])
+        self.assertIsNone(out0['last_pub_server_name'])
+        self.assertIsNone(out0['last_pub_server_pid'])
+        self.assertIsNone(out0['last_pub_has_gd'])
 
 # ################################################################################################################################
 
     def _run_get_topic_test_multiple_topics(self, command:'str') -> 'None':
-        pass
+
+        # Note that the prefix itself is unique to ensure that it will not repeat
+        prefix = '/test/' + datetime.utcnow().isoformat() + '/'
+
+        # Sleep for a moment to make sure that prefix and the rest are different
+        sleep(0.05)
+
+        # Both
+        topic_name0 = prefix + datetime.utcnow().isoformat()
+
+        # Again, ensure the names are different
+        sleep(0.05)
+
+        topic_name1 = prefix + datetime.utcnow().isoformat()
+
+        # Create both topics now
+        create_cli_params0 = ['pubsub', 'create-topic', '--name', topic_name0]
+        create_cli_params1 = ['pubsub', 'create-topic', '--name', topic_name1]
+
+        create_out0 = self.run_zato_cli_json_command(create_cli_params0) # type: anydict
+        create_out1 = self.run_zato_cli_json_command(create_cli_params1) # type: anydict
+
+        # Command to get the two topics with - note that it uses the prefix to ensure that both are returned ..
+        get_cli_params = ['pubsub', command, '--name', prefix]
+
+        # Now, we expect to get that one topic back
+        out_get = self.run_zato_cli_json_command(get_cli_params) # type: any_
+
+        # Both topics must be on output
+        self.assertTrue(len(out_get), 2)
+
+        # Extract them now ..
+        out0 = out_get[0] # type: anydict
+        out1 = out_get[1] # type: anydict
+
+        # .. and run our assertions ..
+
+        self.assertEqual(out0['id'],   create_out0['id'])
+        self.assertEqual(out0['name'], create_out0['name'])
+        self.assertEqual(out0['name'], topic_name0)
+
+        self.assertEqual(out1['id'],   create_out1['id'])
+        self.assertEqual(out1['name'], create_out1['name'])
+        self.assertEqual(out1['name'], topic_name1)
+
+        # .. these are new topics and we do not expect any sort of publication-related information associated with it
+
+        self.assertEqual(out0['current_depth_gd'], 0)
+        self.assertIsNone(out0['last_pub_time'])
+        self.assertIsNone(out0['last_pub_msg_id'])
+        self.assertIsNone(out0['last_endpoint_name'])
+        self.assertIsNone(out0['last_pub_server_name'])
+        self.assertIsNone(out0['last_pub_server_pid'])
+        self.assertIsNone(out0['last_pub_has_gd'])
+
+        self.assertEqual(out1['current_depth_gd'], 0)
+        self.assertIsNone(out1['last_pub_time'])
+        self.assertIsNone(out1['last_pub_msg_id'])
+        self.assertIsNone(out1['last_endpoint_name'])
+        self.assertIsNone(out1['last_pub_server_name'])
+        self.assertIsNone(out1['last_pub_server_pid'])
+        self.assertIsNone(out1['last_pub_has_gd'])
 
 # ################################################################################################################################
 
@@ -114,14 +228,24 @@ class PubSubTopicTestCase(CommandLineTestCase):
 
 # ################################################################################################################################
 
+    def _run_get_topic_already_published_to(self, command:'str') -> 'None':
+        pass
+
+# ################################################################################################################################
+
     def test_get_topics(self) -> 'None':
 
         # Pub/sub command to run
         command = 'get-topics'
 
         # Run all tests
+        self._run_get_topic_no_such_topic(command)
         self._run_get_topic_test_one_topic(command)
-        self._run_get_topic_test_one_topic(command)
+        self._run_get_topic_test_multiple_topics(command)
+        # self._run_get_topic_default_keys(command)
+        # self._run_get_topic_all_keys(command)
+        # self._run_get_topic_only_selected_keys(command)
+        # self._run_get_topic_already_published_to(command)
 
 # ################################################################################################################################
 
@@ -131,8 +255,13 @@ class PubSubTopicTestCase(CommandLineTestCase):
         command = 'get-topic'
 
         # Run all tests
+        self._run_get_topic_no_such_topic(command)
         self._run_get_topic_test_one_topic(command)
-        self._run_get_topic_test_one_topic(command)
+        self._run_get_topic_test_multiple_topics(command)
+        # self._run_get_topic_default_keys(command)
+        # self._run_get_topic_all_keys(command)
+        # self._run_get_topic_only_selected_keys(command)
+        # self._run_get_topic_already_published_to(command)
 
 # ################################################################################################################################
 # ################################################################################################################################
