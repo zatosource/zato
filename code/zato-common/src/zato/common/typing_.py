92635cb058189632ffa22c210386b807092ca9ea
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-02-08 22:36:03 +0100

    GH #755 - Working on max. retention time.

--- code/zato-common/src/zato/common/typing_.py
+++ code/zato-common/src/zato/common/typing_.py
@@ -10,6 +10,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 # ################################################################################################################################
 
 # stdlib
+from datetime import datetime
 from typing import           \
     Any as any_,             \
     BinaryIO as binaryio_,   \
@@ -78,6 +79,7 @@ cast_        = cast_
 commondict   = dict_[str, union_[int, str_, bool, float, anydict, anylist, None]]
 dictlist     = list_[anydict]
 dictnone     = optional[anydict]
+dtnone       = optional[datetime]
 floatnone    = optional[float]
 generator_   = generator_
 intanydict   = dict_[int, any_]
