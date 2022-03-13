be730ab5c6a1fd3b136b931be1db17aacd1bf4ae
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-03-13 14:51:52 +0100

    GH #755 - Adding WSXChannelManager.

--- code/zato-server/test/zato/wsx/test_outconn_connect.py
+++ code/zato-server/test/zato/wsx/test_outconn_connect.py
@@ -15,13 +15,8 @@ from unittest import main
 
 # Zato
 from zato.common.test import CommandLineTestCase
+from zato.common.test.wsx_ import WSXChannelManager
 from zato.server.generic.api.outconn_wsx import OutconnWSXWrapper
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-if 0:
-    from zato.common.typing_ import anydict
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -30,44 +25,25 @@ class WSXOutconnConnectTestCase(CommandLineTestCase):
 
 # ################################################################################################################################
 
-    def get_wsx_channel_address(self) -> 'str':
-
-        # Command to invoke ..
-        cli_params = ['wsx', 'create-channel']
-
-        # .. get its response as a dict ..
-        out = self.run_zato_cli_json_command(cli_params) # type: anydict
-
-        # .. extract an address of a newly created WSX channel ..
-        address = out['address']
-
-        # .. and return it to our caller.
-        return address
-
-# ################################################################################################################################
-
     def test_connect_ok(self) -> 'None':
 
-        # A newly created channel for us to connect to
-        wsx_channel_address = self.get_wsx_channel_address()
+        with WSXChannelManager(self) as wsx_channel_address:
 
-        # A configuration dict for the outconn wrapper
-        config = {}
-        config['name'] = 'test_connect_ok'
-        config['username'] = 'test_connect_ok'
-        config['secret'] = 'test_connect_ok'
-        config['auth_url'] = config['address'] = wsx_channel_address
-        config['pool_size'] = 1
-        config['is_zato'] = True
-        config['is_active'] = True
-        config['needs_spawn'] = False
-        config['queue_build_cap'] = 30
-        config['subscription_list'] = ''
+            # A configuration dict for the outconn wrapper
+            config = {}
+            config['name'] = 'test_connect_ok'
+            config['username'] = 'test_connect_ok'
+            config['secret'] = 'test_connect_ok'
+            config['auth_url'] = config['address'] = wsx_channel_address
+            config['pool_size'] = 1
+            config['is_zato'] = True
+            config['is_active'] = True
+            config['needs_spawn'] = False
+            config['queue_build_cap'] = 30
+            config['subscription_list'] = ''
 
-        wrapper = OutconnWSXWrapper(config, None)
-        wrapper.build_queue()
-
-        pass
+            wrapper = OutconnWSXWrapper(config, None)
+            wrapper.build_queue()
 
 # ################################################################################################################################
 # ################################################################################################################################
