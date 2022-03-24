dc2dd2c9129ad8dd930d3cc23f4264eb1b013dac
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-03-24 07:24:23 +0100

    GH #755 - Adding type hints.

--- code/zato-common/src/zato/common/odb/model/__init__.py
+++ code/zato-common/src/zato/common/odb/model/__init__.py
@@ -27,7 +27,8 @@ from zato.common.typing_ import cast_
 # ################################################################################################################################
 
 if 0:
-    from zato.common.typing_ import intnone
+    from zato.common.typing_ import floatnone, intnone
+    floatnone = floatnone
     intnone = intnone
 
 # ################################################################################################################################
@@ -2345,7 +2346,7 @@ class PubSubMessage(Base):
 
     pub_time = Column(Numeric(20, 7, asdecimal=False), nullable=False) # When the row was created
     ext_pub_time = Column(Numeric(20, 7, asdecimal=False), nullable=True) # When the message was created by publisher
-    expiration_time = Column(Numeric(20, 7, asdecimal=False), nullable=True)
+    expiration_time = cast_('floatnone', Column(Numeric(20, 7, asdecimal=False), nullable=True))
     last_updated = Column(Numeric(20, 7, asdecimal=False), nullable=True)
 
     data = Column(Text(2 * 10 ** 9), nullable=False) # 2 GB to prompt a promotion to LONGTEXT under MySQL
@@ -2354,9 +2355,9 @@ class PubSubMessage(Base):
     data_prefix_short = Column(String(200), nullable=False)
     data_format = Column(String(200), nullable=False, server_default=PUBSUB.DEFAULT.DATA_FORMAT)
     mime_type = Column(String(200), nullable=False, server_default=PUBSUB.DEFAULT.MIME_TYPE)
-    size = Column(Integer, nullable=False)
-    priority = Column(Integer, nullable=False, server_default=str(PUBSUB.PRIORITY.DEFAULT))
-    expiration = Column(BigInteger, nullable=False, server_default='0')
+    size = cast_('int', Column(Integer, nullable=False))
+    priority = cast_('int', Column(Integer, nullable=False, server_default=str(PUBSUB.PRIORITY.DEFAULT)))
+    expiration = cast_('int', Column(BigInteger, nullable=False, server_default='0'))
     has_gd = Column(Boolean(), nullable=False, server_default=sa_true()) # Guaranteed delivery
 
     # Is the message in at least one delivery queue, meaning that there is at least one
