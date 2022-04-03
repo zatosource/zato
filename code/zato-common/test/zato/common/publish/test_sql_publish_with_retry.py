1d34da4a188ae148ea660fd0c91570ee6e295382
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-04-03 19:11:28 +0200

    GH #755 - Working on test_sql_publish_with_retry.

--- code/zato-common/test/zato/common/publish/test_sql_publish_with_retry.py
+++ code/zato-common/test/zato/common/publish/test_sql_publish_with_retry.py
@@ -8,10 +8,18 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 
 # stdlib
 import os
+from contextlib import closing
 from unittest import main, TestCase
 
 # Zato
 from zato.common.odb.api import PoolStore, SessionWrapper
+from zato.common.odb.query.pubsub.publish import sql_publish_with_retry
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if 0:
+    from zato.common.typing_ import dictlist
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -45,7 +53,7 @@ class SQLPublishWithRetryTestCase(TestCase):
                 'port': os.environ['ZATO_ODB_PORT'],
                 'username': os.environ['ZATO_ODB_USERNAME'],
                 'password': os.environ['ZATO_ODB_PASSWORD'],
-                database: database,
+                'db_name': database,
             }
 
             self.store = PoolStore()
@@ -57,6 +65,57 @@ class SQLPublishWithRetryTestCase(TestCase):
 
 # ################################################################################################################################
 
+    def get_gd_msg_list(self) -> 'dictlist':
+        out = []
+
+        msg1 = {
+            'cluster_id': 1,
+            'data': 'abc',
+            'data_prefix': 'abc',
+            'data_prefix_short': 'abc',
+            'deliver_to_sk': [],
+            'delivery_count': 0,
+            'delivery_status': '2',
+            'expiration': 2,
+            'expiration_time': 3.1,
+            'expiration_time_iso': '',
+            'ext_client_id': 'ext.client.1',
+            'ext_pub_time': '0.9',
+            'ext_pub_time_iso': '',
+            'group_id': '',
+            'has_gd': True,
+            'in_reply_to': '',
+            'is_in_sub_queue': True,
+            'mime_type': 'text/plain',
+            'position_in_group': 1,
+            'pub_correl_id': '',
+            'pub_msg_id': 'zpsm001',
+            'pub_pattern_matched': 'pub=/*',
+            'pub_time': '1.1',
+            'pub_time_iso': '',
+            'published_by_id': 8,
+            'recv_time': 1.0,
+            'recv_time_iso': '',
+            'reply_to_sk': [],
+            'server_name': '',
+            'server_pid': 0,
+            'size': 3,
+            'sub_key': '',
+            'sub_pattern_matched': {'zpsk.wsx.ext.client.1.111': 'sub=/*'},
+            'topic_id': 53,
+            'topic_name': '/*',
+            'zato_ctx': '{\n\n}'
+        }
+
+        msg2 = {}
+
+        # out.append(msg1)
+        # out.append(msg2)
+
+        return out
+
+# ################################################################################################################################
+
     def test_sql_publish_with_retry(self):
 
         # Skip the test if we are not to run.
@@ -64,7 +123,37 @@ class SQLPublishWithRetryTestCase(TestCase):
             return
 
         # If we are here, it means that we can proceed.
-        pass
+
+        now = 1.0
+        cid = 'abc'
+
+        topic_id = 1234
+        topic_name = '/test-sql-publish-with-retry-1'
+
+        cluster_id = 1
+        pub_counter = 1
+
+        new_session_func = self.odb.session
+
+        gd_msg_list = self.get_gd_msg_list()
+        subscriptions_by_topic = []
+
+        with closing(self.odb.session()) as session:
+
+            publish_with_retry_manager = sql_publish_with_retry(
+                now,
+                cid,
+                topic_id,
+                topic_name,
+                cluster_id,
+                pub_counter,
+                session,
+                new_session_func,
+                gd_msg_list,
+                subscriptions_by_topic
+            )
+
+            publish_with_retry_manager
 
 # ################################################################################################################################
 # ################################################################################################################################
