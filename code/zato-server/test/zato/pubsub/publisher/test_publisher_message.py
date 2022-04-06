d702f6616c1c4bbc804911972177fc572ea043ce
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-04-06 19:53:57 +0200

    GH #755 - Working on test_build_message_default.

--- code/zato-server/test/zato/pubsub/publisher/test_publisher_message.py
+++ code/zato-server/test/zato/pubsub/publisher/test_publisher_message.py
@@ -7,12 +7,13 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # stdlib
+from copy import deepcopy
 from unittest import TestCase
 
 # Zato
 from zato.common.test.pubsub.publisher import PublisherTestData
 from zato.common.typing_ import cast_
-from zato.server.pubsub.publisher import Publisher
+from zato.server.pubsub.publisher import Publisher, PubRequest
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -26,18 +27,37 @@ if 0:
 
 class PublisherMessageTestCase(TestCase):
 
+    def get_default_request(
+        self,
+        *,
+        cid,        # type: str
+        topic_name, # type: str
+    ) -> 'PubRequest':
+
+        data = {
+            'topic_name': topic_name
+        }
+
+        out = PubRequest._zato_from_dict(data, extra={'cid':cid})
+        return out
+
+# ################################################################################################################################
+
     def test_get_data_prefixes(self) -> 'None':
 
+        # Make a deep copy so as not to interfere with other tests.
+        test_data = deepcopy(PublisherTestData)
+
         # Make them shorted for the purposes of our test
-        PublisherTestData.pubsub.data_prefix_len = 7
-        PublisherTestData.pubsub.data_prefix_short_len = 3
+        test_data.pubsub.data_prefix_len = 7
+        test_data.pubsub.data_prefix_short_len = 3
 
         publisher = Publisher(
-            pubsub = PublisherTestData.pubsub,
-            server = cast_('ParallelServer', PublisherTestData.server),
-            marshal_api = PublisherTestData.server.marshal_api,
-            service_invoke_func = PublisherTestData.service_invoke_func,
-            new_session_func = PublisherTestData.new_session_func,
+            pubsub = test_data.pubsub,
+            server = cast_('ParallelServer', test_data.server),
+            marshal_api = test_data.server.marshal_api,
+            service_invoke_func = test_data.service_invoke_func,
+            new_session_func = test_data.new_session_func,
         )
 
         data = '1234567890'
@@ -45,6 +65,18 @@ class PublisherMessageTestCase(TestCase):
 
         self.assertEqual(data_prefix, '1234567')
         self.assertEqual(data_prefix_short, '123')
+
+# ################################################################################################################################
+
+    def test_build_message_default(self) -> 'None':
+
+        cid = 'abc'
+        topic_name = '/my-topic'
+
+        request = self.get_default_request(
+            cid = cid,
+            topic_name = topic_name
+        )
 
 # ################################################################################################################################
 # ################################################################################################################################
