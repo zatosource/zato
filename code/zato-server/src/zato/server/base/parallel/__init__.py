06b73f6bc7aae022d902dcfe1e29b71d218e2525
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-04-11 17:34:17 +0200

    GH #755 - Use full paths to read config files.

--- code/zato-server/src/zato/server/base/parallel/__init__.py
+++ code/zato-server/src/zato/server/base/parallel/__init__.py
@@ -617,7 +617,7 @@ class ParallelServer(BrokerMessageReceiver, ConfigLoader, HTTPHandler):
 
             user_conf_full_path = os.path.join(dir_name, file_name)
             user_config_name = get_user_config_name(file_name)
-            conf = get_config_from_file(dir_name, user_conf_full_path)
+            conf = get_config_from_file(user_conf_full_path, file_name)
 
             # Not used at all in this type of configuration
             conf.pop('user_config_items', None)
