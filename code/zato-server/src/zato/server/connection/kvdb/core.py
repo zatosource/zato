771741790b5478b40c121888bf743a3c8212c335
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-04-25 20:15:21 +0200

    GH #1077 - Adding type hints.

--- code/zato-server/src/zato/server/connection/kvdb/core.py
+++ code/zato-server/src/zato/server/connection/kvdb/core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2021, Zato Source s.r.o. https://zato.io
+Copyright (C) 2022, Zato Source s.r.o. https://zato.io
 
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
@@ -25,6 +25,7 @@ from zato.common.ext.dataclasses import dataclass
 # ################################################################################################################################
 
 if 0:
+    from zato.common.typing_ import any_, strnone
     from zato.server.connection.kvdb.list_ import ListRepo
     from zato.server.connection.kvdb.number import NumberRepo
     from zato.server.connection.kvdb.object_ import ObjectRepo
@@ -45,26 +46,29 @@ logger = getLogger('zato')
 class ObjectCtx:
 
     # A unique identifer assigned to this event by Zato
-    id: str
+    id: 'str'
 
     # A correlation ID assigned by Zato - multiple events may have the same CID
-    cid: str = None
+    cid: 'strnone' = None
 
     # Timestamp of this event, as assigned by Zato
-    timestamp: str = None
+    timestamp: 'strnone' = None
 
     # The actual business data
-    data: object = None
+    data: 'any_' = None
 
 # ################################################################################################################################
 # ################################################################################################################################
 
 class BaseRepo(InRAMStore):
 
-    sync_state = None
-
-    def __init__(self, name, data_path, sync_threshold=ZatoKVDB.DefaultSyncThreshold, sync_interval=ZatoKVDB.DefaultSyncInterval):
-        # type: (str, str, int, int) -> None
+    def __init__(
+        self,
+        name,      # type: str
+        data_path, # type: str
+        sync_threshold=ZatoKVDB.DefaultSyncThreshold, # type: int
+        sync_interval=ZatoKVDB.DefaultSyncInterval    # type: int
+    ) -> 'None':
 
         super().__init__(sync_threshold, sync_interval)
 
@@ -76,119 +80,109 @@ class BaseRepo(InRAMStore):
 
 # ################################################################################################################################
 
-    def _append(self, *args, **kwargs):
-        # type: (object, object) -> ObjectCtx
+    def _append(self, *args:'any_', **kwargs:'any_') -> 'ObjectCtx':
         raise NotImplementedError('BaseRepo._append')
 
-    def _get(self, *args, **kwargs):
-        # type: (object, object) -> ObjectCtx
+    def _get(self, *args:'any_', **kwargs:'any_') -> 'ObjectCtx':
         raise NotImplementedError('BaseRepo._get')
 
-    def _set(self, *args, **kwargs):
-        # type: (object, object) -> None
+    def _set(self, *args:'any_', **kwargs:'any_') -> 'None':
         raise NotImplementedError('BaseRepo._set')
 
-    def _get_list(self, *args, **kwargs):
-        # type: (object, object) -> list[ObjectCtx]
+    def _get_list(self, *args:'any_', **kwargs:'any_') -> 'list[ObjectCtx]':
         raise NotImplementedError('BaseRepo._get_list')
 
-    def _delete(self, *args, **kwargs):
-        # type: (object, object) -> list[ObjectCtx]
+    def _delete(self, *args:'any_', **kwargs:'any_') -> 'list[ObjectCtx]':
         raise NotImplementedError('BaseRepo._delete')
 
-    def _remove_all(self, *args, **kwargs):
-        # type: (object, object) -> None
+    def _remove_all(self, *args:'any_', **kwargs:'any_') -> 'None':
         raise NotImplementedError('BaseRepo._remove_all')
 
-    def _clear(self, *args, **kwargs):
-        # type: (object, object) -> None
+    def _clear(self, *args:'any_', **kwargs:'any_') -> 'None':
         raise NotImplementedError('BaseRepo._clear')
 
-    def _get_size(self, *args, **kwargs):
-        # type: (object, object) -> int
+    def _get_size(self, *args:'any_', **kwargs:'any_') -> 'int':
         raise NotImplementedError('BaseRepo._get_size')
 
-    def _incr(self, *args, **kwargs):
-        # type: (object, object) -> int
+    def _incr(self, *args:'any_', **kwargs:'any_') -> 'int':
         raise NotImplementedError('BaseRepo._incr')
 
-    def _decr(self, *args, **kwargs):
-        # type: (object, object) -> int
+    def _decr(self, *args:'any_', **kwargs:'any_') -> 'int':
         raise NotImplementedError('BaseRepo._decr')
 
 # ################################################################################################################################
 
-    def append(self, *args, **kwargs):
+    def append(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._append(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def get(self, *args, **kwargs):
+    def get(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._get(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def get_many(self, *args, **kwargs):
+    def get_many(self, *args:'any_', **kwargs:'any_'):
         # type: (object, object) -> dict
         with self.update_lock:
             return self._get_many(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def set(self, *args, **kwargs):
+    def set(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._set(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def get_list(self, *args, **kwargs):
+    def get_list(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._get_list(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def delete(self, *args, **kwargs):
+    def delete(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._delete(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def remove_all(self, *args, **kwargs):
+    def remove_all(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._remove_all(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def clear(self, *args, **kwargs):
+    def clear(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._clear(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def get_size(self, *args, **kwargs):
+    def get_size(self, *args:'any_', **kwargs:'any_'):
         with self.update_lock:
             return self._get_size(*args, **kwargs)
 
 # ################################################################################################################################
 
-    def incr(self, key, *args, **kwargs):
+    def incr(self, key, *args:'any_', **kwargs:'any_'):
         lock = self.get_lock(key)
         with lock:
             return self._incr(key, *args, **kwargs)
 
 # ################################################################################################################################
 
-    def decr(self, key, *args, **kwargs):
+    def decr(self, key, *args:'any_', **kwargs:'any_'):
         lock = self.get_lock(key)
         with lock:
             return self._decr(key, *args, **kwargs)
 
 # ################################################################################################################################
 
-    def _loads(self, data):
-        # type: (bytes) -> None
+    def _loads(self, data:'bytes') -> 'None':
+        print(111, data)
         data = json_loads(data) # type: dict
         if data:
 
@@ -202,8 +196,7 @@ class BaseRepo(InRAMStore):
 
 # ################################################################################################################################
 
-    def loads(self, data):
-        # type: (bytes) -> int
+    def loads(self, data:'bytes') -> 'None':
         with self.update_lock:
             return self._loads(data)
 
@@ -234,8 +227,7 @@ class BaseRepo(InRAMStore):
 
 # ################################################################################################################################
 
-    def save_data(self):
-        # type: () -> bytes
+    def save_data(self) -> 'None':
         with self.update_lock:
             with open(self.data_path, 'wb') as f:
                 data = self._dumps()
@@ -243,8 +235,7 @@ class BaseRepo(InRAMStore):
 
 # ################################################################################################################################
 
-    def set_data_path(self, data_path):
-        # type: (str) -> None
+    def set_data_path(self, data_path:'str') -> 'None':
         self.data_path = data_path
 
 # ################################################################################################################################
@@ -258,8 +249,13 @@ class KVDB:
 
 # ################################################################################################################################
 
-    def internal_create_list_repo(self, repo_name, data_path=None, max_size=1000, page_size=50):
-        # type: (str) -> ListRepo
+    def internal_create_list_repo(
+        self,
+        repo_name,     # type: str
+        data_path='',  # type: str
+        max_size=1000, # type: int
+        page_size=50   # type: int
+    ) -> 'ListRepo':
 
         # Zato
         from zato.server.connection.kvdb.list_ import ListRepo
@@ -269,8 +265,13 @@ class KVDB:
 
 # ################################################################################################################################
 
-    def internal_create_number_repo(self, repo_name, data_path=None, max_size=1000, page_size=50):
-        # type: (str) -> NumberRepo
+    def internal_create_number_repo(
+        self,
+        repo_name,     # type: str
+        data_path='',  # type: str
+        max_size=1000, # type: int
+        page_size=50   # type: int
+    ) -> 'NumberRepo':
 
         # Zato
         from zato.server.connection.kvdb.number import NumberRepo
@@ -280,8 +281,11 @@ class KVDB:
 
 # ################################################################################################################################
 
-    def internal_create_object_repo(self, repo_name, data_path=None):
-        # type: (str, str) -> ObjectRepo
+    def internal_create_object_repo(
+        self,
+        repo_name,     # type: str
+        data_path=None # type: strnone
+    ) -> 'ObjectRepo':
 
         # Zato
         from zato.server.connection.kvdb.object_ import ObjectRepo
@@ -291,49 +295,42 @@ class KVDB:
 
 # ################################################################################################################################
 
-    def get(self, repo_name):
-        # type: (str) -> ListRepo
+    def get(self, repo_name:'str') -> 'ListRepo':
         return self.repo.get(repo_name)
 
 # ################################################################################################################################
 
-    def append(self, repo_name, ctx):
-        # type: (str, ObjectCtx) -> None
+    def append(self, repo_name:'str', ctx:'ObjectCtx') -> 'None':
         repo = self.repo[repo_name] # type: ListRepo
         repo.append(ctx)
 
 # ################################################################################################################################
 
-    def get_object(self, repo_name, object_id):
-        # type: (str, str) -> ObjectCtx
+    def get_object(self, repo_name:'str', object_id:'str') -> 'ObjectCtx':
         repo = self.repo[repo_name] # type: ListRepo
         return repo.get(object_id)
 
 # ################################################################################################################################
 
-    def get_list(self, repo_name, cur_page=1, page_size=50):
-        # type: (str, int, int) -> None
+    def get_list(self, repo_name:'str', cur_page:'int'=1, page_size:'int'=50) -> 'None':
         repo = self.repo[repo_name] # type: ListRepo
         return repo.get_list(cur_page, page_size)
 
 # ################################################################################################################################
 
-    def delete(self, repo_name, object_id):
-        # type: (str) -> None
+    def delete(self, repo_name:'str', object_id:'str') -> 'any_':
         repo = self.repo[repo_name] # type: ListRepo
         return repo.delete(object_id)
 
 # ################################################################################################################################
 
-    def remove_all(self, repo_name):
-        # type: (str) -> None
+    def remove_all(self, repo_name:'str') -> 'None':
         repo = self.repo[repo_name] # type: ListRepo
         repo.remove_all()
 
 # ################################################################################################################################
 
-    def get_size(self, repo_name):
-        # type: (str) -> int
+    def get_size(self, repo_name:'str') -> 'int':
         repo = self.repo[repo_name] # type: ListRepo
         return repo.get_size()
 
