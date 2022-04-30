844e3099e70dbbc5f56bcedaa76fb56902afdd6c
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-04-30 19:34:51 +0200

    GH #1195 - Adding Salesforce connection forms.

--- code/zato-common/src/zato/common/model/salesforce.py
+++ code/zato-common/src/zato/common/model/salesforce.py
@@ -15,14 +15,8 @@ class SalesforceConfigObject:
         self.id   = None      # type: int
         self.name = None      # type: str
         self.is_active = None # type: bool
-        self.sec_type = None  # type: str
-        self.security_id = None # type: str
-        self.is_audit_log_sent_active = None       # type: bool
-        self.is_audit_log_received_active = None   # type: bool
-        self.max_len_messages_sent = None          # type: int
-        self.max_len_messages_received = None      # type: int
-        self.max_bytes_per_message_sent = None     # type: int
-        self.max_bytes_per_message_received = None # type: int
+        self.api_version = None  # type: str
+        self.username = None # type: str
 
 # ################################################################################################################################
 # ################################################################################################################################
