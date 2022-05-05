4a108b0cdff1d73a35094cc4b3c7fa93b4457bb8
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-05-05 19:57:56 +0200

    GH #1195 - Working on Jira connections (Dashboard).

--- code/zato-web-admin/src/zato/admin/static/js/cloud/jira.js
+++ code/zato-web-admin/src/zato/admin/static/js/cloud/jira.js
@@ -62,8 +62,6 @@ $.fn.zato.cloud.jira.data_table.new_row = function(item, data, include_tr) {
 
     // 2
     row += String.format("<td>{0}</td>", item.username);
-    row += String.format('<!--<td><a href="/zato/cloud/jira/invoke/{0}/{1}/{2}/?cluster={3}">Invoke</a></td>-->',
-        item.id, item.name, $.fn.zato.slugify(item.name), cluster_id);
 
     // 3
     row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.cloud.jira.edit('{0}')\">Edit</a>", item.id));
