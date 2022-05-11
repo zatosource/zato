3b90c353e7e39d9380f53224f04e809fd5168292
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-05-11 08:15:51 +0200

    GH #1196 - Working on Jira connections.

--- code/zato-web-admin/src/zato/admin/web/views/cloud/jira.py
+++ code/zato-web-admin/src/zato/admin/web/views/cloud/jira.py
@@ -29,8 +29,7 @@ class Index(_Index):
 
     class SimpleIO(_Index.SimpleIO):
         input_required = 'cluster_id', 'type_'
-        output_required = 'id', 'name', 'is_active', 'is_internal', 'address', 'username', 'api_version', \
-            'password', 'consumer_key', 'consumer_secret'
+        output_required = 'id', 'name', 'is_active', 'is_cloud', 'api_version', 'address', 'username'
         output_optional = generic_attrs
         output_repeated = True
 
@@ -49,9 +48,7 @@ class _CreateEdit(CreateEdit):
     method_allowed = 'POST'
 
     class SimpleIO(CreateEdit.SimpleIO):
-        input_required = 'name', 'is_internal', 'address', 'username', 'api_version', \
-            'password', 'consumer_key', 'consumer_secret'
-        input_optional = ('is_active', 'pool_size') + generic_attrs
+        input_required = 'id', 'name', 'is_active', 'is_cloud', 'api_version', 'address', 'username'
         output_required = 'id', 'name'
 
 # ################################################################################################################################
