eab47b204bc6e20709b066fae56effa87be6a0d9
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-08-12 15:28:58 +0200

    GH #1200 - Adding dev.service.ide.

--- code/zato-web-admin/src/zato/admin/web/views/service/ide.py
+++ code/zato-web-admin/src/zato/admin/web/views/service/ide.py
@@ -41,7 +41,53 @@ class IDE(BaseCallView):
             'cluster_id':self.req.zato.cluster_id,
         }
 
+        print()
+        print(111, response.data)
+        print()
+
         return TemplateResponse(self.req, self.template, return_data)
 
 # ################################################################################################################################
 # ################################################################################################################################
+
+'''
+# -*- coding: utf-8 -*-
+
+
+# Zato
+from zato.server.service import Service
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class ServiceIDE(Service):
+    name = 'dev.service.ide'
+
+    input = '-service_name'
+    output = '-service_source', 'file_list', 'file_num', 'service_num', 'file_num_human', 'service_num_human'
+
+    def handle(self):
+
+        # Default data structures to fill out with details
+        service_source = None
+        file_list = []
+        file_num = 0
+        service_num = 0
+        file_num_human = '0 files'
+        service_num_human = '0 services'
+
+        response = {
+            'service_source': service_source,
+            'file_list': file_list,
+            'file_num': file_num,
+            'service_num': service_num,
+            'file_num_human': file_num_human,
+            'service_num_human': service_num_human,
+        }
+
+        self.response.payload = response
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+'''
