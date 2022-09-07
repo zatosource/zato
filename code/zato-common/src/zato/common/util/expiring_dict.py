29aa2401cee396b3d142f07bc3f9618363ca0a81
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-09-07 18:31:43 +0200

    GH #109 - Adding expiring_dict.py.

---
+++ code/zato-common/src/zato/common/util/expiring_dict.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2022, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+"""
+### Original license ###
+
+Copyright (c) 2019 David Parker
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# stdlib
+from collections.abc import MutableMapping
+from time import gmtime
+
+# gevent
+from gevent import sleep
+from gevent.threading import Thread, Lock
+
+# SortedContainers
+from sortedcontainers import SortedKeyList
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class ExpiringDict(MutableMapping):
+    def __init__(self, ttl=None, interval=0.100, *args, **kwargs):
+        """
+        Create an ExpiringDict class, optionally passing in a time-to-live
+        number in seconds that will act globally as an expiration time for keys.
+
+        If omitted, the dict will work like a normal dict by default, expiring
+        only keys explicity set via the `.ttl` method.
+        """
+        self._store = dict(*args, **kwargs)
+        self._keys = SortedKeyList(key=lambda x: x[0])
+        self._ttl = ttl
+        self._lock = Lock()
+        self._interval = interval
+
+        Thread(target=self._worker, daemon=True).start()
+
+    def flush(self):
+        now = gmtime()
+        max_index = 0
+        with self._lock:
+            for index, (timestamp, key) in enumerate(self._keys):
+                if timestamp > now:  # rest of the timestamps in future
+                    max_index = index
+                    break
+                try:
+                    del self._store[key]
+                except KeyError:
+                    pass  # Ignore it if it was deleted early
+            del self._keys[0:max_index]
+
+    def _worker(self):
+        while True:
+            self.flush()
+            sleep(self._interval)
+
+    def __setitem__(self, key, value):
+        """
+        Set `value` of `key` in dict. `key` will be automatically
+        deleted if the `ttl` option was provided for this object.
+        """
+        if self._ttl:
+            self._set_with_expire(key, value, self._ttl)
+        else:
+            self._store[key] = value
+
+    def ttl(self, key, value, ttl):
+        """
+        Set `value` of `key` in dict to expire after `ttl` seconds.
+        Overrides object level `ttl` setting.
+        """
+        self._set_with_expire(key, value, ttl)
+
+    def _set_with_expire(self, key, value, ttl):
+        self._lock.acquire()
+        self._keys.add((gmtime() + ttl, key))
+        self._store[key] = value
+        self._lock.release()
+
+    def __delitem__(self, key):
+        del self._store[key]
+
+    def __getitem__(self, key):
+        return self._store[key]
+
+    def __iter__(self):
+        return iter(self._store)
+
+    def __len__(self):
+        return len(self._store)
+
+# ################################################################################################################################
+# ################################################################################################################################
