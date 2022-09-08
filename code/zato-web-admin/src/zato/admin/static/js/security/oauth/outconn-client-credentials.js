f47ff5ed3bafb7486acc303d1fa59a6e7515b575
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-09-08 13:46:30 +0200

    GH #109 - Working on OAuth connections (outgoing client credentials).

--- code/zato-web-admin/src/zato/admin/static/js/security/oauth/outconn-client-credentials.js
+++ code/zato-web-admin/src/zato/admin/static/js/security/oauth/outconn-client-credentials.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     $.fn.zato.data_table.class_ = $.fn.zato.data_table.OAuth;
     $.fn.zato.data_table.new_row_func = $.fn.zato.security.oauth.data_table.new_row;
     $.fn.zato.data_table.parse();
-    $.fn.zato.data_table.setup_forms(['name', 'username']);
+    $.fn.zato.data_table.setup_forms(['name', 'username', 'auth_server_url', 'scopes']);
 })
 
 
@@ -42,12 +42,17 @@ $.fn.zato.security.oauth.data_table.new_row = function(item, data, include_tr) {
     row += "<td class='numbering'>&nbsp;</td>";
     row += "<td class='impexp'><input type='checkbox' /></td>";
     row += String.format('<td>{0}</td>', item.name);
+
     row += String.format('<td>{0}</td>', item.username);
     row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.data_table.change_password({0}, \"Change secret\")'>Change secret</a>", item.id));
     row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.security.oauth.edit('{0}')\">Edit</a>", item.id));
+
     row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.security.oauth.delete_({0});'>Delete</a>", item.id));
     row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
     row += String.format("<td class='ignore'>{0}</td>", is_active);
+
+    row += String.format("<td class='ignore'>{0}</td>", item.auth_server_url);
+    row += String.format("<td class='ignore'>{0}</td>", item.scopes);
 
     if(include_tr) {
         row += '</tr>';
--- code/zato-web-admin/src/zato/admin/web/forms/security/oauth/outconn_client_credentials.py
+++ code/zato-web-admin/src/zato/admin/web/forms/security/oauth/outconn_client_credentials.py
@@ -9,14 +9,25 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 # Django
 from django import forms
 
+# Zato
+from zato.common.api import OAuth as OAuthCommon
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+_default = OAuthCommon.Default
+
 # ################################################################################################################################
 # ################################################################################################################################
 
 class CreateForm(forms.Form):
     id = forms.CharField(widget=forms.HiddenInput())
+
     name = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'style':'width:100%'}))
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'style':'width:100%'}))
-    z  kzhxd 970w-ww = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'style':'width:100%'}))
+
+    auth_server_url = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}), initial=_default.Auth_Server_URL)
+    scopes = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%; height:80px'}), initial='\n'.join(_default.Scopes))
 
     def __init__(self, prefix=None, post_data=None):
         super(CreateForm, self).__init__(post_data, prefix=prefix)
