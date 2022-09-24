f66d44eb2dc0c8e2b01176ede9e9b01ee49c92a5
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-09-24 18:29:48 +0200

    GH #1070 - Adding MessageMiddleware for Django.

--- code/zato-web-admin/src/zato/admin/settings.py
+++ code/zato-web-admin/src/zato/admin/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'zato.admin.middleware.ZatoMiddleware',
 ]
 
