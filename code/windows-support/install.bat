1b39a3943633fa4b307e663d25709617611ca7bc
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-12-03 18:24:48 +0100

    GH #1070 - Working on Windows packages.

--- code/windows-support/install.bat
+++ code/windows-support/install.bat
@@ -11,22 +11,16 @@ rem
 rem Local aliases
 rem
 set curdir=%~dp0
-set python_bin_dir=%curdir%\windows-python-embedded-%python_version%
+set python_bin_dir=%curdir%\bundle-ext\python-windows\python-%python_version%
 set python_cmd=%python_bin_dir%\python.exe
 
 rem
 rem Ensure that git considers our directory as safe
 rem
-git config --add safe.directory curdir\..
-
-rem
-rem Install prerequisites
-rem
-cd python_bin_dir
-rem %python_cmd% %python_bin_dir%\get-pip.py
+git config --add safe.directory %curdir%\..
 
 rem
 rem Actually install all Python dependencies
 rem
 rem echo Setting up environment in %curdir%
-start "Zato install" call %curdir%\_windows-run-install.bat
+start "Zato install" call %curdir%\run-install.bat
