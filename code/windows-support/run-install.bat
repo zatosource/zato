b34c1725dd342a498cd2c5ae5eb1eeacb838884e
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-12-03 18:24:48 +0100

    GH #1070 - Working on Windows packages.

--- code/windows-support/run-install.bat
+++ code/windows-support/run-install.bat
@@ -8,7 +8,7 @@ rem
 rem Local aliases
 rem
 set curdir=%~dp0
-set python_bin_dir=%curdir%\windows-python-embedded-%python_version%
+set python_bin_dir=%curdir%\..\bundle-ext\python-windows\python-%python_version%
 set python_cmd=%python_bin_dir%\python.exe
 
 rem
@@ -22,9 +22,9 @@ rem
 rem Install Zato
 rem
 rem cd %python_bin_dir%\
-%python_cmd% %curdir%\util\zato_environment.py install
+%python_cmd% %curdir%\..\util\zato_environment.py install
 
 rem
 rem Confirm the version installed
 rem
-rem %python_bin_dir%\Scripts\zato.py --version
+rem %python_cmd% %python_bin_dir%\Scripts\zato-bin.py --version
