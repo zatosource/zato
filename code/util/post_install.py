d386693c3ced7b26ef61f409f1e2b3a16767a3f2
Dariusz Suchojad <dsuch-github@m.zato.io>
2022-12-11 21:54:44 +0100

    GH #1070 - Working on Windows packages.

--- code/util/post_install.py
+++ code/util/post_install.py
@@ -11,6 +11,7 @@ import logging
 import os
 import sys
 from glob import glob
+from pathlib import Path
 from platform import system as platform_system
 from shutil import copy as shutil_copy
 
@@ -44,103 +45,49 @@ site_packages_relative = ['lib', 'site-packages']
 # ################################################################################################################################
 # ################################################################################################################################
 
-#
-# This is where the source code is built under Windows and this is what we are replacing during the installation
-# with a final path that the package has been installed to, e.g. C:\Users\Jane\LocalAppData\ZatoSource\Zato\Zato-3.2-python38
-#
-
-build_dir_list = [
-
-    #
-    # 3.9
-    #
-
-    r'c:\Users\dsuch\projects\src-zato\zato-3.2-python-3.9\code',
-    r'C:\Users\dsuch\projects\src-zato\zato-3.2-python-3.9\code',
-
-    r'c:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.9\\code',
-    r'C:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.9\\code',
-
-    #
-    # 3.10
-    #
-
-    r'c:\Users\dsuch\projects\src-zato\zato-3.2-python-3.10\code',
-    r'C:\Users\dsuch\projects\src-zato\zato-3.2-python-3.10\code',
-
-    r'c:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.10\\code',
-    r'C:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.10\\code',
-
-    #
-    # 3.11
-    #
-
-    r'c:\Users\dsuch\projects\src-zato\zato-3.2-python-3.11\code',
-    r'C:\Users\dsuch\projects\src-zato\zato-3.2-python-3.11\code',
-
-    r'c:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.11\\code',
-    r'C:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.11\\code',
-
-    #
-    # 3.12
-    #
-
-    r'c:\Users\dsuch\projects\src-zato\zato-3.2-python-3.12\code',
-    r'C:\Users\dsuch\projects\src-zato\zato-3.2-python-3.12\code',
-
-    r'c:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.12\\code',
-    r'C:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.12\\code',
-
-    #
-    # 3.13
-    #
-
-    r'c:\Users\dsuch\projects\src-zato\zato-3.2-python-3.13\code',
-    r'C:\Users\dsuch\projects\src-zato\zato-3.2-python-3.13\code',
-
-    r'c:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.13\\code',
-    r'C:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.13\\code',
-
-    #
-    # 3.14
-    #
-
-    r'c:\Users\dsuch\projects\src-zato\zato-3.2-python-3.14\code',
-    r'C:\Users\dsuch\projects\src-zato\zato-3.2-python-3.14\code',
-
-    r'c:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.14\\code',
-    r'C:\\Users\\dsuch\\projects\\src-zato\\zato-3.2-python-3.14\\code',
-
-    #
-    # 3.15
-    #
-
-    r'c:\Users\dsuch\projects\src-zato\zato-3.2-python-3.15\code',
-    r'C:\Users\dsuch\projects\src-zato\zato-3.2-python-3.15\code',
-
-    r'c:\\Users\\dsuch\\projects\src-zato\\zato-3.2-python-3.15\\code',
-    r'C:\\Users\\dsuch\\projects\src-zato\\zato-3.2-python-3.15\\code',
-
-]
-
-# ################################################################################################################################
-# ################################################################################################################################
-
-class WindowsPostInstall:
-    """ Code to run after a Windows package has been just installed.
+class PostInstall:
+    """ Code to run after a Zato package has been just installed.
     """
-    def __init__(self, base_dir:'str', bin_dir:'str') -> 'None':
+
+    build_dir_to_base_depth: 'int'
+
+    base_dir: 'str'
+    bin_dir:  'str'
+
+    # A directory where Python binaries are in, e.g. python3.11, python3.12 etc.
+    python_dir: 'str'
+
+    # The full path to the python_dir directory
+    python_dir_full: 'str'
+
+    # The directory that this installation was originally build in,
+    # e.g. the one that was used on the build server.
+    orig_build_dir: 'str'
+
+    # Packages are installed here
+    site_packages_dir: 'str'
+
+    # This is the path to the directory that 'zato.bat' command is in
+    zato_windows_bin_dir: 'str'
+
+    # Full path to 'zato.bat'
+    zato_windows_bin_path: 'str'
+
+# ################################################################################################################################
+
+    def init(self, base_dir:'str', bin_dir:'str') -> 'None':
+
         self.base_dir = base_dir
         self.bin_dir = bin_dir
 
         # Packages are installed here
         self.site_packages_dir = os.path.join(self.base_dir, *site_packages_relative)
 
-        # This is the path to the directory that 'zato.py' command is in
-        self.zato_windows_bin_dir = os.path.join(self.base_dir, 'windows-bin')
+        # This is the path to the directory that 'zato.bat' command is in
+        # self.zato_windows_bin_dir = os.path.join(self.base_dir, 'windows-bin')
 
         # Full path to 'zato.py'
-        self.zato_windows_bin_path = os.path.join(self.zato_windows_bin_dir, 'zato.py')
+        # self.zato_windows_bin_path = os.path.join(self.zato_windows_bin_dir, 'zato.bat')
 
 # ################################################################################################################################
 
@@ -189,18 +136,17 @@ class WindowsPostInstall:
             # Now, we can get the contents of the original file
             data = open(name, 'r').read() # type: str
 
-            # Work only with files that actually need to be updated
-            for build_dir in build_dir_list:
+            if self.orig_build_dir in data:
 
-                if build_dir in data:
+                # Replace the build directory with the actual installation directory ..
+                data = data.replace(self.orig_build_dir, self.base_dir)
 
-                    # Replace the build directory with the actual installation directory ..
-                    data = data.replace(build_dir, self.base_dir)
+                # .. and save the data on disk.
+                # f = open(name, 'w')
+                # _ = f.write(data)
+                # f.close()
 
-                    # .. and save the data on disk.
-                    f = open(name, 'w')
-                    _ = f.write(data)
-                    f.close()
+                print(333, name)
 
 # ################################################################################################################################
 
@@ -233,16 +179,6 @@ class WindowsPostInstall:
 
         # Actually updates the files now
         self.update_files(files_dir, patterns, to_ignore)
-
-# ################################################################################################################################
-
-    def copy_zato_binary(self) -> 'None':
-
-        # This is the path that 'zato.py' was saved to by default
-        zato_py_path = os.path.join(self.bin_dir, 'zato.py')
-
-        # Copy the binary to its final destination
-        shutil_copy(zato_py_path, self.zato_windows_bin_path)
 
 # ################################################################################################################################
 
@@ -306,9 +242,8 @@ class WindowsPostInstall:
 
     def update_paths(self) -> 'None':
         self.update_site_packages_files()
-        self.update_bin_files()
-        self.copy_zato_binary()
-        self.set_git_root_dir_config()
+        # self.update_bin_files()
+        # self.set_git_root_dir_config()
 
 # ################################################################################################################################
 
@@ -316,25 +251,178 @@ class WindowsPostInstall:
         self.update_windows_registry()
 
 # ################################################################################################################################
+
+    def get_impl_base_dir(self) -> 'str':
+        raise NotImplementedError('Must be implemented by subclasses')
+
+# ################################################################################################################################
+
+    def run_impl(self) -> 'None':
+        raise NotImplementedError('Must be implemented by subclasses')
+
+# ################################################################################################################################
+
+    def get_orig_build_dir(self) -> 'str':
+        raise NotImplementedError('Must be implemented by subclasses')
+
+# ################################################################################################################################
+
+    def get_python_dir(self) -> 'str':
+        raise NotImplementedError('Must be implemented by subclasses')
+
+# ################################################################################################################################
+
+    def get_python_dir_full(self) -> 'str':
+        raise NotImplementedError('Must be implemented by subclasses')
+
+# ################################################################################################################################
+
+    def get_site_packages_dir(self) -> 'str':
+        raise NotImplementedError('Must be implemented by subclasses')
+
+# ################################################################################################################################
+
+    def get_base_dir(self) -> 'str':
+
+        # Base directory may be given explicitly or we will need build it in relation to our own location
+        if len(sys.argv) > 1:
+            base_dir = sys.argv[1]
+            return base_dir
+        else:
+            return self.get_impl_base_dir()
+
+# ################################################################################################################################
+
+    def run(self) -> 'None':
+
+        # Prepare paths ..
+        self.base_dir         = self.get_base_dir()
+        self.orig_build_dir   = self.get_orig_build_dir()
+        self.python_dir       = self.get_python_dir()
+        self.python_dir_full  = self.get_python_dir_full()
+        self.site_packages_dir = self.get_site_packages_dir()
+
+        # .. and actually run the process.
+        self.run_impl()
+
+        """
+        curdir = os.path.dirname(os.path.abspath(__file__))
+
+        base_dir = os.path.join(curdir, '..')
+        base_dir = os.path.abspath(base_dir)
+
+        base_dir = base_dir.replace('\\', '\\\\')
+
+        bin_dir = os.path.join(base_dir, 'Scripts')
+        bin_dir = os.path.abspath(bin_dir)
+        """
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class WindowsPostInstall(PostInstall):
+    pass
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class NonWindowsPostInstall(PostInstall):
+
+    build_dir_to_base_depth = 2
+
+# ################################################################################################################################
+
+    def run_impl(self) -> 'None':
+        self.update_paths()
+
+# ################################################################################################################################
+
+    def get_impl_base_dir(self) -> 'str':
+        curdir = os.path.dirname(os.path.abspath(__file__))
+        base_dir = os.path.join(curdir, '..')
+        base_dir = os.path.abspath(base_dir)
+        return base_dir
+
+# ################################################################################################################################
+
+    def get_python_dir(self) -> 'any_':
+
+        python_dir = 'default-python_dir'
+
+        # This the directory where the Python directory will be found ..
+        lib_dir = os.path.join(self.base_dir, 'lib')
+
+        # .. list all the directories in the lib dir ..
+        for item in sorted(os.listdir(lib_dir)):
+
+            # .. accept the one that is a Python one ..
+            if item.startswith('python'):
+                python_dir = item
+                break
+
+        # .. and return the result to the caller.
+        return python_dir
+
+# ################################################################################################################################
+
+    def get_python_dir_full(self) -> 'str':
+
+        python_dir_full = os.path.join(self.base_dir, 'lib', self.python_dir)
+        python_dir_full = os.path.abspath(python_dir_full)
+
+        return python_dir_full
+
+# ################################################################################################################################
+
+    def get_site_packages_dir(self) -> 'str':
+
+        site_packages_dir = os.path.join(self.python_dir_full, 'site-packages')
+        return site_packages_dir
+
+# ################################################################################################################################
+
+    def get_orig_build_dir(self) -> 'str':
+
+        # Full path to the zato command ..
+        zato_bin_path = os.path.join(self.base_dir, 'bin', 'zato')
+
+        # .. read the whole contents ..
+        lines = open(zato_bin_path).readlines()
+
+        # .. our path will be in the first line ..
+        bin_line = lines[0]
+        bin_line = bin_line.strip()
+        bin_line = bin_line.replace('#!', '')
+
+        #
+        # .. Now, we have something like this in bin_line:
+        # .. /home/user/projects/zatosource-zato/3.2/code/bin/python
+        # .. and we would like to remove the trailing parts to have this:
+        # .. /home/user/projects/zatosource-zato/3.2/code/
+        #
+        # .. How many parts to remove will depend on what operating system we are on
+        # .. which is why it is our subclasses that tell it to us below.
+
+        # Turn what we have so far into a Path object so it is easier to process it ..
+        bin_path = Path(bin_line)
+
+        # .. extract the original build directory now ..
+        orig_build_dir = bin_path.parts[:-self.build_dir_to_base_depth]
+
+        # .. turn it into a list ..
+        orig_build_dir = os.sep.join(orig_build_dir)
+
+        # .. and return it to our caller.
+        return orig_build_dir
+
+# ################################################################################################################################
 # ################################################################################################################################
 
 if __name__ == '__main__':
 
-    curdir = os.path.dirname(os.path.abspath(__file__))
-
-    base_dir = os.path.join(curdir, '..')
-    base_dir = os.path.abspath(base_dir)
-
-    base_dir = base_dir.replace('\\', '\\\\')
-
-    bin_dir = os.path.join(base_dir, 'Scripts')
-    bin_dir = os.path.abspath(bin_dir)
-
-    command = sys.argv[1]
-
-    util = WindowsPostInstall(base_dir, bin_dir)
-    func = getattr(util, command)
-    func()
+    class_ = WindowsPostInstall if is_windows else NonWindowsPostInstall
+    instance = class_()
+    instance.run()
 
 # ################################################################################################################################
 # ################################################################################################################################
