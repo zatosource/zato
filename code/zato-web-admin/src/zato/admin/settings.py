2f85fbf9877f97d832e324baa77f5c70e53852b3
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-02-21 22:46:19 +0100

    GH #755 - Adding more headers.

--- code/zato-web-admin/src/zato/admin/settings.py
+++ code/zato-web-admin/src/zato/admin/settings.py
@@ -59,7 +59,7 @@ USE_I18N = True
 
 DEBUG = False
 APPEND_SLASH = True
-SECURE_CONTENT_TYPE_NOSNIFF = False
+SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # Absolute path to the directory that holds media.
 # Example: '/home/media/media.lawrence.com/'
