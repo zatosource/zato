273d2c4b77976f1e55a91fcc6a2229a84269007e
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-03-13 16:35:33 +0100

    GH #755 - Working on service endpoints.

--- code/zato-server/src/zato/server/service/internal/pubsub/subscription.py
+++ code/zato-server/src/zato/server/service/internal/pubsub/subscription.py
@@ -448,6 +448,9 @@ class SubscribeService(SubscribeServiceImpl):
     def _handle_subscription(self, ctx:'SubCtx') -> 'None':
         self._subscribe_impl(ctx)
 
+class SubscribeSrv(SubscribeService):
+    pass
+
 # ################################################################################################################################
 
 class Create(_Subscribe):
@@ -468,7 +471,8 @@ class Create(_Subscribe):
             topic_name = [topic_name]
 
         if not(topic_list_text or topic_list_json or topic_name):
-            raise BadRequest(self.cid, 'No topics to subscribe to were given on input')
+            # raise BadRequest(self.cid, 'No topics to subscribe to were given on input')
+            return
         else:
             if topic_list_text:
                 topic_list = topic_list_text
