0b337cce7d87389f06c21d8ced4ae5550b0d65c6
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-04-25 18:26:27 +0200

    GH #1219 - Working on Keysight connections.

---
+++ code/zato-web-admin/src/zato/admin/web/forms/vendors/keysight_vision.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2022, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# Django
+from django import forms
+
+# Zato
+from zato.admin.web.forms import WithAuditLog
+from zato.common.api import Atlassian as AtlassianCommon
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+_default = AtlassianCommon.Default
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class CreateForm(WithAuditLog):
+
+    name = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+    is_cloud = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+
+    api_version = forms.CharField(widget=forms.TextInput(attrs={'style':'width:20%'}), initial=_default.API_Version)
+    address = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}), initial=_default.Address)
+    username = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
+
+    password = forms.CharField(strip=False, widget=forms.PasswordInput(attrs={'style':'width:100%'}))
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class EditForm(CreateForm):
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+    is_cloud = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+
+# ################################################################################################################################
+# ################################################################################################################################
---
+++ code/zato-web-admin/src/zato/admin/web/views/vendors/keysight_vision.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2023, Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# Zato
+from zato.admin.web.forms import ChangePasswordForm
+from zato.admin.web.forms.vendors.keysight_vision import CreateForm, EditForm
+from zato.admin.web.views import change_password as _change_password, CreateEdit, Delete as _Delete, Index as _Index, \
+    method_allowed
+from zato.common.api import GENERIC, generic_attrs
+from zato.common.model.atlassian_ import AtlassianConfigObject
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class Index(_Index):
+    method_allowed = 'GET'
+    url_name = 'vendors-keysight-vision'
+    template = 'zato/vendors/keysight-vision.html'
+    service_name = 'zato.generic.connection.get-list'
+    output_class = AtlassianConfigObject
+    paginate = True
+
+    class SimpleIO(_Index.SimpleIO):
+        input_required = 'cluster_id', 'type_'
+        output_required = 'id', 'name', 'is_active', 'is_cloud', 'api_version', 'address', 'username'
+        output_optional = generic_attrs
+        output_repeated = True
+
+# ################################################################################################################################
+
+    def handle(self):
+        return {
+            'create_form': CreateForm(),
+            'edit_form': EditForm(prefix='edit'),
+            'change_password_form': ChangePasswordForm()
+        }
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class _CreateEdit(CreateEdit):
+    method_allowed = 'POST'
+
+    class SimpleIO(CreateEdit.SimpleIO):
+        input_required = 'id', 'name', 'is_active', 'is_cloud', 'api_version', 'address', 'username'
+        output_required = 'id', 'name'
+
+# ################################################################################################################################
+
+    def populate_initial_input_dict(self, initial_input_dict):
+        initial_input_dict['type_'] = GENERIC.CONNECTION.TYPE.CLOUD_CONFLUENCE
+        initial_input_dict['is_internal'] = False
+        initial_input_dict['is_channel'] = False
+        initial_input_dict['is_outgoing'] = True
+        initial_input_dict['is_outconn'] = False
+        initial_input_dict['sec_use_rbac'] = False
+        initial_input_dict['recv_timeout'] = 250
+        initial_input_dict['pool_size'] = 20
+
+# ################################################################################################################################
+
+    def success_message(self, item):
+        return 'Successfully {} Confluence cloud connection `{}`'.format(self.verb, item.name)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class Create(_CreateEdit):
+    url_name = 'vendors-keysight-vision-create'
+    service_name = 'zato.generic.connection.create'
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class Edit(_CreateEdit):
+    url_name = 'vendors-keysight-vision-edit'
+    form_prefix = 'edit-'
+    service_name = 'zato.generic.connection.edit'
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class Delete(_Delete):
+    url_name = 'vendors-keysight-vision-delete'
+    error_message = 'Could not delete Confluence connection'
+    service_name = 'zato.generic.connection.delete'
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+@method_allowed('POST')
+def change_password(req):
+    return _change_password(req, 'zato.generic.connection.change-password', success_msg='API token updated')
+
+# ################################################################################################################################
