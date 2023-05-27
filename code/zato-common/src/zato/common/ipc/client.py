75f70b2075d7a75a9ff82cef1c89348ee5fed528
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-05-27 11:58:47 +0200

    GH #755 - Working on multi-CPU processes.

--- code/zato-common/src/zato/common/ipc/client.py
+++ code/zato-common/src/zato/common/ipc/client.py
@@ -6,29 +6,12 @@ Copyright (C) 2023, Zato Source s.r.o. https://zato.io
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
-# Zato
-from zato.common.api import IPC
-from zato.common.ipc import IPCEndpoint, Request
-
 # ################################################################################################################################
 # ################################################################################################################################
 
-class Publisher(IPCEndpoint):
+class Client:
     """ Sends outgoing IPC messages to any party listening for them.
     """
-    socket_method = 'connect'
-    socket_type = 'pub'
-
-    def publish(self, payload, service='', target_pid=None, action=IPC.ACTION.INVOKE_SERVICE, reply_to_fifo=None):
-        request = Request(self.name, self.pid)
-
-        request.payload = payload
-        request.service = service
-        request.action = action
-        request.target_pid = target_pid
-        request.reply_to_fifo = reply_to_fifo
-
-        self.socket.send_pyobj(request)
 
 # ################################################################################################################################
 # ################################################################################################################################
