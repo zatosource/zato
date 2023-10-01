007d29a80d0c7b5bad2264a41e33b36f2c8a2747
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-10-01 19:28:04 +0200

    GH #755 - Working on extract_pickup_from_items.

--- code/zato-common/src/zato/common/util/hot_deploy_.py
+++ code/zato-common/src/zato/common/util/hot_deploy_.py
@@ -6,9 +6,12 @@ Copyright (C) 2023, Zato Source s.r.o. https://zato.io
 Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# stdlib
+from pathlib import Path
+
 # Zato
 from zato.common.api import HotDeploy
-from zato.common.hot_deploy_ import HotDeployTree
+from zato.common.hot_deploy_ import HotDeployProject
 from zato.common.typing_ import cast_
 from zato.common.util.file_system import resolve_path
 
@@ -16,7 +19,40 @@ from zato.common.util.file_system import resolve_path
 # ################################################################################################################################
 
 if 0:
-    from zato.common.typing_ import iterator_, strdictdict
+    from zato.common.typing_ import iterator_, list_, strdictdict
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+def get_project_info(
+    root,          # type: str
+    src_dir_name,  # type: str
+    max_levels=10, # type: int
+) -> 'HotDeployProject | None':
+
+    # Visit up to max_levels of the root directory,
+    # trying to find a directory that holds the source code of a project / services.
+
+    # Local aliases
+    root_path = Path(root)
+
+    # Assume we will not find such a directory
+    out = None
+
+    # Any potential project directories will go here
+    project_dirs:'list_[Path]' = []
+
+    # Look up the project directories recursively ..
+    for item in root_path.rglob(src_dir_name):
+
+        # .. if any is found, append it for later use ..
+        project_dirs.append(item)
+
+    print()
+    print(111, project_dirs)
+    print()
+
+    return out
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -24,7 +60,8 @@ if 0:
 def extract_pickup_from_items(
     base_dir,      # type: str
     pickup_config, # type: strdictdict
-) -> 'iterator_[str | HotDeployTree]':
+    src_dir_name,  # type: str
+) -> 'iterator_[str | HotDeployProject]':
 
     # Go through each piece of the hot-deployment configuration ..
     for key, value in pickup_config.items(): # type: ignore
@@ -40,6 +77,13 @@ def extract_pickup_from_items(
 
                 # .. this will resolve home directories and environment variables ..
                 pickup_from = resolve_path(pickup_from, base_dir)
+
+                # .. check if this path points to a project ..
+                if project := get_project_info(pickup_from, src_dir_name):
+
+                    print()
+                    print(111, project)
+                    print()
 
                 yield pickup_from
 
