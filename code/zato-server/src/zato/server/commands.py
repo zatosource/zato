ff7141689d457fd28c1e86866c74e2097d2fea35
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-10-22 03:15:52 +0200

    GH #755 - Use the newer format.

--- code/zato-server/src/zato/server/commands.py
+++ code/zato-server/src/zato/server/commands.py
@@ -380,7 +380,7 @@ class CommandsFacade:
 # ################################################################################################################################
 
     def run_enmasse_async(self, file_path:'str') -> 'CommandResult':
-        command = f'enmasse --import --replace-odb-objects --input {file_path} {self.server.base_dir} --verbose'
+        command = f'enmasse --import --replace --input {file_path} {self.server.base_dir} --verbose'
         result = self.run_zato_cli_async(command, callback=self._on_enmasse_completed)
         return result
 
