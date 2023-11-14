db9ad4daae79084db00ae8e9977318a6520c6405
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-11-14 22:08:37 +0100

    GH #755 - Adding NTLM.

--- code/zato-web-admin/src/zato/admin/web/forms/outgoing/wsx.py
+++ code/zato-web-admin/src/zato/admin/web/forms/outgoing/wsx.py
@@ -18,10 +18,10 @@ class CreateForm(DataFormatForm):
     address = forms.CharField(widget=forms.TextInput(attrs={'style':'width:100%'}))
     is_zato = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
     has_auto_reconnect = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
-    on_connect_service_name = forms.ChoiceField(widget=forms.Select())
-    on_message_service_name = forms.ChoiceField(widget=forms.Select())
-    on_close_service_name = forms.ChoiceField(widget=forms.Select())
-    security_def = forms.ChoiceField(widget=forms.Select())
+    on_connect_service_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
+    on_message_service_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
+    on_close_service_name = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
+    security_def = forms.ChoiceField(widget=forms.Select(attrs={'style':'width:100%'}))
     subscription_list = forms.CharField(widget=forms.Textarea(attrs={'style':'width:100%'}))
 
     def __init__(self, security_list=None, prefix=None, post_data=None, req=None):
