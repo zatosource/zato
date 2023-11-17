7e94fdd819ecee4482d6fcdbe436e832ed7a85e0
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-11-17 13:36:18 +0100

    GH #755 - Unserialize data from bytes.

--- code/zato-common/src/zato/common/ipc/client.py
+++ code/zato-common/src/zato/common/ipc/client.py
@@ -7,7 +7,7 @@ Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # stdlib
-from json import loads
+from json import dumps, loads
 
 # requests
 from requests import post as requests_post
@@ -16,7 +16,6 @@ from requests import post as requests_post
 from zato.common.api import IPC as Common_IPC
 from zato.common.broker_message import SERVER_IPC
 from zato.common.typing_ import dataclass
-from zato.common.json_ import dumps
 
 # ################################################################################################################################
 # ################################################################################################################################
