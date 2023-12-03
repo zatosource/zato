49d686d2afa4c8d88fab8475832a4bdab6cc00dc
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-12-03 16:02:13 +0100

    GH #755 - Upgrading jQuery (before_submit_hook).

--- code/zato-web-admin/src/zato/admin/static/js/pubsub/subscription.js
+++ code/zato-web-admin/src/zato/admin/static/js/pubsub/subscription.js
@@ -36,9 +36,9 @@ $(document).ready(function() {
     $.fn.zato.data_table.new_row_func = $.fn.zato.pubsub.subscription.data_table.new_row;
     $.fn.zato.data_table.new_row_func_update_in_place = true;
     $.fn.zato.data_table.add_row_hook = $.fn.zato.pubsub.subscription.add_row_hook;
-    $.fn.zato.data_table.parse();
     $.fn.zato.data_table.before_populate_hook = $.fn.zato.pubsub.subscription.cleanup_hook;
     $.fn.zato.data_table.before_submit_hook = $.fn.zato.pubsub.subscription.before_submit_hook;
+    $.fn.zato.data_table.parse();
     $.fn.zato.data_table.setup_forms(elems_required);
 
     // Maps values from selects to IDs of elements that should be made required
@@ -182,63 +182,6 @@ $.fn.zato.pubsub.subscription.cleanup_hook = function(form, _unused_prefix) {
     $('#multi-select-div').html(blank);
 
     return true;
-}
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-$.fn.zato.pubsub.subscription.before_submit_hook = function(form) {
-
-    var form = $(form);
-    var is_valid = true;
-    var rest_chosen_pattern = "#id_out_rest_http_soap_id_chosen a";
-
-    // Clear anything potentially left over from the previous run
-    $.fn.zato.remove_css_attention(rest_chosen_pattern);
-
-    if(!$.fn.zato.is_form_valid(form)) {
-        is_valid = false;
-    }
-
-    var is_edit = form.attr('id').includes('edit');
-    var prefix = is_edit ? 'edit-' : '';
-    var endpoint_type = $('#id_' + prefix + 'endpoint_type').val();
-
-    var server_id       = $('#id_' + prefix + 'server_id');
-    var delivery_method = $('#id_' + prefix + 'delivery_method');
-    var out_http_method = $('#id_' + prefix + 'out_http_method');
-    var out_rest_http_soap_id = $('#id_' + prefix + 'out_rest_http_soap_id');
-
-    if(endpoint_type == 'rest') {
-
-        if(!server_id.val()) {
-            $.fn.zato.draw_attention_to(server_id);
-            is_valid = false;
-        }
-
-        if(!delivery_method.val()) {
-            $.fn.zato.draw_attention_to(delivery_method);
-            is_valid = false;
-        }
-
-        if(!out_http_method.val()) {
-            $.fn.zato.draw_attention_to(out_http_method);
-            is_valid = false;
-        }
-
-        if(delivery_method.val() == 'notify') {
-            if(!out_rest_http_soap_id.val()) {
-                $.fn.zato.draw_attention_to(rest_chosen_pattern);
-                is_valid = false;
-            }
-        }
-    }
-
-    var disabled_input = $('#multi-select-input');
-    if(disabled_input.length) {
-        $.fn.zato.draw_attention_to(disabled_input);
-        is_valid = false;
-    }
-    return is_valid;
 }
 
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
