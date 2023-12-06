a368c330590f11d5b5bc3d825f6ac102e25b80d3
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-12-06 16:25:13 +0100

    GH #755 - Working on pub/sub topics.

--- code/zato-common/src/zato/common/odb/query/common.py
+++ code/zato-common/src/zato/common/odb/query/common.py
@@ -20,7 +20,7 @@ if 0:
     from sqlalchemy import Column
     from sqlalchemy.orm.session import Session as SASession
     from zato.common.odb.model.base import Base as BaseTable
-    from zato.common.typing_ import any_, anylist, anylistnone, intlist, strlist
+    from zato.common.typing_ import any_, anylist, anylistnone, strlist
     Column = Column
 
 # ################################################################################################################################
@@ -32,7 +32,7 @@ TopicTable:'any_' = PubSubTopic.__table__
 # ################################################################################################################################
 # ################################################################################################################################
 
-def get_object_list_by_where(
+def get_object_list_by_where_impl(
     session:'SASession',
     table:'BaseTable',
     where_list:'str | anylistnone'
@@ -40,7 +40,7 @@ def get_object_list_by_where(
     """ Returns ODB objects matching the input query.
     """
     # Local variables
-    where_list = where_list or []
+    where_list = where_list if where_list is not None else []
     where_list = where_list if isinstance(where_list, list) else [where_list]
 
     q = select([
@@ -54,11 +54,13 @@ def get_object_list_by_where(
 
 # ################################################################################################################################
 
-def get_object_list_by_name_pattern(
+def get_object_list_by_where(
     session:'SASession',
     table:'BaseTable',
-    pattern:'str',
-    extra_where_list:'str | anylistnone'
+    value:'str | strlist',
+    extra_where_list:'str | anylistnone',
+    attr_name:'str',
+    attr_func:'str',
 ) -> 'anylist':
     """ Returns ODB objects matching the input query.
     """
@@ -66,12 +68,66 @@ def get_object_list_by_name_pattern(
     extra_where_list = extra_where_list or []
     extra_where_list = extra_where_list if isinstance(extra_where_list, list) else [extra_where_list]
 
-    name_where:'any_' = table.c.name.contains(pattern)
+    attr = getattr(table.c, attr_name)
+    func = getattr(attr, attr_func)
+
+    name_where:'any_' = func(value)
 
     where_list = [name_where]
     where_list.extend(extra_where_list)
 
-    result = get_object_list_by_where(session, table, where_list)
+    result = get_object_list_by_where_impl(session, table, where_list)
+    return result
+
+# ################################################################################################################################
+
+def get_object_list_by_id_list(
+    session:'SASession',
+    table:'BaseTable',
+    object_name_list:'str',
+    extra_where_list:'str | anylistnone'=None
+) -> 'anylist':
+    """ Returns ODB objects whose ID is equal to the ones from input.
+    """
+    # Parameters for the where criteria
+    attr_name = 'id'
+    attr_func = 'in_'
+
+    result = get_object_list_by_where(session, table, object_name_list, extra_where_list, attr_name, attr_func)
+    return result
+
+# ################################################################################################################################
+
+def get_object_list_by_name_list(
+    session:'SASession',
+    table:'BaseTable',
+    object_name_list:'str',
+    extra_where_list:'str | anylistnone'=None
+) -> 'anylist':
+    """ Returns ODB objects whose name is equal to the ones from input.
+    """
+    # Parameters for the where criteria
+    attr_name = 'name'
+    attr_func = 'in_'
+
+    result = get_object_list_by_where(session, table, object_name_list, extra_where_list, attr_name, attr_func)
+    return result
+
+# ################################################################################################################################
+
+def get_object_list_by_name_contains(
+    session:'SASession',
+    table:'BaseTable',
+    pattern:'str | strlist',
+    extra_where_list:'str | anylistnone'=None
+) -> 'anylist':
+    """ Returns ODB objects whose name contains the input pattern.
+    """
+    # Parameters for the where criteria
+    attr_name = 'name'
+    attr_func = 'contains'
+
+    result = get_object_list_by_where(session, table, pattern, extra_where_list, attr_name, attr_func)
     return result
 
 # ################################################################################################################################
@@ -91,7 +147,7 @@ def get_rest_list_by_name_pattern(session:'SASession', pattern:'str', is_channel
     # .. build an addition where part depending on what we need ..
     connection_where:'any_' = RESTTable.c.connection == connection
 
-    result = get_object_list_by_name_pattern(session, table, pattern, connection_where)
+    result = get_object_list_by_name_contains(session, table, pattern, connection_where)
     return result
 
 # ################################################################################################################################
@@ -108,33 +164,6 @@ def get_rest_outgoing_list_by_name_pattern(session:'SASession', pattern:'str') -
     """ Returns REST outgoing connections matching the pattern.
     """
     result = get_rest_list_by_name_pattern(session, pattern, False)
-    return result
-
-# ################################################################################################################################
-
-def get_topic_list_by_id_list(session:'SASession', topic_id_list:'intlist') -> 'anylist':
-    """ Returns topics matching the input list of IDs.
-    """
-    where = TopicTable.c.id.in_(topic_id_list)
-    result = get_object_list_by_where(session, TopicTable, where)
-    return result
-
-# ################################################################################################################################
-
-def get_topic_list_by_name_list(session:'SASession', topic_name_list:'strlist') -> 'anylist':
-    """ Returns topics matching the input list of names.
-    """
-    where = TopicTable.c.name.in_(topic_name_list)
-    result = get_object_list_by_where(session, TopicTable, where)
-    return result
-
-# ################################################################################################################################
-
-def get_topic_list_by_name_pattern(session:'SASession', pattern:'str') -> 'anylist':
-    """ Returns topics matching the input list of names.
-    """
-    where = TopicTable.c.name.contains(pattern)
-    result = get_object_list_by_where(session, TopicTable, where)
     return result
 
 # ################################################################################################################################
