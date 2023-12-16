c4daae971d262b8de4cb81d501602efb257fa39f
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-12-16 02:35:44 +0100

    GH #755 - Working on enmasse for pub/sub.

--- code/zato-cli/test/zato/enmasse_/test_enmasse.py
+++ code/zato-cli/test/zato/enmasse_/test_enmasse.py
@@ -26,6 +26,7 @@ from zato.common.test.enmasse_.base import BaseEnmasseTestCase
 from zato.common.test.enmasse_._template_complex_01 import template_complex_01
 from zato.common.test.enmasse_._template_complex_02 import template_complex_02
 from zato.common.test.enmasse_._template_complex_03 import template_complex_03
+from zato.common.test.enmasse_._template_complex_04 import template_complex_04
 from zato.common.test.enmasse_._template_simple_01 import template_simple_01
 from zato.common.test.enmasse_._template_simple_02 import template_simple_02
 from zato.common.util.open_ import open_w
@@ -116,7 +117,7 @@ class EnmasseTestCase(BaseEnmasseTestCase):
 
 # ################################################################################################################################
 
-    def xtest_enmasse_complex_ok_01(self) -> 'None':
+    def test_enmasse_complex_ok_01(self) -> 'None':
         self._test_enmasse_ok(template_complex_01)
 
 # ################################################################################################################################
@@ -126,17 +127,22 @@ class EnmasseTestCase(BaseEnmasseTestCase):
 
 # ################################################################################################################################
 
-    def xtest_enmasse_complex_ok_03(self) -> 'None':
+    def test_enmasse_complex_ok_03(self) -> 'None':
         self._test_enmasse_ok(template_complex_03)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_simple_ok_01(self) -> 'None':
+    def test_enmasse_complex_ok_04(self) -> 'None':
+        self._test_enmasse_ok(template_complex_04)
+
+# ################################################################################################################################
+
+    def test_enmasse_simple_ok_01(self) -> 'None':
         self._test_enmasse_ok(template_simple_01)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_simple_ok_02(self) -> 'None':
+    def test_enmasse_simple_ok_02(self) -> 'None':
         self._test_enmasse_ok(template_simple_02)
 
 # ################################################################################################################################
