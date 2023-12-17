90e9b8b696faa4aa615ffea5cb10b9d0d8d16280
Dariusz Suchojad <dsuch-github@m.zato.io>
2023-12-17 13:48:42 +0100

    GH #755 - Working on enmasse for pub/sub.

--- code/zato-cli/test/zato/enmasse_/test_enmasse.py
+++ code/zato-cli/test/zato/enmasse_/test_enmasse.py
@@ -119,32 +119,32 @@ class EnmasseTestCase(BaseEnmasseTestCase):
 
 # ################################################################################################################################
 
-    def xtest_enmasse_complex_ok_01(self) -> 'None':
+    def test_enmasse_complex_ok_01(self) -> 'None':
         self._test_enmasse_ok(template_complex_01)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_complex_ok_02(self) -> 'None':
+    def test_enmasse_complex_ok_02(self) -> 'None':
         self._test_enmasse_ok(template_complex_02)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_complex_ok_03(self) -> 'None':
+    def test_enmasse_complex_ok_03(self) -> 'None':
         self._test_enmasse_ok(template_complex_03)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_complex_ok_04(self) -> 'None':
+    def test_enmasse_complex_ok_04(self) -> 'None':
         self._test_enmasse_ok(template_complex_04)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_complex_ok_05(self) -> 'None':
+    def test_enmasse_complex_ok_05(self) -> 'None':
         self._test_enmasse_ok(template_complex_05)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_simple_ok_01(self) -> 'None':
+    def test_enmasse_simple_ok_01(self) -> 'None':
         self._test_enmasse_ok(template_simple_01)
 
 # ################################################################################################################################
@@ -154,12 +154,12 @@ class EnmasseTestCase(BaseEnmasseTestCase):
 
 # ################################################################################################################################
 
-    def xtest_enmasse_simple_ok_03(self) -> 'None':
+    def test_enmasse_simple_ok_03(self) -> 'None':
         self._test_enmasse_ok(template_simple_03)
 
 # ################################################################################################################################
 
-    def xtest_enmasse_service_does_not_exit(self) -> 'None':
+    def test_enmasse_service_does_not_exit(self) -> 'None':
 
         # We are going to wait that many seconds for enmasse to complete
         start = datetime.utcnow()
