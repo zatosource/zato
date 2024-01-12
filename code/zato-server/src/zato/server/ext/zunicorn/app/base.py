a4d0f63ba0b54eed83faf95b454b66562235188a
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-01-12 14:28:45 +0100

    GH #755 - Updating the licence.

--- code/zato-server/src/zato/server/ext/zunicorn/app/base.py
+++ code/zato-server/src/zato/server/ext/zunicorn/app/base.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 """
 Copyright (C) 2019, Zato Source s.r.o. https://zato.io
 
-Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 """
