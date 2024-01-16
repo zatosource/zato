6d5e96a269873425109816a913365a06b5b2ab5b
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-01-16 17:54:04 +0100

    GH #1222 - Working on groups.

--- code/zato-server/src/zato/server/service/internal/groups/ctx.py
+++ code/zato-server/src/zato/server/service/internal/groups/ctx.py
@@ -6,6 +6,9 @@ Copyright (C) 2024, Zato Source s.r.o. https://zato.io
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# stdlib
+from logging import getLogger
+
 # gevent
 from gevent.lock import RLock
 
@@ -19,8 +22,13 @@ from zato.server.service import Service
 # ################################################################################################################################
 
 if 0:
-    from zato.common.typing_ import dict_, intlist, intset, list_, strdict
+    from zato.common.typing_ import dict_, intlist, intnone, intset, list_
     from zato.server.base.parallel import ParallelServer
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logger = getLogger(__name__)
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -49,34 +57,37 @@ class SecurityGroupCtx:
     """ An instance of this class is attached to each channel using security groups.
     """
 
+    # ID of a channel this ctx object is attached to
     channel_id: 'int'
+
+    # IDs of all the security groups attached to this channel
     security_groups: 'intset'
 
+    # Maps usernames to _BasicAuthSecDef objects
     basic_auth_credentials: 'dict_[str, _BasicAuthSecDef]'
+
+    # Maps header values to _APIKeySecDef objects
     apikey_credentials: 'dict_[str, _APIKeySecDef]'
 
     def __init__(self) -> 'None':
 
-        # IDs of all the security groups attached to this channel
         security_groups:'intset' = set()
-
-        # Maps usernames to _BasicAuthSecDef objects
         self.basic_auth_credentials = {}
-
-        # Maps header values to _APIKeySecDef objects
         self.apikey_credentials = {}
 
         self._lock = RLock()
 
 # ################################################################################################################################
 
-    def check_security_basic_auth(self, username:'str', password:'str') -> 'bool':
+    def check_security_basic_auth(self, cid:'str', channel_name:'str', username:'str', password:'str') -> 'intnone':
 
         if sec_info := self.basic_auth_credentials.get(username):
-            password = sec_info.password
-            is_string_equal
+            if is_string_equal(password, sec_info.password):
+                return sec_info.security_id
+            else:
+                logger.info(f'Invalid password; username={username}; channel={channel_name}; cid={cid}')
         else:
-            return False
+            logger.info(f'Username not found; username={username}; channel={channel_name}; cid={cid}')
 
 # ################################################################################################################################
 
@@ -92,8 +103,12 @@ class SecurityGroupCtx:
 
 # ################################################################################################################################
 
-    def check_security_apikey(self, header:'str', value:'str') -> 'bool':
-        pass
+    def check_security_apikey(self, cid:'str', channel_name:'str', header_value:'str') -> 'intnone':
+
+        if sec_info := self.apikey_credentials.get(header_value):
+            return sec_info.security_id
+        else:
+            logger.info(f'Invalid API key; channel={channel_name}; cid={cid}')
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -195,7 +210,14 @@ class BuildCtx(Service):
         builder = SecurityGroupCtxBuilder(self.server)
         ctx = builder.build_ctx(channel_id, security_groups)
 
-        ctx
+        cid = 'cid.1'
+        channel_name = 'channel.1'
+
+        result = ctx.check_security_basic_auth(cid, channel_name, '111', '111')
+        print('QQQ-1', result)
+
+        result = ctx.check_security_apikey(cid, channel_name, '222')
+        print('QQQ-2', result)
 
 # ################################################################################################################################
 # ################################################################################################################################
