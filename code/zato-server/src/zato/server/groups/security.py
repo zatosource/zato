cd3093ed07535a3fd4534d46ac2e4da0bcfc3337
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-01-16 12:29:22 +0100

    GH #1222 - Working on groups.

--- code/zato-server/src/zato/server/groups/security.py
+++ code/zato-server/src/zato/server/groups/security.py
@@ -1,4 +1,4 @@
-'''# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 Copyright (C) 2024, Zato Source s.r.o. https://zato.io
@@ -94,8 +94,14 @@ class BuildCtx(Service):
     name = 'dev.groups.build-ctx'
 
     def handle(self):
-        pass
+
+        channel_id = 85
+        security_groups = [1, 3]
+
+        builder = SecurityGroupCtxBuilder(self.server)
+        ctx = builder.build_ctx(channel_id, security_groups)
+
+        ctx
 
 # ################################################################################################################################
 # ################################################################################################################################
-'''
