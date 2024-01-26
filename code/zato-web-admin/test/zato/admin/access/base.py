d0fca5a68c8c0aeb778f93606555979ea8835d4a
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-01-26 12:38:58 +0100

    GH #1222 - Working on groups.

--- code/zato-web-admin/test/zato/admin/access/base.py
+++ code/zato-web-admin/test/zato/admin/access/base.py
@@ -197,6 +197,8 @@ class BaseTestCase(TestCase):
             if item.url.startswith(Config.web_admin_address):
                 if 'zato/stats/user/' in item.url:
                     continue
+                elif 'zato/groups/' in item.url:
+                    continue
                 if item.response:
                     if item.response.status_code not in Config.status_ok: # type: ignore
                         if item.path not in Config.to_skip_status:
