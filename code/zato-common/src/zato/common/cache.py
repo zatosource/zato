f073a0ce06b8783732dafb672f678aaca17e9df1
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-02-07 18:17:14 +0100

    GH #1196 - Working on Jira connections.

--- code/zato-common/src/zato/common/cache.py
+++ code/zato-common/src/zato/common/cache.py
@@ -1,4 +1,5 @@
-'''# -*- coding: utf-8 -*-
+'''
+# -*- coding: utf-8 -*-
 
 # stdlib
 import os
@@ -13,7 +14,7 @@ from zato.server.service import Service
 
 if 0:
     from bunch import Bunch
-    from zato.common.typing_ import any_, anydict, callable_, dictlist, strlistdict
+    from zato.common.typing_ import any_, anydict, callable_, dictlist, strdict, strlistdict
     from zato.server.base.parallel import ParallelServer
     from zato.server.connection.cache import Cache
     from zato.server.connection.jira_ import JiraClient
@@ -128,6 +129,7 @@ class JiraDataBuilder:
         config_stanza  = os.environ['Zato_Test_Jira_Config_Stanza']
 
         self.config:'Bunch' = self.service.user_config[config_file][config_stanza]
+        self.config
 
 # ################################################################################################################################
 
@@ -156,7 +158,7 @@ class JiraDataBuilder:
 
 # ################################################################################################################################
 
-    def _get_custom_fields_from_file(self) -> 'anydict':
+    def _get_all_field_types_from_file(self) -> 'anydict':
 
         # Our response to produce
         out:'strlistdict' = {}
@@ -172,13 +174,13 @@ class JiraDataBuilder:
 
 # ################################################################################################################################
 
-    def _get_custom_fields_from_jira(self) -> 'anydict':
+    def _get_all_field_types_from_jira(self) -> 'anydict':
 
         # Our response to produce
         out:'strlistdict' = {}
 
         client = self._get_jira_client()
-        fields:'dictlist' = client.get_all_custom_fields()
+        fields:'any_' = client.get_all_fields() # type: ignore
 
         for field in fields:
             key = field['id']
@@ -186,24 +188,29 @@ class JiraDataBuilder:
             keys = out.setdefault(value, [])
             keys.append(key)
 
+        # Sort the output
+        _out:'anydict' = {}
+        for key in sorted(out):
+            _out[key] = out[key]
+
+        return _out
+
+# ################################################################################################################################
+
+    def _on_field_types_missing_in_cache(self) -> 'anydict':
+
+        out = self._get_all_field_types_from_jira()
         return out
 
 # ################################################################################################################################
 
-    def _on_custom_fields_missing_in_cache(self) -> 'anydict':
-
-        out = self._get_custom_fields_from_jira()
-        return out
-
-# ################################################################################################################################
-
-    def get_custom_fields(self) -> 'anydict':
+    def get_all_field_types(self) -> 'anydict':
 
         config = DictHolderConfig()
         config.name = 'jira'
         config.cache_key_suffix = self.conn_name
         config.server = self.service.server
-        config.on_data_missing_func = self._on_custom_fields_missing_in_cache
+        config.on_data_missing_func = self._on_field_types_missing_in_cache
 
         holder = DictHolder(config)
         data = holder.get_all()
@@ -212,11 +219,98 @@ class JiraDataBuilder:
 
 # ################################################################################################################################
 
-    def get_tickets(self, status:'str') -> 'dictlist':
+    def _extract_fields_from_ticket(self, ticket:'strdict') -> 'strdict':
+
+        # Type hints
+        fields:'any_'
+
+        # Our response to produce
+        out:'strdict' = {}
+        field_types:'strdict' = self.get_all_field_types()
+
+        # Local variables
+        fields = ticket['fields']
+
+        # Extract the ticket type ..
+        ticket_type = fields['issuetype']
+        ticket_type = ticket_type['name']
+
+        # .. find the names of the fields that we need to read from the ticket ..
+        fields_key = f'fields_{ticket_type}'
+        fields_to_extract:'any_' = self.config[fields_key]
+        fields_to_extract = sorted(fields_to_extract)
+
+        # .. go through all the fields to be extract ..
+        for field_name in fields_to_extract:
+
+            # .. get the list of IDs that this name maps to
+            field_id_list = field_types[field_name]
+
+            # .. find the first matching value from the potential IDs ..
+            for field_id in field_id_list:
+
+                # .. if there are multiple IDs for that field name, ..
+                # .. the first one with a business value will be used ..
+                value = fields[field_id]
+
+                # .. note that we can always set it ..
+                out[field_name] = value
+
+                # .. but if actually was a business value, we skip other IDs ..
+                # .. the end effect being that we can always return something here ..
+                # .. even if it is only None. Note also that we do not support ..
+                # .. two different IDs with a business value in both ..
+                if value:
+                    continue
+
+        # Now, we can return the response to our caller
+        return out
+
+# ################################################################################################################################
+
+    def _extract_fields_from_tickets(self, tickets:'dictlist') -> 'dictlist':
+
+        # Local variables
+        item:'strdict'
+
+        # Our response to produce
+        out:'dictlist' = []
+
+        for item in tickets:
+            item = self._extract_fields_from_ticket(item)
+            out.append(item)
+
+        # Now, we can return the response to our caller
+        return out
+
+# ################################################################################################################################
+
+    def get_tickets(
+        self,
+        *,
+        board_name:'str'='',
+        ticket_type:'str'='',
+        status:'str'=''
+    ) -> 'dictlist':
+
+        # Base query that will always match ..
+        jql = 'votes >= 0'
+
+        # .. board names (projects) are optional ..
+        if board_name:
+            jql += f' and project = {board_name}'
+
+        # .. ticket types are optional ..
+        if ticket_type:
+            jql += f' and issuetype = {ticket_type}'
+
+        # .. statuses are optional ..
+        if status:
+            jql = f' and status = "{status}"'
 
         client = self._get_jira_client()
-        tickets:'dictlist' = client.jql_get_list_of_tickets(jql=None)
-        tickets
+        tickets:'dictlist' = client.jql_get_list_of_tickets(jql=jql)
+        tickets = self._extract_fields_from_tickets(tickets)
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -227,10 +321,13 @@ class MyService(Service):
 
         builder = JiraDataBuilder(self)
 
-        custom_fields = builder.get_custom_fields()
+        custom_fields = builder.get_all_field_types()
         custom_fields
 
-        tickets = builder.get_tickets('')
+        board_name = 'ABC'
+        ticket_type = '"123"'
+
+        tickets = builder.get_tickets(board_name=board_name, ticket_type=ticket_type)
         tickets
 
 # ################################################################################################################################
