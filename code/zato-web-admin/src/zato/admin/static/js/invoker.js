42ba70a948e38270e8f17593fc28124e79196c8f
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-05-05 20:22:14 +0200

    GH #1200 - Working on the IDE (invoker).

--- code/zato-web-admin/src/zato/admin/static/js/invoker.js
+++ code/zato-web-admin/src/zato/admin/static/js/invoker.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
 $.fn.zato.invoker.on_invoke_submitted = function() {
 
     const options = {
+        "form_id": "#invoke-service-form",
         "on_started_activate_blinking": ["#invoking-please-wait"],
+        "on_started_remove_hidden": ["#result-header-separator"],
         "on_ended_draw_attention": ["#result-header"],
     }
     $.fn.zato.invoker.run_sync_invoker(options);
@@ -18,23 +20,69 @@ $.fn.zato.invoker.on_invoke_submitted = function() {
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
+$.fn.zato.invoker.invoke = function(
+    url,
+    data,
+    callback,
+    data_type,
+    context
+) {
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        headers: {'X-CSRFToken': $.cookie('csrftoken')},
+        complete: callback,
+        dataType: data_type,
+        context: context
+    });
+};
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 $.fn.zato.invoker.run_sync_invoker = function(options) {
 
     console.log("Options 1: " +  Object.entries(options));
-    console.log("Options 2: " + options["on_started_activate_blinking"]);
 
     // Local variables
+    let form_id = options["form_id"];
     let on_started_activate_blinking = options["on_started_activate_blinking"];
+    let on_started_remove_hidden = options["on_started_remove_hidden"];
     let on_ended_draw_attention = options["on_ended_draw_attention"];
 
-    // Start by enabling blinking for all the elements that should blink
+    // Show elements that by default were hidden
+    on_started_remove_hidden.each(function(elem) {
+        $(elem).removeClass("hidden");
+    });
+
+    // Enable blinking for all the elements that should blink
     on_started_activate_blinking.each(function(elem) {
         $.fn.zato.toggle_css_class($(elem), "hidden", "invoker-blinking");
     });
 
+    // Submit the form, if we have one on input
+    if(form_id) {
+        let form = $(form_id);
+        let form_data = new FormData(form[0]);
+
+        $.ajax({
+            type: "POST",
+            url: "address",// where you wanna post
+            data: form_data,
+            processData: false,
+            contentType: false,
+            error: function(jqXHR, text_status, error_message) {
+                console.log(error_message);
+            },
+            success: function(data) {console.log(data)}
+        });
+
+    }
+
     // End by draw attention to specific elements
-    on_ended_draw_attention.each(function(elem) {
+    on_ended_draw_attention2.each(function(elem) {
         let _elem = $(elem);
+        _elem.removeClass("hidden");
         _elem.removeClass("invoker-draw-attention");
         _elem.addClass("invoker-draw-attention", 1);
     });
@@ -69,16 +117,24 @@ $.fn.zato.invoker.run_sync_invoker = function(options) {
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-$.fn.zato.invoker.invoke = function() {
-    $.ajax({
-        type: 'POST',
-        url: url,
-        data: data,
-        headers: {'X-CSRFToken': $.cookie('csrftoken')},
-        complete: callback,
-        dataType: data_type,
-        context: context
-    });
-};
+/*
+$(document).ready(function() {
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    var _callback = function(data, status, xhr){
+        var success = status == 'success';
+        var msg = success ? data : data.responseText;
+        $.fn.zato.user_message(success, msg);
+    }
+
+    var options = {
+        success: _callback,
+        error:  _callback,
+        resetForm: false,
+    };
+
+    $('#invoke_service_form').submit(function() {
+        $(this).ajaxSubmit(options);
+        return false;
+    });
+});
+*/
