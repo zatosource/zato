3aea1cfb15857cb05aaf48b54370e8193195baff
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-05-05 19:48:13 +0200

    GH #1200 - Working on the IDE (invoker).

--- code/zato-web-admin/src/zato/admin/static/js/invoker.js
+++ code/zato-web-admin/src/zato/admin/static/js/invoker.js
@@ -1,28 +1,45 @@
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 $(document).ready(function() {
     $('#invoke-service').click($.fn.zato.invoker.on_invoke_submitted);
 });
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 $.fn.zato.invoker.on_invoke_submitted = function() {
 
     const options = {
         "on_started_activate_blinking": ["#invoking-please-wait"],
-        "on_ended": 123,
+        "on_ended_draw_attention": ["#result-header"],
     }
     $.fn.zato.invoker.run_sync_invoker(options);
 }
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 $.fn.zato.invoker.run_sync_invoker = function(options) {
 
     console.log("Options 1: " +  Object.entries(options));
     console.log("Options 2: " + options["on_started_activate_blinking"]);
 
+    // Local variables
     let on_started_activate_blinking = options["on_started_activate_blinking"];
+    let on_ended_draw_attention = options["on_ended_draw_attention"];
 
     // Start by enabling blinking for all the elements that should blink
     on_started_activate_blinking.each(function(elem) {
         $.fn.zato.toggle_css_class($(elem), "hidden", "invoker-blinking");
     });
+
+    // End by draw attention to specific elements
+    on_ended_draw_attention.each(function(elem) {
+        let _elem = $(elem);
+        _elem.removeClass("invoker-draw-attention");
+        _elem.addClass("invoker-draw-attention", 1);
+    });
+
+    /*
 
     var _callback = function(data, status, xhr){
         var success = status == 'success';
@@ -46,8 +63,11 @@ $.fn.zato.invoker.run_sync_invoker = function(options) {
 
     form.ajaxSubmit(options);
     return false;
+    */
 
 }
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 $.fn.zato.invoker.invoke = function() {
     $.ajax({
@@ -60,3 +80,5 @@ $.fn.zato.invoker.invoke = function() {
         context: context
     });
 };
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
