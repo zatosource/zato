2493ec8292f2e34982d9e56928e704c9a00d2c96
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-05-11 13:24:51 +0200

    GH #1200 - Working on the IDE (invoker).

--- code/zato-web-admin/src/zato/admin/static/js/invoker.js
+++ code/zato-web-admin/src/zato/admin/static/js/invoker.js
@@ -116,6 +116,10 @@ $.fn.zato.invoker.on_submit_form_ended_common = function(
         response_data = $.fn.zato.to_json(response.data[0].zato_env);
     }
 
+    console.log("Data "+ data);
+    console.log("Has "+ has_response);
+    console.log("RSP "+ response_data);
+
     $("#result-header").text(status);
     $("#data-response").text(response_data);
 }
