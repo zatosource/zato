87824be648e7418bf635136869b37f047b259ad3
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-05-12 19:24:59 +0200

    GH #1200 - Working on the IDE (invoker).

--- code/zato-web-admin/src/zato/admin/urls.py
+++ code/zato-web-admin/src/zato/admin/urls.py
@@ -181,6 +181,8 @@ urlpatterns += [
         login_required(service_ide.get_service), name='service-ide-get-service'),
     url(r'^zato/service/ide/get-file/(?P<fs_location>.*)/$',
         login_required(service_ide.get_file), name='service-ide-get-file'),
+    url(r'^zato/service/ide/get-file-list/$',
+        login_required(service_ide.get_file_list), name='service-ide-get-file-list'),
     url(r'^zato/service/ide/(?P<object_type>.*)/(?P<name>.*)/$',
         login_required(service_ide.IDE()), name=service_ide.IDE.url_name),
     ]
