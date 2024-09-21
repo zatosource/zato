318c77fc164b72e96f29aec385702780c60ff019
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-09-21 18:57:51 +0200

    GH #755 - Moving posix-ipc to zato_environment.py.

--- code/util/zato_environment.py
+++ code/util/zato_environment.py
@@ -459,6 +459,10 @@ class EnvironmentManager:
             'zato-ext-bunch==1.2'
         ]
 
+        # This needs to be installed here rather than via requirements.txt
+        if not is_windows:
+            packages.append('posix-ipc==1.0.0')
+
         for package in packages:
 
             # Set up the command ..
