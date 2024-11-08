c236b1a4ef412cc93f9b14e71504cdfeb0901ebe
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-11-08 20:16:12 +0100

    GH #755 - Flake8 cleanup.

--- code/zato-common/src/zato/common/ipc/api.py
+++ code/zato-common/src/zato/common/ipc/api.py
@@ -69,7 +69,7 @@ class IPCAPI:
 
         server_type_suffix = f':{pid}'
 
-        IPCServer.start(
+        IPCServer.start_from_repo_location(
             base_dir=base_dir,
             bind_host=bind_host,
             bind_port=bind_port,
