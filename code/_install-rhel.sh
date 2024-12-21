2b34b59f563fdee40329533267126503867febbf
Dariusz Suchojad <dsuch-github@m.zato.io>
2024-12-21 12:06:14 +0100

    GH #755 - Adding support for Python 3.12.

--- code/_install-rhel.sh
+++ code/_install-rhel.sh
@@ -22,10 +22,6 @@ then
     fi
 fi
 
-if [[ "$INSTALL_PYTHON" == "y" ]]; then
-    PYTHON_DEPENDENCIES="python3-devel"
-fi
-
 os_version=`lsb_release -sir > /dev/null 2>&1 || true`
 
 if [[ $os_version == CentOS\ 8* ]]
@@ -39,7 +35,7 @@ fi
 
 sudo ${INSTALL_CMD} install -y \
     bzip2 bzip2-devel curl cyrus-sasl-devel gcc-c++ git haproxy \
-    libffi libffi-devel openldap-devel openssl \
+    libffi libffi-devel openldap-devel openssl python3-devel \
     openssl-devel patch postgresql-devel suitesparse wget ${PYTHON_DEPENDENCIES}
 
 curl https://bootstrap.pypa.io/get-pip.py | $(type -p $PY_BINARY)
