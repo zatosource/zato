7389e5decdd8f349098dcbd61c9f056382afed97
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-18 00:54:35 +0200

    GH #755 - Removing old code.

--- code/zato-server/src/zato/server/connection/connector/subprocess_/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-"""
-Copyright (C) 2019, Zato Source s.r.o. https://zato.io
-
-Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
-"""
