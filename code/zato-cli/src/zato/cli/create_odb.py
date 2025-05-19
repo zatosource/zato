fcd75a1f8a09835824d7a6be0a431f314c156276
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-19 12:51:42 +0200

    GH #755 - Removing old code.

--- code/zato-cli/src/zato/cli/create_odb.py
+++ code/zato-cli/src/zato/cli/create_odb.py
@@ -13,9 +13,6 @@ from datetime import datetime
 from getpass import getuser
 from socket import gethostname
 
-# SQLAlchemy
-from sqlalchemy.dialects.postgresql.base import PGTypeCompiler
-
 # Zato
 from zato.cli import common_odb_opts, is_arg_given, ZatoCommand
 from zato.common.odb.model import AlembicRevision, Base, ZatoInstallState
@@ -45,58 +42,16 @@ class Create(ZatoCommand):
 
     def execute(self, args, show_output=True):
 
-        # Alembic
-        try:
-            from alembic.migration import MigrationContext
-            from alembic.operations import Operations
-        except ImportError:
-            has_alembic = False
-        else:
-            has_alembic = True
-
         engine = self._get_engine(args)
         session = self._get_session(engine)
 
-        if engine.dialect.has_table(engine.connect(), 'install_state'):
+        Base.metadata.create_all(engine)
 
-            if is_arg_given(args, 'skip-if-exists'):
-                if show_output:
-                    if self.verbose:
-                        self.logger.debug('ODB already exists, skipped its creation')
-                    else:
-                        self.logger.info('OK')
+        if show_output:
+            if self.verbose:
+                self.logger.debug('ODB created successfully')
             else:
-                if show_output:
-                    version = session.query(ZatoInstallState.version).one().version
-                    msg = (
-                        'The ODB (v. {}) already exists, not creating it. ' + \
-                        "Use the 'zato delete odb' command first if you'd like to start afresh and " + \
-                        'recreate all ODB objects.').format(version)
-                    self.logger.error(msg)
-
-                return self.SYS_ERROR.ODB_EXISTS
-
-        else:
-
-            Base.metadata.create_all(engine)
-
-            if has_alembic:
-
-                state = ZatoInstallState(None, VERSION, datetime.now(), gethostname(), getuser())
-                alembic_rev = AlembicRevision(LATEST_ALEMBIC_REVISION)
-
-                session.add(state)
-                session.add(alembic_rev)
-                session.commit()
-
-                alembic_ctx = MigrationContext.configure(engine.connect())
-                alembic_ops = Operations(alembic_ctx)
-
-            if show_output:
-                if self.verbose:
-                    self.logger.debug('ODB created successfully')
-                else:
-                    self.logger.info('OK')
+                self.logger.info('OK')
 
 # ################################################################################################################################
 # ################################################################################################################################
