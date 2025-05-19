443eb27da63b4513f1ce153d06bafe9e4b7fd6d1
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-19 15:16:58 +0200

    GH #755 - Flake8 cleanup.

--- code/zato-cli/src/zato/cli/create_odb.py
+++ code/zato-cli/src/zato/cli/create_odb.py
@@ -6,19 +6,9 @@ Copyright (C) 2019, Zato Source s.r.o. https://zato.io
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-# stdlib
-from datetime import datetime
-from getpass import getuser
-from socket import gethostname
-
 # Zato
-from zato.cli import common_odb_opts, is_arg_given, ZatoCommand
-from zato.common.odb.model import AlembicRevision, Base, ZatoInstallState
-
-LATEST_ALEMBIC_REVISION = '0028_ae3419a9'
-VERSION = 1
+from zato.cli import common_odb_opts, ZatoCommand
+from zato.common.odb.model import Base
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -43,7 +33,6 @@ class Create(ZatoCommand):
     def execute(self, args, show_output=True):
 
         engine = self._get_engine(args)
-        session = self._get_session(engine)
 
         Base.metadata.create_all(engine)
 
