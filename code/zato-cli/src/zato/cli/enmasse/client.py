ff24ba124cee3d971b9b07e15b1eb109dd8b99d0
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-26 16:40:12 +0200

    GH #755 - Working on enmasse.

--- code/zato-cli/src/zato/cli/enmasse/client.py
+++ code/zato-cli/src/zato/cli/enmasse/client.py
@@ -7,7 +7,12 @@ Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # stdlib
+import logging
 import os
+
+# SQLAlchemy
+from sqlalchemy import MetaData, inspect
+from sqlalchemy.exc import SQLAlchemyError
 
 # Zato
 from zato.common.crypto.api import ServerCryptoManager
@@ -18,8 +23,14 @@ from zato.common.util.cli import read_stdin_data
 # ################################################################################################################################
 # ################################################################################################################################
 
+# Set up logging
+logger = logging.getLogger(__name__)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
 if 0:
-    from zato.common.typing_ import any_, strnone
+    from zato.common.typing_ import any_, strnone, boolnone, strdict
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -61,7 +72,102 @@ def get_session_from_server_dir(server_dir:'str', stdin_data:'strnone'=None) -> 
 # ################################################################################################################################
 # ################################################################################################################################
 
+def cleanup(prefix:'str', server_dir:'str', stdin_data:'strnone'=None) -> 'None':
+    """ Deletes all objects from database tables whose name starts with the given prefix.
+    Continues deletion until no errors are raised.
+    """
+    # Get a session from the server directory
+    session = get_session_from_server_dir(server_dir, stdin_data)
+
+    try:
+        # Get the engine from the session
+        engine = session.bind
+
+        # Create an inspector to examine the database
+        inspector = inspect(engine)
+
+        # Reflect metadata to get all tables
+        metadata = MetaData()
+        metadata.reflect(bind=engine)
+
+        # Get all table names
+        table_names = inspector.get_table_names()
+        logger.info(f'Found {len(table_names)} tables in database')
+
+        # Track if we made any changes
+        changes_made = True
+
+        # Continue until no more changes are made
+        while changes_made:
+            changes_made = False
+
+            # Go through each table
+            for table_name in table_names:
+                try:
+                    # Execute a delete query for objects with names starting with the prefix
+                    result = session.execute(
+                        f'DELETE FROM {table_name} WHERE name LIKE "{prefix}%"'
+                    )
+
+                    # Commit the changes
+                    session.commit()
+
+                    # If rows were deleted
+                    if result.rowcount > 0:
+                        changes_made = True
+                        logger.info(f'Deleted {result.rowcount} rows from {table_name} with prefix {prefix}')
+
+                except SQLAlchemyError as e:
+                    # Roll back in case of error
+                    session.rollback()
+                    logger.info(f'Could not delete from {table_name}: {e}')
+
+                    # If the table doesn't have a name column, try to use an alternate strategy
+                    try:
+                        # Get column information
+                        columns = inspector.get_columns(table_name)
+                        column_names = [col['name'] for col in columns]
+
+                        # Look for potential name-like columns
+                        name_columns = [col for col in column_names if 'name' in col.lower()]
+
+                        if name_columns:
+                            name_col = name_columns[0]  # Use the first name-like column
+                            result = session.execute(
+                                f'DELETE FROM {table_name} WHERE {name_col} LIKE "{prefix}%"'
+                            )
+                            session.commit()
+
+                            if result.rowcount > 0:
+                                changes_made = True
+                                logger.info(f'Deleted {result.rowcount} rows from {table_name} using column {name_col}')
+                    except SQLAlchemyError:
+                        session.rollback()
+                        # Just skip this table if we can't determine how to delete by prefix
+                        pass
+
+        logger.info(f'Cleanup completed for prefix {prefix}')
+
+    finally:
+        # Always close the session
+        session.close()
+
+# ################################################################################################################################
+
+def cleanup_enmasse() -> 'None':
+    """ Cleans up all database objects with the 'enmasse' prefix using the default server path.
+    """
+    server_path = os.path.expanduser('~/env/qs-1/server1')
+    cleanup('enmasse', server_path)
+    logger.info('Enmasse cleanup completed')
+
+# ################################################################################################################################
+# ################################################################################################################################
+
 if __name__ == '__main__':
+
+    # Configure logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     # Create a session using the example server path
     server_path = os.path.expanduser('~/env/qs-1/server1')
