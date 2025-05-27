0feb7be548521ee8c53240fe9d59d9209d753509
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-27 13:20:09 +0200

    GH #755 - Working on enmasse.

--- code/zato-cli/src/zato/cli/enmasse/importers/group.py
+++ code/zato-cli/src/zato/cli/enmasse/importers/group.py
@@ -11,12 +11,12 @@ from contextlib import closing
 import logging
 
 # SQLAlchemy
-from sqlalchemy import and_, select
+from sqlalchemy import and_, select, delete
 
 # Zato
 from zato.common.api import Groups
 from zato.common.odb.model import GenericObject
-from zato.server.groups.base import GroupsManager
+from zato.common.odb.query.generic import GroupsWrapper
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -47,13 +47,15 @@ class GroupImporter:
         """ Returns all security groups from the database, keyed by name.
         """
         out = {}
-        cluster_id = self.importer.server.cluster_id
+        cluster_id = self.importer.cluster_id
 
         # Use a single query to get all groups
         with closing(session) as session:
-            # Build a query for all groups
-            query = select([GenericObject]).\
-                where(and_(
+            # Build a query for all groups - explicitly select columns
+            query = select([
+                GenericObject.id,
+                GenericObject.name
+                ]).where(and_(
                     GenericObject.type_ == Groups.Type.Group_Parent,
                     GenericObject.subtype == Groups.Type.API_Clients,
                     GenericObject.cluster_id == cluster_id,
@@ -64,9 +66,10 @@ class GroupImporter:
 
             # Map the results
             for group in groups:
-                name = group.name
+                # SQLAlchemy result row needs to be accessed like a dict
+                name = group['name']
                 out[name] = {
-                    'id': group.id,
+                    'id': group['id'],
                     'name': name,
                     'members': []
                 }
@@ -76,9 +79,12 @@ class GroupImporter:
                 # Get the group IDs
                 group_ids = [group_data['id'] for group_data in out.values()]
 
-                # Build a query for all members of these groups
-                member_query = select([GenericObject]).\
-                    where(and_(
+                # Build a query for all members of these groups - explicitly select columns
+                member_query = select([
+                    GenericObject.id,
+                    GenericObject.name,
+                    GenericObject.parent_object_id
+                    ]).where(and_(
                         GenericObject.type_ == Groups.Type.Group_Member,
                         GenericObject.subtype == Groups.Type.API_Clients,
                         GenericObject.parent_object_id.in_(group_ids),
@@ -90,60 +96,139 @@ class GroupImporter:
 
                 # Process members and add them to their respective groups
                 for member in members:
-                    parent_id = member.parent_object_id
+                    parent_id = member['parent_object_id']
 
                     # Find which group this member belongs to
                     for group_data in out.values():
                         if group_data['id'] == parent_id:
-                            # Extract the security definition ID from the member name
-                            # The format is {sec_def_id}-{group_id}
-                            member_name = member.name
-                            sec_def_id = member_name.split('-')[0] if '-' in member_name else None
+                            # Extract member name from composite name (member_id-group_id)
+                            composite_name = member['name']
+                            parts = composite_name.split('-')
 
-                            if sec_def_id:
-                                # Reverse lookup the security definition name
-                                for sec_name, sec_data in self.importer.sec_defs.items():
-                                    if str(sec_data['id']) == sec_def_id:
+                            # Member names must be in the expected format
+                            if len(parts) >= 2:
+                                member_type, member_id = parts[0], parts[1]
+
+                                # Find the original member name from security definitions
+                                for sec_name, sec_def in self.importer.sec_defs.items():
+                                    if str(sec_def.get('id')) == member_id and sec_def.get('type') == member_type:
                                         group_data['members'].append(sec_name)
                                         break
+
+                            break
 
         return out
 
 # ################################################################################################################################
 
-    def compare_groups(self, yaml_groups:'anylist', db_groups:'anydict') -> 'tuple[anylist, anylist]':
-        """ Compares groups from YAML with groups from database and returns lists of groups to create and update.
+    def delete_group(self, group_name, group_id, session:'SASession') -> 'None':
+        """ Deletes a group and all its members from the database.
         """
-        to_create = []
-        to_update = []
+        cluster_id = self.importer.cluster_id
 
-        for group in yaml_groups:
-            name = group['name']
+        with closing(session) as session:
+            # First, delete all members of the group
+            member_delete = delete(GenericObject).where(
+                and_(
+                    GenericObject.type_ == Groups.Type.Group_Member,
+                    GenericObject.subtype == Groups.Type.API_Clients,
+                    GenericObject.parent_object_id == group_id,
+                    GenericObject.cluster_id == cluster_id
+                )
+            )
+            session.execute(member_delete)
 
-            if name not in db_groups:
-                logger.info('Group %s not found in DB, will create new', name)
-                to_create.append(group)
-            else:
-                logger.info('Group %s exists in DB with id=%s', name, db_groups[name]['id'])
+            # Then delete the group itself
+            group_delete = delete(GenericObject).where(
+                and_(
+                    GenericObject.id == group_id,
+                    GenericObject.type_ == Groups.Type.Group_Parent,
+                    GenericObject.subtype == Groups.Type.API_Clients,
+                    GenericObject.cluster_id == cluster_id
+                )
+            )
+            session.execute(group_delete)
 
-                # Check if members are different
-                needs_update = False
-                yaml_members = set(group.get('members', []))
-                db_members = set(db_groups[name].get('members', []))
+            # Commit all changes
+            session.commit()
 
-                if yaml_members != db_members:
-                    needs_update = True
-                    logger.info('Members differ for group %s: YAML=%s DB=%s',
-                               name, sorted(yaml_members), sorted(db_members))
+            logger.info('Deleted group %s with ID %s', group_name, group_id)
 
-                if needs_update:
-                    # Add the database ID to the group for later update
-                    group['id'] = db_groups[name]['id']
-                    to_update.append(group)
-                else:
-                    logger.info('No update needed for group %s', name)
+# ################################################################################################################################
 
-        return to_create, to_update
+    def create_group(self, group:'anydict', session:'SASession') -> 'any_':
+        """ Creates a new security group and adds members to it.
+        """
+        group_name = group['name']
+        members = group.get('members', [])
+        cluster_id = self.importer.cluster_id
+
+        # Create the group
+        with closing(session) as session:
+            wrapper = GroupsWrapper(session, cluster_id)
+
+            # Prepare the group data
+            group_data = {
+                'name': group_name,
+                'opaque1': '',  # Equivalent to _generic_attr_name
+            }
+
+            # Create the group using create_many
+            group_list = [group_data]
+            insert = wrapper.create_many(
+                group_list,
+                Groups.Type.Group_Parent,
+                Groups.Type.API_Clients
+            )
+            session.execute(insert)
+
+            # Get the new group ID - need to query the database since create_many doesn't return it
+            query = select([GenericObject.id]).where(
+                and_(
+                    GenericObject.name == group_name,
+                    GenericObject.type_ == Groups.Type.Group_Parent,
+                    GenericObject.subtype == Groups.Type.API_Clients,
+                    GenericObject.cluster_id == cluster_id
+                )
+            )
+            group_id_result = session.execute(query).fetchone()
+            if not group_id_result:
+                raise ValueError(f'Could not find newly created group: {group_name}')
+
+            group_id = group_id_result['id']
+
+            # Add members if there are any
+            if members:
+                logger.info('Adding members to new group %s: %s', group_name, sorted(members))
+                member_ids = self._resolve_member_names(members)
+                if member_ids:
+                    # Add members directly using GroupsWrapper
+                    member_list = []
+                    for member_id in member_ids:
+                        # Create a composite name
+                        name = f'{member_id}-{group_id}'
+                        member_list.append({
+                            'name': name,
+                            'opaque1': '',
+                        })
+
+                    # Insert members in bulk
+                    if member_list:
+                        insert = wrapper.create_many(
+                            member_list,
+                            Groups.Type.Group_Member,
+                            Groups.Type.API_Clients,
+                            parent_object_id=group_id
+                        )
+                        session.execute(insert)
+
+            # Commit all changes
+            session.commit()
+        return {
+            'id': group_id,
+            'name': group_name,
+            'members': members
+        }
 
 # ################################################################################################################################
 
@@ -153,120 +238,57 @@ class GroupImporter:
         member_ids = []
 
         for member_name in member_names:
-            if member_name in self.importer.sec_defs:
-                sec_type = self.importer.sec_defs[member_name]['sec_type']
-                sec_id = self.importer.sec_defs[member_name]['id']
-                member_id = f'{sec_type}-{sec_id}'
-                member_ids.append(member_id)
-            else:
-                logger.warning('Security definition %s not found', member_name)
+            # Look up the security definition by name
+            sec_def = self.importer.sec_defs.get(member_name)
+            if not sec_def:
+                logger.warning('Security definition %s not found, skipping', member_name)
+                continue
+
+            # Get the ID and type from the security definition
+            sec_id = sec_def.get('id')
+            sec_type = sec_def.get('type')
+
+            if not sec_id or not sec_type:
+                logger.warning('Security definition %s has no ID or type, skipping', member_name)
+                continue
+
+            # Create a member ID in the format type-id
+            member_id = f'{sec_type}-{sec_id}'
+            member_ids.append(member_id)
 
         return member_ids
-
-# ################################################################################################################################
-
-    def create_group(self, group:'anydict', session:'SASession') -> 'any_':
-        """ Creates a new security group and adds members to it.
-        """
-        group_name = group['name']
-        members = group.get('members', [])
-
-        # Use a server's groups manager to create the group
-        with closing(session) as session:
-            groups_manager = GroupsManager(self.importer.server, session)
-            group_id = groups_manager.create_group(Groups.Type.API_Clients, group_name)
-
-            # If there are members to add, resolve their names to IDs and add them
-            if members:
-                member_ids = self._resolve_member_names(members)
-                if member_ids:
-                    groups_manager.add_members_to_group(group_id, member_ids)
-
-            # Return the created group info
-            return {
-                'id': group_id,
-                'name': group_name,
-                'members': members
-            }
-
-# ################################################################################################################################
-
-    def update_group(self, group:'anydict', session:'SASession') -> 'any_':
-        """ Updates an existing security group with new members.
-        """
-        group_id = group['id']
-        group_name = group['name']
-        members = group.get('members', [])
-
-        # Get current group members from database
-        db_groups = self.get_groups_from_db(session)
-        db_members = set(db_groups[group_name].get('members', []))
-        yaml_members = set(members)
-
-        # Use a server's groups manager to update the group
-        with closing(session) as session:
-            groups_manager = GroupsManager(self.importer.server, session)
-
-            # Members to add: in YAML but not in DB
-            members_to_add = yaml_members - db_members
-            if members_to_add:
-                logger.info('Adding members to group %s: %s', group_name, sorted(members_to_add))
-                member_ids = self._resolve_member_names(members_to_add)
-                if member_ids:
-                    groups_manager.add_members_to_group(group_id, member_ids)
-
-            # Members to remove: in DB but not in YAML
-            members_to_remove = db_members - yaml_members
-            if members_to_remove:
-                logger.info('Removing members from group %s: %s', group_name, sorted(members_to_remove))
-                member_ids = self._resolve_member_names(members_to_remove)
-                if member_ids:
-                    groups_manager.remove_members_from_group(group_id, member_ids)
-
-            # Return the updated group info
-            return {
-                'id': group_id,
-                'name': group_name,
-                'members': members
-            }
 
 # ################################################################################################################################
 
     def sync_groups(self, group_list:'anylist', session:'SASession') -> 'tuple[anylist, anylist]':
         """ Synchronizes security groups from YAML with the database.
         """
-        # Lists to store created and updated groups
-        created_groups = []
-        updated_groups = []
+        # Lists to store processed groups
+        processed_groups = []
 
         try:
             # Get existing groups from database
             db_groups = self.get_groups_from_db(session)
 
-            # Compare YAML groups with database groups
-            to_create, to_update = self.compare_groups(group_list, db_groups)
+            # Process each group from YAML
+            for group in group_list:
+                group_name = group['name']
 
-            # Create new groups
-            for group in to_create:
-                try:
-                    created_group = self.create_group(group, session)
-                    created_groups.append(created_group)
-                    logger.info('Created group %s', group['name'])
-                except Exception as e:
-                    logger.error('Error creating group %s: %s', group['name'], e)
-                    raise
+                # Check if the group already exists
+                if group_name in db_groups:
+                    existing_group = db_groups[group_name]
+                    group_id = existing_group['id']
 
-            # Update existing groups
-            for group in to_update:
-                try:
-                    updated_group = self.update_group(group, session)
-                    updated_groups.append(updated_group)
-                    logger.info('Updated group %s', group['name'])
-                except Exception as e:
-                    logger.error('Error updating group %s: %s', group['name'], e)
-                    raise
+                    # Delete the existing group and all its members
+                    logger.info('Group %s already exists - deleting it first', group_name)
+                    self.delete_group(group_name, group_id, session)
 
-            return created_groups, updated_groups
+                # Create the group with all its members from YAML
+                logger.info('Creating group %s with members from YAML', group_name)
+                created_group = self.create_group(group, session)
+                processed_groups.append(created_group)
+
+            return processed_groups, []
 
         except Exception:
             logger.exception('Error synchronizing groups')
