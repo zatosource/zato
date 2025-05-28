8b92d695638eea27f400aa6ce68f728eb58d51a2
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-28 19:30:17 +0200

    GH #755 - Working on enmasse (SMTP).

--- code/zato-cli/src/zato/cli/enmasse/importers/email_smtp.py
+++ code/zato-cli/src/zato/cli/enmasse/importers/email_smtp.py
@@ -11,8 +11,8 @@ import logging
 from uuid import uuid4
 
 # Zato
-from zato.common.odb.model import OutgoingOdoo, Cluster, to_json
-from zato.common.odb.query import out_odoo_list
+from zato.common.odb.model import SMTP, Cluster, to_json
+from zato.common.odb.query import email_smtp_list
 from zato.common.util.sql import set_instance_opaque_attrs
 
 # ################################################################################################################################
@@ -31,43 +31,43 @@ logger = logging.getLogger(__name__)
 # ################################################################################################################################
 # ################################################################################################################################
 
-class OdooImporter:
+class SMTPImporter:
 
     def __init__(self, importer:'EnmasseYAMLImporter') -> 'None':
 
         self.importer = importer
-        self.odoo_defs = {}
+        self.smtp_defs = {}
 
 # ################################################################################################################################
 
-    def _process_odoo_defs(self, query_result:'any_', out:'dict') -> 'None':
+    def _process_smtp_defs(self, query_result:'any_', out:'dict') -> 'None':
         definitions = to_json(query_result, return_as_dict=True)
-        logger.info('Processing %d odoo connection definitions', len(definitions))
+        logger.info('Processing %d SMTP connection definitions', len(definitions))
 
         for item in definitions:
             name = item['name']
-            logger.info('Processing odoo connection definition: %s (id=%s)', name, item.get('id'))
+            logger.info('Processing SMTP connection definition: %s (id=%s)', name, item.get('id'))
             out[name] = item
 
 # ################################################################################################################################
 
-    def get_odoo_defs_from_db(self, session:'SASession', cluster_id:'int') -> 'anydict':
+    def get_smtp_defs_from_db(self, session:'SASession', cluster_id:'int') -> 'anydict':
         out = {}
 
-        logger.info('Retrieving odoo connection definitions from database for cluster_id=%s', cluster_id)
-        odoo_connections = out_odoo_list(session, cluster_id)
+        logger.info('Retrieving SMTP connection definitions from database for cluster_id=%s', cluster_id)
+        smtp_connections = email_smtp_list(session, cluster_id)
 
-        self._process_odoo_defs(odoo_connections, out)
-        logger.info('Total odoo connection definitions from DB: %d', len(out))
+        self._process_smtp_defs(smtp_connections, out)
+        logger.info('Total SMTP connection definitions from DB: %d', len(out))
 
         for name in out:
-            logger.info('DB odoo connection def: name=%s', name)
+            logger.info('DB SMTP connection def: name=%s', name)
 
         return out
 
 # ################################################################################################################################
 
-    def compare_odoo_defs(self, yaml_defs:'anylist', db_defs:'anydict') -> 'tuple':
+    def compare_smtp_defs(self, yaml_defs:'anylist', db_defs:'anydict') -> 'tuple':
 
         # Find items to create and update
         to_create = []
@@ -92,64 +92,65 @@ class OdooImporter:
 
 # ################################################################################################################################
 
-    def create_odoo_definition(self, odoo_def:'anydict', session:'SASession') -> 'any_':
+    def create_smtp_definition(self, smtp_def:'anydict', session:'SASession') -> 'any_':
 
         # Get the cluster instance from the importer
         cluster = self.importer.get_cluster(session)
 
-        # Create a new odoo connection instance
-        odoo_conn = OutgoingOdoo(cluster)
+        # Create a new SMTP connection instance
+        smtp_conn = SMTP(cluster)
 
-        # Set default values
-        defaults = {
-            'is_active': True,
-            'protocol': 'jsonrpc',
-            'port': 8069,
-            'pool_size': 10,
-        }
+        # Define attribute values
+        name = smtp_def['name']
+        is_active = smtp_def.get('is_active', True)
+        host = smtp_def['host']
+        port = smtp_def['port']
+        timeout = smtp_def.get('timeout', 60)
+        is_debug = smtp_def.get('is_debug', False)
+        mode = smtp_def.get('mode', 'plain')
+        ping_address = smtp_def.get('ping_address', '')
+        
+        # Set attributes
+        smtp_conn.name = name
+        smtp_conn.is_active = is_active
+        smtp_conn.host = host
+        smtp_conn.port = port
+        smtp_conn.timeout = timeout
+        smtp_conn.is_debug = is_debug
+        smtp_conn.mode = mode
+        smtp_conn.ping_address = ping_address
 
-        # Apply defaults unless overridden in YAML
-        for key, default_value in defaults.items():
-            value = odoo_def.get(key, default_value)
-            setattr(odoo_conn, key, value)
-
-        # Required fields
-        required_fields = ['name', 'host', 'user', 'database']
-        for field in required_fields:
-            if field in odoo_def:
-                setattr(odoo_conn, field, odoo_def[field])
-            else:
-                logger.error('Missing required field %s for odoo connection %s', field, odoo_def.get('name', 'unknown'))
-                raise ValueError(f'Missing required field {field} for odoo connection')
+        # Optional username - empty string if not provided
+        smtp_conn.username = smtp_def.get('username', '') or ''
 
         # Set password if provided, otherwise generate one
-        if 'password' in odoo_def:
-            odoo_conn.password = odoo_def['password']
+        if 'password' in smtp_def:
+            smtp_conn.password = smtp_def['password']
         else:
-            odoo_conn.password = uuid4().hex
+            smtp_conn.password = uuid4().hex
 
         # Set any opaque attributes from the configuration
-        set_instance_opaque_attrs(odoo_conn, odoo_def)
+        set_instance_opaque_attrs(smtp_conn, smtp_def)
 
         # Add to session and flush to get ID
-        session.add(odoo_conn)
+        session.add(smtp_conn)
         session.flush()
 
-        return odoo_conn
+        return smtp_conn
 
 # ################################################################################################################################
 
-    def update_odoo_definition(self, odoo_def:'anydict', session:'SASession') -> 'any_':
+    def update_smtp_definition(self, smtp_def:'anydict', session:'SASession') -> 'any_':
 
-        odoo_id = odoo_def['id']
-        def_name = odoo_def['name']
+        smtp_id = smtp_def['id']
+        def_name = smtp_def['name']
 
-        logger.info('Updating odoo connection definition: name=%s id=%s', def_name, odoo_id)
+        logger.info('Updating SMTP connection definition: name=%s id=%s', def_name, smtp_id)
 
-        odoo_conn = session.query(OutgoingOdoo).filter_by(id=odoo_id).one()
+        smtp_conn = session.query(SMTP).filter_by(id=smtp_id).one()
 
         # Update all attributes provided in YAML
-        for key, value in odoo_def.items():
+        for key, value in smtp_def.items():
 
             # Skip special fields that shouldn't be directly updated
             if key not in ['id', 'type']:
@@ -158,50 +159,54 @@ class OdooImporter:
                 if key == 'password' and not value:
                     continue
 
-                # Set the attribute on the odoo connection object
-                setattr(odoo_conn, key, value)
+                # Special handling for username - must be empty string not None
+                if key == 'username' and value is None:
+                    value = ''
+
+                # Set the attribute on the SMTP connection object
+                setattr(smtp_conn, key, value)
 
         # Set any opaque attributes
-        set_instance_opaque_attrs(odoo_conn, odoo_def)
+        set_instance_opaque_attrs(smtp_conn, smtp_def)
 
-        session.add(odoo_conn)
-        return odoo_conn
+        session.add(smtp_conn)
+        return smtp_conn
 
 # ################################################################################################################################
 
-    def sync_odoo_definitions(self, odoo_list:'anylist', session:'SASession') -> 'listtuple':
-        logger.info('Processing %d odoo connection definitions from YAML', len(odoo_list))
+    def sync_smtp_definitions(self, smtp_list:'anylist', session:'SASession') -> 'listtuple':
+        logger.info('Processing %d SMTP connection definitions from YAML', len(smtp_list))
 
-        db_defs = self.get_odoo_defs_from_db(session, self.importer.cluster_id)
-        to_create, to_update = self.compare_odoo_defs(odoo_list, db_defs)
+        db_defs = self.get_smtp_defs_from_db(session, self.importer.cluster_id)
+        to_create, to_update = self.compare_smtp_defs(smtp_list, db_defs)
 
         out_created = []
         out_updated = []
 
         try:
-            logger.info('Creating %d new odoo connection definitions', len(to_create))
+            logger.info('Creating %d new SMTP connection definitions', len(to_create))
             for item in to_create:
 
                 # Keep track of things that already exist
-                existing_odoo = session.query(OutgoingOdoo).filter(OutgoingOdoo.name == item.get('name')).first()
-                if existing_odoo:
-                    logger.info('Odoo connection with name %s already exists, skipping', item.get('name'))
+                existing_smtp = session.query(SMTP).filter(SMTP.name == item.get('name')).first()
+                if existing_smtp:
+                    logger.info('SMTP connection with name %s already exists, skipping', item.get('name'))
                     continue
 
-                instance = self.create_odoo_definition(item, session)
-                logger.info('Created odoo connection definition: name=%s id=%s', instance.name, instance.id)
+                instance = self.create_smtp_definition(item, session)
+                logger.info('Created SMTP connection definition: name=%s id=%s', instance.name, instance.id)
                 out_created.append(instance)
 
                 # Store the mapping for future reference
-                self.odoo_defs[instance.name] = {
+                self.smtp_defs[instance.name] = {
                     'id': instance.id,
                     'name': instance.name,
                 }
 
-            logger.info('Updating %d existing odoo connection definitions', len(to_update))
+            logger.info('Updating %d existing SMTP connection definitions', len(to_update))
             for item in to_update:
-                instance = self.update_odoo_definition(item, session)
-                logger.info('Updated odoo connection definition: name=%s id=%s', instance.name, instance.id)
+                instance = self.update_smtp_definition(item, session)
+                logger.info('Updated SMTP connection definition: name=%s id=%s', instance.name, instance.id)
                 out_updated.append(instance)
 
             logger.info('Committing changes: created=%d updated=%d', len(out_created), len(out_updated))
@@ -209,7 +214,7 @@ class OdooImporter:
             logger.info('Successfully committed all changes')
 
         except Exception as e:
-            logger.error('Error syncing odoo connection definitions: %s', e)
+            logger.error('Error syncing SMTP connection definitions: %s', e)
             logger.exception('Full exception details:')
             session.rollback()
             raise
