14ddcc385e4071d582af782fb954786cec8cff77
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-29 12:40:00 +0200

    GH #755 - Working on enmasse (outgoing REST).

--- code/zato-cli/src/zato/cli/enmasse/config.py
+++ code/zato-cli/src/zato/cli/enmasse/config.py
@@ -20,7 +20,7 @@ class _object_type:
     LDAP = 'ldap'                                 # 1
     Microsoft_365 = 'cloud_microsoft_365'         # 1
     Odoo = 'odoo'                                 # 1
-    Outgoing_REST = 'outgoing_rest'               #
+    Outgoing_REST = 'outgoing_rest'               # 1
     Outgoing_SOAP = 'outgoing_soap'               #
     Search_ElasticSearch = 'elastic_search'       # 1
     SQL  = 'sql'                                  # 1
