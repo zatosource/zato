3975012ee524f95d592e7e1a79e7fbbe5c97b0a7
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-29 08:21:34 +0200

    GH #755 - Working on enmasse (Confluence).

--- code/zato-cli/src/zato/cli/enmasse/importers/confluence.py
+++ code/zato-cli/src/zato/cli/enmasse/importers/confluence.py
@@ -31,6 +31,8 @@ logger = logging.getLogger(__name__)
 # ################################################################################################################################
 # ################################################################################################################################
 
+connection_type = GENERIC.CONNECTION.TYPE.CLOUD_CONFLUENCE
+
 connection_defaults = {
     'is_active': True,
     'type_': GENERIC.CONNECTION.TYPE.CLOUD_CONFLUENCE,
@@ -64,34 +66,34 @@ class ConfluenceImporter:
 
 # ################################################################################################################################
 
-    def _process_confluence_defs(self, query_result:'any_', out:'dict') -> 'None':
+    def _process_defs(self, query_result:'any_', out:'dict') -> 'None':
         definitions = to_json(query_result, return_as_dict=True)
-        logger.info('Processing %d confluence connection definitions', len(definitions))
+        logger.info('Processing %d connection definitions (%s)', len(definitions), connection_type)
 
         for item in definitions:
             name = item['name']
-            logger.info('Processing confluence connection definition: %s (id=%s)', name, item.get('id'))
+            logger.info('Processing connection definition: %s (id=%s) (%s)', name, item.get('id'), connection_type)
             out[name] = item
 
 # ################################################################################################################################
 
-    def get_confluence_defs_from_db(self, session:'SASession', cluster_id:'int') -> 'anydict':
+    def get_defs_from_db(self, session:'SASession', cluster_id:'int') -> 'anydict':
         out = {}
 
-        logger.info('Retrieving confluence connection definitions from database for cluster_id=%s', cluster_id)
-        confluence_connections = connection_list(session, cluster_id, GENERIC.CONNECTION.TYPE.CLOUD_CONFLUENCE, False)
+        logger.info('Retrieving connection definitions from database (%s)', connection_type)
+        connections = connection_list(session, cluster_id, connection_type, False)
 
-        self._process_confluence_defs(confluence_connections, out)
-        logger.info('Total confluence connection definitions from DB: %d', len(out))
+        self._process_defs(connections, out)
+        logger.info('Total connection definitions from DB: %d (%s)', len(out), connection_type)
 
         for name in out:
-            logger.info('DB confluence connection def: name=%s', name)
+            logger.info('DB connection def: name=%s (%s)', name, connection_type)
 
         return out
 
 # ################################################################################################################################
 
-    def compare_confluence_defs(self, yaml_defs:'anylist', db_defs:'anydict') -> 'tuple':
+    def compare_defs(self, yaml_defs:'anylist', db_defs:'anydict') -> 'tuple':
 
         # Find items to create and update
         to_create = []
@@ -116,7 +118,7 @@ class ConfluenceImporter:
 
 # ################################################################################################################################
 
-    def create_confluence_definition(self, connection_config:'anydict', session:'SASession') -> 'any_':
+    def create_definition(self, connection_def:'anydict', session:'SASession') -> 'any_':
 
         # Get the cluster instance from the importer
         cluster = self.importer.get_cluster(session)
@@ -127,22 +129,22 @@ class ConfluenceImporter:
 
         # Apply defaults unless overridden in YAML
         for key, default_value in connection_defaults.items():
-            value = connection_config.get(key, default_value)
+            value = connection_def.get(key, default_value)
             setattr(connection, key, value)
 
         # Set required fields - they will always exist
         for attr in connection_required_attrs:
-            setattr(connection, attr, connection_config[attr])
+            setattr(connection, attr, connection_def[attr])
 
         for key in connection_secret_keys:
-            if key in connection_config and connection_config[key]:
-                connection.secret = connection_config[key]
+            if key in connection_def and connection_def[key]:
+                connection.secret = connection_def[key]
                 break
 
         # Build extra_fields using the defaults
         extra_fields = {}
         for field, default in connection_extra_field_defaults.items():
-            value = connection_config.get(field, default)
+            value = connection_def.get(field, default)
             if value is not None:
                 extra_fields[field] = value
 
@@ -150,7 +152,7 @@ class ConfluenceImporter:
         extra_fields = {key: value for key, value in extra_fields.items() if value is not None}
 
         # Set any opaque attributes from the configuration
-        set_instance_opaque_attrs(connection, connection_config, extra_fields)
+        set_instance_opaque_attrs(connection, connection_def, extra_fields)
 
         # Add to session and flush to get ID
         session.add(connection)
@@ -160,75 +162,59 @@ class ConfluenceImporter:
 
 # ################################################################################################################################
 
-    def update_confluence_definition(self, confluence_def:'anydict', session:'SASession') -> 'any_':
+    def update_definition(self, connection_def:'anydict', session:'SASession') -> 'any_':
 
-        confluence_id = confluence_def['id']
-        def_name = confluence_def['name']
+        connection_id = connection_def['id']
+        def_name = connection_def['name']
 
-        logger.info('Updating confluence connection definition: name=%s id=%s', def_name, confluence_id)
+        logger.info('Updating connection definition (%s): name=%s id=%s', connection_type, def_name, connection_id)
 
-        confluence_conn = session.query(GenericConn).filter_by(id=confluence_id).one()
+        connection = session.query(GenericConn).filter_by(id=connection_id).one()
 
-        # Set required fields using the same approach as create
-        required_attrs = ['name', 'address', 'username']
-        for attr in required_attrs:
-            setattr(confluence_conn, attr, confluence_def[attr])
+        for attr in connection_required_attrs:
+            setattr(connection, attr, connection_def[attr])
 
-        # Set secret - using a list of possible keys with priority order
-        secret_keys = ['password', 'secret', 'api_token']
-        for key in secret_keys:
-            if key in confluence_def and confluence_def[key]:
-                confluence_conn.secret = confluence_def[key]
+        for key in connection_secret_keys:
+            if key in connection_def and connection_def[key]:
+                connection.secret = connection_def[key]
                 break
-
-        # Additional Confluence-specific fields for opaque attributes
-        # Define fields with their default values
-        extra_field_defaults = {
-            'site_url': None,
-            'auth_token': None,
-            'is_cloud': True,
-            'api_version': 'v1',
-        }
 
         # Build extra_fields using the defaults
         extra_fields = {}
-        for field, default in extra_field_defaults.items():
-            value = confluence_def.get(field, default)
+        for field, default in connection_extra_field_defaults.items():
+            value = connection_def.get(field, default)
             if value is not None:
                 extra_fields[field] = value
 
-        # Filter out None values
-        extra_fields = {k: v for k, v in extra_fields.items() if v is not None}
+        set_instance_opaque_attrs(connection, connection_def, extra_fields)
 
-        set_instance_opaque_attrs(confluence_conn, confluence_def, extra_fields)
-
-        session.add(confluence_conn)
-        return confluence_conn
+        session.add(connection)
+        return connection
 
 # ################################################################################################################################
 
-    def sync_confluence_definitions(self, confluence_list:'anylist', session:'SASession') -> 'listtuple':
-        logger.info('Processing %d confluence connection definitions from YAML', len(confluence_list))
+    def sync_definitions(self, conn_list:'anylist', session:'SASession') -> 'listtuple':
+        logger.info('Processing %d connection definitions from YAML (%s)', len(conn_list), connection_type)
 
-        db_defs = self.get_confluence_defs_from_db(session, self.importer.cluster_id)
-        to_create, to_update = self.compare_confluence_defs(confluence_list, db_defs)
+        db_defs = self.get_defs_from_db(session, self.importer.cluster_id)
+        to_create, to_update = self.compare_defs(conn_list, db_defs)
 
         out_created = []
         out_updated = []
 
         try:
-            logger.info('Creating %d new confluence connection definitions', len(to_create))
+            logger.info('Creating %d new connection definitions (%s)', len(to_create), connection_type)
             for item in to_create:
 
                 # Keep track of things that already exist
-                existing_confluence = session.query(GenericConn).filter(GenericConn.name == item.get('name'), GenericConn.type_ == GENERIC.CONNECTION.TYPE.CLOUD_CONFLUENCE).first() # type: ignore
+                existing_conn = session.query(GenericConn).filter(GenericConn.name==item.get('name'), GenericConn.type_==connection_type).first() # type: ignore
 
-                if existing_confluence:
-                    logger.info('Confluence connection with name %s already exists, skipping', item.get('name'))
+                if existing_conn:
+                    logger.info('Connection with name %s already exists, skipping (%s)', item.get('name'), connection_type)
                     continue
 
-                instance = self.create_confluence_definition(item, session)
-                logger.info('Created confluence connection definition: name=%s id=%s', instance.name, instance.id)
+                instance = self.create_definition(item, session)
+                logger.info('Created connection definition: name=%s id=%s (%s)', instance.name, instance.id, connection_type)
                 out_created.append(instance)
 
                 # Store the mapping for future reference
@@ -237,10 +223,10 @@ class ConfluenceImporter:
                     'name': instance.name,
                 }
 
-            logger.info('Updating %d existing confluence connection definitions', len(to_update))
+            logger.info('Updating %d existing connection definitions (%s)', len(to_update), connection_type)
             for item in to_update:
-                instance = self.update_confluence_definition(item, session)
-                logger.info('Updated confluence connection definition: name=%s id=%s', instance.name, instance.id)
+                instance = self.update_definition(item, session)
+                logger.info('Updated connection definition: name=%s id=%s (%s)', instance.name, instance.id, connection_type)
                 out_updated.append(instance)
 
             logger.info('Committing changes: created=%d updated=%d', len(out_created), len(out_updated))
@@ -248,7 +234,7 @@ class ConfluenceImporter:
             logger.info('Successfully committed all changes')
 
         except Exception as e:
-            logger.error('Error syncing confluence connection definitions: %s', e)
+            logger.error('Error syncing connection definitions: %s (%s)', e, connection_type)
             logger.exception('Full exception details:')
             session.rollback()
             raise
