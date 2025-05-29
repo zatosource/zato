3a344a82c66b8f5b2e47c532fd0219bc21817d38
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-29 15:51:56 +0200

    GH #755 - Working on enmasse (exporters).

---
+++ code/zato-cli/src/zato/cli/enmasse/exporters/ldap.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+import logging
+
+# Zato
+from zato.common.odb.model import LDAP
+from zato.common.odb.query import ldap_connection_list
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if 0:
+    from sqlalchemy.orm.session import Session as SASession
+    from zato.common.typing_ import any_, anydict, anylist
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logger = logging.getLogger(__name__)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class LDAPExporter:
+
+    def __init__(self, exporter:'EnmasseYAMLExporter') -> 'None':
+        self.exporter = exporter
+
+# ################################################################################################################################
+
+    def export_ldap_definitions(self, session:'SASession') -> 'list':
+        """ Export LDAP connection definitions from the database.
+        """
+        logger.info('Exporting LDAP connections from cluster_id=%s', self.exporter.cluster_id)
+        
+        # Get LDAP connections from the database
+        ldap_connections = ldap_connection_list(session, self.exporter.cluster_id)
+        
+        ldap_defs = []
+        
+        for conn in ldap_connections:
+            logger.info('Processing LDAP connection: %s', conn.name)
+            
+            # Create a dictionary representation of the LDAP connection
+            ldap_def = {
+                'name': conn.name,
+                'username': conn.username,
+                'auth_type': conn.auth_type,
+                'server_list': conn.server_list,
+                'is_active': conn.is_active
+            }
+            
+            # Store in exporter's LDAP definitions
+            self.exporter.ldap_defs[conn.name] = {
+                'id': conn.id,
+                'name': conn.name
+            }
+            
+            # Add to results
+            ldap_defs.append(ldap_def)
+            
+        logger.info('Exported %d LDAP connections', len(ldap_defs))
+        return ldap_defs
+
+# ################################################################################################################################
+# ################################################################################################################################
---
+++ code/zato-cli/src/zato/cli/enmasse/exporters/sql.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+import logging
+
+# Zato
+from zato.common.odb.model import SQLConnectionPool
+from zato.common.odb.query import sql_connection_pool_list
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if 0:
+    from sqlalchemy.orm.session import Session as SASession
+    from zato.common.typing_ import any_, anydict, anylist
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logger = logging.getLogger(__name__)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class SQLExporter:
+
+    def __init__(self, exporter:'EnmasseYAMLExporter') -> 'None':
+        self.exporter = exporter
+
+# ################################################################################################################################
+
+    def export_sql_definitions(self, session:'SASession') -> 'list':
+        """ Export SQL connection pool definitions from the database.
+        """
+        logger.info('Exporting SQL connection pools from cluster_id=%s', self.exporter.cluster_id)
+        
+        # Get SQL connection pools from the database
+        sql_pools = sql_connection_pool_list(session, self.exporter.cluster_id)
+        
+        sql_defs = []
+        
+        for pool in sql_pools:
+            logger.info('Processing SQL connection pool: %s', pool.name)
+            
+            # Create a dictionary representation of the SQL connection pool
+            sql_def = {
+                'name': pool.name,
+                'type': pool.engine,  # Map to 'type' in YAML
+                'host': pool.host,
+                'port': pool.port,
+                'db_name': pool.db_name,
+                'username': pool.username,
+                'is_active': pool.is_active
+            }
+            
+            # Add pool_size if not default
+            if pool.pool_size != 5:  # Assuming 5 is the default
+                sql_def['pool_size'] = pool.pool_size
+                
+            # Add extra connection parameters if present
+            if pool.extra:
+                sql_def['extra'] = pool.extra
+            
+            # Store in exporter's SQL definitions
+            self.exporter.sql_defs[pool.name] = {
+                'id': pool.id,
+                'name': pool.name
+            }
+            
+            # Add to results
+            sql_defs.append(sql_def)
+            
+        logger.info('Exported %d SQL connection pools', len(sql_defs))
+        return sql_defs
+
+# ################################################################################################################################
+# ################################################################################################################################
