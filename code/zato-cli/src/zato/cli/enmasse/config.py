0f38860130719c4a3a7c476090a4e68b9263a5ac
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-30 13:51:11 +0200

    GH #755 - Working on enmasse (SMTP exporter).

--- code/zato-cli/src/zato/cli/enmasse/config.py
+++ code/zato-cli/src/zato/cli/enmasse/config.py
@@ -13,8 +13,8 @@ class _object_type:
     Cache = 'cache'                               # 1 2
     Confluence = 'confluence'                     # 1
     Channel_REST = 'channel_rest'                 # 1
-    Email_IMAP = 'email_imap'                     # 1
-    Email_SMTP = 'email_smtp'                     # 1
+    Email_IMAP = 'email_imap'                     # 1 2
+    Email_SMTP = 'email_smtp'                     # 1 2
     Groups = 'groups'                             # 1
     Jira = 'jira'                                 # 1
     LDAP = 'ldap'                                 # 1
