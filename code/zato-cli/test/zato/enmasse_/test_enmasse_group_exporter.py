2a535ab9242bfb646407ec85060f7dabd5dad05e
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-30 21:32:05 +0200

    GH #755 - Working on enmasse (groups exporter tests).

--- code/zato-cli/test/zato/enmasse_/test_enmasse_group_exporter.py
+++ code/zato-cli/test/zato/enmasse_/test_enmasse_group_exporter.py
@@ -15,8 +15,7 @@ from unittest import TestCase, main
 from zato.cli.enmasse.client import cleanup_enmasse, get_session_from_server_dir
 from zato.cli.enmasse.exporter import EnmasseYAMLExporter
 from zato.cli.enmasse.importer import EnmasseYAMLImporter
-from zato.cli.enmasse.importers.apikey import APIKeyImporter
-from zato.cli.enmasse.importers.basic_auth import BasicAuthImporter
+from zato.cli.enmasse.importers.security import SecurityImporter
 from zato.cli.enmasse.importers.group import GroupImporter
 from zato.common.test.enmasse_._template_complex_01 import template_complex_01
 from zato.common.typing_ import cast_
@@ -25,7 +24,7 @@ from zato.common.typing_ import cast_
 # ################################################################################################################################
 
 if 0:
-    from zato.common.typing_ import any_, stranydict, SASession
+    from zato.common.typing_ import stranydict, SASession
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -43,8 +42,7 @@ class TestEnmasseGroupExporter(TestCase):
 
         # Importers are needed to set up the database state for export tests
         self.importer = EnmasseYAMLImporter()
-        self.apikey_importer = APIKeyImporter(self.importer)
-        self.basic_auth_importer = BasicAuthImporter(self.importer)
+        self.security_importer = SecurityImporter(self.importer) # Handles all security definition types
         self.group_importer = GroupImporter(self.importer)
 
         self.yaml_config = cast_('stranydict', None)
@@ -65,22 +63,22 @@ class TestEnmasseGroupExporter(TestCase):
         # Import security definitions first, as groups depend on them
         security_defs_from_yaml = self.yaml_config.get('security', [])
         if security_defs_from_yaml:
-
-            apikey_defs = [item for item in security_defs_from_yaml if item.get('type') == 'apikey']
-            basic_auth_defs = [item for item in security_defs_from_yaml if item.get('type') == 'basic_auth']
-
-            if apikey_defs:
-                _, _ = self.apikey_importer.sync_apikey_definitions(apikey_defs, self.session)
-
-            if basic_auth_defs:
-                _, _ = self.basic_auth_importer.sync_basic_auth_definitions(basic_auth_defs, self.session)
+            # SecurityImporter handles all types of security definitions
+            _, _ = self.security_importer.sync_security_definitions(security_defs_from_yaml, self.session)
 
         # Import group definitions
         groups_from_yaml = self.yaml_config.get('groups', [])
         if groups_from_yaml:
-            _, _ = self.group_importer.sync_group_definitions(groups_from_yaml, self.session)
+            _, _ = self.group_importer.sync_groups(groups_from_yaml, self.session)
 
         self.session.commit()
+
+# ################################################################################################################################
+
+    def test_group_export(self) -> 'None':
+        """ Tests the export of group definitions.
+        """
+        self._setup_test_environment()
 
 # ################################################################################################################################
 
