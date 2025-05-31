93e7224c01a19d7907cda472e8ea4c34627fed09
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-05-31 10:14:29 +0200

    GH #755 - Working on enmasse (REST channels exporter tests).

---
+++ code/zato-cli/test/zato/enmasse_/test_enmasse_channel_exporter.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+import logging
+import os
+import tempfile
+from unittest import TestCase, main
+
+# Zato
+from zato.cli.enmasse.client import cleanup_enmasse, get_session_from_server_dir
+from zato.cli.enmasse.exporter import EnmasseYAMLExporter
+from zato.cli.enmasse.importer import EnmasseYAMLImporter
+from zato.cli.enmasse.importers.security import SecurityImporter
+from zato.cli.enmasse.importers.group import GroupImporter
+from zato.common.test.enmasse_._template_complex_01 import template_complex_01
+from zato.common.typing_ import cast_
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if 0:
+    from zato.common.typing_ import stranydict, SASession
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class TestEnmasseGroupExporter(TestCase):
+    """ Tests exporting Security Group definitions to YAML-compatible dicts using enmasse.
+    """
+
+    def setUp(self) -> 'None':
+        self.server_path = os.path.expanduser('~/env/qs-1/server1')
+
+        self.temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.yaml')
+        _ = self.temp_file.write(template_complex_01.encode('utf-8'))
+        self.temp_file.close()
+
+        # Importers are needed to set up the database state for export tests
+        self.importer = EnmasseYAMLImporter()
+        self.security_importer = SecurityImporter(self.importer) # Handles all security definition types
+        self.group_importer = GroupImporter(self.importer)
+
+        self.yaml_config = cast_('stranydict', None)
+        self.session = cast_('SASession', None)
+
+# ################################################################################################################################
+
+    def _setup_test_environment(self) -> 'None':
+
+        if not self.session:
+            self.session = get_session_from_server_dir(self.server_path)
+
+        if not self.yaml_config:
+            self.yaml_config = self.importer.from_path(self.temp_file.name)
+
+        self.session.commit()
+
+# ################################################################################################################################
+
+    def test_rest_channel_export(self) -> 'None':
+        """ Tests the export of REST channel definitions.
+        """
+        self._setup_test_environment()
+
+# ################################################################################################################################
+
+    def tearDown(self) -> 'None':
+        if self.session:
+            self.session.close()
+        os.unlink(self.temp_file.name)
+        cleanup_enmasse()
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    _ = main()
+
+
+# ################################################################################################################################
+# ################################################################################################################################
