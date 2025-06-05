01b96a4c74a124f5f5e1b366b6ed6a454ccf52c9
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-06-05 14:47:55 +0200

    GH #755 - Working on enmasse.

--- code/zato-cli/src/zato/cli/enmasse/importers/microsoft_365.py
+++ code/zato-cli/src/zato/cli/enmasse/importers/microsoft_365.py
@@ -6,6 +6,9 @@ Copyright (C) 2025, Zato Source s.r.o. https://zato.io
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# stdlib
+import logging
+
 # Zato
 from zato.common.api import GENERIC
 from zato.cli.enmasse.importers.generic import GenericConnectionImporter
@@ -13,8 +16,7 @@ from zato.cli.enmasse.importers.generic import GenericConnectionImporter
 # ################################################################################################################################
 # ################################################################################################################################
 
-# ################################################################################################################################
-# ################################################################################################################################
+logger = logging.getLogger(__name__)
 
 class Microsoft365Importer(GenericConnectionImporter):
 
@@ -42,6 +44,30 @@ class Microsoft365Importer(GenericConnectionImporter):
 
     connection_secret_keys = ['secret_value']
     connection_required_attrs = ['name', 'client_id', 'tenant_id']
+
+# ################################################################################################################################
+
+    def _process_scopes(self, connection_def):
+
+        scopes = connection_def.get('scopes')
+
+        # If scopes is a list, convert it to a newline-delimited string
+        if isinstance(scopes, list):
+            connection_def['scopes'] = '\n'.join(scopes)
+
+        return connection_def
+
+# ################################################################################################################################
+
+    def create_definition(self, connection_def, session):
+        connection_def = self._process_scopes(connection_def)
+        return super().create_definition(connection_def, session)
+
+# ################################################################################################################################
+
+    def update_definition(self, connection_def, session):
+        connection_def = self._process_scopes(connection_def)
+        return super().update_definition(connection_def, session)
 
 # ################################################################################################################################
 # ################################################################################################################################
