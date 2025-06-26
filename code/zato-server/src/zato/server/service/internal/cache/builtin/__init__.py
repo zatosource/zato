94a1f50beaa327cb6e8a38f3d655530f117b4a64
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-06-26 09:53:09 +0200

    GH #755 - Working on AMQP.

--- code/zato-server/src/zato/server/service/internal/cache/builtin/__init__.py
+++ code/zato-server/src/zato/server/service/internal/cache/builtin/__init__.py
@@ -33,7 +33,7 @@ broker_message_prefix = 'BUILTIN_'
 list_func = cache_builtin_list
 skip_create_integrity_error = True
 skip_if_exists = True
-output_optional_extra = ['current_size', 'id']
+output_optional_extra = ['current_size']
 
 # ################################################################################################################################
 
