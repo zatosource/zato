be439c39f04b81743c04dfc633ccf4a235cb5378
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-06-28 11:08:10 +0200

    GH #755 - Working on AMQP.

---
+++ code/zato-common/src/zato/common/pubsub/common.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+class BrokerConfig:
+    protocol:'str'
+    address:'str'
+    vhost:'str'
+    username:'str'
+    password:'str'
