c30972f4bbfa26c7539dc6197f07f05fcd831fb2
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-06-28 11:42:57 +0200

    GH #755 - Working on AMQP.

--- code/zato-common/src/zato/common/pubsub/util.py
+++ code/zato-common/src/zato/common/pubsub/util.py
@@ -12,11 +12,20 @@ import os
 # Zato
 from zato.common.pubsub.common import BrokerConfig
 
+# ################################################################################################################################
+# ################################################################################################################################
+
 def get_broker_config() -> 'BrokerConfig':
+
     config = BrokerConfig()
+
     config.protocol = os.environ['Zato_Broker_Protocol']
     config.address = os.environ['Zato_Broker_Address']
     config.vhost = os.environ['Zato_Broker_Virtual_Host']
     config.username = os.environ['Zato_Broker_Username']
     config.password = os.environ['Zato_Broker_Password']
+
     return config
+
+# ################################################################################################################################
+# ################################################################################################################################
