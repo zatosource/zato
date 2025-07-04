2f4aeb8e2f535daff75d104e08519af0acfb51a5
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-04 18:29:28 +0200

    GH #755 - Working on AMQP.

--- code/zato-broker/src/zato/broker/__init__.py
+++ code/zato-broker/src/zato/broker/__init__.py
@@ -14,7 +14,17 @@ from zato.common.api import ZATO_NONE
 from zato.common.broker_message import SERVICE
 from zato.common.util.api import new_cid
 from zato.common.util.config import resolve_env_variables
-from zato.broker.message_handler import BrokerMessageHandler
+from zato.broker.message_handler import handle_broker_msg
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if 0:
+    from zato.broker.client import BrokerClient
+    from zato.server.base.worker import WorkerStore
+
+    BrokerClient = BrokerClient
+    WorkerStore = WorkerStore
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -33,6 +43,9 @@ service_invoke = SERVICE.INVOKE.value
 class BrokerMessageReceiver:
     """ A class that knows how to handle messages received from other worker processes.
     """
+    broker_client: 'BrokerClient'
+    worker_store: 'WorkerStore'
+
     def __init__(self):
         self.broker_client_id = '{}-{}'.format(ZATO_NONE, new_cid())
         self.broker_callbacks = {}
@@ -46,13 +59,16 @@ class BrokerMessageReceiver:
         (because in this case '1000' is the code for creating a new scheduler's job, see zato.common.broker_message for the list
         of all actions).
         """
-        # Use the shared BrokerMessageHandler with our own context and functions
-        result = BrokerMessageHandler.handle_message(
-            msg=msg,
-            context=self.worker_store,
-            preprocess_func=self.preprocess_msg,
-            filter_func=self.filter
-        )
+        # Apply preprocessing first
+        msg = self.preprocess_msg(msg)
+
+        # Apply filtering
+        if not self.filter(msg):
+            logger.info('Rejecting broker message `%r`', msg)
+            return
+
+        # Use the shared handler
+        result = handle_broker_msg(msg, self.worker_store)
 
         # If message was handled and it's a service invocation that needs a reply
         if result.was_handled and result.action_code == service_invoke:
