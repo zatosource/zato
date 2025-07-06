8e398ab1eca38a287f67a8d4c408d54d95e7adfa
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-06 13:29:41 +0200

    GH #755 - Working on AMQP.

--- code/zato-web-admin/src/zato/admin/static/js/pubsub/topic.js
+++ code/zato-web-admin/src/zato/admin/static/js/pubsub/topic.js
@@ -2,10 +2,12 @@
 
 $.fn.zato.data_table.PubSubTopic = new Class({
     toString: function() {
-        var s = '<PubSubTopic id:{0} name:{1} description:{2}>';
+        var s = '<PubSubTopic id:{0} name:{1} description:{2} publisher_count:{3} subscriber_count:{4}>';
         return String.format(s, this.id ? this.id : '(none)',
                                 this.name ? this.name : '(none)',
-                                this.description ? this.description : '(none)');
+                                this.description ? this.description : '(none)',
+                                this.publisher_count ? this.publisher_count : '0',
+                                this.subscriber_count ? this.subscriber_count : '0');
     }
 });
 
@@ -41,11 +43,15 @@ $.fn.zato.pubsub.topic.data_table.new_row = function(item, data, include_tr) {
     row += "<td class='impexp'><input type='checkbox' /></td>";
     row += String.format('<td>{0}</td>', item.name);
     row += String.format('<td>{0}</td>', item.description || '<span class="form_hint">---</span>');
+    row += String.format('<td style="text-align:center">{0}</td>', item.publisher_count || '0');
+    row += String.format('<td style="text-align:center">{0}</td>', item.subscriber_count || '0');
     row += String.format('<td>{0}</td>', String.format("<a href=\"javascript:$.fn.zato.pubsub.topic.edit('{0}')\">Edit</a>", item.id));
     row += String.format('<td>{0}</td>', String.format("<a href='javascript:$.fn.zato.pubsub.topic.delete_({0});'>Delete</a>", item.id));
     row += String.format("<td class='ignore item_id_{0}'>{0}</td>", item.id);
     row += String.format("<td class='ignore'>{0}</td>", is_active);
     row += String.format("<td class='ignore'>{0}</td>", item.description || "");
+    row += String.format("<td class='ignore'>{0}</td>", item.publisher_count || "0");
+    row += String.format("<td class='ignore'>{0}</td>", item.subscriber_count || "0");
 
     if(include_tr) {
         row += '</tr>';
