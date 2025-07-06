6ec51eff6857a7bef7a5688df6f5be52dd3adb8a
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-06 11:43:16 +0200

    GH #755 - Working on AMQP.

---
+++ code/zato-web-admin/src/zato/admin/web/forms/pubsub/topic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# Django
+from django import forms
+
+class CreateForm(forms.Form):
+    id = forms.CharField(widget=forms.HiddenInput())
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'style':'width:90%'}))
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'checked':'checked'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'style':'width:90%; height:80px'}), required=False)
+
+class EditForm(CreateForm):
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput())
