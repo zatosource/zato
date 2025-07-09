74446e0823b88d32d8627e1c492e9537c256d9ab
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-09 17:20:17 +0200

    GH #755 - Working on AMQP.

--- code/zato-web-admin/src/zato/admin/web/views/pubsub/subscription.py
+++ code/zato-web-admin/src/zato/admin/web/views/pubsub/subscription.py
@@ -61,16 +61,6 @@ class Create(CreateEdit):
         input_optional = 'is_active', 'rest_push_endpoint_id'
         output_required = 'id', 'sub_key', 'is_active', 'created', 'topic_name', 'sec_name', 'delivery_type'
 
-    def handle(self):
-
-        logger.info('[DEBUG] Create.handle: Request method=%s', self.req.method)
-        logger.info('[DEBUG] Create.handle: Request POST data=%s', dict(self.req.POST))
-        logger.info('[DEBUG] Create.handle: Request GET data=%s', dict(self.req.GET))
-
-        # Check topic_id specifically
-        topic_id_list = self.req.POST.getlist('create-topic_id')
-        logger.info('[DEBUG] Create.handle: topic_id_list type=%s, value=%s', type(topic_id_list), topic_id_list)
-
     def _get_input_dict(self):
 
         input_dict = {}
@@ -78,8 +68,8 @@ class Create(CreateEdit):
         # Map topic_id form field (which can be multiple) to topic_id_list service input
         if self.req.method == 'POST':
             topic_ids = self.req.POST.getlist('create-topic_id')
-            input_dict['topic_id_list'] = topic_ids
-            logger.info('[DEBUG] Create._get_input_dict: Mapped topic_id_list=%s', topic_ids)
+            if topic_ids:
+                input_dict['topic_id_list'] = topic_ids
 
         return input_dict
 
@@ -91,7 +81,6 @@ class Create(CreateEdit):
         if self.req.method == 'POST':
             topic_ids = self.req.POST.getlist('topic_id')
             input_dict['topic_id_list'] = topic_ids
-            logger.info('[DEBUG] Create.pre_process_input_dict: Set topic_id_list=%s', topic_ids)
 
             # Map other form fields
             field_mapping = {
@@ -108,7 +97,6 @@ class Create(CreateEdit):
                         input_dict[service_field] = value == 'on'
                     else:
                         input_dict[service_field] = value
-                    logger.info('[DEBUG] Create.pre_process_input_dict: Set %s=%s', service_field, input_dict[service_field])
 
     def success_message(self, item):
         return 'Successfully created pub/sub subscription'
@@ -126,16 +114,6 @@ class Edit(CreateEdit):
         input_optional = 'is_active', 'rest_push_endpoint_id'
         output_required = 'id', 'sub_key', 'topic_name', 'sec_name', 'delivery_type', 'is_active'
 
-    def handle(self):
-        logger.info('[DEBUG] Edit.handle: Request method=%s', self.req.method)
-        logger.info('[DEBUG] Edit.handle: Request POST data=%s', dict(self.req.POST))
-        logger.info('[DEBUG] Edit.handle: Request GET data=%s', dict(self.req.GET))
-
-        # Check topic_id specifically
-        topic_id_list = self.req.POST.getlist('edit-topic_id')
-        logger.info('[DEBUG] Edit.handle: topic_id_list type=%s, value=%s', type(topic_id_list), topic_id_list)
-
-
     def _get_input_dict(self):
 
         input_dict = {}
@@ -143,8 +121,8 @@ class Edit(CreateEdit):
         # Map topic_id form field (which can be multiple) to topic_id_list service input
         if self.req.method == 'POST':
             topic_ids = self.req.POST.getlist('edit-topic_id')
-            input_dict['topic_id_list'] = topic_ids
-            logger.info('[DEBUG] Edit._get_input_dict: Mapped topic_id_list=%s', topic_ids)
+            if topic_ids:
+                input_dict['topic_id_list'] = topic_ids
 
         return input_dict
 
@@ -156,7 +134,6 @@ class Edit(CreateEdit):
         if self.req.method == 'POST':
             topic_ids = self.req.POST.getlist('edit-topic_id')
             input_dict['topic_id_list'] = topic_ids
-            logger.info('[DEBUG] Edit.pre_process_input_dict: Set topic_id_list=%s', topic_ids)
 
             # Map other form fields
             field_mapping = {
@@ -168,13 +145,12 @@ class Edit(CreateEdit):
             }
 
             for form_field, service_field in field_mapping.items():
-                value = self.req.POST.get(form_field)
-                if value:
+                if service_field in self.req.POST and self.req.POST[service_field]:
+                    value = self.req.POST[service_field]
                     if service_field == 'is_active':
                         input_dict[service_field] = value == 'on'
                     else:
                         input_dict[service_field] = value
-                    logger.info('[DEBUG] Edit.pre_process_input_dict: Set %s=%s', service_field, input_dict[service_field])
 
     def success_message(self, item):
         return 'Successfully updated pub/sub subscription'
