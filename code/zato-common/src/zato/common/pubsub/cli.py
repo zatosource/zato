d59de5442adfbb40c7fd0bdd3818c55d73d5c1ad
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-13 12:00:47 +0200

    GH #755 - Working on AMQP.

--- code/zato-common/src/zato/common/pubsub/cli.py
+++ code/zato-common/src/zato/common/pubsub/cli.py
@@ -29,7 +29,7 @@ if 0:
 
 # Setup basic logging
 basicConfig(
-    level=INFO,
+    level=DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler()
