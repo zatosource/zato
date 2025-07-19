cdd0271343236bf1c8becf3736fca347b9cf596d
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-19 11:46:48 +0200

    GH #755 - Working on AMQP.

--- code/zato-common/test/zato/common/matcher/test_matcher_misc.py
+++ code/zato-common/test/zato/common/matcher/test_matcher_misc.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2024, Zato Source s.r.o. https://zato.io
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
 
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
@@ -25,7 +25,8 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_regex_dos_nested_wildcards(self):
-        """Test potential regex DoS with nested wildcards"""
+        """ Test potential regex DoS with nested wildcards.
+        """
         permissions = [{'pattern': '**.**.**.**', 'access_type': PubSub.API_Client.Publisher}]
         self.matcher.add_client(self.client_id, permissions)
 
@@ -36,7 +37,8 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_case_sensitivity_bypass_reserved_names(self):
-        """Test case variations of reserved names"""
+        """ Test case variations of reserved names.
+        """
         reserved_patterns = ['ZaTo.admin.**', 'zato.admin.**', 'ZPSK.secret.**', 'zpsk.secret.**']
 
         for pattern in reserved_patterns:
@@ -47,13 +49,14 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_unicode_homograph_attacks(self):
-        """Test non-ASCII characters are rejected"""
+        """ Test non-ASCII characters are rejected.
+        """
         # Non-ASCII patterns that should be rejected
         non_ascii_patterns = [
-            'ᴢᴀᴛᴏ.admin.**',  # Small caps
+            'ᴢᴀᴛᴏ.admin.**',   # Small caps
             'ᴢᴘsᴋ.secret.**',  # Small caps
-            'zato​.admin.**',  # Zero-width space
-            'tëst.admin.**'   # Accented character
+            'zato​.admin.**',   # Zero-width space
+            'tëst.admin.**'    # Accented character
         ]
 
         for pattern in non_ascii_patterns:
@@ -64,7 +67,8 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_wildcard_boundary_exploitation(self):
-        """Test wildcard boundary conditions"""
+        """ Test wildcard boundary conditions.
+        """
         permissions = [{'pattern': '**.admin.**', 'access_type': PubSub.API_Client.Publisher}]
         self.matcher.add_client(self.client_id, permissions)
 
@@ -85,7 +89,8 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_regex_special_chars_in_topics(self):
-        """Test regex special characters in topic names"""
+        """ Test regex special characters in topic names.
+        """
         permissions = [{'pattern': 'admin.*', 'access_type': PubSub.API_Client.Publisher}]
         self.matcher.add_client(self.client_id, permissions)
 
@@ -108,7 +113,8 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_empty_segment_abuse(self):
-        """Test empty segment manipulation"""
+        """ Test empty segment manipulation.
+        """
         permissions = [
             {'pattern': 'admin.*.secret', 'access_type': PubSub.API_Client.Publisher},
             {'pattern': 'admin..secret', 'access_type': PubSub.API_Client.Subscriber}
@@ -126,10 +132,11 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_permission_escalation_overlapping(self):
-        """Test permission escalation through overlapping patterns"""
+        """ Test permission escalation through overlapping patterns.
+        """
         permissions = [
-            {'pattern': 'admin.*', 'access_type': PubSub.API_Client.Subscriber},  # Less privilege
-            {'pattern': 'admin.delete', 'access_type': PubSub.API_Client.Publisher}  # More privilege
+            {'pattern': 'admin.*', 'access_type': PubSub.API_Client.Subscriber},    # Less privilege
+            {'pattern': 'admin.delete', 'access_type': PubSub.API_Client.Publisher} # More privilege
         ]
         self.matcher.add_client(self.client_id, permissions)
 
@@ -144,7 +151,8 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_long_topic_names(self):
-        """Test very long topic names"""
+        """ Test very long topic names.
+        """
         permissions = [{'pattern': 'test.**', 'access_type': PubSub.API_Client.Publisher}]
         self.matcher.add_client(self.client_id, permissions)
 
@@ -158,7 +166,7 @@ class PatternMatcherMiscTestCase(TestCase):
 # ################################################################################################################################
 
     def test_publisher_subscriber(self):
-        """Test Publisher_Subscriber permission abuse"""
+
         permissions = [
             {'pattern': 'restricted.admin.**', 'access_type': PubSub.API_Client.Publisher_Subscriber}
         ]
