96de878549202c784945a464d40bf329c7eba32b
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-19 09:22:05 +0200

    GH #755 - Working on AMQP.

--- code/zato-common/src/zato/common/pubsub/matcher.py
+++ code/zato-common/src/zato/common/pubsub/matcher.py
@@ -17,6 +17,9 @@ from gevent.lock import RLock
 # Zato
 from zato.common.api import PubSub
 
+# ################################################################################################################################
+# ################################################################################################################################
+
 if 0:
     from typing import Pattern
     from zato.common.typing_ import anydict, strlist
@@ -89,6 +92,8 @@ class PatternMatcher:
         self._clients: 'Dict[str, ClientPermissions]' = {}
         self._pattern_cache: 'Dict[str, CacheEntry]' = {}
 
+# ################################################################################################################################
+
     def _compile_pattern(self, pattern:'str') -> 'Pattern[str]':
         """ Compile a topic pattern to regex, with caching.
         """
@@ -111,6 +116,8 @@ class PatternMatcher:
 
         return compiled_regex
 
+# ################################################################################################################################
+
     def _parse_permissions(self, permissions:'List[anydict]') -> 'ParsedPermissions':
         """ Parse permission dicts into pub and sub patterns.
         """
@@ -118,6 +125,7 @@ class PatternMatcher:
         sub_patterns = []
 
         for perm_dict in permissions:
+
             access_type = perm_dict.get('access_type', '')
             pattern = perm_dict.get('pattern', '')
 
@@ -132,7 +140,10 @@ class PatternMatcher:
         parsed_perms = ParsedPermissions()
         parsed_perms.pub_patterns = pub_patterns
         parsed_perms.sub_patterns = sub_patterns
+
         return parsed_perms
+
+# ################################################################################################################################
 
     def add_client(self, client_id:'str', permissions:'List[anydict]') -> 'None':
         """ Add a new client with permissions.
@@ -163,12 +174,14 @@ class PatternMatcher:
             # Sort patterns alphabetically with wildcards last
             pub_pattern_infos.sort(key=self._pattern_info_sort_key)
             sub_pattern_infos.sort(key=self._pattern_info_sort_key)
-            
+
             client_perms = ClientPermissions()
             client_perms.client_id = client_id
             client_perms.pub_patterns = pub_pattern_infos
             client_perms.sub_patterns = sub_pattern_infos
             self._clients[client_id] = client_perms
+
+# ################################################################################################################################
 
     def remove_client(self, client_id:'str') -> 'None':
         """ Remove a client and all its permissions.
@@ -177,14 +190,17 @@ class PatternMatcher:
             if client_id in self._clients:
                 del self._clients[client_id]
 
+# ################################################################################################################################
+
     def set_permissions(self, client_id:'str', permissions:'List[anydict]') -> 'None':
         """ Set/update all permissions for a client.
         """
         with self._lock:
+
             if client_id not in self._clients:
                 self.add_client(client_id, permissions)
             else:
-                # Update existing client
+
                 parsed_perms = self._parse_permissions(permissions)
 
                 pub_pattern_infos = []
@@ -214,22 +230,27 @@ class PatternMatcher:
                 self._clients[client_id].pub_patterns = pub_pattern_infos
                 self._clients[client_id].sub_patterns = sub_pattern_infos
 
+# ################################################################################################################################
+
     def _pattern_sort_key(self, pattern_info:'PatternInfo') -> 'tuple[str, bool]':
         """ Sort key for patterns: alphabetical with wildcards last.
         """
         has_wildcard = '*' in pattern_info.pattern
         return (pattern_info.pattern, has_wildcard)
-    
+
+# ################################################################################################################################
+
     def _pattern_info_sort_key(self, pattern_info:'PatternInfo') -> 'tuple[str, bool]':
         """ Sort key for PatternInfo objects: alphabetical with wildcards last.
         """
         has_wildcard = '*' in pattern_info.pattern
         return (pattern_info.pattern, has_wildcard)
 
+# ################################################################################################################################
+
     def evaluate(self, client_id:'str', topic:'str', operation:'str') -> 'EvaluationResult':
         """ Evaluate if a client can perform an operation on a topic.
         """
-        # No lock needed for read-only evaluation
         if client_id not in self._clients:
             result = EvaluationResult()
             result.is_ok = False
@@ -292,21 +313,28 @@ class PatternMatcher:
         result.reason = 'No matching pattern found'
         return result
 
+# ################################################################################################################################
+
     def get_client_count(self) -> 'int':
         """ Get the number of registered clients.
         """
         return len(self._clients)
+
+# ################################################################################################################################
 
     def get_cache_size(self) -> 'int':
         """ Get the size of the pattern cache.
         """
         return len(self._pattern_cache)
 
+# ################################################################################################################################
+
     def clear_cache(self) -> 'None':
         """ Clear the pattern cache.
         """
         with self._lock:
             self._pattern_cache.clear()
+
             # Recompile all patterns for existing clients
             for client_perms in self._clients.values():
                 for pattern_info in client_perms.pub_patterns:
