a94ed30c256b1c64fc0a6d4d7fbefc883e2d6687
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-07-25 16:45:36 +0200

    GH #755 - Working on AMQP.

--- code/zato-common/test/zato/common/pubsub/test_unsubscribe.py
+++ code/zato-common/test/zato/common/pubsub/test_unsubscribe.py
@@ -7,30 +7,38 @@ Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
 # stdlib
-import unittest
+from unittest import main, TestCase
 
 # requests
 import requests
 
 # Zato
-from zato.common.api import REST_Server
+from zato.common.api import PubSub
 
 # ################################################################################################################################
 # ################################################################################################################################
 
-class TestUnsubscribe(unittest.TestCase):
+class TestUnsubscribe(TestCase):
 
     def test_unsubscribe_success(self):
-        topic_name = 'test.topic'
-        sub_key = 'test-sub-key'
 
-        url = f'http://localhost:{REST_Server.Default_Port}/unsubscribe/{topic_name}'
+        topic_name = 'demo.1'
+
+        url = f'http://localhost:{PubSub.REST_Server.Default_Port}/pubsub/unsubscribe/topic/{topic_name}'
         auth = ('demo', 'demo')
 
-        response = requests.post(url, auth=auth, json={'sub_key': sub_key})
+        response = requests.post(url, auth=auth)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['status'], 'success')
+
+        response_data = response.json()
+        self.assertEqual(response_data['status'], 'success')
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if __name__ == '__main__':
+    _ = main()
 
 # ################################################################################################################################
 # ################################################################################################################################
