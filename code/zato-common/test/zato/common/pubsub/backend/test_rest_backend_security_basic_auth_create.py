4b225fd42f6c7ee527f147b96ac5af318fcd1d80
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-03 09:42:05 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/backend/test_rest_backend_security_basic_auth_create.py
+++ code/zato-common/test/zato/common/pubsub/backend/test_rest_backend_security_basic_auth_create.py
@@ -23,6 +23,41 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
         self.broker_client = BrokerClient()
         self.rest_server = PubSubRESTServer('localhost', 8080)
         self.backend = RESTBackend(self.rest_server, self.broker_client)
+        
+        # Test data constants
+        self.test_cid = 'test-cid-123'
+        self.test_username = 'test_user'
+        self.test_password = 'secure_password_123'
+        
+        self.empty_password_cid = 'test-cid-empty'
+        self.empty_password_username = 'empty_password_user'
+        self.empty_password = ''
+        
+        self.special_chars_cid = 'test-cid-special'
+        self.special_chars_username = 'user@domain.com'
+        self.special_chars_password = 'password123'
+        
+        self.unicode_cid = 'test-cid-unicode'
+        self.unicode_username = 'unicode_user_ñ'
+        self.unicode_password = 'password_ü123'
+        
+        self.long_values_cid = 'test-cid-long'
+        self.long_username = 'a' * 100
+        self.long_password = 'b' * 200
+        
+        self.numeric_cid = 12345
+        self.numeric_cid_username = 'numeric_cid_user'
+        self.numeric_cid_password = 'numeric_password'
+        
+        self.preserve_cid = 'test-cid-preserve'
+        self.preserve_username = 'preserve_user'
+        self.preserve_password = 'preserve_password'
+        
+        self.duplicate_cid_1 = 'test-cid-duplicate'
+        self.duplicate_cid_2 = 'test-cid-duplicate-2'
+        self.duplicate_username = 'duplicate_user'
+        self.duplicate_password_1 = 'first_password'
+        self.duplicate_password_2 = 'second_password'
 
 # ################################################################################################################################
 
@@ -30,20 +65,20 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
 
         # Create the broker message
         msg = {
-            'cid': 'test-cid-123',
-            'sec_name': 'test_user',
-            'password': 'secure_password_123'
+            'cid': self.test_cid,
+            'sec_name': self.test_username,
+            'password': self.test_password
         }
 
         # Verify user doesn't exist initially
-        self.assertNotIn('test_user', self.rest_server.users)
+        self.assertNotIn(self.test_username, self.rest_server.users)
 
         # Call the method under test
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert user was added to users dict
-        self.assertIn('test_user', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['test_user'], 'secure_password_123')
+        self.assertIn(self.test_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.test_username], self.test_password)
 
 # ################################################################################################################################
 
@@ -51,17 +86,17 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
 
         # Create the broker message with empty password
         msg = {
-            'cid': 'test-cid-empty',
-            'sec_name': 'empty_password_user',
-            'password': ''
+            'cid': self.empty_password_cid,
+            'sec_name': self.empty_password_username,
+            'password': self.empty_password
         }
 
         # Call the method under test
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert user was added with empty password
-        self.assertIn('empty_password_user', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['empty_password_user'], '')
+        self.assertIn(self.empty_password_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.empty_password_username], self.empty_password)
 
 # ################################################################################################################################
 
@@ -69,17 +104,17 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
 
         # Create the broker message with special characters in username
         msg = {
-            'cid': 'test-cid-special',
-            'sec_name': 'user@domain.com',
-            'password': 'password123'
+            'cid': self.special_chars_cid,
+            'sec_name': self.special_chars_username,
+            'password': self.special_chars_password
         }
 
         # Call the method under test
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert user was added with special character username
-        self.assertIn('user@domain.com', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['user@domain.com'], 'password123')
+        self.assertIn(self.special_chars_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.special_chars_username], self.special_chars_password)
 
 # ################################################################################################################################
 
@@ -87,58 +122,67 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
 
         # Create the broker message with unicode characters
         msg = {
-            'cid': 'test-cid-unicode',
-            'sec_name': 'unicode_user_ñ',
-            'password': 'password_ü123'
+            'cid': self.unicode_cid,
+            'sec_name': self.unicode_username,
+            'password': self.unicode_password
         }
 
         # Call the method under test
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert user was added with unicode characters
-        self.assertIn('unicode_user_ñ', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['unicode_user_ñ'], 'password_ü123')
+        self.assertIn(self.unicode_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.unicode_username], self.unicode_password)
 
 # ################################################################################################################################
 
     def test_on_broker_msg_SECURITY_BASIC_AUTH_CREATE_with_long_values(self):
 
         # Create the broker message with long values
-        long_username = 'a' * 100
-        long_password = 'b' * 200
         msg = {
-            'cid': 'test-cid-long',
-            'sec_name': long_username,
-            'password': long_password
+            'cid': self.long_values_cid,
+            'sec_name': self.long_username,
+            'password': self.long_password
         }
 
         # Call the method under test
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert user was added with long values
-        self.assertIn(long_username, self.rest_server.users)
-        self.assertEqual(self.rest_server.users[long_username], long_password)
+        self.assertIn(self.long_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.long_username], self.long_password)
 
 # ################################################################################################################################
 
     def test_on_broker_msg_SECURITY_BASIC_AUTH_CREATE_multiple_calls(self):
 
+        # Test data for multiple users
+        multi_cid_1 = 'test-cid-1'
+        multi_cid_2 = 'test-cid-2'
+        multi_cid_3 = 'test-cid-3'
+        multi_username_1 = 'user1'
+        multi_username_2 = 'user2'
+        multi_username_3 = 'user3'
+        multi_password_1 = 'password1'
+        multi_password_2 = 'password2'
+        multi_password_3 = 'password3'
+        
         # Create multiple broker messages
         messages = [
             {
-                'cid': 'test-cid-1',
-                'sec_name': 'user1',
-                'password': 'password1'
+                'cid': multi_cid_1,
+                'sec_name': multi_username_1,
+                'password': multi_password_1
             },
             {
-                'cid': 'test-cid-2',
-                'sec_name': 'user2',
-                'password': 'password2'
+                'cid': multi_cid_2,
+                'sec_name': multi_username_2,
+                'password': multi_password_2
             },
             {
-                'cid': 'test-cid-3',
-                'sec_name': 'user3',
-                'password': 'password3'
+                'cid': multi_cid_3,
+                'sec_name': multi_username_3,
+                'password': multi_password_3
             }
         ]
 
@@ -148,32 +192,35 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
 
         # Assert all users were added
         self.assertEqual(len(self.rest_server.users), 3)
-        self.assertIn('user1', self.rest_server.users)
-        self.assertIn('user2', self.rest_server.users)
-        self.assertIn('user3', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['user1'], 'password1')
-        self.assertEqual(self.rest_server.users['user2'], 'password2')
-        self.assertEqual(self.rest_server.users['user3'], 'password3')
+        self.assertIn(multi_username_1, self.rest_server.users)
+        self.assertIn(multi_username_2, self.rest_server.users)
+        self.assertIn(multi_username_3, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[multi_username_1], multi_password_1)
+        self.assertEqual(self.rest_server.users[multi_username_2], multi_password_2)
+        self.assertEqual(self.rest_server.users[multi_username_3], multi_password_3)
 
 # ################################################################################################################################
 
     def test_on_broker_msg_SECURITY_BASIC_AUTH_CREATE_preserves_message_data(self):
 
         # Create the broker message with additional fields
+        extra_field_value = 'should_be_ignored'
+        another_field_value = 123
+        
         msg = {
-            'cid': 'test-cid-preserve',
-            'sec_name': 'preserve_user',
-            'password': 'preserve_password',
-            'extra_field': 'should_be_ignored',
-            'another_field': 123
+            'cid': self.preserve_cid,
+            'sec_name': self.preserve_username,
+            'password': self.preserve_password,
+            'extra_field': extra_field_value,
+            'another_field': another_field_value
         }
 
         # Call the method under test
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert only the required fields were used and user was created
-        self.assertIn('preserve_user', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['preserve_user'], 'preserve_password')
+        self.assertIn(self.preserve_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.preserve_username], self.preserve_password)
 
 # ################################################################################################################################
 
@@ -181,17 +228,17 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
 
         # Create the broker message with numeric CID
         msg = {
-            'cid': 12345,
-            'sec_name': 'numeric_cid_user',
-            'password': 'numeric_password'
+            'cid': self.numeric_cid,
+            'sec_name': self.numeric_cid_username,
+            'password': self.numeric_cid_password
         }
 
         # Call the method under test
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert user was created regardless of CID type
-        self.assertIn('numeric_cid_user', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['numeric_cid_user'], 'numeric_password')
+        self.assertIn(self.numeric_cid_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.numeric_cid_username], self.numeric_cid_password)
 
 # ################################################################################################################################
 
@@ -221,31 +268,31 @@ class RESTBackendSecurityBasicAuthCreateTestCase(TestCase):
 
         # Create the broker message
         msg = {
-            'cid': 'test-cid-duplicate',
-            'sec_name': 'duplicate_user',
-            'password': 'first_password'
+            'cid': self.duplicate_cid_1,
+            'sec_name': self.duplicate_username,
+            'password': self.duplicate_password_1
         }
 
         # Call the method under test first time
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg)
 
         # Assert user was created
-        self.assertIn('duplicate_user', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['duplicate_user'], 'first_password')
+        self.assertIn(self.duplicate_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.duplicate_username], self.duplicate_password_1)
 
         # Create second message with same user but different password
         msg2 = {
-            'cid': 'test-cid-duplicate-2',
-            'sec_name': 'duplicate_user',
-            'password': 'second_password'
+            'cid': self.duplicate_cid_2,
+            'sec_name': self.duplicate_username,
+            'password': self.duplicate_password_2
         }
 
         # Call the method under test second time
         self.backend.on_broker_msg_SECURITY_BASIC_AUTH_CREATE(msg2)
 
         # Assert user still exists with original password (no overwrite)
-        self.assertIn('duplicate_user', self.rest_server.users)
-        self.assertEqual(self.rest_server.users['duplicate_user'], 'first_password')
+        self.assertIn(self.duplicate_username, self.rest_server.users)
+        self.assertEqual(self.rest_server.users[self.duplicate_username], self.duplicate_password_1)
 
 # ################################################################################################################################
 # ################################################################################################################################
