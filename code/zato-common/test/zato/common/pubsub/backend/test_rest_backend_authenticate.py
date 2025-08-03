85b32a421a39a7b38e970377aa40ff7ff17cfe3e
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-03 10:15:43 +0200

    GH #755 - Working on AMQP (tests).

---
+++ code/zato-common/test/zato/common/pubsub/backend/test_rest_backend_authenticate.py
@@ -0,0 +1,368 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+from base64 import b64encode
+from unittest import main, TestCase
+import warnings
+
+# Zato
+from zato.common.pubsub.server.rest_base import BaseRESTServer, UnauthorizedException
+from zato.broker.client import BrokerClient
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class RESTBackendAuthenticateTestCase(TestCase):
+
+    def setUp(self):
+
+        # Suppress ResourceWarnings from gevent
+        warnings.filterwarnings('ignore', category=ResourceWarning)
+
+        self.broker_client = BrokerClient()
+        self.rest_server = BaseRESTServer('localhost', 8080)
+
+        # Test data constants
+        self.test_cid = 'test-cid-123'
+        self.test_username = 'test_user'
+        self.test_password = 'secure_password_123'
+
+        self.invalid_username = 'invalid_user'
+        self.invalid_password = 'invalid_password'
+
+        self.empty_username = ''
+        self.empty_password = ''
+
+        self.special_chars_username = 'user@domain.com'
+        self.special_chars_password = 'password!@#$%'
+
+        self.unicode_username = 'unicode_user_ñ'
+        self.unicode_password = 'password_ü123'
+
+        self.long_username = 'a' * 100
+        self.long_password = 'b' * 200
+
+        # Add test users to server
+        self.rest_server.users[self.test_username] = self.test_password
+        self.rest_server.users[self.special_chars_username] = self.special_chars_password
+        self.rest_server.users[self.unicode_username] = self.unicode_password
+        self.rest_server.users[self.long_username] = self.long_password
+
+    def _create_basic_auth_header(self, username, password):
+        """Helper method to create Basic Auth header"""
+        credentials = f'{username}:{password}'
+        encoded_credentials = b64encode(credentials.encode('utf-8')).decode('ascii')
+        return f'Basic {encoded_credentials}'
+
+    def _create_environ(self, auth_header=None, path_info='/test'):
+        """Helper method to create WSGI environ dict"""
+        environ = {
+            'PATH_INFO': path_info,
+            'REQUEST_METHOD': 'GET'
+        }
+        if auth_header:
+            environ['HTTP_AUTHORIZATION'] = auth_header
+        return environ
+
+# ################################################################################################################################
+
+    def test_authenticate_with_valid_credentials(self):
+
+        # Create valid auth header
+        auth_header = self._create_basic_auth_header(self.test_username, self.test_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert authentication succeeded
+        self.assertEqual(result, self.test_username)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_invalid_username(self):
+
+        # Create auth header with invalid username
+        auth_header = self._create_basic_auth_header(self.invalid_username, self.test_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_invalid_password(self):
+
+        # Create auth header with invalid password
+        auth_header = self._create_basic_auth_header(self.test_username, self.invalid_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_no_authorization_header(self):
+
+        # Create environ without auth header
+        environ = self._create_environ()
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_empty_authorization_header(self):
+
+        # Create environ with empty auth header
+        environ = self._create_environ('')
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_malformed_authorization_header(self):
+
+        # Create environ with malformed auth header
+        environ = self._create_environ('NotBasic invalid_header')
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_invalid_base64_encoding(self):
+
+        # Create environ with invalid base64 in auth header
+        environ = self._create_environ('Basic invalid_base64!')
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_empty_username(self):
+
+        # Create auth header with empty username
+        auth_header = self._create_basic_auth_header(self.empty_username, self.test_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_empty_password(self):
+
+        # Add user with empty password
+        empty_password_user = 'empty_password_user'
+        self.rest_server.users[empty_password_user] = self.empty_password
+
+        # Create auth header with empty password
+        auth_header = self._create_basic_auth_header(empty_password_user, self.empty_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert authentication succeeded
+        self.assertEqual(result, empty_password_user)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_special_characters_in_credentials(self):
+
+        # Create auth header with special characters
+        auth_header = self._create_basic_auth_header(self.special_chars_username, self.special_chars_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert authentication succeeded
+        self.assertEqual(result, self.special_chars_username)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_unicode_characters(self):
+
+        # Create auth header with unicode characters
+        auth_header = self._create_basic_auth_header(self.unicode_username, self.unicode_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert authentication succeeded
+        self.assertEqual(result, self.unicode_username)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_long_credentials(self):
+
+        # Create auth header with long credentials
+        auth_header = self._create_basic_auth_header(self.long_username, self.long_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert authentication succeeded
+        self.assertEqual(result, self.long_username)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_colon_in_password(self):
+
+        # Add user with colon in password
+        colon_password_user = 'colon_password_user'
+        colon_password = 'password:with:colons'
+        self.rest_server.users[colon_password_user] = colon_password
+
+        # Create auth header with colon in password
+        auth_header = self._create_basic_auth_header(colon_password_user, colon_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert authentication succeeded
+        self.assertEqual(result, colon_password_user)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_different_path_info(self):
+
+        # Create valid auth header
+        auth_header = self._create_basic_auth_header(self.test_username, self.test_password)
+        environ = self._create_environ(auth_header, '/different/path')
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert authentication succeeded regardless of path
+        self.assertEqual(result, self.test_username)
+
+# ################################################################################################################################
+
+    def test_authenticate_case_sensitive_username(self):
+
+        # Create auth header with different case username
+        auth_header = self._create_basic_auth_header(self.test_username.upper(), self.test_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test and expect exception (case sensitive)
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_case_sensitive_password(self):
+
+        # Create auth header with different case password
+        auth_header = self._create_basic_auth_header(self.test_username, self.test_password.upper())
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test and expect exception (case sensitive)
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_multiple_calls_same_credentials(self):
+
+        # Create valid auth header
+        auth_header = self._create_basic_auth_header(self.test_username, self.test_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method multiple times
+        result1 = self.rest_server.authenticate(self.test_cid, environ)
+        result2 = self.rest_server.authenticate(self.test_cid, environ)
+        result3 = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert all calls succeeded with same result
+        self.assertEqual(result1, self.test_username)
+        self.assertEqual(result2, self.test_username)
+        self.assertEqual(result3, self.test_username)
+
+# ################################################################################################################################
+
+    def test_authenticate_with_bearer_token_header(self):
+
+        # Create environ with Bearer token instead of Basic auth
+        environ = self._create_environ('Bearer some_token_value')
+
+        # Call the method under test and expect exception
+        with self.assertRaises(UnauthorizedException) as cm:
+            _ = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert exception contains correct CID
+        self.assertEqual(cm.exception.cid, self.test_cid)
+
+# ################################################################################################################################
+
+    def test_authenticate_preserves_users_dict(self):
+
+        # Store initial users dict
+        initial_users = dict(self.rest_server.users)
+
+        # Create valid auth header
+        auth_header = self._create_basic_auth_header(self.test_username, self.test_password)
+        environ = self._create_environ(auth_header)
+
+        # Call the method under test
+        result = self.rest_server.authenticate(self.test_cid, environ)
+
+        # Assert users dict was not modified
+        self.assertEqual(self.rest_server.users, initial_users)
+        self.assertEqual(result, self.test_username)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if __name__ == '__main__':
+    _ = main()
+
+# ################################################################################################################################
+# ################################################################################################################################
