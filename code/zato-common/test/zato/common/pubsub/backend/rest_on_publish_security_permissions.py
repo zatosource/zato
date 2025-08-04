2be95ce99c0d5b4951e31e020c0aeac526e94713
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-04 19:30:02 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/backend/rest_on_publish_security_permissions.py
+++ code/zato-common/test/zato/common/pubsub/backend/rest_on_publish_security_permissions.py
@@ -5,3 +5,9 @@ Copyright (C) 2025, Zato Source s.r.o. https://zato.io
 
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
+
+# This file is intentionally empty.
+# Permission tests have been split into separate files:
+# - rest_on_publish_permissions_allowed.py
+# - rest_on_publish_permissions_denied.py  
+# - rest_on_publish_permissions_integration.py
