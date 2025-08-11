edebf5bdd6d26876b93affcde476880502b65062
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-11 11:25:24 +0200

    GH #755 - Working on AMQP (tests).

---
+++ code/zato-common/test/zato/common/pubsub/rest_server/test_requests_unsubscribe.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+import logging
+import os
+import time
+from unittest import main, TestCase
+
+# requests
+import requests
+from requests.auth import HTTPBasicAuth
+
+# PyYAML
+from yaml import safe_load as yaml_load
+
+# Zato
+from zato.common.pubsub.util import get_broker_config, cleanup_broker_impl
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(name)s:%(lineno)d - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class PubSubRESTServerUnsubscribeTestCase(TestCase):
+    """ Test cases for the pub/sub REST server unsubscribe functionality.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Set up test configuration.
+        """
+        # Check if config environment variable exists
+        config_path = os.environ.get('Zato_PubSub_YAML_Config_File')
+        if not config_path:
+            cls.skip_tests = True
+            return
+
+        cls.skip_tests = False
+
+        # Load configuration
+        with open(config_path, 'r') as f:
+            cls.config = yaml_load(f)
+
+        # Extract demo user credentials
+        cls.username = 'demo'
+        cls.password = cls.config['security'][0]['password']
+        cls.auth = HTTPBasicAuth(cls.username, cls.password)
+
+        # Server configuration
+        cls.base_url = 'http://127.0.0.1:44556'
+
+        # Test topics from config
+        cls.test_topics = [
+            cls.config['pubsub_topic'][0]['name'],
+            cls.config['pubsub_topic'][1]['name'],
+            cls.config['pubsub_topic'][2]['name']
+        ]
+
+# ################################################################################################################################
+
+    def setUp(self):
+        """ Skip tests if no config available.
+        """
+        if self.skip_tests:
+            self.skipTest('Zato_PubSub_YAML_Config environment variable not set')
+
+# ################################################################################################################################
+
+    def tearDown(self):
+        """ Clean up after tests.
+        """
+        if self.skip_tests:
+            return
+
+        # Clean up broker
+        broker_config = get_broker_config()
+        _ = cleanup_broker_impl(broker_config, 15672)
+
+        # Unsubscribe from all topics to clear any existing subscriptions
+        for topic in self.test_topics:
+            try:
+                _ = requests.post(
+                    f'{self.base_url}/pubsub/unsubscribe/topic/{topic}',
+                    auth=self.auth
+                )
+            except Exception:
+                pass
+
+# ################################################################################################################################
+
+    def test_subscribe_then_unsubscribe(self):
+        """ Test subscribing to a topic and then unsubscribing.
+        """
+        topic = self.test_topics[0]  # demo.1
+
+        # Subscribe to topic
+        response = requests.post(
+            f'{self.base_url}/pubsub/subscribe/topic/{topic}',
+            auth=self.auth
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertTrue(data['is_ok'])
+
+        # Wait 0.1 second
+        time.sleep(0.1)
+
+        # Unsubscribe from topic
+        response = requests.post(
+            f'{self.base_url}/pubsub/unsubscribe/topic/{topic}',
+            auth=self.auth
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertTrue(data['is_ok'])
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+if __name__ == '__main__':
+    _ = main()
+
+# ################################################################################################################################
+# ################################################################################################################################
