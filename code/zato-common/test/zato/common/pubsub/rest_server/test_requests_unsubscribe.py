1ac6cad8bb467d9893da35faf78173f1948c5f3c
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-11 14:04:26 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/rest_server/test_requests_unsubscribe.py
+++ code/zato-common/test/zato/common/pubsub/rest_server/test_requests_unsubscribe.py
@@ -31,6 +31,9 @@ class PubSubRESTServerUnsubscribeTestCase(PubSubRESTServerBaseTestCase):
     def test_subscribe_then_unsubscribe(self):
         """ Test subscribing to a topic and then unsubscribing.
         """
+        # Skip automatic unsubscribe in tearDown since this test handles it manually
+        self.skip_auto_unsubscribe = True
+
         topic = self.test_topics[0]  # demo.1
 
         # Subscribe to topic
@@ -54,24 +57,25 @@ class PubSubRESTServerUnsubscribeTestCase(PubSubRESTServerBaseTestCase):
         self.assertIn('demo', subscriptions[topic])
         self.assertIn('sub_key', subscriptions[topic]['demo'])
 
-        # Unsubscribe from topic
-        response = requests.post(
-            f'{self.base_url}/pubsub/unsubscribe/topic/{topic}',
-            auth=self.auth
-        )
-        self.assertEqual(response.status_code, 200)
-        data = response.json()
-        self.assertTrue(data['is_ok'])
+        # Unsubscribe from all topics
+        for topic_name in self.test_topics:
+            response = requests.post(
+                f'{self.base_url}/pubsub/unsubscribe/topic/{topic_name}',
+                auth=self.auth
+            )
+            self.assertEqual(response.status_code, 200)
+            data = response.json()
+            self.assertTrue(data['is_ok'])
 
-        # Check diagnostics after unsubscribe - subscription should be removed
-        diagnostics_after_unsubscribe = self._call_diagnostics()
-        self.assertIsNotNone(diagnostics_after_unsubscribe)
-        self.assertTrue(diagnostics_after_unsubscribe['is_ok'])
-        self.assertIn('data', diagnostics_after_unsubscribe)
+            # Check diagnostics after each unsubscribe
+            diagnostics_after_unsubscribe = self._call_diagnostics()
+            self.assertIsNotNone(diagnostics_after_unsubscribe)
+            self.assertTrue(diagnostics_after_unsubscribe['is_ok'])
+            self.assertIn('data', diagnostics_after_unsubscribe)
 
-        # Verify subscription no longer exists for this topic
-        subscriptions = diagnostics_after_unsubscribe['data']['subscriptions']
-        self.assertNotIn(topic, subscriptions)
+            # Verify subscription no longer exists for this topic
+            subscriptions = diagnostics_after_unsubscribe['data']['subscriptions']
+            self.assertNotIn(topic_name, subscriptions)
 
 # ################################################################################################################################
 # ################################################################################################################################
