df0ef871407688913773aadc4ef603c757143fdc
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-15 11:40:25 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/src/zato/common/api.py
+++ code/zato-common/src/zato/common/api.py
@@ -1145,9 +1145,8 @@ class PubSub:
         # How many seconds a consumer will wait in its drain_events call
         Consumer = 3
 
-        # Must be bigger than the Consumer timeout to give a consumer enough time
-        # to drain its events.
-        Invoke_Sync = Consumer * 3
+        # Must be bigger than the Consumer timeout to give a consumer enough time to drain its events.
+        Invoke_Sync = Consumer * 2
 
     class API_Client:
         Publisher = 'publisher'
