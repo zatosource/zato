5fd02f00c9bc67730e84b1be65829afe09d7497c
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-15 18:03:33 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/rest_server/test_requests.py
+++ code/zato-common/test/zato/common/pubsub/rest_server/test_requests.py
@@ -8,11 +8,7 @@ Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 
 # stdlib
 import logging
-import time
 from unittest import main
-
-# requests
-import requests
 
 # Zato
 from zato.common.test.unittest_pubsub_requests import PubSubRESTServerBaseTestCase
@@ -29,6 +25,40 @@ class PubSubRESTServerTestCase(PubSubRESTServerBaseTestCase):
     """ Test cases for the pub/sub REST server.
     """
     skip_auto_unsubscribe = True
+
+# ################################################################################################################################
+
+    def test_full_path(self) -> 'None':
+        """ Test full path with enmasse configuration.
+        """
+        # sh
+        from sh import ErrorReturnCode
+
+        # Zato
+        from zato.common.util.cli import get_zato_sh_command
+        from zato.common.test.config import TestConfig
+
+        # Set environment variable to avoid requiring server
+        import os
+        os.environ['Zato_Needs_Config_Reload'] = 'False'
+
+        try:
+            # Get the command
+            command = get_zato_sh_command()
+
+            # Invoke enmasse with the config path
+            out = command('enmasse', TestConfig.server_location, '--import', '--input', self.config_path, '--verbose') # type: ignore
+
+            # Check for successful import
+            self.assertEqual(out.exit_code, 0)
+
+        except ErrorReturnCode as e:
+            stdout = e.stdout.decode('utf8')
+            stderr = e.stderr
+
+            logger.error(f'Enmasse failed - stdout: {stdout}')
+            logger.error(f'Enmasse failed - stderr: {stderr}')
+            self.fail(f'Caught an exception while invoking enmasse; stdout -> {stdout}')
 
 # ################################################################################################################################
 # ################################################################################################################################
