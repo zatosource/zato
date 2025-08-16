dc1b2b8b5453e761d9edd6774943e2433072a1c9
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-16 16:31:41 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/src/zato/common/test/unittest_pubsub_requests.py
+++ code/zato-common/src/zato/common/test/unittest_pubsub_requests.py
@@ -39,7 +39,6 @@ if 0:
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(name)s:%(lineno)d - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 # ################################################################################################################################
