494c327ff9580688083cb171fea66ddda2a1c301
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-30 13:10:56 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/perftest/config.js
+++ code/zato-common/test/zato/common/pubsub/perftest/config.js
@@ -7,6 +7,25 @@ export const MAX_TOPICS = parseInt(__ENV.Zato_Test_PubSub_OpenAPI_Max_Topics);
 export const VUS = parseInt(__ENV.Zato_Test_PubSub_OpenAPI_VUs);
 export const ITERATIONS_PER_VU = parseInt(__ENV.Zato_Test_PubSub_OpenAPI_Iterations_Per_VU);
 
+if (!BASE_URL) {
+  throw new Error('Environment variable Zato_Test_PubSub_OpenAPI_URL is not set');
+}
+if (!USERNAME) {
+  throw new Error('Environment variable Zato_Test_PubSub_OpenAPI_Username is not set');
+}
+if (!PASSWORD) {
+  throw new Error('Environment variable Zato_Test_PubSub_OpenAPI_Password is not set');
+}
+if (isNaN(MAX_TOPICS)) {
+  throw new Error('Environment variable Zato_Test_PubSub_OpenAPI_Max_Topics is not set or invalid');
+}
+if (isNaN(VUS)) {
+  throw new Error('Environment variable Zato_Test_PubSub_OpenAPI_VUs is not set or invalid');
+}
+if (isNaN(ITERATIONS_PER_VU)) {
+  throw new Error('Environment variable Zato_Test_PubSub_OpenAPI_Iterations_Per_VU is not set or invalid');
+}
+
 export const auth = encoding.b64encode(`${USERNAME}:${PASSWORD}`);
 
 export const headers = {
