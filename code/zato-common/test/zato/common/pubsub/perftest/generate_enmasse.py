f88011e56620ed72e0917772af130f8e024ceae2
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-08-31 08:07:39 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/perftest/generate_enmasse.py
+++ code/zato-common/test/zato/common/pubsub/perftest/generate_enmasse.py
@@ -17,6 +17,12 @@ import yaml
 # ################################################################################################################################
 # ################################################################################################################################
 
+if 0:
+    from zato.common.typing_ import strdict
+
+# ################################################################################################################################
+# ################################################################################################################################
+
 log_format = '%(asctime)s - %(levelname)s - %(name)s:%(lineno)d - %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=log_format)
 
@@ -25,32 +31,43 @@ logger = logging.getLogger('zato')
 # ################################################################################################################################
 # ################################################################################################################################
 
-def main(users: 'int') -> 'None':
-    # Get the path to the YAML config file using relative path
-    current_dir = Path(__file__).parent
-    server_config_dir = current_dir / '..' / '..' / '..' / '..' / '..' / 'src' / 'zato' / 'common' / 'pubsub' / 'server'
-    config_path = server_config_dir / 'config.yaml'
-    config_path = config_path.resolve()
+class EnmasseGenerator:
 
-    multi_config_path = server_config_dir / 'config.multi.yaml'
-    multi_config_path = multi_config_path.resolve()
+    def __init__(self):
+        self.current_dir = Path(__file__).parent
+        self.server_config_dir = self.current_dir / '..' / '..' / '..' / '..' / '..' / 'src' / 'zato' / 'common' / 'pubsub' / 'server'
+        self.config_path = (self.server_config_dir / 'config.yaml').resolve()
+        self.multi_config_path = (self.server_config_dir / 'config.multi.yaml').resolve()
 
-    # Read and parse the YAML file
-    with open(config_path, 'r', encoding='utf-8') as f:
-        config_data = yaml.safe_load(f)
+# ################################################################################################################################
 
-    # Create copy with multi config name
-    with open(multi_config_path, 'w', encoding='utf-8') as f:
-        yaml.safe_dump(config_data, f, default_flow_style=False, allow_unicode=True)
+    def load_config(self) -> 'strdict':
+        with open(self.config_path, 'r', encoding='utf-8') as f:
+            return yaml.safe_load(f)
 
-    # Process the configuration data
-    logger.info(f'Loaded configuration from: {config_path}')
-    logger.info(f'Created copy at: {multi_config_path}')
-    logger.info(f'Users: {users}')
-    logger.info(f'Security definitions: {len(config_data.get("security", []))}')
-    logger.info(f'Topics: {len(config_data.get("pubsub_topic", []))}')
-    logger.info(f'Permissions: {len(config_data.get("pubsub_permission", []))}')
-    logger.info(f'Subscriptions: {len(config_data.get("pubsub_subscription", []))}')
+# ################################################################################################################################
+
+    def create_multi_config(self, config_data:'strdict') -> 'None':
+        with open(self.multi_config_path, 'w', encoding='utf-8') as f:
+            yaml.safe_dump(config_data, f, default_flow_style=False, allow_unicode=True)
+
+# ################################################################################################################################
+
+    def log_config_info(self, config_data:'strdict', users:'int') -> 'None':
+        logger.info(f'Loaded configuration from: {self.config_path}')
+        logger.info(f'Created copy at: {self.multi_config_path}')
+        logger.info(f'Users: {users}')
+        logger.info(f'Security definitions: {len(config_data.get("security", []))}')
+        logger.info(f'Topics: {len(config_data.get("pubsub_topic", []))}')
+        logger.info(f'Permissions: {len(config_data.get("pubsub_permission", []))}')
+        logger.info(f'Subscriptions: {len(config_data.get("pubsub_subscription", []))}')
+
+# ################################################################################################################################
+
+    def generate(self, users:'int') -> 'None':
+        config_data = self.load_config()
+        self.create_multi_config(config_data)
+        self.log_config_info(config_data, users)
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -59,7 +76,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate enmasse configuration')
     _ = parser.add_argument('--users', type=int, default=1, help='Number of users (default: 1)')
     args = parser.parse_args()
-    main(args.users)
+    generator = EnmasseGenerator()
+    generator.generate(args.users)
 
 # ################################################################################################################################
 # ################################################################################################################################
