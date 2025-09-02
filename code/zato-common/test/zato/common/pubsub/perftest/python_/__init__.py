128200d47ceb9856349a9a209195f564220ae45e
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-09-02 21:40:47 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/perftest/python_/__init__.py
+++ code/zato-common/test/zato/common/pubsub/perftest/python_/__init__.py
@@ -12,10 +12,15 @@ _ = patch_all()
 
 # stdlib
 import logging
+import os
+from json import dumps
 from logging import getLogger
 
 # gevent
 from gevent import spawn
+
+# requests
+import requests
 
 # ################################################################################################################################
 # ################################################################################################################################
@@ -46,10 +51,81 @@ class Invoker:
 
 # ################################################################################################################################
 
+    def _before_start(self) -> 'None':
+        """ Called before starting the invoker.
+        """
+        pass
+
+# ################################################################################################################################
+
+    def _get_config(self) -> 'dict':
+        """ Get configuration from environment variables.
+        """
+        return {
+            'base_url': os.environ['Zato_Test_PubSub_OpenAPI_URL'],
+            'username': os.environ['Zato_Test_PubSub_OpenAPI_Username'],
+            'password': os.environ['Zato_Test_PubSub_OpenAPI_Password'],
+            'max_topics': int(os.environ['Zato_Test_PubSub_OpenAPI_Max_Topics']),
+        }
+
+# ################################################################################################################################
+
+    def _create_payload(self, topic_name: 'str') -> 'dict':
+        """ Create message payload for publishing.
+        """
+        return {
+            'data': {
+                'message': f'Test message from Python invoker',
+                'topic': topic_name,
+            },
+            'priority': 5,
+            'expiration': 3600,
+        }
+
+# ################################################################################################################################
+
+    def _publish_message(self, url: 'str', payload: 'dict', headers: 'dict', auth: 'tuple') -> 'None':
+        """ Publish a single message to the broker.
+        """
+        response = requests.post(url, data=dumps(payload), headers=headers, auth=auth)
+
+        topic_name = payload['data']['topic']
+        if response.status_code == 200:
+            logger.info(f'Published message to {topic_name}')
+        else:
+            logger.error(f'Failed to publish to {topic_name}: {response.status_code} - {response.text}')
+
+# ################################################################################################################################
+
+    def _start(self) -> 'None':
+        """ Main invoker logic.
+        """
+        config = self._get_config()
+        auth = (config['username'], config['password'])
+        headers = {'Content-Type': 'application/json'}
+
+        for topic_num in range(1, config['max_topics'] + 1):
+            topic_name = f'demo.{topic_num}'
+            url = f'{config["base_url"]}/pubsub/topic/{topic_name}'
+            payload = self._create_payload(topic_name)
+
+            self._publish_message(url, payload, headers, auth)
+
+# ################################################################################################################################
+
+    def _after_start(self) -> 'None':
+        """ Called after starting the invoker.
+        """
+        pass
+
+# ################################################################################################################################
+
     def start(self) -> 'None':
         """ Start the invoker.
         """
-        pass
+        self._before_start()
+        self._start()
+        self._after_start()
 
 # ################################################################################################################################
 # ################################################################################################################################
