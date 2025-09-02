6d210b9f81956d53b0ae4cf46b4382748553e762
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-09-02 22:30:39 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/test/zato/common/pubsub/perftest/python_/__init__.py
+++ code/zato-common/test/zato/common/pubsub/perftest/python_/__init__.py
@@ -46,12 +46,12 @@ logger = getLogger(__name__)
 # ################################################################################################################################
 # ################################################################################################################################
 
-class Invoker:
-    """ Placeholder class for Invoker instances.
+class Producer:
+    """ Placeholder class for Producer instances.
     """
-    def __init__(self, reqs_per_invoker:'int'=1, invoker_id:'int'=0, reqs_per_second:'float'=1.0, max_topics:'int'=3) -> 'None':
-        self.reqs_per_invoker = reqs_per_invoker
-        self.invoker_id = invoker_id
+    def __init__(self, reqs_per_producer:'int'=1, producer_id:'int'=0, reqs_per_second:'float'=1.0, max_topics:'int'=3) -> 'None':
+        self.reqs_per_producer = reqs_per_producer
+        self.producer_id = producer_id
         self.reqs_per_second = reqs_per_second
         self.max_topics = max_topics
 
@@ -70,16 +70,16 @@ class Invoker:
         base_url = os.environ['Zato_Test_PubSub_OpenAPI_URL']
         username = os.environ['Zato_Test_PubSub_OpenAPI_Username']
         password = os.environ['Zato_Test_PubSub_OpenAPI_Password']
-        reqs_per_invoker = self.reqs_per_invoker
+        reqs_per_producer = self.reqs_per_producer
         reqs_per_second = self.reqs_per_second
         max_topics = self.max_topics
-
+        
         return {
             'base_url': base_url,
             'username': username,
             'password': password,
             'max_topics': max_topics,
-            'reqs_per_invoker': reqs_per_invoker,
+            'reqs_per_producer': reqs_per_producer,
             'reqs_per_second': reqs_per_second,
         }
 
@@ -108,7 +108,7 @@ class Invoker:
         if response.status_code == 200:
             pass
         else:
-            logger.error(f'Invoker {self.invoker_id}: Failed to publish to {topic_name}: {response.status_code} - {response.text}')
+            logger.error(f'Producer {self.producer_id}: Failed to publish to {topic_name}: {response.status_code} - {response.text}')
 
 # ################################################################################################################################
 
@@ -119,31 +119,31 @@ class Invoker:
         auth = (config['username'], config['password'])
         headers = {'Content-Type': 'application/json'}
 
-        reqs_per_invoker = config['reqs_per_invoker']
+        reqs_per_producer = config['reqs_per_producer']
         reqs_per_second = config['reqs_per_second']
         max_topics = config['max_topics']
         max_topics_range = max_topics + 1
-
-        request_interval = 1.0 / reqs_per_second
-        total_messages = reqs_per_invoker * max_topics
-        message_count = 0
         
+        request_interval = 1.0 / reqs_per_second
+        total_messages = reqs_per_producer * max_topics
+        message_count = 0
+
         if total_messages < 1000:
             log_interval = 10
         else:
             log_interval = 50
-        
-        for _ in range(reqs_per_invoker):
+
+        for _ in range(reqs_per_producer):
             for topic_num in range(1, max_topics_range):
                 message_count += 1
                 start_time = utcnow()
-                
+
                 topic_name = f'demo.{topic_num}'
                 url = f'{config["base_url"]}/pubsub/topic/{topic_name}'
                 payload = self._create_payload(topic_name)
 
                 if message_count == 1 or message_count % log_interval == 0:
-                    logger.info(f'Invoker {self.invoker_id}: Sending message {message_count}/{total_messages} to {topic_name} ({reqs_per_second} req/s)')
+                    logger.info(f'Producer {self.producer_id}: Sending message {message_count}/{total_messages} to {topic_name} ({reqs_per_second} req/s)')
                 self._publish_message(url, payload, headers, auth)
 
                 end_time = utcnow()
@@ -172,32 +172,32 @@ class Invoker:
 # ################################################################################################################################
 # ################################################################################################################################
 
-class InvokerManager:
-    """ Creates new instances of Invoker class in greenlets.
+class ProducerManager:
+    """ Creates new instances of Producer class in greenlets.
     """
 
-    def _start_invoker(self, reqs_per_invoker:'int', invoker_id:'int', reqs_per_second:'float', max_topics:'int') -> 'any_':
-        """ Creates a new Invoker instance in a greenlet.
+    def _start_producer(self, reqs_per_producer:'int', producer_id:'int', reqs_per_second:'float', max_topics:'int') -> 'any_':
+        """ Creates a new Producer instance in a greenlet.
         """
-        invoker = Invoker(reqs_per_invoker, invoker_id, reqs_per_second, max_topics)
-        greenlet = spawn(invoker.start)
+        producer = Producer(reqs_per_producer, producer_id, reqs_per_second, max_topics)
+        greenlet = spawn(producer.start)
         return greenlet
 
 # ################################################################################################################################
 
-    def run(self, num_invokers:'int', reqs_per_invoker:'int'=1, reqs_per_second:'float'=1.0, max_topics:'int'=3) -> 'None':
-        """ Run the specified number of invokers and wait for completion.
+    def run(self, num_producers:'int', reqs_per_producer:'int'=1, reqs_per_second:'float'=1.0, max_topics:'int'=3) -> 'None':
+        """ Run the specified number of producers and wait for completion.
         """
-        if num_invokers == 1:
-            noun = 'invoker'
+        if num_producers == 1:
+            noun = 'producer'
         else:
-            noun = 'invokers'
+            noun = 'producers'
 
-        logger.info(f'Starting {num_invokers} {noun}')
+        logger.info(f'Starting {num_producers} {noun}')
 
         greenlets = []
-        for invoker_id in range(1, num_invokers + 1):
-            greenlet = self._start_invoker(reqs_per_invoker, invoker_id, reqs_per_second, max_topics)
+        for producer_id in range(1, num_producers + 1):
+            greenlet = self._start_producer(reqs_per_producer, producer_id, reqs_per_second, max_topics)
             greenlets.append(greenlet)
 
         # Wait for all greenlets to complete
@@ -212,15 +212,15 @@ if __name__ == '__main__':
     # stdlib
     import argparse
 
-    parser = argparse.ArgumentParser(description='Invoker Manager')
-    _ = parser.add_argument('--num-invokers', type=int, default=1, help='Number of invokers to start')
-    _ = parser.add_argument('--reqs-per-invoker', type=int, default=1, help='Number of requests each invoker should send')
-    _ = parser.add_argument('--reqs-per-second', type=float, default=1.0, help='Number of requests per second each invoker should make')
+    parser = argparse.ArgumentParser(description='Producer Manager')
+    _ = parser.add_argument('--num-producers', type=int, default=1, help='Number of producers to start')
+    _ = parser.add_argument('--reqs-per-producer', type=int, default=1, help='Number of requests each producer should send')
+    _ = parser.add_argument('--reqs-per-second', type=float, default=1.0, help='Number of requests per second each producer should make')
     _ = parser.add_argument('--max-topics', type=int, default=3, help='Number of topics to publish to')
     args = parser.parse_args()
 
-    manager = InvokerManager()
-    manager.run(args.num_invokers, args.reqs_per_invoker, args.reqs_per_second, args.max_topics)
+    manager = ProducerManager()
+    manager.run(args.num_producers, args.reqs_per_producer, args.reqs_per_second, args.max_topics)
 
 # ################################################################################################################################
 # ################################################################################################################################
