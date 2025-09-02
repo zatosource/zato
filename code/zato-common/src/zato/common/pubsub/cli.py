72c324d9f62239e4b098a0a18270ac9d5ab3cf8d
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-09-02 12:10:51 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/src/zato/common/pubsub/cli.py
+++ code/zato-common/src/zato/common/pubsub/cli.py
@@ -294,7 +294,7 @@ curl -u demo:demo -X DELETE http://localhost:44556/pubsub/subscribe/topic/demo.1
 curl -u demo:demo -X GET http://localhost:44556/pubsub/admin/diagnostics; echo
 
 curl -u demo:demo -X POST http://localhost:44556/pubsub/subscribe/topic/demo.1; echo
-curl -u demo:demo -X POST http://localhost:44556/pubsub/topic/demo.1 -d '{"data": "First message", "priority": 7, expiration: 25}'; echo
+curl -u demo:demo -X POST http://localhost:44556/pubsub/topic/demo.1 -d '{"data": "First message", "priority": 7, "expiration": 250000000}'; echo
 curl -u demo:demo -X POST http://localhost:44556/pubsub/topic/demo.1 -d '{"data": "Second message", "priority": 5}'; echo
 curl -u demo:demo -X POST http://localhost:44556/pubsub/messages/get -d '{"max_messages": 10, "max_len": 1000000}'; echo
 curl -u demo:demo -X POST http://localhost:44556/pubsub/messages/get -d '{"max_messages": 10}'; echo
