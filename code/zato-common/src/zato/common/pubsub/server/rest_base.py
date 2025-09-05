cf947dfd0a08d28431006031806ceafc213bb0b9
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-09-05 10:30:31 +0200

    GH #755 - Working on AMQP (tests).

--- code/zato-common/src/zato/common/pubsub/server/rest_base.py
+++ code/zato-common/src/zato/common/pubsub/server/rest_base.py
@@ -446,6 +446,65 @@ class BaseRESTServer(BaseServer):
 
 # ################################################################################################################################
 
+    def on_subscribe(self, cid:'str', environ:'anydict', start_response:'any_', topic_name:'str') -> 'APIResponse':
+        """ Handle subscription request.
+        """
+        # Log what we're doing ..
+        logger.info(f'[{cid}] Processing subscribe request')
+
+        # .. make sure the client is allowed to carry out this action ..
+        username = self.authenticate(cid, environ)
+
+        # .. validate topic name ..
+        from zato.common.pubsub.util import validate_topic_name
+        validate_topic_name(topic_name)
+
+        # .. check if user has permission to subscribe to this topic ..
+        permission_result = self.backend.pattern_matcher.evaluate(username, topic_name, 'subscribe')
+        if not permission_result.is_ok:
+            logger.warning(f'[{cid}] User {username} denied subscribe access to topic {topic_name}: {permission_result.reason}')
+            raise UnauthorizedException(cid, 'Permission denied')
+
+        # Subscribe to topic using backend
+        result = self.backend.register_subscription(cid, topic_name, username=username, should_invoke_server=True)
+
+        response = APIResponse()
+        response.cid = cid
+        response.is_ok = result.is_ok
+        response.status = result.status
+        return response
+
+# ################################################################################################################################
+
+    def on_unsubscribe(self, cid:'str', environ:'anydict', start_response:'any_', topic_name:'str') -> 'APIResponse':
+        """ Handle unsubscribe request.
+        """
+        logger.info(f'[{cid}] Processing unsubscribe request')
+
+        # .. make sure the client is allowed to carry out this action ..
+        username = self.authenticate(cid, environ)
+
+        # .. validate topic name ..
+        from zato.common.pubsub.util import validate_topic_name
+        validate_topic_name(topic_name)
+
+        # .. check if user has permission to subscribe to this topic ..
+        permission_result = self.backend.pattern_matcher.evaluate(username, topic_name, 'subscribe')
+        if not permission_result.is_ok:
+            logger.warning(f'[{cid}] User {username} denied subscribe access to topic {topic_name}: {permission_result.reason}')
+            raise UnauthorizedException(cid, 'Permission denied')
+
+        # Unsubscribe from topic using backend
+        result = self.backend.unregister_subscription(cid, topic_name, username=username)
+
+        response = APIResponse()
+        response.is_ok = result.is_ok
+        response.cid = cid
+
+        return response
+
+# ################################################################################################################################
+
     def run(self) -> 'None':
         """ Run the server using gevent's WSGIServer.
         """
