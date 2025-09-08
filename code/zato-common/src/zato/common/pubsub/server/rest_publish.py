772e7cddf3373491712c06b59f72994bbe6645c7
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-09-08 13:28:05 +0200

    GH #755 - Working on AMQP (stats).

--- code/zato-common/src/zato/common/pubsub/server/rest_publish.py
+++ code/zato-common/src/zato/common/pubsub/server/rest_publish.py
@@ -130,9 +130,9 @@ class PubSubRESTServerPublish(BaseRESTServer):
 
         # .. build our response ..
         response:'APIResponse' = {
-            'is_ok': result['is_ok'],
+            'is_ok': result['is_ok'], # type: ignore
             'cid': cid,
-            'msg_id': result['msg_id']
+            'msg_id': result['msg_id'], # type: ignore
         }
 
         # .. and return it to the caller.
