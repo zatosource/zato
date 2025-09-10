c702ff01552b809a01df729435cced1d7cc7004f
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-09-10 21:16:38 +0200

    GH #755 - Working on AMQP (stats).

--- code/zato-common/src/zato/common/pubsub/cli.py
+++ code/zato-common/src/zato/common/pubsub/cli.py
@@ -132,9 +132,7 @@ class GunicornApplication(BaseApplication):
     def load_config(self):
         # Apply valid configuration options
         for key, value in self.options.items():
-            if key == 'worker_class':
-                object.__setattr__(self.cfg, 'worker_class', value)
-            elif key in self.cfg.settings and value is not None: # type: ignore
+            if key in self.cfg.settings and value is not None: # type: ignore
                 self.cfg.set(key.lower(), value) # type: ignore
 
         # We need to set this one explicitly because otherwise gunicorn insists it be an int (min=1)
@@ -261,7 +259,7 @@ def start_server(args:'argparse.Namespace') -> 'OperationResult':
         options = {
             'bind': f'{args.host}:{port}',
             'workers': args.workers,
-            'worker_class': TimingWorker,
+            'worker_class': 'zato.common.pubsub.cli.TimingWorker',
             'timeout': 30,
             'keepalive': 2,
             'loglevel': 'has_debug' if args.has_debug else 'info',
