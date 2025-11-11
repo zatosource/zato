a2da2eed7923b91d317e2904d216ec5af2264f8d
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-11-11 18:48:19 +0100

    GH #755 - Working on EDA.

--- code/zato-server/src/zato/server/service/__init__.py
+++ code/zato-server/src/zato/server/service/__init__.py
@@ -665,7 +665,6 @@ class Service:
                 try:
 
                     # This obtains the response
-                    data_format = data_format or DATA_FORMAT.JSON
                     response = set_response_func(service, data_format=data_format, transport=transport, **kwargs)
 
                     # If this was fan-out/fan-in we need to always notify our callbacks no matter the result
