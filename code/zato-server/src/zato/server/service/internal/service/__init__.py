faf3ad700b1706e0ff19a6e81b2b6998b7b29692
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-11-13 14:24:54 +0100

    GH #755 - Working on EDA.

--- code/zato-server/src/zato/server/service/internal/service/__init__.py
+++ code/zato-server/src/zato/server/service/internal/service/__init__.py
@@ -765,10 +765,10 @@ class UploadPackage(AdminService):
                 self.logger.info('DEBUG file_name_full after replace: %s', file_name_full)
 
         try:
-            os.chmod(file_name_full, 0o644)
-            self.logger.info('DEBUG chmod 644 successful for: %s', file_name_full)
+            os.chmod(file_name_full, 0o664)
+            self.logger.info('DEBUG chmod 664 successful for: %s', file_name_full)
         except Exception as e:
-            self.logger.info('DEBUG chmod 644 failed for: %s, error: %s', file_name_full, e)
+            self.logger.info('DEBUG chmod 664 failed for: %s, error: %s', file_name_full, e)
 
         self.logger.info('DEBUG opening file: %s', file_name_full)
         with open(file_name_full, 'wb') as tf:
