00997ddc5ea91e1d715a3f5adf10ad988d9f12b8
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-12-24 08:25:24 +0100

    GH #755 - Working on in-app updates.

--- code/zato-web-admin/src/zato/admin/web/views/in_app_updates.py
+++ code/zato-web-admin/src/zato/admin/web/views/in_app_updates.py
@@ -6,13 +6,38 @@ Copyright (C) 2021, Zato Source s.r.o. https://zato.io
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
+# stdlib
+import os
+import subprocess
+
+# Django
 from django.template.response import TemplateResponse
 
+# Zato
 from zato.admin.web.views import method_allowed
+from zato.admin.web.views.updates import find_file_in_parents
 
 @method_allowed('GET')
 def index(req):
+    current_version = '4.1.0'
+    
+    zato_path = os.path.sep.join(['code', 'bin', 'zato'])
+    zato_binary = find_file_in_parents(zato_path)
+    
+    if zato_binary:
+        try:
+            result = subprocess.run(
+                [zato_binary, '--version'],
+                capture_output=True,
+                text=True,
+                timeout=5
+            )
+            if result.returncode == 0:
+                current_version = result.stdout.strip()
+        except Exception:
+            pass
+    
     return TemplateResponse(req, 'zato/in-app-updates/index.html', {
-        'current_version': '4.1.0',
+        'current_version': current_version,
         'latest_version': '4.2.0'
     })
