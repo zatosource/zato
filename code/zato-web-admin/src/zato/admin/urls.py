69c89fd8cacc322cf8abf7204da30040af9b831b
Dariusz Suchojad <dsuch-github@m.zato.io>
2025-12-30 23:45:05 +0100

    GH #755 - Working on in-app updates.

--- code/zato-web-admin/src/zato/admin/urls.py
+++ code/zato-web-admin/src/zato/admin/urls.py
@@ -817,6 +817,8 @@ urlpatterns += [
 
     url(r'^zato/updates/$',
         login_required(updates.index), name='in-app-updates'),
+    url(r'^zato/updates/check-availability$',
+        login_required(updates.check_availability), name='updates-check-availability'),
     url(r'^zato/updates/check-latest-version$',
         login_required(updates.check_latest_version), name='updates-check-latest-version'),
     url(r'^zato/updates/get-latest-audit-entry$',
