1d2b2702f33276c3b6692cc9571a7fe15dad8c43
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-01-02 18:58:16 +0100

    GH #755 - Working on EDA.

--- code/zato-web-admin/src/zato/admin/web/views/settings_config.py
+++ code/zato-web-admin/src/zato/admin/web/views/settings_config.py
@@ -6,7 +6,8 @@ Copyright (C) 2025, Zato Source s.r.o. https://zato.io
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
 
-updates_config = {
-    'title': 'Install updates',
-    'api_prefix': '/zato/updates/'
-}
+updates_page_config = {}
+updates_page_config['title'] = 'Install updates'
+updates_page_config['api_prefix'] = '/zato/updates/'
+updates_page_config['step1_label'] = 'Download'
+updates_page_config['step2_label'] = 'Install'
