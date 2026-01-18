55b6750b52d0d21ed897ddba267f69d8d50ffdf5
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-01-18 17:09:56 +0100

    GH #755 - Working on OpenAPI.

--- code/zato-web-admin/src/zato/admin/web/views/openapi_.py
+++ code/zato-web-admin/src/zato/admin/web/views/openapi_.py
@@ -13,6 +13,9 @@ import sys
 import tempfile
 from logging import getLogger
 
+# PyYAML
+import yaml
+
 # Django
 from django.http import HttpResponse
 from django.http.response import HttpResponseServerError
@@ -95,6 +98,11 @@ def import_objects(req):
 
         logger.info('import_objects: received data:\n%s', dumps(parsed_data, indent=2))
 
+        enmasse_config = build_enmasse_config(parsed_data)
+        enmasse_yaml = yaml.dump(enmasse_config, default_flow_style=False, sort_keys=False)
+
+        logger.info('import_objects: enmasse YAML:\n%s', enmasse_yaml)
+
         response_data['success'] = True
         response_data['message'] = 'Import received'
 
@@ -104,6 +112,74 @@ def import_objects(req):
         logger.exception('import_objects exception')
         response_data['error'] = str(e)
         return json_response(response_data, success=False)
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+def build_enmasse_config(items):
+
+    enmasse_config = {
+        'security': [],
+        'outgoing_rest': []
+    }
+
+    security_defs = {}
+
+    for item in items:
+        server = item['server']
+        name = item['name']
+        path = item['path']
+        auth = item['auth']
+        content_type = item.get('content_type', 'application/json')
+
+        data_format = 'json'
+        if 'xml' in content_type.lower():
+            data_format = 'xml'
+
+        outgoing_rest_item = {
+            'name': name,
+            'host': server,
+            'url_path': path,
+            'data_format': data_format,
+            'timeout': 600
+        }
+
+        if auth:
+            sec_name = f'openapi.{auth}.{name}'
+            outgoing_rest_item['security'] = sec_name
+
+            if sec_name not in security_defs:
+                sec_type = map_auth_to_security_type(auth)
+                security_defs[sec_name] = {
+                    'name': sec_name,
+                    'type': sec_type
+                }
+
+        enmasse_config['outgoing_rest'].append(outgoing_rest_item)
+
+    for sec_def in security_defs.values():
+        enmasse_config['security'].append(sec_def)
+
+    if not enmasse_config['security']:
+        del enmasse_config['security']
+
+    return enmasse_config
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+def map_auth_to_security_type(auth):
+    auth_lower = auth.lower()
+    if 'basic' in auth_lower:
+        return 'basic_auth'
+    elif 'bearer' in auth_lower or 'oauth' in auth_lower:
+        return 'bearer_token'
+    elif 'api' in auth_lower and 'key' in auth_lower:
+        return 'apikey'
+    elif 'ntlm' in auth_lower:
+        return 'ntlm'
+    else:
+        return 'basic_auth'
 
 # ################################################################################################################################
 # ################################################################################################################################
