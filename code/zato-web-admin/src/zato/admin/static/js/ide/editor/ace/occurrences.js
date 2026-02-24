8a883536af3821d36616e3ed1fe32b94c166080b
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-02-24 01:06:28 +0100

    GH #755 - Working on AI.

--- code/zato-web-admin/src/zato/admin/static/js/ide/editor/ace/occurrences.js
+++ code/zato-web-admin/src/zato/admin/static/js/ide/editor/ace/occurrences.js
@@ -6,14 +6,10 @@
         setupOccurrenceHighlight: function(editor, instance) {
             var self = this;
 
-            console.log('[Occurrences] setupOccurrenceHighlight called');
-
             instance.occurrenceMarkers = [];
             instance.primaryOccurrenceMarker = null;
 
             editor.on('click', function(e) {
-                console.log('[Occurrences] click event, ctrlKey:', e.domEvent.ctrlKey);
-
                 if (e.domEvent.ctrlKey) {
                     return;
                 }
@@ -23,38 +19,16 @@
                 var pos = editor.getCursorPosition();
                 var token = editor.session.getTokenAt(pos.row, pos.column);
 
-                console.log('[Occurrences] pos:', JSON.stringify(pos), 'token:', JSON.stringify(token));
-
                 if (!token || !self.isNavigableToken(token)) {
-                    console.log('[Occurrences] token not navigable');
                     return;
                 }
 
                 var word = token.value;
                 if (!word || !/^[A-Za-z_][A-Za-z0-9_]*$/.test(word)) {
-                    console.log('[Occurrences] word invalid:', word);
                     return;
                 }
 
-                var cursorLine = pos.row + 1;
-
-                console.log('[Occurrences] ZatoIDELocalSymbols:', typeof ZatoIDELocalSymbols, 'localSymbols:', !!instance.localSymbols);
-
-                if (typeof ZatoIDELocalSymbols === 'undefined' || !instance.localSymbols) {
-                    console.log('[Occurrences] no local symbols');
-                    return;
-                }
-
-                console.log('[Occurrences] localSymbols.functions:', JSON.stringify(Object.keys(instance.localSymbols.functions)));
-                console.log('[Occurrences] localSymbols.variables:', JSON.stringify(Object.keys(instance.localSymbols.variables)));
-                console.log('[Occurrences] looking for word:', word, 'at line:', cursorLine);
-
-                var definition = ZatoIDELocalSymbols.findDefinition(instance, word, cursorLine);
-                console.log('[Occurrences] findDefinition result:', JSON.stringify(definition));
-
-                var occurrences = ZatoIDELocalSymbols.findAllOccurrences(editor, instance, word, cursorLine);
-                console.log('[Occurrences] found occurrences:', occurrences.length, JSON.stringify(occurrences));
-
+                var occurrences = self.findAllWordOccurrences(editor, word);
                 if (occurrences.length === 0) {
                     return;
                 }
@@ -67,13 +41,33 @@
             });
         },
 
+        findAllWordOccurrences: function(editor, word) {
+            var content = editor.getValue();
+            var lines = content.split('\n');
+            var occurrences = [];
+
+            var regex = new RegExp('\\b' + word + '\\b', 'g');
+
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                var match;
+                while ((match = regex.exec(line)) !== null) {
+                    occurrences.push({
+                        line: i + 1,
+                        column: match.index,
+                        length: word.length
+                    });
+                }
+            }
+
+            return occurrences;
+        },
+
         highlightOccurrences: function(editor, instance, occurrences, clickedRow, clickedColumn) {
             var self = this;
             var Range = ace.require('ace/range').Range;
 
             self.clearOccurrenceHighlights(editor, instance);
-
-            console.log('[highlightOccurrences] adding', occurrences.length, 'markers, clickedRow:', clickedRow, 'clickedColumn:', clickedColumn);
 
             for (var i = 0; i < occurrences.length; i++) {
                 var occ = occurrences[i];
@@ -81,8 +75,6 @@
 
                 var isPrimary = (occ.line - 1 === clickedRow && occ.column === clickedColumn);
                 var markerClass = isPrimary ? 'ace_occurrence_primary' : 'ace_occurrence_secondary';
-
-                console.log('[highlightOccurrences] marker', i, 'line:', occ.line, 'col:', occ.column, 'isPrimary:', isPrimary, 'class:', markerClass);
 
                 var markerId = editor.session.addMarker(range, markerClass, 'text', true);
 
