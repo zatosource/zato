455bc452b1b8b634a1f04ade9103ab43460482ff
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-04-26 21:32:27 +0200

    GH #755 - Working on pub/sub.

--- code/zato-common/src/zato/common/ext/broker/__init__.py
+++ code/zato-common/src/zato/common/ext/broker/__init__.py
@@ -5,5 +5,3 @@ Copyright (C) 2026, Zato Source s.r.o. https://zato.io
 
 Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 """
-
-from zato.common.ext.broker.client import BrokerClient, BrokerPubSub
