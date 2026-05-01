664663454fc1ec82274b8e1a455a2b2f7f1ebf55
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-05-01 20:56:01 +0200

    GH #755 - Working on rate limiting.

---
+++ code/zato-common/src/zato/common/rate_limiting/common.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+import time
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+Microseconds_Per_Second = 1_000_000
+Microtokens_Per_Token = 1_000_000
+
+Seconds_Per_Minute = 60
+Seconds_Per_Hour = 3600
+Seconds_Per_Day = 86400
+
+January = 1
+December = 12
+
+Window_Unit_Second = 'second'
+Window_Unit_Minute = 'minute'
+Window_Unit_Hour   = 'hour'
+Window_Unit_Day    = 'day'
+Window_Unit_Month  = 'month'
+
+_all_window_units = {
+    Window_Unit_Second,
+    Window_Unit_Minute,
+    Window_Unit_Hour,
+    Window_Unit_Day,
+    Window_Unit_Month,
+}
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+class RateLimitError(Exception):
+    """ Represents a rate limiter failure: invalid configuration, arithmetic overflow, or a clock error.
+    """
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+def current_time_us() -> 'int':
+    """ Returns the current wall clock time in microseconds since Unix epoch.
+    """
+    now_seconds = time.time()
+    out = int(now_seconds * Microseconds_Per_Second)
+    return out
+
+# ################################################################################################################################
+
+def validate_window_unit(value:'str') -> 'str':
+    """ Returns the value unchanged if it is a known window unit, raises RateLimitError otherwise.
+    """
+    if value in _all_window_units:
+        out = value
+        return out
+
+    raise RateLimitError(f'Unknown window_unit: {value}')
+
+# ################################################################################################################################
+# ################################################################################################################################
---
+++ code/zato-common/src/zato/common/rate_limiting/fixed_window.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2025, Zato Source s.r.o. https://zato.io
+
+Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# stdlib
+import datetime
+
+# Zato
+from zato.common.rate_limiting.common import December, January, Microseconds_Per_Second, RateLimitError, \
+    Seconds_Per_Minute, Seconds_Per_Hour, Seconds_Per_Day, Window_Unit_Second, Window_Unit_Minute, \
+    Window_Unit_Hour, Window_Unit_Day, Window_Unit_Month
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+_period_by_unit = {
+    Window_Unit_Second: 1,
+    Window_Unit_Minute: Seconds_Per_Minute,
+    Window_Unit_Hour:   Seconds_Per_Hour,
+    Window_Unit_Day:    Seconds_Per_Day,
+}
+
+# ################################################################################################################################
+# ################################################################################################################################
+
+def compute_window_end_us(unit:'str', now_us:'int') -> 'int':
+    """ Returns the microsecond timestamp at which the current natural window ends.
+    """
+    period = _period_by_unit.get(unit)
+
+    if period is not None:
+        now_secs = now_us // Microseconds_Per_Second
+        window_start = now_secs - now_secs % period
+        window_end_secs = window_start + period
+        out = window_end_secs * Microseconds_Per_Second
+        return out
+
+    if unit == Window_Unit_Month:
+        now_secs = now_us // Microseconds_Per_Second
+        now = datetime.datetime.fromtimestamp(now_secs, tz=datetime.timezone.utc)
+
+        year = now.year
+        month = now.month
+
+        if month == December:
+            next_year = year + 1
+            next_month = January
+        else:
+            next_year = year
+            next_month = month + 1
+
+        first_of_next = datetime.datetime(next_year, next_month, 1, tzinfo=datetime.timezone.utc)
+        boundary_secs = int(first_of_next.timestamp())
+        out = boundary_secs * Microseconds_Per_Second
+        return out
+
+    raise RateLimitError(f'Unknown window_unit: {unit}')
+
+# ################################################################################################################################
+# ################################################################################################################################
