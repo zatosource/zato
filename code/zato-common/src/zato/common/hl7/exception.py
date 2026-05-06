b6b887335823d22794f908f0bb5141bc39276bc6
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-05-06 22:13:51 +0200

    GH #755 - Working on HL7.

--- code/zato-common/src/zato/common/hl7/exception.py
+++ code/zato-common/src/zato/common/hl7/exception.py
@@ -9,7 +9,7 @@ Licensed under AGPLv3, see LICENSE.txt for terms and conditions.
 class HL7Exception(Exception):
     """ Raised when an HL7 parsing or processing error occurs.
     """
-    def __init__(self, msg:'str', data:'object'=None, orig_exc:'object'=None) -> 'None':
+    def __init__(self, message:'str', data:'object'=None, original_exception:'object'=None) -> 'None':
         self.data = data
-        self.orig_exc = orig_exc
-        super().__init__(msg)
+        self.original_exception = original_exception
+        super().__init__(message)
