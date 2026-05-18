dd5008e7350bb2074943d5cf40ea400419ae1939
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-05-18 17:54:11 +0200

    GH #755 - Working on pub/sub.

--- code/tests/python/zato-common/pubsub/unsub_cleanup/test_ack_one_then_unsub.py
+++ code/tests/python/zato-common/pubsub/unsub_cleanup/test_ack_one_then_unsub.py
@@ -27,12 +27,12 @@ logger = logging.getLogger(__name__)
 # ################################################################################################################################
 # ################################################################################################################################
 
-class TestUnsubWithoutAck(BaseUnsubCleanupTestCase):
-    """ Publish 3 messages, 2 subs, unsubscribe A without acking -
-        sub_pending:{sub_a} key gone, all 3 pending sets no longer contain sub_a, sub_b intact, all files exist.
+class TestAckOneThenUnsub(BaseUnsubCleanupTestCase):
+    """ Publish 3 messages, 2 subs, sub A acks message 1, then unsubscribe A -
+        sub_pending:{sub_a} gone, all pending sets contain sub_b only, all files exist.
     """
 
-    def test_unsub_without_ack_clears_sub_a(self) -> 'None':
+    def test_ack_one_then_unsub_clears_sub_a(self) -> 'None':
 
         # Subscribe two consumers ..
         sub_key_a = f'sub.unsub_a.{self._run_id}'
@@ -43,36 +43,33 @@ class TestUnsubWithoutAck(BaseUnsubCleanupTestCase):
 
         # .. publish 3 messages and collect their data_refs ..
         data_refs:'strlist' = []
+
         for _ in range(3):
             _ = self.publish()
             data_ref = self.get_data_ref_from_stream()
             data_refs.append(data_ref)
 
-        # .. confirm sub_a is in all pending sets before unsubscribe ..
-        for data_ref in data_refs:
-            has_a = self.has_pending_member(data_ref, sub_key_a)
-            self.assertTrue(has_a)
+        # .. fetch messages for sub_a and ack only the first one ..
+        messages = self.backend.fetch_messages(sub_key_a)
+        first_message = messages[0]
 
-        # .. confirm sub_pending:{sub_a} has all 3 data_refs ..
-        for data_ref in data_refs:
-            has_ref = self.has_sub_pending_member(sub_key_a, data_ref)
-            self.assertTrue(has_ref)
+        self.backend.ack_message(
+            first_message['_stream_name'], sub_key_a,
+            first_message['_redis_message_id'], first_message['_data_ref'])
 
-        # .. unsubscribe sub_a without acking anything ..
+        # .. now unsubscribe sub_a ..
         self.backend.unsubscribe(sub_key_a, self.topic_name)
 
-        # .. sub_pending:{sub_a} key is gone (no members) ..
+        # .. sub_pending:{sub_a} key is gone ..
         sub_pending_key = f'zato:pubsub:sub_pending:{sub_key_a}'
         count = self.redis.scard(sub_pending_key)
         self.assertEqual(count, 0)
 
-        # .. all 3 pending sets no longer contain sub_a ..
+        # .. all 3 pending sets contain only sub_b ..
         for data_ref in data_refs:
             has_a = self.has_pending_member(data_ref, sub_key_a)
             self.assertFalse(has_a)
 
-        # .. sub_b is still in all 3 pending sets ..
-        for data_ref in data_refs:
             has_b = self.has_pending_member(data_ref, sub_key_b)
             self.assertTrue(has_b)
 
