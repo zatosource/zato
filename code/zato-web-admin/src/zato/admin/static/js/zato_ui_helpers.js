0156eb58f4989da8640c6e0699adf654df5a4895
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-05-21 22:54:11 +0200

    GH #755 - Working on pub/sub.

--- code/zato-web-admin/src/zato/admin/static/js/zato_ui_helpers.js
+++ code/zato-web-admin/src/zato/admin/static/js/zato_ui_helpers.js
@@ -5,11 +5,33 @@
 
     $.namespace('zato.ui_helpers');
 
+    var _labels = {
+        copiedToClipboard: 'Copied to clipboard'
+    };
+
 // ////////////////////////////////////////////////////////////////////////
 
     $.fn.zato.ui_helpers.esc_html = function(text) {
-        return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+
+        // Escape ampersands first ..
+        var out = text.replace(/&/g, '&amp;');
+
+        // .. then angle brackets ..
+        out = out.replace(/</g, '&lt;');
+        out = out.replace(/>/g, '&gt;');
+
+        // .. and finally double quotes.
+        out = out.replace(/"/g, '&quot;');
+
+        return out;
     };
+
+// ////////////////////////////////////////////////////////////////////////
+
+    function _dismissTooltip(tip) {
+        tip.hide();
+        tip.destroy();
+    }
 
 // ////////////////////////////////////////////////////////////////////////
 
@@ -18,17 +40,19 @@
             content: message,
             trigger: 'manual',
             placement: 'top',
-            duration: [100, 100]
+            duration: [100, 100],
+            onShown: function(instance) {
+                setTimeout(function() { _dismissTooltip(instance); }, 600);
+            }
         });
         tip.show();
-        setTimeout(function() { tip.hide(); tip.destroy(); }, 600);
     };
 
 // ////////////////////////////////////////////////////////////////////////
 
     $.fn.zato.ui_helpers.copy_to_clipboard = function(element, text) {
         navigator.clipboard.writeText(text).then(function() {
-            $.fn.zato.ui_helpers.flash_tooltip(element, 'Copied to clipboard');
+            $.fn.zato.ui_helpers.flash_tooltip(element, _labels.copiedToClipboard);
         });
     };
 
