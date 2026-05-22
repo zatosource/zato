f8e8badc77cd7508be5fc6b8e46203263ca69e57
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-05-22 19:49:55 +0200

    GH #755 - Working on the invoker.

--- code/zato-web-admin/src/zato/admin/static/ace-builds/src/mode-graphqlschema.js
+++ code/zato-web-admin/src/zato/admin/static/ace-builds/src/mode-graphqlschema.js
@@ -25,20 +25,132 @@ var GraphQLSchemaHighlightRules = function() {
         "constant.language": constants
     }, "identifier");
 
+    var operationKeywords = /^(?:query|mutation|subscription|fragment)$/;
+
     this.$rules = {
       "start" : [ {
         token : "comment",
         regex : "#.*$"
       }, {
+        token : "string",
+        regex : '"',
+        next : [
+          {token: "constant.language.escape", regex: /\\(?:u[\da-fA-F]{4}|.)/},
+          {token: "string", regex: '"', next: "start"},
+          {defaultToken: "string"}
+        ]
+      }, {
+        token : "constant.numeric",
+        regex : /\d+\.?\d*[eE]?[+\-]?\d*/
+      }, {
+        token : "variable",
+        regex : /\$[_A-Za-z][_0-9A-Za-z]*/
+      }, {
+        token : "support.function",
+        regex : /@[_A-Za-z][_0-9A-Za-z]*/
+      }, {
+        token : function(value) {
+          var mapped = keywordMapper(value);
+          if (mapped !== "identifier") {
+            if (operationKeywords.test(value)) {
+              return "keyword";
+            }
+            return mapped;
+          }
+          return "entity.name.function";
+        },
+        regex : "[a-zA-Z_][a-zA-Z0-9_]*\\b",
+        next : "afterIdent"
+      }, {
+        token : "punctuation.operator",
+        regex : /[,:!]/
+      }, {
+        token : "keyword.operator",
+        regex : /\.\.\./
+      }, {
+        token : "keyword.operator",
+        regex : /=/
+      }, {
         token : "paren.lparen",
-        regex : /[\[({]/,
-        next  : "start"
+        regex : /[\[({]/
       }, {
         token : "paren.rparen",
         regex : /[\])}]/
+      } ],
+      "afterIdent" : [ {
+        token : "comment",
+        regex : "#.*$",
+        next : "start"
       }, {
-        token : keywordMapper,
-        regex : "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
+        token : "paren.lparen",
+        regex : /\(/
+      }, {
+        token : "paren.lparen",
+        regex : /\{/,
+        next : "fields"
+      }, {
+        token : "entity.name.function",
+        regex : "[a-zA-Z_][a-zA-Z0-9_]*\\b"
+      }, {
+        token : "text",
+        regex : /\s+/
+      }, {
+        token : "text",
+        regex : "",
+        next : "start"
+      } ],
+      "fields" : [ {
+        token : "comment",
+        regex : "#.*$"
+      }, {
+        token : "string",
+        regex : '"',
+        next : [
+          {token: "constant.language.escape", regex: /\\(?:u[\da-fA-F]{4}|.)/},
+          {token: "string", regex: '"', next: "fields"},
+          {defaultToken: "string"}
+        ]
+      }, {
+        token : "constant.numeric",
+        regex : /\d+\.?\d*[eE]?[+\-]?\d*/
+      }, {
+        token : "variable",
+        regex : /\$[_A-Za-z][_0-9A-Za-z]*/
+      }, {
+        token : "support.function",
+        regex : /@[_A-Za-z][_0-9A-Za-z]*/
+      }, {
+        token : "keyword.operator",
+        regex : /\.\.\./,
+        next : "start"
+      }, {
+        token : function(value) {
+          var mapped = keywordMapper(value);
+          if (mapped !== "identifier") {
+            return mapped;
+          }
+          return "variable.language";
+        },
+        regex : "[a-zA-Z_][a-zA-Z0-9_]*\\b"
+      }, {
+        token : "punctuation.operator",
+        regex : /[,:!]/
+      }, {
+        token : "keyword.operator",
+        regex : /=/
+      }, {
+        token : "paren.lparen",
+        regex : /\{/
+      }, {
+        token : "paren.lparen",
+        regex : /\(/
+      }, {
+        token : "paren.rparen",
+        regex : /\)/
+      }, {
+        token : "paren.rparen",
+        regex : /\}/,
+        next : "start"
       } ]
     };
     this.normalizeRules();
