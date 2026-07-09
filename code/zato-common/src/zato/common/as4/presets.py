4c4658e6912c49b2b4cd8213602d4ed0f54a6e22
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-07-09 14:32:41 +0200

    GH #755 - Working on AS4.

--- code/zato-common/src/zato/common/as4/presets.py
+++ code/zato-common/src/zato/common/as4/presets.py
@@ -37,6 +37,13 @@ _ubl_despatch_advice = 'urn:oasis:names:specification:ubl:schema:xsd:DespatchAdv
 # The customization identifier of Peppol BIS Billing 3.0.
 _billing_3_customization = 'urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0'
 
+# The customization identifiers of the PINT billing specializations.
+_pint_anz_customization = 'urn:peppol:pint:billing-1@aunz-1'
+_pint_jp_customization  = 'urn:peppol:pint:billing-1@jp-1'
+
+# The process identifier every PINT billing exchange uses.
+_pint_billing_process = 'urn:peppol:bis:billing'
+
 # The identifier scheme every Peppol process identifier uses.
 _process_scheme = 'cenbii-procid-ubl'
 
@@ -74,6 +81,38 @@ document_type_presets = {
         process_id = 'urn:fdc:peppol.eu:poacc:bis:despatch_advice:3',
         process_scheme = _process_scheme,
         document_standard = _ubl_despatch_advice,
+        document_type_version = '2.1',
+    ),
+
+    'peppol-pint-anz-invoice': DocumentTypePreset(
+        document_type = f'{_ubl_invoice}::Invoice##{_pint_anz_customization}::2.1',
+        process_id = _pint_billing_process,
+        process_scheme = _process_scheme,
+        document_standard = _ubl_invoice,
+        document_type_version = '2.1',
+    ),
+
+    'peppol-pint-anz-credit-note': DocumentTypePreset(
+        document_type = f'{_ubl_credit_note}::CreditNote##{_pint_anz_customization}::2.1',
+        process_id = _pint_billing_process,
+        process_scheme = _process_scheme,
+        document_standard = _ubl_credit_note,
+        document_type_version = '2.1',
+    ),
+
+    'peppol-pint-jp-invoice': DocumentTypePreset(
+        document_type = f'{_ubl_invoice}::Invoice##{_pint_jp_customization}::2.1',
+        process_id = _pint_billing_process,
+        process_scheme = _process_scheme,
+        document_standard = _ubl_invoice,
+        document_type_version = '2.1',
+    ),
+
+    'peppol-pint-jp-credit-note': DocumentTypePreset(
+        document_type = f'{_ubl_credit_note}::CreditNote##{_pint_jp_customization}::2.1',
+        process_id = _pint_billing_process,
+        process_scheme = _process_scheme,
+        document_standard = _ubl_credit_note,
         document_type_version = '2.1',
     ),
 }
