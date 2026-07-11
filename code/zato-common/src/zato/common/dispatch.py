fc293a0250e613e8106a912fffeb34100c9b5251
Dariusz Suchojad <dsuch-github@m.zato.io>
2026-07-12 01:10:21 +0200

    GH #755 - Working on Django.

--- code/zato-common/src/zato/common/dispatch.py
+++ code/zato-common/src/zato/common/dispatch.py
@@ -40,6 +40,16 @@ class Dispatcher:
                     if update in name:
                         self._listen(value.value, callback, **opaque)
 
+    def unlisten(self, callback):
+        """ Removes the callback from all the events it was registered for, e.g. when its owner is being replaced
+        during a configuration reload.
+        """
+        with self.lock:
+            for values in self.listeners.values():
+                # Note that this needs an equality check, not an identity one, because bound methods
+                # are created anew each time they are accessed, so 'is' would never match.
+                values[:] = [(item_callback, opaque) for item_callback, opaque in values if item_callback != callback]
+
     def notify(self, event, ctx):
         with self.lock:
             for ev, values in self.listeners.items():
